c398accd5526dfea59d857a01823adc547a2ff9f
framm <framm@0c6e7542-c601-0410-84e7-c038aed88b3b>
2007-10-07 17:39:38 +0000

    - forgot to add this with the previous commit
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@349 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/gui/layer/DataChangeListener.java
@@ -0,0 +1,8 @@
+// License: GPL. Copyright 2007 by Immanuel Scholz and others
+package org.openstreetmap.josm.gui.layer;
+
+public interface DataChangeListener {
+
+	public void dataChanged(OsmDataLayer l);
+	
+}
