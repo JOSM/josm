0788e44cdba3fa88f63554ae534d4f0f9fff1073
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2008-07-19 19:55:20 +0000

    i18n fix
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@731 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/AutoScaleAction.java
+++ src/org/openstreetmap/josm/actions/AutoScaleAction.java
@@ -51,7 +51,7 @@ public class AutoScaleAction extends JosmAction {
 	}
     
 	public AutoScaleAction(String mode) {
-		super(tr("Zoom to {0}", mode), "dialogs/autoscale/"+mode, tr("Zoom the view to {0}.", tr(mode)), AutoScaleAction.getModeShortcut(mode), 0, true);
+		super(tr("Zoom to {0}", tr(mode)), "dialogs/autoscale/"+mode, tr("Zoom the view to {0}.", tr(mode)), AutoScaleAction.getModeShortcut(mode), 0, true);
 		String modeHelp = Character.toUpperCase(mode.charAt(0))+mode.substring(1);
 		putValue("help", "Action/AutoScale/"+modeHelp);
 		this.mode = mode;
