620ce15020dfa62701990bd5f9191b941984df13
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2008-08-07 11:40:51 +0000

    reduce double draws. Closes #734
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@757 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/osm/visitor/MapPaintVisitor.java
+++ src/org/openstreetmap/josm/data/osm/visitor/MapPaintVisitor.java
@@ -180,22 +180,22 @@ public class MapPaintVisitor implements Visitor {
             }
             orderNumber++;
 
-            if (area && fillAreas) {
-                // hack to make direction arrows visible against filled background
-                if (showDirection)
-                    drawSeg(lastN, n, w.selected ? selectedColor : untaggedColor, showDirection, width, true);
-            } else {
-                if (area) {
-                    drawSeg(lastN, n, w.selected ? selectedColor : colour, showDirection, width, true);
-                } else {
-                    if (realWidth > 0 && useRealWidth && !showDirection) {
-                        int tmpWidth = (int) (100 /  (float) (circum / realWidth));
-                        if (tmpWidth > width) width = tmpWidth;
-                    }
+            if (area) {
+                if(fillAreas)
+                {
+                    // hack to make direction arrows visible against filled background
+                    if (showDirection)
+                        drawSeg(lastN, n, w.selected ? selectedColor : untaggedColor, showDirection, width, true);
                 }
+                else
+                    drawSeg(lastN, n, w.selected ? selectedColor : colour, showDirection, width, true);
+            } else {
+                if (realWidth > 0 && useRealWidth && !showDirection) {
+                    int tmpWidth = (int) (100 /  (float) (circum / realWidth));
+                    if (tmpWidth > width) width = tmpWidth;
+                }
+                drawSeg(lastN, n, w.selected ? selectedColor : colour, showDirection, width, dashed);
             }
-
-            drawSeg(lastN, n, w.selected ? selectedColor : colour, showDirection, width, dashed);
 
             if (showOrderNumber)
                 drawOrderNumber(lastN, n, orderNumber);
