a0c062caab279a27ec6e9a7ad35accbb53dd3cca
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2008-09-02 16:50:42 +0000

    don't enforce buffered reading
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@909 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/tagging/TaggingPreset.java
+++ src/org/openstreetmap/josm/gui/tagging/TaggingPreset.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.HashMap;
@@ -382,7 +383,7 @@ public class TaggingPreset extends AbstractAction {
 		}
 	}
 
-	private static List<TaggingPreset> readAll(BufferedReader in) throws SAXException {
+	private static List<TaggingPreset> readAll(Reader in) throws SAXException {
 		XmlObjectParser parser = new XmlObjectParser();
 		parser.mapOnStart("item", TaggingPreset.class);
 		parser.mapOnStart("separator", TaggingPresetSeparator.class);
