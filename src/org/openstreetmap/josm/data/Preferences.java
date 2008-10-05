07bde6132bcfa3f8a6051f9ea11afb5aea7b4d1d
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2008-10-05 10:50:00 +0000

    close bug #1628
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@1018 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/Preferences.java
+++ src/org/openstreetmap/josm/data/Preferences.java
@@ -31,7 +31,14 @@ import org.openstreetmap.josm.tools.ColorHelper;
  * @author imi
  */
 public class Preferences {
-
+    
+    /**
+    * Internal storage for the preferenced directory. 
+    * Do not access this variable directly!
+    * @see #getPreferencesDirFile()
+    */
+    private File preferencesDirFile = null;
+     
 	public static interface PreferenceChangedListener {
 		void preferenceChanged(String key, String newValue);
 	}
@@ -79,12 +86,24 @@ public class Preferences {
 		return path + File.separator;
 	}
 
-	public File getPreferencesDirFile() {
-		if (System.getenv("APPDATA") != null)
-			return new File(System.getenv("APPDATA"), "JOSM");
-		return new File(System.getProperty("user.home"), ".josm");
-	}
-	
+    public File getPreferencesDirFile() {
+        if (preferencesDirFile != null)
+            return preferencesDirFile;
+        String path;
+        path = System.getProperty("josm.home");
+        if (path != null) {
+            preferencesDirFile = new File(path);
+        } else {
+            path = System.getenv("APPDATA");
+            if (path != null) {
+                preferencesDirFile = new File(path, "JOSM");
+            } else {
+                preferencesDirFile = new File(System.getProperty("user.home"), ".josm");
+            }
+        }
+        return preferencesDirFile;
+    }
+    
 	public File getPluginsDirFile() {
 		return new File(getPreferencesDirFile(), "plugins");
 	}
