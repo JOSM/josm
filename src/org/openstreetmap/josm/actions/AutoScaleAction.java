54aeb8459dad8aee27a0782e529193f0452bba4f
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-01-19 13:07:58 +0000

    applied #2046. patch by Detlef Reichl
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@1302 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/AutoScaleAction.java
+++ src/org/openstreetmap/josm/actions/AutoScaleAction.java
@@ -11,6 +11,7 @@ import java.util.Collection;
 import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
 import org.openstreetmap.josm.gui.layer.Layer;
@@ -22,7 +23,7 @@ import org.openstreetmap.josm.tools.Shortcut;
  */
 public class AutoScaleAction extends JosmAction {
 
-    public static final String[] modes = { marktr("data"), marktr("layer"), marktr("selection"), marktr("conflict") };
+    public static final String[] modes = { marktr("data"), marktr("layer"), marktr("selection"), marktr("conflict"), marktr("download") };
     private final String mode;
 
     private static int getModeShortcut(String mode) {
@@ -39,6 +40,9 @@ public class AutoScaleAction extends JosmAction {
         }
         if (mode.equals("conflict")) {
             shortcut = KeyEvent.VK_4;
+        }
+        if (mode.equals("download")) {
+            shortcut = KeyEvent.VK_5;
         }
 
         return shortcut;
@@ -83,6 +87,23 @@ public class AutoScaleAction extends JosmAction {
             // especially if the bbox contains one single node, but helpful
             // in most other cases as well.
             v.enlargeBoundingBox();
+        }
+        else if (mode.equals("download")) {
+            if (Main.pref.hasKey("osm-download.bounds")) {
+                try {
+                    String bounds[] = Main.pref.get("osm-download.bounds").split(";");
+                    double minlat = Double.parseDouble(bounds[0]);
+                    double minlon = Double.parseDouble(bounds[1]);
+                    double maxlat = Double.parseDouble(bounds[2]);
+                    double maxlon = Double.parseDouble(bounds[3]);
+                    
+                    v.visit(Main.proj.latlon2eastNorth(new LatLon(minlat, minlon)));
+                    v.visit(Main.proj.latlon2eastNorth(new LatLon(maxlat, maxlon)));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return v;
     }
