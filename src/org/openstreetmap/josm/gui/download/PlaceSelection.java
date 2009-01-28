5a881e77bb4386496617adecc36bfd3e727dde02
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-01-28 14:17:24 +0000

    typo
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@1346 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/download/PlaceSelection.java
+++ src/org/openstreetmap/josm/gui/download/PlaceSelection.java
@@ -117,7 +117,7 @@ public class PlaceSelection implements DownloadSelection {
             catch (NullPointerException x)
             {
                 x.printStackTrace(); // SAXException does not chain correctly
-                throw new SAXException(tr("NullPointerException, Possibly some missing tags."), x);
+                throw new SAXException(tr("NullPointerException, possibly some missing tags."), x);
             }
         }
         /**
