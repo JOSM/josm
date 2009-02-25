d0fb483cfd58b6ceea1f5851928a1a061356fc01
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-02-25 15:04:52 +0000

    fixed #2208 - color handling for layer colors
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@1445 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/preferences/ColorPreference.java
+++ src/org/openstreetmap/josm/gui/preferences/ColorPreference.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -26,6 +27,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.BorderFactory;
@@ -42,6 +44,11 @@ public class ColorPreference implements PreferenceSetting {
 
     private DefaultTableModel tableModel;
     private JTable colors;
+    private ArrayList<String> del = new ArrayList<String>();
+
+    JButton colorEdit;
+    JButton defaultSet;
+    JButton remove;
 
     /**
      * Set the colors to be shown in the preference table. This method creates a table model if
@@ -135,33 +142,9 @@ public class ColorPreference implements PreferenceSetting {
         fixColorPrefixes();
         setColorModel(Main.pref.getAllColors());
 
-        colors = new JTable(tableModel) {
-            @Override public boolean isCellEditable(int row, int column) {
-                return false;
-            }
-        };
-        colors.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        final TableCellRenderer oldColorsRenderer = colors.getDefaultRenderer(Object.class);
-        colors.setDefaultRenderer(Object.class, new TableCellRenderer(){
-            public Component getTableCellRendererComponent(JTable t, Object o, boolean selected, boolean focus, int row, int column) {
-                if (column == 1) {
-                    JLabel l = new JLabel(ColorHelper.color2html((Color)o));
-                    l.setBackground((Color)o);
-                    l.setOpaque(true);
-                    return l;
-                }
-                return oldColorsRenderer.getTableCellRendererComponent(t,getName(o.toString()),selected,focus,row,column);
-            }
-        });
-        colors.getColumnModel().getColumn(1).setWidth(100);
-
-        JButton colorEdit = new JButton(tr("Choose"));
+        colorEdit = new JButton(tr("Choose"));
         colorEdit.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                if (colors.getSelectedRowCount() == 0) {
-                    JOptionPane.showMessageDialog(gui, tr("Please select a color."));
-                    return;
-                }
                 int sel = colors.getSelectedRow();
                 JColorChooser chooser = new JColorChooser((Color)colors.getValueAt(sel, 1));
                 int answer = JOptionPane.showConfirmDialog(gui, chooser,
@@ -171,13 +154,9 @@ public class ColorPreference implements PreferenceSetting {
                     colors.setValueAt(chooser.getColor(), sel, 1);
             }
         });
-        JButton defaultSet = new JButton(tr("Set to default"));
+        defaultSet = new JButton(tr("Set to default"));
         defaultSet.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                if (colors.getSelectedRowCount() == 0) {
-                    JOptionPane.showMessageDialog(gui, tr("Please select a color."));
-                    return;
-                }
                 int sel = colors.getSelectedRow();
                 String name = (String)colors.getValueAt(sel, 0);
                 Color c = Main.pref.getDefaultColor(name);
@@ -197,6 +176,44 @@ public class ColorPreference implements PreferenceSetting {
                 }
             }
         });
+        remove = new JButton(tr("Remove"));
+        remove.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                int sel = colors.getSelectedRow();
+                del.add((String)colors.getValueAt(sel, 0));
+                tableModel.removeRow(sel);
+            }
+        });
+        remove.setEnabled(false);
+        colorEdit.setEnabled(false);
+        defaultSet.setEnabled(false);
+
+        colors = new JTable(tableModel) {
+            @Override public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+            @Override public void valueChanged(ListSelectionEvent e) {
+                super.valueChanged(e);
+                int sel = colors.getSelectedRow();
+                remove.setEnabled(sel > 0 && isRemoveColor(sel));
+                colorEdit.setEnabled(sel > 0);
+                defaultSet.setEnabled(sel > 0);
+            }
+        };
+        colors.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        final TableCellRenderer oldColorsRenderer = colors.getDefaultRenderer(Object.class);
+        colors.setDefaultRenderer(Object.class, new TableCellRenderer(){
+            public Component getTableCellRendererComponent(JTable t, Object o, boolean selected, boolean focus, int row, int column) {
+                if (column == 1) {
+                    JLabel l = new JLabel(ColorHelper.color2html((Color)o));
+                    l.setBackground((Color)o);
+                    l.setOpaque(true);
+                    return l;
+                }
+                return oldColorsRenderer.getTableCellRendererComponent(t,getName(o.toString()),selected,focus,row,column);
+            }
+        });
+        colors.getColumnModel().getColumn(1).setWidth(100);
         colors.setToolTipText(tr("Colors used by different objects in JOSM."));
         colors.setPreferredScrollableViewportSize(new Dimension(100,112));
 
@@ -211,7 +228,13 @@ public class ColorPreference implements PreferenceSetting {
         buttonPanel.add(colorEdit, GBC.std().insets(0,5,0,0));
         buttonPanel.add(defaultSet, GBC.std().insets(5,5,5,0));
         buttonPanel.add(defaultAll, GBC.std().insets(0,5,0,0));
+        buttonPanel.add(remove, GBC.std().insets(0,5,0,0));
         gui.displaycontent.addTab(tr("Colors"), panel);
+    }
+
+    Boolean isRemoveColor(int row)
+    {
+        return ((String)colors.getValueAt(row, 0)).startsWith("layer ");
     }
 
     /**
@@ -226,6 +249,8 @@ public class ColorPreference implements PreferenceSetting {
 
     public boolean ok() {
         Boolean ret = false;
+        for(String d : del)
+            Main.pref.put("color."+d, null);
         for (int i = 0; i < colors.getRowCount(); ++i) {
             String key = (String)colors.getValueAt(i, 0);
             if(Main.pref.putColor(key, (Color)colors.getValueAt(i, 1)))
