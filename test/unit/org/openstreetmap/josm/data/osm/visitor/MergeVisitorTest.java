98f996609b1fe653dd83124a8239d82afc63a7fb
Gubaer <Gubaer@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-07-08 20:06:58 +0000

    patch by dmuecke - improved unit tests and test targets in build.xml
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@1751 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/osm/visitor/MergeVisitorTest.java
+++ test/unit/org/openstreetmap/josm/data/osm/visitor/MergeVisitorTest.java
@@ -32,29 +32,32 @@ public class MergeVisitorTest {
 
     @BeforeClass
     static public void init() {
-        testProperties = new Properties();
 
-        // load properties
-        //
-        try {
-            testProperties.load(MergeVisitorTest.class.getResourceAsStream("/test-unit-env.properties"));
-        } catch(Exception e){
-            logger.log(Level.SEVERE, MessageFormat.format("failed to load property file ''{0}''", "/test-unit-env.properties"));
-            fail(MessageFormat.format("failed to load property file ''{0}''", "/test-unit-env.properties"));
-        }
+        if(System.getProperty("josm.home") == null){
+            testProperties = new Properties();
 
-        // check josm.home
-        //
-        String josmHome = testProperties.getProperty("josm.home");
-        if (josmHome == null) {
-            fail(MessageFormat.format("property ''{0}'' not set in test environment", "josm.home"));
-        } else {
-            File f = new File(josmHome);
-            if (! f.exists() || ! f.canRead()) {
-                fail(MessageFormat.format("property ''{0}'' points to ''{1}'' which is either not existing or not readable", "josm.home", josmHome));
+            // load properties
+            //
+            try {
+                testProperties.load(MergeVisitorTest.class.getResourceAsStream("/test-unit-env.properties"));
+            } catch(Exception e){
+                logger.log(Level.SEVERE, MessageFormat.format("failed to load property file ''{0}''", "/test-unit-env.properties"));
+                fail(MessageFormat.format("failed to load property file ''{0}''", "/test-unit-env.properties"));
             }
+
+            // check josm.home
+            //
+            String josmHome = testProperties.getProperty("josm.home");
+            if (josmHome == null) {
+                fail(MessageFormat.format("property ''{0}'' not set in test environment", "josm.home"));
+            } else {
+                File f = new File(josmHome);
+                if (! f.exists() || ! f.canRead()) {
+                    fail(MessageFormat.format("property ''{0}'' points to ''{1}'' which is either not existing or not readable", "josm.home", josmHome));
+                }
+            }
+            System.setProperty("josm.home", josmHome);
         }
-        System.setProperty("josm.home", josmHome);
         Main.pleaseWaitDlg = new PleaseWaitDialog(null);
         Main.pref.init(false);
 
@@ -64,7 +67,7 @@ public class MergeVisitorTest {
 
     /**
      * two identical nodes, even in id and version. No confict expected.
-     * 
+     *
      * Can happen if data is loaded in two layers and then merged from one layer
      * on the other.
      */
@@ -103,7 +106,7 @@ public class MergeVisitorTest {
     /**
      * two  nodes, my is unmodified, their is updated and has a higher version
      * => their version is going to be the merged version
-     * 
+     *
      */
     @Test
     public void nodeSimple_locallyUnmodifiedNoConflict() {
@@ -142,7 +145,7 @@ public class MergeVisitorTest {
     /**
      * node with same id, my is modified, their has a higher version
      * => results in a conflict
-     * 
+     *
      * Use case: node which is modified locally and updated by another mapper on
      * the server
      */
@@ -180,7 +183,7 @@ public class MergeVisitorTest {
     /**
      * node with same id, my is deleted, their has a higher version
      * => results in a conflict
-     * 
+     *
      * Use case: node which is deleted locally and updated by another mapper on
      * the server
      */
@@ -216,7 +219,7 @@ public class MergeVisitorTest {
     /**
      * My node is visible, their version has a higher version and is not visible
      * => create a conflict
-     * 
+     *
      */
     @Test
     public void nodeSimple_VisibleConflict() {
@@ -250,7 +253,7 @@ public class MergeVisitorTest {
     /**
      * My node is deleted, their node has the same id and version and is not deleted.
      * => mine has precedence
-     * 
+     *
      */
     @Test
     public void nodeSimple_DeleteConflict_2() {
@@ -280,9 +283,9 @@ public class MergeVisitorTest {
 
     /**
      * My and their node are new but semantically equal. My node is deleted.
-     * 
+     *
      * => create a conflict
-     * 
+     *
      */
     @Test
     public void nodeSimple_DeleteConflict_3() {
@@ -308,9 +311,9 @@ public class MergeVisitorTest {
 
     /**
      * My and their node are new but semantically equal. Both are deleted.
-     * 
+     *
      * => take mine
-     * 
+     *
      */
     @Test
     public void nodeSimple_DeleteConflict_4() {
@@ -338,7 +341,7 @@ public class MergeVisitorTest {
     /**
      * their node is not visible and doesn't exist in my data set
      * => ignore their node
-     * 
+     *
      */
     @Test
     public void nodeSimple_InvisibleNodeInTheirDataset() {
@@ -371,9 +374,9 @@ public class MergeVisitorTest {
     /**
      * their node has no assigned id (id == 0) and is semantically equal to one of my
      * nodes with id == 0
-     * 
+     *
      * => merge it onto my node.
-     * 
+     *
      */
     @Test
     public void nodeSimple_NoIdSemanticallyEqual() {
@@ -421,9 +424,9 @@ public class MergeVisitorTest {
 
     /**
      * my node is incomplete, their node is complete
-     * 
+     *
      * => merge it onto my node. My node becomes complete
-     * 
+     *
      */
     @Test
     public void nodeSimple_IncompleteNode() {
@@ -460,9 +463,9 @@ public class MergeVisitorTest {
     /**
      * their way has a higher version and different tags. the nodes are the same. My
      * way is not modified. Merge is possible. No conflict.
-     * 
+     *
      * => merge it onto my way.
-     * 
+     *
      */
     @Test
     public void waySimple_IdenicalNodesDifferentTags() {
@@ -530,9 +533,9 @@ public class MergeVisitorTest {
     /**
      * their way has a higher version and different tags. And it has more nodes. Two
      * of the existing nodes are modified.
-     * 
+     *
      * => merge it onto my way, no conflict
-     * 
+     *
      */
     @Test
     public void waySimple_AdditionalNodesAndChangedNodes() {
@@ -604,9 +607,9 @@ public class MergeVisitorTest {
 
     /**
      * their way has a higher version and different nodes. My way is modified.
-     * 
+     *
      * => merge it onto my way not possbile, conflict
-     * 
+     *
      */
     @Test
     public void waySimple_DifferentNodesAndMyIsModified() {
@@ -680,9 +683,9 @@ public class MergeVisitorTest {
 
     /**
      * their way is not visible anymore.
-     * 
+     *
      * => conflict
-     * 
+     *
      */
     @Test
     public void waySimple_TheirVersionNotVisible() {
@@ -730,9 +733,9 @@ public class MergeVisitorTest {
      * my and their way have no ids,  nodes they refer to have an id. but
      * my and  their way are semantically equal. so technical attributes of
      * their way can be merged on my way. No conflict.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
     @Test
     public void waySimple_twoWaysWithNoId_NodesWithId() {
@@ -794,7 +797,7 @@ public class MergeVisitorTest {
      * my and their way have no ids, neither do the nodes they refer to. but
      * my and  their way are semantically equal. so technical attributes of
      * their way can be merged on my way. No conflict.
-     * 
+     *
      */
     @Test
     public void waySimple_twoWaysWithNoId_NodesWithoutId() {
