10b7f6ba7a2ecd8a5662983a1acb9c283764013a
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-08-04 19:39:23 +0000

    fixed EPSG code
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@1909 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/projection/Lambert.java
+++ src/org/openstreetmap/josm/data/projection/Lambert.java
@@ -145,7 +145,7 @@ public class Lambert implements Projection {
     }
 
     public String toCode() {
-        return "EPSG::"+(27571+currentZone);
+        return "EPSG:"+(27571+currentZone-1);
     }
 
     public String getCacheDirectoryName() {
