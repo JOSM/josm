66e73244f06a9303a89953099fd1c64ad301899f
Gubaer <Gubaer@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-09-06 21:16:11 +0000

    fixed #3382: Opening a /user/ page fails for usernames with a space (" ") in them
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@2073 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/dialogs/UserListDialog.java
+++ src/org/openstreetmap/josm/gui/dialogs/UserListDialog.java
@@ -200,7 +200,7 @@ public class UserListDialog extends ToggleDialog implements SelectionChangedList
         protected String createInfoUrl(Object infoObject) {
             User user = (User)infoObject;
             try {
-                return getBaseUserUrl() + "/" + URLEncoder.encode(user.getName(), "UTF-8");
+                return getBaseUserUrl() + "/" + URLEncoder.encode(user.getName(), "UTF-8").replaceAll("\\+", "%20");
             } catch(UnsupportedEncodingException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(
