4a8e30a4dbfc5bec2762935e9a6959a2e234fcd2
Gubaer <Gubaer@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-09-29 17:02:42 +0000

    see #3614: JOSM not displaying all loaded ways argh, this should do it ;-)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@2216 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/osm/OsmPrimitive.java
+++ src/org/openstreetmap/josm/data/osm/OsmPrimitive.java
@@ -123,13 +123,7 @@ abstract public class OsmPrimitive implements Comparable<OsmPrimitive>, Tagged {
      */
     private long id = 0;
 
-    private volatile byte flags =
-        ~FLAG_DELETED
-        & ~FLAG_DISABLED
-        & ~FLAG_FILTERED
-        & ~FLAG_MODIFIED
-        & ~FLAG_SELECTED
-        | FLAG_VISIBLE;   // visible per default
+    private volatile byte flags = FLAG_VISIBLE;   // visible per default
 
     /**
      * User that last modified this primitive, as specified by the server.
