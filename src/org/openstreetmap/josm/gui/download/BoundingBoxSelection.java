ccf3e63e600fd8687bb3e8b56cf77275cc1f7850
Gubaer <Gubaer@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-11-07 17:02:39 +0000

    fixed #454: Right-click context menus in download text-entry boxes
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@2403 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/download/BoundingBoxSelection.java
+++ src/org/openstreetmap/josm/gui/download/BoundingBoxSelection.java
@@ -6,14 +6,25 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.FlavorEvent;
+import java.awt.datatransfer.FlavorListener;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
@@ -25,6 +36,7 @@ import javax.swing.text.JTextComponent;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.tools.GBC;
+import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.OsmUrlToBounds;
 
 /**
@@ -38,11 +50,11 @@ import org.openstreetmap.josm.tools.OsmUrlToBounds;
 public class BoundingBoxSelection implements DownloadSelection {
 
     private JTextField[] latlon = null;
-    private final JTextArea osmUrl = new JTextArea();
+    private final JTextArea tfOsmUrl = new JTextArea();
     private final JTextArea showUrl = new JTextArea();
     private DownloadDialog parent;
 
-    
+
     protected void buildDownloadAreaInputFields() {
         latlon = new JTextField[4];
         for(int i=0; i< 4; i++) {
@@ -57,7 +69,7 @@ public class BoundingBoxSelection implements DownloadSelection {
         latChecker = new LatValueChecker(latlon[2]);
         latlon[2].addFocusListener(latChecker);
         latlon[2].addActionListener(latChecker);
-        
+
         LonValueChecker lonChecker = new LonValueChecker(latlon[1]);
         latlon[1].addFocusListener(lonChecker);
         latlon[1].addActionListener(lonChecker);
@@ -65,21 +77,20 @@ public class BoundingBoxSelection implements DownloadSelection {
         lonChecker = new LonValueChecker(latlon[3]);
         latlon[3].addFocusListener(lonChecker);
         latlon[3].addActionListener(lonChecker);
-        
     }
-    
+
     public void addGui(final DownloadDialog gui) {
         buildDownloadAreaInputFields();
-        JPanel dlg = new JPanel(new GridBagLayout());
+        final JPanel dlg = new JPanel(new GridBagLayout());
 
-        osmUrl.getDocument().addDocumentListener(new OsmUrlRefresher());
+        tfOsmUrl.getDocument().addDocumentListener(new OsmUrlRefresher());
 
         // select content on receiving focus. this seems to be the default in the
         // windows look+feel but not for others. needs invokeLater to avoid strange
         // side effects that will cancel out the newly made selection otherwise.
-        osmUrl.addFocusListener(new SelectAllOnFocusHandler(osmUrl));
-        osmUrl.setLineWrap(true);
-        osmUrl.setBorder(latlon[0].getBorder());
+        tfOsmUrl.addFocusListener(new SelectAllOnFocusHandler(tfOsmUrl));
+        tfOsmUrl.setLineWrap(true);
+        tfOsmUrl.setBorder(latlon[0].getBorder());
 
         dlg.add(new JLabel(tr("min lat")), GBC.std().insets(10,20,5,0));
         dlg.add(latlon[0], GBC.std().insets(0,20,0,0));
@@ -91,7 +102,32 @@ public class BoundingBoxSelection implements DownloadSelection {
         dlg.add(latlon[3], GBC.eol());
 
         dlg.add(new JLabel(tr("URL from www.openstreetmap.org (you can paste an URL here to download the area)")), GBC.eol().insets(10,20,5,0));
-        dlg.add(osmUrl, GBC.eop().insets(10,0,5,0).fill());
+        dlg.add(tfOsmUrl, GBC.eop().insets(10,0,5,0).fill());
+        tfOsmUrl.addMouseListener(
+                new MouseAdapter() {
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+                        checkPopup(e);
+                    }
+
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        checkPopup(e);
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+                        checkPopup(e);
+                    }
+
+                    private void checkPopup(MouseEvent e) {
+                        if (e.isPopupTrigger()) {
+                            OsmUrlPopup popup = new OsmUrlPopup();
+                            popup.show(tfOsmUrl, e.getX(), e.getY());
+                        }
+                    }
+                }
+        );
         dlg.add(showUrl, GBC.eop().insets(10,0,5,5));
         showUrl.setEditable(false);
         showUrl.setBackground(dlg.getBackground());
@@ -101,12 +137,12 @@ public class BoundingBoxSelection implements DownloadSelection {
         this.parent = gui;
     }
 
-    
+
     public void setDownloadArea(Bounds area) {
         updateBboxFields(area);
         updateUrl(area);
     }
-    
+
     public Bounds getDownloadArea() {
         double[] values = new double[4];
         for (int i=0; i < 4; i++) {
@@ -119,13 +155,13 @@ public class BoundingBoxSelection implements DownloadSelection {
         if (!LatLon.isValidLat(values[0]) || !LatLon.isValidLon(values[1]))
             return null;
         if (!LatLon.isValidLat(values[2]) || !LatLon.isValidLon(values[3]))
-            return null;        
+            return null;
         return new Bounds(values);
     }
 
     private boolean parseURL(DownloadDialog gui) {
-        Bounds b = OsmUrlToBounds.parse(osmUrl.getText());
-        if(b == null) return false;        
+        Bounds b = OsmUrlToBounds.parse(tfOsmUrl.getText());
+        if(b == null) return false;
         gui.boundingBoxChanged(b,BoundingBoxSelection.this);
         updateBboxFields(b);
         updateUrl(b);
@@ -147,11 +183,11 @@ public class BoundingBoxSelection implements DownloadSelection {
         if (area == null) return;
         showUrl.setText(OsmUrlToBounds.getURL(area));
     }
-    
-    
+
+
     class LatValueChecker extends FocusAdapter implements ActionListener{
         private JTextField tfLatValue;
-        
+
         private Border errorBorder = BorderFactory.createLineBorder(Color.RED, 1);
         protected void setErrorMessage(String msg) {
             if (msg != null) {
@@ -159,14 +195,14 @@ public class BoundingBoxSelection implements DownloadSelection {
                 tfLatValue.setToolTipText(msg);
             } else {
                 tfLatValue.setBorder(UIManager.getBorder("TextField.border"));
-                tfLatValue.setToolTipText("");                
+                tfLatValue.setToolTipText("");
             }
         }
-     
+
         public LatValueChecker(JTextField tfLatValue) {
             this.tfLatValue = tfLatValue;
         }
-        
+
         protected void check() {
             double value = 0;
             try {
@@ -177,21 +213,21 @@ public class BoundingBoxSelection implements DownloadSelection {
             }
             if (!LatLon.isValidLat(value)) {
                 setErrorMessage(tr("Value for latitude in range [-90,90] required.", tfLatValue.getText()));
-                return;            
+                return;
             }
             setErrorMessage(null);
         }
-        
+
         @Override
         public void focusLost(FocusEvent e) {
             check();
         }
 
         public void actionPerformed(ActionEvent e) {
-            check();            
-        }        
+            check();
+        }
     }
-    
+
     class LonValueChecker extends FocusAdapter implements ActionListener {
         private JTextField tfLonValue;
         private Border errorBorder = BorderFactory.createLineBorder(Color.RED, 1);
@@ -201,21 +237,21 @@ public class BoundingBoxSelection implements DownloadSelection {
                 tfLonValue.setToolTipText(msg);
             } else {
                 tfLonValue.setBorder(UIManager.getBorder("TextField.border"));
-                tfLonValue.setToolTipText("");                
+                tfLonValue.setToolTipText("");
             }
         }
-        
+
         public LonValueChecker(JTextField tfLonValue) {
             this.tfLonValue = tfLonValue;
         }
-        
+
         protected void check() {
             double value = 0;
             try {
                 value = Double.parseDouble(tfLonValue.getText());
             } catch(NumberFormatException ex) {
-               setErrorMessage(tr("The string ''{0}'' isn''t a valid double value.", tfLonValue.getText()));
-               return;
+                setErrorMessage(tr("The string ''{0}'' isn''t a valid double value.", tfLonValue.getText()));
+                return;
             }
             if (!LatLon.isValidLon(value)) {
                 setErrorMessage(tr("Value for longitude in range [-180,180] required.", tfLonValue.getText()));
@@ -223,7 +259,7 @@ public class BoundingBoxSelection implements DownloadSelection {
             }
             setErrorMessage(null);
         }
-        
+
         @Override
         public void focusLost(FocusEvent e) {
             check();
@@ -231,24 +267,72 @@ public class BoundingBoxSelection implements DownloadSelection {
 
         public void actionPerformed(ActionEvent e) {
             check();
-        }        
+        }
     }
-    
+
     class SelectAllOnFocusHandler extends FocusAdapter {
         private JTextComponent tfTarget;
         public SelectAllOnFocusHandler(JTextComponent tfTarget) {
-            this.tfTarget = tfTarget;            
+            this.tfTarget = tfTarget;
         }
-        
+
         @Override
         public void focusGained(FocusEvent e) {
             tfTarget.selectAll();
-        }        
+        }
     }
-    
+
     class OsmUrlRefresher implements DocumentListener {
         public void changedUpdate(DocumentEvent e) { parseURL(parent); }
         public void insertUpdate(DocumentEvent e) { parseURL(parent); }
         public void removeUpdate(DocumentEvent e) { parseURL(parent); }
+    }
+
+    class PasteUrlAction extends AbstractAction implements FlavorListener {
+
+        public PasteUrlAction() {
+            putValue(NAME, tr("Paste"));
+            putValue(SMALL_ICON, ImageProvider.get("paste"));
+            putValue(SHORT_DESCRIPTION, tr("Paste URL from clipboard"));
+            Toolkit.getDefaultToolkit().getSystemClipboard().addFlavorListener(this);
+        }
+
+        protected String getClipboardContent() {
+            Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+            try {
+                if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    String text = (String)t.getTransferData(DataFlavor.stringFlavor);
+                    return text;
+                }
+            } catch (UnsupportedFlavorException ex) {
+                ex.printStackTrace();
+                return null;
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+            return null;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            String content = getClipboardContent();
+            if (content != null) {
+                tfOsmUrl.setText(content);
+            }
+        }
+
+        protected void updateEnabledState() {
+            setEnabled(getClipboardContent() != null);
+        }
+
+        public void flavorsChanged(FlavorEvent e) {
+            updateEnabledState();
+        }
+    }
+
+    class OsmUrlPopup extends JPopupMenu {
+        public OsmUrlPopup() {
+            add(new PasteUrlAction());
+        }
     }
 }
