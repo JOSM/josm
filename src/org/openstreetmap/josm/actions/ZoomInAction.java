20625ac6b52bf505bac773338a85a3f5a608407b
Gubaer <Gubaer@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-11-29 13:44:17 +0000

    fixed #2669: Problem with zoom keys fixed #3269: Plus/minus buttons don't work Couldn't get '+' to work on a swiss german keyboard (don't know why) but 'numpad +' and 'numpad -' now zoom in and zoom out too
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@2550 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/ZoomInAction.java
+++ src/org/openstreetmap/josm/actions/ZoomInAction.java
@@ -1,11 +1,14 @@
 // License: GPL. Copyright 2007 by Immanuel Scholz and others
 package org.openstreetmap.josm.actions;
 
-import static org.openstreetmap.josm.tools.I18n.tr;
 import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.tools.Shortcut;
@@ -13,12 +16,22 @@ import org.openstreetmap.josm.tools.Shortcut;
 public final class ZoomInAction extends JosmAction {
 
     public ZoomInAction() {
-        super(tr("Zoom In"), "dialogs/zoomin", tr("Zoom In"),
-                Shortcut.registerShortcut("view:zoomin", tr("View: {0}", tr("Zoom In")), KeyEvent.VK_PLUS, Shortcut.GROUP_DIRECT), true);
+        super(
+                tr("Zoom In"),
+                "dialogs/zoomin",
+                tr("Zoom In"),
+                Shortcut.registerShortcut("view:zoomin", tr("View: {0}", tr("Zoom In")),KeyEvent.VK_PLUS, Shortcut.GROUP_DIRECT),
+                true
+        );
         putValue("help", ht("/Action/ZoomIn"));
+        // make numpad + behave like + (action is already registred)
+        Main.contentPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ADD,0), tr("Zoom In"));
     }
 
     public void actionPerformed(ActionEvent e) {
+        Object name = Main.contentPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).get(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, 0));
+        Main.contentPane.getActionMap().put(name, this);
+
         if (!Main.isDisplayingMapView()) return;
         Main.map.mapView.zoomToFactor(0.9);
     }
--- src/org/openstreetmap/josm/actions/ZoomOutAction.java
+++ src/org/openstreetmap/josm/actions/ZoomOutAction.java
@@ -1,11 +1,14 @@
 // License: GPL. Copyright 2007 by Immanuel Scholz and others
 package org.openstreetmap.josm.actions;
 
-import static org.openstreetmap.josm.tools.I18n.tr;
 import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.tools.Shortcut;
@@ -16,6 +19,8 @@ public final class ZoomOutAction extends JosmAction {
         super(tr("Zoom Out"), "dialogs/zoomout", tr("Zoom Out"),
                 Shortcut.registerShortcut("view:zoomout", tr("View: {0}", tr("Zoom Out")), KeyEvent.VK_MINUS, Shortcut.GROUP_DIRECT), true);
         putValue("help", ht("/Action/ZoomOut"));
+        // make numpad - behave like -
+        Main.contentPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_SUBTRACT,0), tr("Zoom Out"));
     }
 
     public void actionPerformed(ActionEvent e) {
