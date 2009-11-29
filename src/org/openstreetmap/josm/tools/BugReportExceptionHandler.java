be73d73f1ceb46d97f21d830ed2af90ecb6122e1
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-11-29 12:09:43 +0000

    fix #4035 - patch by avar - directly fill bug report request
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@2544 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/BugReportExceptionHandler.java
+++ src/org/openstreetmap/josm/tools/BugReportExceptionHandler.java
@@ -21,6 +21,11 @@ import javax.swing.JTextArea;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.ShowStatusReportAction;
 import org.openstreetmap.josm.plugins.PluginHandler;
+import org.openstreetmap.josm.tools.Base64;
+
+import java.net.URL;
+import java.net.URLEncoder;
+import org.openstreetmap.josm.tools.UrlLabel;
 
 /**
  * An exception handler that asks the user to send a bug report.
@@ -64,11 +69,41 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
                     String text = ShowStatusReportAction.getReportHeader()
                     + stack.getBuffer().toString();
 
+                    URL url = new URL("http://josm.openstreetmap.de/josmticket?" +
+                                      "data="+
+                                      Base64.encode(
+                                      // To note that it came from this code
+                                      "keywords=template_report&" +
+                                      "description=" + java.net.URLEncoder.encode(
+                                                         // Note: This doesn't use tr() intentionally, we want bug reports in English
+                                                         "What steps will reproduce the problem?\n"
+                                                            + " 1. \n"
+                                                            + " 2. \n"
+                                                            + " 3. \n"
+                                                            + "\n"
+                                                            + "What is the expected result?\n\n"
+                                                            + "What happens instead?\n\n"
+                                                            + "Please provide any additional information below. Attach a screenshot if\n"
+                                                            + "possible.\n\n"
+                                                            + "{{{\n" + text + "\n}}}\n",
+                                                         "UTF-8")));
+
                     JPanel p = new JPanel(new GridBagLayout());
-                    p.add(new JLabel("<html>" + tr("Please report a ticket at {0}","http://josm.openstreetmap.de/newticket") +
-                            "<br>" + tr("Include your steps to get to the error (as detailed as possible)!") +
-                            "<br>" + tr("Try updating to the newest version of JOSM and all plugins before reporting a bug.") +
-                            "<br>" + tr("Be sure to include the following information:") + "</html>"), GBC.eol());
+                    p.add(new JLabel(tr("<html>" +
+                                        "<p>You've encountered an error in JOSM. Before you file a bug<br>" +
+                                        "make sure you've updated to the latest version of JOSM here:</p></html>")), GBC.eol());
+                    p.add(new UrlLabel("http://josm.openstreetmap.de/#Download"), GBC.eop().insets(8,0,0,0));
+                    p.add(new JLabel(tr("<html>You should also update your plugins. If neither of those help please<br>" +
+                                        "file a bug in our bugtracker using this link:</p></html>")), GBC.eol());
+                    p.add(new UrlLabel(url.toString(), "http://josm.openstreetmap.de/josmticket?..."), GBC.eop().insets(8,0,0,0));
+                    p.add(new JLabel(tr("<html><p>" +
+                                        "There the the error information provided below should already be<br>" +
+                                        "filled out for you. Please include information on how to reproduce<br>" +
+                                        "the error and try to supply as much detail as possible.</p></html>")), GBC.eop());
+                    p.add(new JLabel(tr("<html><p>" +
+                                        "Alternatively if that doesn't work you can manually fill in the information<br>" +
+                                        "below at this URL:</p></html>")), GBC.eol());
+                    p.add(new UrlLabel("http://josm.openstreetmap.de/newticket"), GBC.eop().insets(8,0,0,0));
                     try {
                         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(text), new ClipboardOwner(){
                             public void lostOwnership(Clipboard clipboard, Transferable contents) {}
@@ -82,7 +117,7 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
                     info.setEditable(false);
                     p.add(new JScrollPane(info), GBC.eop());
 
-                    JOptionPane.showMessageDialog(Main.parent, p, tr("Warning"), JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(Main.parent, p, tr("You've encountered a bug in JOSM"), JOptionPane.ERROR_MESSAGE);
                 } catch (Exception e1) {
                     e1.printStackTrace();
                 }
