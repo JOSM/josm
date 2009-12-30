ba8324f6d82535354d583e6008cfea96ac82fea8
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2009-12-30 20:47:14 +0000

    removed obsolete code (re-add when necessary)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@2713 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/progress/StackableProgress.java
+++
@@ -1,7 +0,0 @@
-// License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.gui.progress;
-
-public interface StackableProgress {
-    public void setChildProgress(double value);
-    public void setSubTaskName(String value);
-}
