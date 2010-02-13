1ee1043eb96d1dc47698999953dd7e8346430bb1
jttt <jttt@0c6e7542-c601-0410-84e7-c038aed88b3b>
2010-02-13 15:19:56 +0000

    Fix #4070 confict management: reseting ID
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@2972 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/command/UndeletePrimitivesCommand.java
+++ src/org/openstreetmap/josm/command/UndeletePrimitivesCommand.java
@@ -5,14 +5,20 @@ import static org.openstreetmap.josm.tools.I18n.trn;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.ListIterator;
 
 import javax.swing.JLabel;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
 
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.tools.ImageProvider;
 
 /**
@@ -22,10 +28,12 @@ import org.openstreetmap.josm.tools.ImageProvider;
  *
  */
 public class UndeletePrimitivesCommand extends ConflictResolveCommand {
-    static private final Logger logger = Logger.getLogger(UndeletePrimitivesCommand.class.getName());
+    //static private final Logger logger = Logger.getLogger(UndeletePrimitivesCommand.class.getName());
 
     /** the node to undelete */
     private final List<OsmPrimitive> toUndelete = new ArrayList<OsmPrimitive>();
+    /** primitives that replaced undeleted primitives */
+    private final List<OsmPrimitive> replacedPrimitives = new ArrayList<OsmPrimitive>();
 
     /**
      * constructor
@@ -50,7 +58,6 @@ public class UndeletePrimitivesCommand extends ConflictResolveCommand {
      * @param node  the node to undelete
      */
     public UndeletePrimitivesCommand(Collection<OsmPrimitive> toUndelete) {
-        this();
         this.toUndelete.addAll(toUndelete);
     }
 
@@ -69,19 +76,68 @@ public class UndeletePrimitivesCommand extends ConflictResolveCommand {
     public boolean executeCommand() {
         super.executeCommand();
 
+        replacedPrimitives.clear();
         for(OsmPrimitive primitive: toUndelete) {
             if(getLayer().getConflicts().hasConflictForMy(primitive)) {
                 rememberConflict(getLayer().getConflicts().getConflictForMy(primitive));
                 getLayer().getConflicts().remove(primitive);
             }
-            primitive.clearOsmId();
+            OsmPrimitive prim;
+            switch (primitive.getType()) {
+            case NODE:
+                prim = new Node((Node)primitive, true);
+                break;
+            case WAY:
+                prim = new Way((Way)primitive, true);
+                break;
+            case RELATION:
+                prim = new Relation((Relation)primitive, true);
+                break;
+            default:
+                throw new AssertionError();
+            }
+            replacedPrimitives.add(prim);
+            replacePrimitive(getLayer().data, primitive, prim);
         }
         return true;
+    }
+
+    private void replacePrimitive(DataSet dataSet, OsmPrimitive oldPrim, OsmPrimitive newPrim) {
+        dataSet.addPrimitive(newPrim);
+        for (OsmPrimitive referrer: oldPrim.getReferrers()) {
+            if (referrer instanceof Way) {
+                Way w = (Way)referrer;
+                List<Node> nodes = w.getNodes();
+                Collections.replaceAll(nodes, (Node)oldPrim, (Node)newPrim);
+                w.setNodes(nodes);
+                w.setModified(true);
+            } else if (referrer instanceof Relation) {
+                Relation r = (Relation)referrer;
+                List<RelationMember> members = r.getMembers();
+                ListIterator<RelationMember> it = members.listIterator();
+                while (it.hasNext()) {
+                    RelationMember rm = it.next();
+                    if (rm.getMember() == oldPrim) {
+                        it.set(new RelationMember(rm.getRole(), newPrim));
+                    }
+                }
+                r.setMembers(members);
+                r.setModified(true);
+            }
+        }
+        dataSet.removePrimitive(oldPrim);
+    }
+
+    @Override
+    public void undoCommand() {
+        for (int i=0; i<toUndelete.size(); i++) {
+            replacePrimitive(getLayer().data, replacedPrimitives.get(i), toUndelete.get(i));
+        }
+        super.undoCommand();
     }
 
     @Override
     public void fillModifiedData(Collection<OsmPrimitive> modified, Collection<OsmPrimitive> deleted,
             Collection<OsmPrimitive> added) {
-        modified.addAll(toUndelete);
     }
 }
