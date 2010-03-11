9d472b024f8a73d0f1f1d40047464512584cb37d
Gubaer <Gubaer@0c6e7542-c601-0410-84e7-c038aed88b3b>
2010-03-11 09:16:11 +0000

    applied #4715: patch by mjulius: Typos in ExceptionUtil
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@3109 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/ExceptionUtil.java
+++ src/org/openstreetmap/josm/tools/ExceptionUtil.java
@@ -333,9 +333,9 @@ public class ExceptionUtil {
      */
     public static String explainNestedIllegalDataException(OsmTransferException e) {
         IllegalDataException ide = getNestedException(e, IllegalDataException.class);
-        String message = tr("<html>Failed to download data."
-                + "Its format is either unsupported, ill-formed, and/or inconsistent.<br>"
-                + "<br>Details(untranslated): {0}</html>", ide.getMessage());
+        String message = tr("<html>Failed to download data. "
+                + "It's format is either unsupported, ill-formed, and/or inconsistent.<br>"
+                + "<br>Details (untranslated): {0}</html>", ide.getMessage());
         e.printStackTrace();
         return message;
     }
