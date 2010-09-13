58a2793ace7d14b61d89a91f0fc75901c95f9da2
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2010-09-13 19:40:18 +0000

    use java 6 features to open url in browser
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@3524 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/OpenBrowser.java
+++ src/org/openstreetmap/josm/tools/OpenBrowser.java
@@ -3,8 +3,10 @@ package org.openstreetmap.josm.tools;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.awt.Desktop;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 import javax.swing.JApplet;
@@ -25,25 +27,43 @@ public class OpenBrowser {
      * @throws IllegalStateException thrown if no platform is set to which opening the URL can be dispatched,
      * {@see Main#platform}
      */
-    public static String displayUrl(String url) throws IllegalStateException {
+    public static String displayUrl(URI uri) {
         if (Main.applet) {
             try {
                 JApplet applet = (JApplet) Main.parent;
-                applet.getAppletContext().showDocument(new URL(url));
+                applet.getAppletContext().showDocument(uri.toURL());
                 return null;
             } catch (MalformedURLException mue) {
                 return mue.getMessage();
             }
         }
 
-        if (Main.platform == null)
-            throw new IllegalStateException(tr("Failed to open URL. There is currently no platform set. Please set a platform first."));
-        try {
-            Main.platform.openUrl(url);
-        } catch (IOException e) {
-            return e.getMessage();
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(uri);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
+        } else {
+            System.err.println("Warning: Desktop class is not supported. Platform dependent fall back for open url in browser.");
+
+            if (Main.platform == null)
+                throw new IllegalStateException(tr("Failed to open URL. There is currently no platform set. Please set a platform first."));
+            try {
+                Main.platform.openUrl(uri.toString());
+            } catch (IOException e) {
+                return e.getMessage();
+            }
         }
         return null;
     }
 
+    public static String displayUrl(String url) {
+        try {
+            return displayUrl(new URI(url));
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 }
