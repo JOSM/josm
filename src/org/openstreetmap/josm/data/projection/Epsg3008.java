29801504a58e0a94de3a92b0ebd0f4c1d4a73752
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2010-12-03 13:45:14 +0000

    applied #5681 (patch by Hanno Hecker) - fix to SWEREF99 / EPSG:3008 projection
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@3692 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/projection/Epsg3008.java
+++ src/org/openstreetmap/josm/data/projection/Epsg3008.java
@@ -5,6 +5,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.coor.EastNorth;
 
 /**
  * SWEREF99 13 30 projection. Based on data from spatialreference.org.
@@ -15,10 +16,13 @@ import org.openstreetmap.josm.data.coor.LatLon;
 public class Epsg3008 extends TransverseMercator {
 
     private final static double UTMScaleFactor = 1.0;
+    private double UTMCentralMeridianRad;
+    private double offsetEastMeters = 150000;
+    private double offsetNorthMeters = 0;
 
     public Epsg3008()
     {
-        setProjectionParameters(13.5, 150000, 0);
+	UTMCentralMeridianRad = Math.toRadians(13.5);
     }
 
     @Override public String toString() {
@@ -49,4 +53,16 @@ public class Epsg3008 extends TransverseMercator {
                 new LatLon(55.2, 12.1),     // new LatLon(-90.0, -180.0),
                 new LatLon(62.26, 14.65));  // new LatLon(90.0, 180.0));
     }
+
+    @Override
+    public EastNorth latlon2eastNorth(LatLon p) {
+        EastNorth a = mapLatLonToXY(Math.toRadians(p.lat()), Math.toRadians(p.lon()), UTMCentralMeridianRad);
+        return new EastNorth(a.east() * UTMScaleFactor + offsetEastMeters, a.north() * UTMScaleFactor + offsetNorthMeters);
+    }
+
+    @Override
+    public LatLon eastNorth2latlon(EastNorth p) {
+        return mapXYToLatLon((p.east() - offsetEastMeters)/UTMScaleFactor, (p.north() - offsetNorthMeters)/UTMScaleFactor, UTMCentralMeridianRad);
+    }
+
 }
