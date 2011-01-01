f8c5c63cd57c6c5576275973570dae715999d2ec
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2011-01-01 13:30:39 +0000

    make dialog component accessible so progress dialog can have blocking modal dialog on top
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@3752 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/progress/PleaseWaitProgressMonitor.java
+++ src/org/openstreetmap/josm/gui/progress/PleaseWaitProgressMonitor.java
@@ -154,4 +154,8 @@ public class PleaseWaitProgressMonitor extends AbstractProgressMonitor {
         dialog.dispose();
         dialog = null;
     }
+    
+    public Component getDialog() {
+        return dialog;
+    }
 }
