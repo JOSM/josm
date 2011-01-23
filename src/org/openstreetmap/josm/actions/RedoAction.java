7f3f124c2bb509799b6f79e9cb13038169084c0b
jttt <jttt@0c6e7542-c601-0410-84e7-c038aed88b3b>
2011-01-23 16:45:01 +0000

    Fix #5862 on startup neither redo nor undo button are greyed out
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@3810 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/RedoAction.java
+++ src/org/openstreetmap/josm/actions/RedoAction.java
@@ -1,8 +1,8 @@
 // License: GPL. Copyright 2007 by Immanuel Scholz and others
 package org.openstreetmap.josm.actions;
 
-import static org.openstreetmap.josm.tools.I18n.tr;
 import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
@@ -36,6 +36,6 @@ public class RedoAction extends JosmAction {
 
     @Override
     protected void updateEnabledState() {
-        setEnabled(Main.map != null);
+        setEnabled(Main.main != null && !Main.main.undoRedo.redoCommands.isEmpty());
     }
 }
