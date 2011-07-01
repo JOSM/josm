968662e46a2111c4ef88dedaec05a7b64f5da6ca
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2011-07-01 09:29:30 +0000

    fix #6528 - toolbar conflict for imagery adjust
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@4192 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/ImageryAdjustAction.java
+++ src/org/openstreetmap/josm/actions/ImageryAdjustAction.java
@@ -46,6 +46,7 @@ public class ImageryAdjustAction extends MapMode implements MouseListener, Mouse
         super(tr("New offset"), "adjustimg",
                 tr("Adjust the position of this imagery layer"), Main.map,
                 cursor);
+        putValue("toolbar", false);
         this.layer = layer;
     }
 
