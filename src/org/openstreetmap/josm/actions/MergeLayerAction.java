031f5ee55b21cf516efeeb30b57032b03b1255af
jttt <jttt@0c6e7542-c601-0410-84e7-c038aed88b3b>
2011-07-27 05:16:41 +0000

    Make merge layers work when more than one layer is selected
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@4265 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/MergeLayerAction.java
+++ src/org/openstreetmap/josm/actions/MergeLayerAction.java
@@ -1,8 +1,8 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.actions;
 
-import static org.openstreetmap.josm.tools.I18n.tr;
 import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
@@ -19,10 +19,23 @@ public class MergeLayerAction extends AbstractMergeAction {
         super(tr("Merge layer"), "dialogs/mergedown", tr("Merge the current layer into another layer"), Shortcut
                 .registerShortcut("system:merge", tr("Edit: {0}", tr("Merge")), KeyEvent.VK_M, Shortcut.GROUP_MENU),
                 false /* register */
-        );
+                );
         putValue("help", ht("/Action/MergeLayer"));
         putValue("toolbar", "action/mergelayer");
         Main.toolbar.register(this);
+    }
+
+    public void merge(List<Layer> sourceLayers) {
+        Layer targetLayer = askTargetLayer(sourceLayers);
+        if (targetLayer == null)
+            return;
+        for (Layer l: sourceLayers) {
+            if (l != targetLayer) {
+                targetLayer.mergeFrom(l);
+                Main.map.mapView.removeLayer(l);
+            }
+        }
+        Main.map.mapView.setActiveLayer(targetLayer);
     }
 
     public void merge(Layer sourceLayer) {
