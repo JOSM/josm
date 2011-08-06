1b1f5cfadd2ce9c7cd06f560d237d832ba69b281
JOSM GitHub mirror <openstreetmap@v.nix.is>
2011-08-06 02:30:06 +0000

    josm-mirror: bumped externals

--- src/org/apache/commons/codec/language/bm/PhoneticEngine.java
+++ src/org/apache/commons/codec/language/bm/PhoneticEngine.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * <p>
@@ -49,6 +50,7 @@ import java.util.Set;
  * @since 2.0
  */
 public class PhoneticEngine {
+
     static class PhonemeBuilder {
 
         public static PhonemeBuilder empty(Languages.LanguageSet languages) {
@@ -61,7 +63,7 @@ public class PhoneticEngine {
             this.phonemes = phonemes;
         }
 
-        public PhonemeBuilder append(String str) {
+        public PhonemeBuilder append(CharSequence str) {
             Set<Rule.Phoneme> newPhonemes = new HashSet<Rule.Phoneme>();
 
             for (Rule.Phoneme ph : this.phonemes) {
@@ -91,19 +93,15 @@ public class PhoneticEngine {
         }
 
         public String makeString() {
-            List<String> sorted = new ArrayList<String>();
+
+            StringBuilder sb = new StringBuilder();
+            // System.err.println(this.phonemes.getClass());
 
             for (Rule.Phoneme ph : this.phonemes) {
-                sorted.add(ph.getPhonemeText());
-            }
-
-            Collections.sort(sorted);
-            StringBuilder sb = new StringBuilder();
-
-            for (String ph : sorted) {
-                if (sb.length() > 0)
+                if (sb.length() > 0) {
                     sb.append("|");
-                sb.append(ph);
+                }
+                sb.append(ph.getPhonemeText());
             }
 
             return sb.toString();
@@ -112,13 +110,13 @@ public class PhoneticEngine {
 
     private static class RulesApplication {
         private final List<Rule> finalRules;
-        private final String input;
+        private final CharSequence input;
 
         private PhonemeBuilder phonemeBuilder;
         private int i;
         private boolean found;
 
-        public RulesApplication(List<Rule> finalRules, String input, PhonemeBuilder phonemeBuilder, int i) {
+        public RulesApplication(List<Rule> finalRules, CharSequence input, PhonemeBuilder phonemeBuilder, int i) {
             if (finalRules == null) {
                 throw new NullPointerException("The finalRules argument must not be null");
             }
@@ -227,11 +225,11 @@ public class PhoneticEngine {
             return phonemeBuilder;
         }
 
-        Set<Rule.Phoneme> phonemes = new HashSet<Rule.Phoneme>();
+        Set<Rule.Phoneme> phonemes = new TreeSet<Rule.Phoneme>();
 
         for (Rule.Phoneme phoneme : phonemeBuilder.getPhonemes()) {
             PhonemeBuilder subBuilder = PhonemeBuilder.empty(phoneme.getLanguages());
-            String phonemeText = phoneme.getPhonemeText();
+            CharSequence phonemeText = phoneme.getPhonemeText();
             // System.err.println("Expanding: " + phonemeText);
 
             for (int i = 0; i < phonemeText.length();) {
@@ -241,7 +239,7 @@ public class PhoneticEngine {
 
                 if (!found) {
                     // System.err.println("Not found. Appending as-is");
-                    subBuilder = subBuilder.append(phonemeText.substring(i, i + 1));
+                    subBuilder = subBuilder.append(phonemeText.subSequence(i, i + 1));
                 }
 
                 i = rulesApplication.getI();
@@ -331,14 +329,13 @@ public class PhoneticEngine {
                     // check for any prefix in the words list
                     String remainder = input.substring(l.length() + 1); // input without the prefix
                     String combined = l + remainder; // input with prefix without space
-                    return encode(remainder) + "-" + encode(combined);
+                    return "(" + encode(remainder) + ")-(" + encode(combined) + ")";
                 }
                 // fixme: this case is invariant on l
-                else if (input.length() >= 2 && input.substring(0, 2).equals("d'")) // check for d'
-                {
+                else if (input.length() >= 2 && input.substring(0, 2).equals("d'")) { // check for d'
                     String remainder = input.substring(2);
                     String combined = "d" + remainder;
-                    return encode(remainder) + "-" + encode(combined);
+                    return "(" + encode(remainder) + ")-(" + encode(combined) + ")";
                 }
             }
         }
