5f56630d09eea4ff7c6b411f9f29f3d3411cad75
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2011-08-29 09:46:28 +0000

    fix last checkin (add file)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@4381 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/actions/CopyCoordinatesAction.java
@@ -0,0 +1,49 @@
+package org.openstreetmap.josm.actions;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.event.ActionEvent;
+import java.util.Collection;
+import java.util.Collections;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.tools.Utils;
+
+public class CopyCoordinatesAction extends JosmAction {
+
+    public CopyCoordinatesAction() {
+        super(tr("Copy Coordinates"), null,
+                tr("Copy coordinates of selected nodes to clipboard."), null, false);
+        putValue("toolbar", "copy/coordinates");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        StringBuilder s = new StringBuilder();
+        for (Node n : getSelectedNodes()) {
+            s.append(n.getCoor().lat());
+            s.append(", ");
+            s.append(n.getCoor().lon());
+            s.append("\n");
+        }
+        Utils.copyToClipboard(s.toString().trim());
+    }
+
+    @Override
+    protected void updateEnabledState() {
+        setEnabled(!getSelectedNodes().isEmpty());
+    }
+
+    @Override
+    protected void updateEnabledState(Collection<? extends OsmPrimitive> selection) {
+        updateEnabledState();
+    }
+
+    private Collection<Node> getSelectedNodes() {
+        if (getCurrentDataSet() == null || getCurrentDataSet().getSelected() == null) {
+            return Collections.emptyList();
+        } else {
+            return Utils.filteredCollection(getCurrentDataSet().getSelected(), Node.class);
+        }
+    }
+}
