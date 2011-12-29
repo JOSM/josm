59e6590b8baf1b0a3ec2c3100703d4d3dc959705
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2011-12-29 18:15:16 +0000

    fix #7034 - building inside building false positives
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@4747 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/validation/tests/BuildingInBuilding.java
+++ src/org/openstreetmap/josm/data/validation/tests/BuildingInBuilding.java
@@ -9,7 +9,10 @@ import java.util.List;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.QuadBuckets;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.data.validation.Severity;
 import org.openstreetmap.josm.data.validation.Test;
@@ -67,13 +70,8 @@ public class BuildingInBuilding extends Test {
         //  |       polygon         |
         //  |_______________________|
         //
-        for (int i=0; i<w.getNodesCount(); i++) {
-            LatLon center = null;
-            if (i > 0) {
-                center = w.getNode(i).getCoor().getCenter(w.getNode(i-1).getCoor());
-            } else if (w.isClosed()) {
-                center = w.getNode(i).getCoor().getCenter(w.getNode(w.getNodesCount()-2).getCoor());
-            }
+        for (int i=1; i<w.getNodesCount(); i++) {
+            LatLon center = w.getNode(i).getCoor().getCenter(w.getNode(i-1).getCoor());
             if (center != null && !isInPolygon(new Node(center), polygon)) {
                 return false;
             }
@@ -84,9 +82,7 @@ public class BuildingInBuilding extends Test {
     @Override
     public void endTest() {
         for (final OsmPrimitive p : primitivesToCheck) {
-            Collection<Way> outer = index.search(p.getBBox());
-            outer = new FilteredCollection<Way>(outer, new Predicate<Way>() {
-
+            Collection<Way> outers = new FilteredCollection<Way>(index.search(p.getBBox()), new Predicate<Way>() {
                 @Override
                 public boolean evaluate(Way object) {
                     if (p.equals(object)) {
@@ -94,17 +90,37 @@ public class BuildingInBuilding extends Test {
                     } else if (p instanceof Node) {
                         return isInPolygon((Node) p, object.getNodes()) || object.getNodes().contains(p);
                     } else if (p instanceof Way) {
-                        return isInPolygon((Way) p, object.getNodes());
+                        return isInPolygon((Way) p, object.getNodes()) && !isInInnerWay((Way)p, object);
                     } else {
                         return false;
                     }
                 }
             });
-            if (!outer.isEmpty()) {
+            if (!outers.isEmpty()) {
                 errors.add(new TestError(this, Severity.WARNING,
                         tr("Building inside building"), BUILDING_INSIDE_BUILDING, p));
             }
         }
+    }
+    
+    private boolean isInInnerWay(Way w, Way outer) {
+        for (OsmPrimitive r : outer.getReferrers()) {
+            if (r instanceof Relation && ((Relation)r).isMultipolygon()) {
+                for (RelationMember m : ((Relation)r).getMembers()) {
+                    if (m.hasRole() && m.getRole().equals("inner") && m.getType().equals(OsmPrimitiveType.WAY)) {
+                        // Only check inner ways actually inside the current outer
+                        Way inner = m.getWay();
+                        if (isInPolygon(inner, outer.getNodes())) {
+                            // If the tested way is inside this inner, outer is a false positive
+                            if (isInPolygon(w, inner.getNodes())) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     private static boolean isBuilding(OsmPrimitive p) {
