959508deed0503f0fdafb916247dd63c915a14ec
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2012-01-08 16:38:25 +0000

    applied #6883 - property toggle dialog: possibility to select and perform actions on several entries at once. (patch by joshdoe)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@4773 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/command/ChangePropertyCommand.java
+++ src/org/openstreetmap/josm/command/ChangePropertyCommand.java
@@ -4,12 +4,15 @@ package org.openstreetmap.josm.command;
 import static org.openstreetmap.josm.tools.I18n.marktr;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JLabel;
 
@@ -31,55 +34,104 @@ public class ChangePropertyCommand extends Command {
      */
     private final List<OsmPrimitive> objects;
     /**
-     * The key that is subject to change.
-     */
-    private final String key;
-    /**
-     * The key value. If it is <code>null</code>, delete all key references with the given
+     * Key and value pairs. If value is <code>null</code>, delete all key references with the given
      * key. Otherwise, change the properties of all objects to the given value or create keys of
      * those objects that do not have the key yet.
      */
-    private final String value;
+    private final AbstractMap<String, String> tags;
 
-    public ChangePropertyCommand(Collection<? extends OsmPrimitive> objects, String key, String value) {
+    /**
+     * Creates a command to change multiple properties of multiple objects
+     *
+     * @param objects the objects to modify
+     * @param tags the properties to set
+     */
+    public ChangePropertyCommand(Collection<? extends OsmPrimitive> objects, AbstractMap<String, String> tags) {
         super();
         this.objects = new LinkedList<OsmPrimitive>();
-        this.key = key;
-        this.value = (value == null || value.isEmpty()) ? null : value;
-        if (this.value == null) {
-            for (OsmPrimitive osm : objects) {
-                if(osm.get(key) != null) {
-                    this.objects.add(osm);
-                }
-            }
-        } else {
-            for (OsmPrimitive osm : objects) {
-                String val = osm.get(key);
-                if (val == null || !this.value.equals(val)) {
-                    this.objects.add(osm);
-                }
-            }
-        }
+        this.tags = tags;
+        init(objects);
     }
 
+    /**
+     * Creates a command to change one property of multiple objects
+     *
+     * @param objects the objects to modify
+     * @param key the key of the property to set
+     * @param value the value of the key to set
+     */
+    public ChangePropertyCommand(Collection<? extends OsmPrimitive> objects, String key, String value) {
+        this.objects = new LinkedList<OsmPrimitive>();
+        this.tags = new HashMap<String, String>(1);
+        this.tags.put(key, value);
+        init(objects);
+    }
+
+    /**
+     * Creates a command to change on property of one object
+     *
+     * @param object the object to modify
+     * @param key the key of the property to set
+     * @param value the value of the key to set
+     */
     public ChangePropertyCommand(OsmPrimitive object, String key, String value) {
         this(Arrays.asList(object), key, value);
+    }
+
+    /**
+     * Initialize the instance by finding what objects will be modified
+     *
+     * @param objects the objects to (possibly) modify
+     */
+    private void init(Collection<? extends OsmPrimitive> objects) {
+        // determine what objects will be modified
+        for (OsmPrimitive osm : objects) {
+            boolean modified = false;
+
+            // loop over all tags
+            for (Map.Entry<String, String> tag : this.tags.entrySet()) {
+                String oldVal = osm.get(tag.getKey());
+                String newVal = tag.getValue();
+
+                if (newVal == null || newVal.isEmpty()) {
+                    if (oldVal != null)
+                        // new value is null and tag exists (will delete tag)
+                        modified = true;
+                }
+                else if (oldVal == null || !newVal.equals(oldVal))
+                    // new value is not null and is different from current value
+                    modified = true;
+            }
+            if (modified)
+                this.objects.add(osm);
+        }
     }
 
     @Override public boolean executeCommand() {
         Main.main.getCurrentDataSet().beginUpdate();
         try {
             super.executeCommand(); // save old
-            if (value == null) {
-                for (OsmPrimitive osm : objects) {
-                    osm.setModified(true);
-                    osm.remove(key);
+
+            for (OsmPrimitive osm : objects) {
+                boolean modified = false;
+
+                 // loop over all tags
+                for (Map.Entry<String, String> tag : this.tags.entrySet()) {
+                    String oldVal = osm.get(tag.getKey());
+                    String newVal = tag.getValue();
+
+                    if (newVal == null || newVal.isEmpty()) {
+                        if (oldVal != null) {
+                            osm.remove(tag.getKey());
+                            modified = true;
+                        }
+                    }
+                    else if (oldVal == null || !newVal.equals(oldVal))
+                        osm.put(tag.getKey(), newVal);
+                        modified = true;
                 }
-            } else {
-                for (OsmPrimitive osm : objects) {
+                if (modified)
                     osm.setModified(true);
-                    osm.put(key, value);
-                }
             }
             return true;
         }
@@ -94,28 +146,45 @@ public class ChangePropertyCommand extends Command {
 
     @Override public JLabel getDescription() {
         String text;
-        if (objects.size() == 1) {
+        if (objects.size() == 1 && tags.size() == 1) {
             OsmPrimitive primitive = objects.iterator().next();
             String msg = "";
-            if (value == null) {
+            Map.Entry<String, String> entry = tags.entrySet().iterator().next();
+            if (entry.getValue() == null) {
                 switch(OsmPrimitiveType.from(primitive)) {
                 case NODE: msg = marktr("Remove \"{0}\" for node ''{1}''"); break;
                 case WAY: msg = marktr("Remove \"{0}\" for way ''{1}''"); break;
                 case RELATION: msg = marktr("Remove \"{0}\" for relation ''{1}''"); break;
                 }
-                text = tr(msg, key, primitive.getDisplayName(DefaultNameFormatter.getInstance()));
+                text = tr(msg, entry.getKey(), primitive.getDisplayName(DefaultNameFormatter.getInstance()));
             } else {
                 switch(OsmPrimitiveType.from(primitive)) {
                 case NODE: msg = marktr("Set {0}={1} for node ''{2}''"); break;
                 case WAY: msg = marktr("Set {0}={1} for way ''{2}''"); break;
                 case RELATION: msg = marktr("Set {0}={1} for relation ''{2}''"); break;
                 }
-                text = tr(msg, key, value, primitive.getDisplayName(DefaultNameFormatter.getInstance()));
+                text = tr(msg, entry.getKey(), entry.getValue(), primitive.getDisplayName(DefaultNameFormatter.getInstance()));
             }
-        } else {
-            text = value == null
-            ? tr("Remove \"{0}\" for {1} objects", key, objects.size())
-                    : tr("Set {0}={1} for {2} objects", key, value, objects.size());
+        } else if (objects.size() > 1 && tags.size() == 1) {
+            Map.Entry<String, String> entry = tags.entrySet().iterator().next();
+            if (entry.getValue() == null)
+                text = tr("Remove \"{0}\" for {1} objects", entry.getKey(), objects.size());
+            else
+                text = tr("Set {0}={1} for {2} objects", entry.getKey(), entry.getValue(), objects.size());
+        }
+        else {
+            boolean allnull = true;
+            for (Map.Entry<String, String> tag : this.tags.entrySet()) {
+                if (tag.getValue() != null) {
+                    allnull = false;
+                    break;
+                }
+            }
+            
+            if (allnull) {
+                text = tr("Deleted {0} properties for {1} objects", tags.size(), objects.size());
+            } else
+                text = tr("Set {0} properties for {1} objects", tags.size(), objects.size());
         }
         return new JLabel(text, ImageProvider.get("data", "key"), JLabel.HORIZONTAL);
     }
