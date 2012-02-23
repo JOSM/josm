e6b9fb780c07c95f8b1e685d08359b4048a3c6fc
akks <akks@0c6e7542-c601-0410-84e7-c038aed88b3b>
2012-02-23 17:32:58 +0000

    fix #7325 - copy URL to clipboard by right click (UrlLabel component)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5019 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/UrlLabel.java
+++ src/org/openstreetmap/josm/tools/UrlLabel.java
@@ -1,21 +1,25 @@
 // License: GPL. Copyright 2007 by Immanuel Scholz and others
 package org.openstreetmap.josm.tools;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JEditorPane;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 /**
  * Label that contains a clickable link.
  * @author Imi
  */
-public class UrlLabel extends JEditorPane implements HyperlinkListener {
+public class UrlLabel extends JEditorPane implements HyperlinkListener, MouseListener {
 
     private String url = "";
     private String description = "";
 
     public UrlLabel() {
         addHyperlinkListener(this);
+        addMouseListener(this);
         setEditable(false);
         setOpaque(false);
     }
@@ -38,7 +42,7 @@ public class UrlLabel extends JEditorPane implements HyperlinkListener {
         } else {
             setText("<html>" + description + "</html>");
         }
-        setToolTipText(url);
+        setToolTipText(String.format("<html>%s<br/>%s</html>",url, tr("Right click = copy to clipboard")));
     }
 
     public void hyperlinkUpdate(HyperlinkEvent e) {
@@ -68,4 +72,21 @@ public class UrlLabel extends JEditorPane implements HyperlinkListener {
         this.description = this.description.replace("&", "&amp;").replace(">", "&gt;").replace("<", "&lt;");
         refresh();
     }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+    @Override
+    public void mouseEntered(MouseEvent e) {    }
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            Utils.copyToClipboard(url);
+        }
+    }
+
 }
