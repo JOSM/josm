953298ecf88d8f1afce2a405b008ff2e65250a41
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2012-07-01 12:27:30 +0000

    fix #7812 - validator: power lines test improvements (no warning for start/end, node with power infrastructure tags, node inside power facilities)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5312 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/validation/tests/PowerLines.java
+++ src/org/openstreetmap/josm/data/validation/tests/PowerLines.java
@@ -3,50 +3,115 @@ package org.openstreetmap.josm.data.validation.tests;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.command.ChangePropertyCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.data.osm.visitor.paint.relations.Multipolygon;
+import org.openstreetmap.josm.data.osm.visitor.paint.relations.Multipolygon.JoinedWay;
+import org.openstreetmap.josm.data.osm.visitor.paint.relations.MultipolygonCache;
 import org.openstreetmap.josm.data.validation.Severity;
 import org.openstreetmap.josm.data.validation.Test;
 import org.openstreetmap.josm.data.validation.TestError;
+import org.openstreetmap.josm.tools.Geometry;
 
 public class PowerLines extends Test {
     
     protected static final int POWER_LINES = 2501;
     
-    protected Map<Way, String> towerPoleTagMap = new HashMap<Way, String>();
+    public static final Collection<String> POWER_LINE_TAGS = Arrays.asList("line", "minor_line");
+    public static final Collection<String> POWER_TOWER_TAGS = Arrays.asList("tower", "pole");
+    public static final Collection<String> POWER_STATION_TAGS = Arrays.asList("station", "sub_station", "plant", "generator");
+    public static final Collection<String> POWER_ALLOWED_TAGS = Arrays.asList("switch", "transformer", "busbar", "generator");
     
+    protected final Map<Way, String> towerPoleTagMap = new HashMap<Way, String>();
+    
+    protected final List<PowerLineError> potentialErrors = new ArrayList<PowerLineError>();
+
+    protected final List<OsmPrimitive> powerStations = new ArrayList<OsmPrimitive>();
+
     public PowerLines() {
         super(tr("Power lines"), tr("Checks for nodes in power lines that do not have a power=tower/pole tag."));
     }
     
     @Override
     public void visit(Way w) {
-        if (w.isUsable() && isPowerLine(w)) {
-            String fixValue = null;
-            boolean erroneous = false;
-            boolean canFix = false;
-            for (Node n : w.getNodes()) {
-                if (!isPowerTower(n)) {
-                    errors.add(new PowerLineError(n, w));
-                    erroneous = true;
-                } else if (fixValue == null) {
-                    // First tower/pole tag found, remember it
-                    fixValue = n.get("power");
-                    canFix = true;
-                } else if (!fixValue.equals(n.get("power"))) {
-                    // The power line contains both "tower" and "pole" -> cannot fix this error
-                    canFix = false;
+        if (w.isUsable()) {
+            if (isPowerLine(w)) {
+                String fixValue = null;
+                boolean erroneous = false;
+                boolean canFix = false;
+                for (Node n : w.getNodes()) {
+                    if (!isPowerTower(n)) {
+                        if (!w.isFirstLastNode(n) && !isPowerAllowed(n)) {
+                            potentialErrors.add(new PowerLineError(n, w));
+                            erroneous = true;
+                        }
+                    } else if (fixValue == null) {
+                        // First tower/pole tag found, remember it
+                        fixValue = n.get("power");
+                        canFix = true;
+                    } else if (!fixValue.equals(n.get("power"))) {
+                        // The power line contains both "tower" and "pole" -> cannot fix this error
+                        canFix = false;
+                    }
                 }
-            }
-            if (erroneous && canFix) {
-                towerPoleTagMap.put(w, fixValue);
+                if (erroneous && canFix) {
+                    towerPoleTagMap.put(w, fixValue);
+                }
+            } else if (w.isClosed() && isPowerStation(w)) {
+                powerStations.add(w);
             }
         }
+    }
+    
+    @Override
+    public void visit(Relation r) {
+        if (r.isMultipolygon() && isPowerStation(r)) {
+            powerStations.add(r);
+        }
+    }    
+
+    @Override
+    public void endTest() {
+        for (PowerLineError e : potentialErrors) {
+            if (!isInPowerStation(e.getNode())) {
+                errors.add(e);
+            }
+        }
+        super.endTest();
+    }
+    
+    protected final boolean isInPowerStation(Node n) {
+        for (OsmPrimitive station : powerStations) {
+            List<List<Node>> nodesLists = new ArrayList<List<Node>>();
+            if (station instanceof Way) {
+                nodesLists.add(((Way)station).getNodes());
+            } else if (station instanceof Relation) {
+                Multipolygon polygon = MultipolygonCache.getInstance().get(Main.map.mapView, (Relation) station);
+                if (polygon != null) {
+                    for (JoinedWay outer : Multipolygon.joinWays(polygon.getOuterWays())) {
+                        nodesLists.add(outer.getNodes());
+                    }
+                }
+            }
+            for (List<Node> nodes : nodesLists) {
+                if (Geometry.nodeInsidePolygon(n, nodes)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
@@ -70,8 +135,16 @@ public class PowerLines extends Test {
      * @return True if power key is set and equal to line/minor_line
      */
     protected static final boolean isPowerLine(Way w) {
-        String v = w.get("power");
-        return v != null && (v.equals("line") || v.equals("minor_line"));
+        return isPowerIn(w, POWER_LINE_TAGS);
+    }
+
+    /**
+     * Determines if the specified primitive denotes a power station.
+     * @param w The way to be tested
+     * @return True if power key is set and equal to station/sub_station/plant
+     */
+    protected static final boolean isPowerStation(OsmPrimitive p) {
+        return isPowerIn(p, POWER_STATION_TAGS);
     }
 
     /**
@@ -80,8 +153,21 @@ public class PowerLines extends Test {
      * @return True if power key is set and equal to tower/pole
      */
     protected static final boolean isPowerTower(Node n) {
-        String v = n.get("power");
-        return v != null && (v.equals("tower") || v.equals("pole"));
+        return isPowerIn(n, POWER_TOWER_TAGS);
+    }
+    
+    /**
+     * Determines if the specified node denotes a power infrastructure allowed on a power line.
+     * @param w The node to be tested
+     * @return True if power key is set and equal to switch/tranformer/busbar/generator
+     */
+    protected static final boolean isPowerAllowed(Node n) {
+        return isPowerIn(n, POWER_ALLOWED_TAGS);
+    }
+    
+    private static final boolean isPowerIn(OsmPrimitive p, Collection<String> values) {
+        String v = p.get("power");
+        return v != null && values != null && values.contains(v);
     }
     
     protected class PowerLineError extends TestError {
@@ -90,6 +176,9 @@ public class PowerLines extends Test {
             super(PowerLines.this, Severity.WARNING, 
                     tr("Missing power tower/pole within power line"), POWER_LINES, n);
             this.line = line;
+        }
+        public final Node getNode() {
+            return (Node) getPrimitives().iterator().next();
         }
     }
 }
