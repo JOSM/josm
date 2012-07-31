e4fc03e6ae385fc3ed0894ccb1852a6f102c0bf7
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2012-07-31 19:48:49 +0000

    see #7914 - Introduce OsmApi.MAX_DOWNLOAD_THREADS and improve javadoc in some classes of josm.io package
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5386 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/io/OsmTransferCanceledException.java
+++ src/org/openstreetmap/josm/io/OsmTransferCanceledException.java
@@ -1,6 +1,9 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.io;
 
+/**
+ * Exception thrown when a communication with the OSM server has been cancelled by the user.
+ */
 public class OsmTransferCanceledException extends OsmTransferException {
 
 }
--- src/org/openstreetmap/josm/io/OsmTransferException.java
+++ src/org/openstreetmap/josm/io/OsmTransferException.java
@@ -1,35 +1,70 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.io;
 
+/**
+ * Exception thrown when a communication error with the OSM server occurs.
+ */
 public class OsmTransferException extends Exception {
 
     private String url = OsmApi.getOsmApi().getBaseUrl();
 
+    /**
+     * Constructs an {@code OsmTransferException} with {@code null} as its error detail message.
+     * The cause is not initialized, and may subsequently be initialized by a call to {@link #initCause}.
+     */
     public OsmTransferException() {
     }
 
+    /**
+     * Constructs an {@code OsmTransferException} with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a call to {@link #initCause}.
+     *
+     * @param message The detail message (which is saved for later retrieval by the {@link #getMessage} method)
+     */
     public OsmTransferException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs an {@code OsmTransferException} with the specified cause and a detail message of 
+     * <tt>(cause==null ? null : cause.toString())</tt> 
+     * (which typically contains the class and detail message of <tt>cause</tt>).
+     *
+     * @param cause the cause (which is saved for later retrieval by the {@link #getCause} method). 
+     *              A <tt>null</tt> value is permitted, and indicates that the cause is nonexistent or unknown.
+     */
     public OsmTransferException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructs an {@code OsmTransferException} with the specified detail message and cause.
+     *
+     * <p> Note that the detail message associated with {@code cause} is <i>not</i> automatically incorporated 
+     * into this exception's detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval by the {@link #getMessage} method)
+     * @param cause   The cause (which is saved for later retrieval by the {@link #getCause} method).
+     *                A null value is permitted, and indicates that the cause is nonexistent or unknown.
+     *
+     */
     public OsmTransferException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Sets the URL related to this error.
+     * @param url the URL related to this error (which is saved for later retrieval by the {@link #getUrl} method).
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
     /**
-     *
-     * @return Api base url or url set using setUrl method
+     * Gets the URL related to this error.
+     * @return API base URL or URL set using the {@link #setUrl} method
      */
     public String getUrl() {
         return url;
     }
-
 }
