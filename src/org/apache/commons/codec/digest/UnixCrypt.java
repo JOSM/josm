7fddcb48549b3c039872bc2dd1d0cc41213535a9
JOSM GitHub mirror <openstreetmap@v.nix.is>
2012-08-31 13:30:12 +0000

    josm-mirror: bumped externals

--- src/org/apache/commons/codec/digest/UnixCrypt.java
+++ src/org/apache/commons/codec/digest/UnixCrypt.java
@@ -35,7 +35,7 @@ import org.apache.commons.codec.Charsets;
  * <p>
  * This class is immutable and thread-safe.
  *
- * @version $Id: UnixCrypt.java 1375781 2012-08-21 20:49:40Z tn $
+ * @version $Id: UnixCrypt.java 1379436 2012-08-31 13:01:25Z ggregory $
  * @since 1.7
  */
 public class UnixCrypt {
@@ -238,8 +238,9 @@ public class UnixCrypt {
         if (salt == null) {
             Random randomGenerator = new Random();
             int numSaltChars = SALT_CHARS.length;
-            salt = "" + SALT_CHARS[Math.abs(randomGenerator.nextInt()) % numSaltChars] +
-                   SALT_CHARS[Math.abs(randomGenerator.nextInt()) % numSaltChars];
+            // Types casts fix two FindBugs report of RV_ABSOLUTE_VALUE_OF_RANDOM_INT.
+            salt = "" + SALT_CHARS[(int)Math.abs((long)randomGenerator.nextInt()) % numSaltChars] +
+                   SALT_CHARS[(int)Math.abs((long)randomGenerator.nextInt()) % numSaltChars];
         } else if (!salt.matches("^[" + B64.B64T + "]{2,}$")) {
             throw new IllegalArgumentException("Invalid salt value: " + salt);
         }
