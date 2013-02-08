810618658f7b6f4b42d655eb9c890814791cad9b
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-02-08 21:37:27 +0000

    mapcss: separate opacity for way casing
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5702 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/mappaint/LineElemStyle.java
+++ src/org/openstreetmap/josm/gui/mappaint/LineElemStyle.java
@@ -183,7 +183,7 @@ public class LineElemStyle extends ElemStyle {
         }
 
         int alpha = 255;
-        Integer pAlpha = Utils.color_float2int(c.get(OPACITY, null, Float.class));
+        Integer pAlpha = Utils.color_float2int(c.get(type.prefix + OPACITY, null, Float.class));
         if (pAlpha != null) {
             alpha = pAlpha;
         }
