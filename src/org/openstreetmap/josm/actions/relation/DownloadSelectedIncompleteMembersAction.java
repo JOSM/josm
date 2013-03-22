4542e55212d4df02970d171bf2b994659da073b0
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-03-22 19:22:57 +0000

    see #7846 - fix copyright + javadoc + potential NPEs
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5794 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/relation/DownloadSelectedIncompleteMembersAction.java
+++ src/org/openstreetmap/josm/actions/relation/DownloadSelectedIncompleteMembersAction.java
@@ -1,13 +1,12 @@
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.actions.relation;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import static javax.swing.Action.NAME;
-import static javax.swing.Action.SHORT_DESCRIPTION;
-import static javax.swing.Action.SMALL_ICON;
-
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
@@ -15,19 +14,26 @@ import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.gui.dialogs.relation.DownloadRelationMemberTask;
 import org.openstreetmap.josm.tools.ImageProvider;
 
-import static org.openstreetmap.josm.tools.I18n.tr;
-
 /**
  * Action for downloading incomplete members of selected relations
+ * @since 5793
  */
 public class DownloadSelectedIncompleteMembersAction extends AbstractRelationAction {
 
+    /**
+     * Constructs a new <code>DownloadSelectedIncompleteMembersAction</code>.
+     */
     public DownloadSelectedIncompleteMembersAction() {
         putValue(SHORT_DESCRIPTION, tr("Download incomplete members of selected relations"));
         putValue(SMALL_ICON, ImageProvider.get("dialogs/relation", "downloadincompleteselected"));
         putValue(NAME, tr("Download incomplete members"));
     }
 
+    /**
+     * Returns the set of incomplete members of the given relations.
+     * @param rels The relations to inspect.
+     * @return The set of incomplete members of the given relations.
+     */
     public Set<OsmPrimitive> buildSetOfIncompleteMembers(Collection<Relation> rels) {
         Set<OsmPrimitive> ret = new HashSet<OsmPrimitive>();
         for (Relation r : rels) {
@@ -37,11 +43,10 @@ public class DownloadSelectedIncompleteMembersAction extends AbstractRelationAct
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (!isEnabled() || relations.isEmpty()) return;
+        if (!isEnabled() || relations.isEmpty() || Main.map==null || Main.map.mapView==null) return;
         Main.worker.submit(new DownloadRelationMemberTask(
                 relations,
                 buildSetOfIncompleteMembers(relations),
                 Main.map.mapView.getEditLayer()));
     }
-
 }
