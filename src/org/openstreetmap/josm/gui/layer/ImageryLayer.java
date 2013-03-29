e64785a8cf2b1af168a186b577fcc0a33c77d2c5
akks <akks@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-03-29 08:31:36 +0000

    fix #8549, patch by Zverikk: Remove [deprecated] imagery offset server support
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5808 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/layer/ImageryLayer.java
+++ src/org/openstreetmap/josm/gui/layer/ImageryLayer.java
@@ -25,20 +25,16 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JSeparator;
-import javax.swing.SwingUtilities;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.ImageryAdjustAction;
 import org.openstreetmap.josm.data.ProjectionBounds;
-import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.data.imagery.ImageryInfo;
 import org.openstreetmap.josm.data.imagery.ImageryInfo.ImageryType;
 import org.openstreetmap.josm.data.imagery.OffsetBookmark;
 import org.openstreetmap.josm.data.preferences.ColorProperty;
 import org.openstreetmap.josm.data.preferences.IntegerProperty;
 import org.openstreetmap.josm.gui.MenuScroller;
-import org.openstreetmap.josm.io.imagery.OffsetServer;
-import org.openstreetmap.josm.io.imagery.OsmosnimkiOffsetServer;
 import org.openstreetmap.josm.tools.ImageProvider;
 
 public abstract class ImageryLayer extends Layer {
@@ -65,22 +61,7 @@ public abstract class ImageryLayer extends Layer {
 
     protected int sharpenLevel;
 
-    protected boolean offsetServerSupported;
-    protected boolean offsetServerUsed;
-    protected OffsetServerThread offsetServerThread;
-
     private final ImageryAdjustAction adjustAction = new ImageryAdjustAction(this);
-    private final AbstractAction useServerOffsetAction = new AbstractAction(tr("(use server offset)")) {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            enableOffsetServer(true);
-        }
-    };
-
-    protected OffsetServerThread createoffsetServerThread() {
-        return new OffsetServerThread(new OsmosnimkiOffsetServer(
-                OsmosnimkiOffsetServer.PROP_SERVER_URL.get()));
-    }
 
     public ImageryLayer(ImageryInfo info) {
         super(info.getName());
@@ -93,10 +74,6 @@ public abstract class ImageryLayer extends Layer {
             icon = ImageProvider.get("imagery_small");
         }
         this.sharpenLevel = PROP_SHARPEN_LEVEL.get();
-        if (OffsetServer.PROP_SERVER_ENABLED.get()) {
-            offsetServerThread = createoffsetServerThread();
-            offsetServerThread.start();
-        }
     }
 
     public double getPPD(){
@@ -163,7 +140,6 @@ public abstract class ImageryLayer extends Layer {
         @Override
         public void actionPerformed(ActionEvent ev) {
             setOffset(b.dx, b.dy);
-            enableOffsetServer(false);
             Main.main.menu.imageryMenu.refreshOffsetMenu();
             Main.map.repaint();
         }
@@ -185,14 +161,6 @@ public abstract class ImageryLayer extends Layer {
         }
     }
 
-    public void enableOffsetServer(boolean enable) {
-        offsetServerUsed = enable;
-        if (offsetServerUsed && !offsetServerThread.isAlive()) {
-            offsetServerThread = createoffsetServerThread();
-            offsetServerThread.start();
-        }
-    }
-
     public JMenuItem getOffsetMenuItem() {
         JMenu subMenu = new JMenu(trc("layer", "Offset"));
         subMenu.setIcon(ImageProvider.get("mapmode", "adjustimg"));
@@ -201,16 +169,9 @@ public abstract class ImageryLayer extends Layer {
 
     public JComponent getOffsetMenuItem(JComponent subMenu) {
         JMenuItem adjustMenuItem = new JMenuItem(adjustAction);
-        if (OffsetBookmark.allBookmarks.isEmpty() && !offsetServerSupported) return adjustMenuItem;
+        if (OffsetBookmark.allBookmarks.isEmpty()) return adjustMenuItem;
 
         subMenu.add(adjustMenuItem);
-        if (offsetServerSupported) {
-            JCheckBoxMenuItem item = new JCheckBoxMenuItem(useServerOffsetAction);
-            if (offsetServerUsed) {
-                item.setSelected(true);
-            }
-            subMenu.add(item);
-        }
         subMenu.add(new JSeparator());
         boolean hasBookmarks = false;
         int menuItemHeight = 0;
@@ -219,7 +180,7 @@ public abstract class ImageryLayer extends Layer {
                 continue;
             }
             JCheckBoxMenuItem item = new JCheckBoxMenuItem(new ApplyOffsetAction(b));
-            if (b.dx == dx && b.dy == dy && !offsetServerUsed) {
+            if (b.dx == dx && b.dy == dy) {
                 item.setSelected(true);
             }
             subMenu.add(item);
@@ -234,7 +195,7 @@ public abstract class ImageryLayer extends Layer {
                 MenuScroller.setScrollerFor((JPopupMenu)subMenu, scrollcount);
             }
         }
-        return (hasBookmarks || offsetServerSupported) ? subMenu : adjustMenuItem;
+        return hasBookmarks ? subMenu : adjustMenuItem;
     }
 
     public BufferedImage sharpenImage(BufferedImage img) {
@@ -262,51 +223,6 @@ public abstract class ImageryLayer extends Layer {
 
         String text = tr("ERROR");
         g.drawString(text, (img.getWidth() + g.getFontMetrics().stringWidth(text)) / 2, img.getHeight()/2);
-    }
-
-    protected class OffsetServerThread extends Thread {
-        OffsetServer offsetServer;
-        EastNorth oldCenter = new EastNorth(Double.NaN, Double.NaN);
-
-        public OffsetServerThread(OffsetServer offsetServer) {
-            this.offsetServer = offsetServer;
-            setDaemon(true);
-        }
-
-        private void updateOffset() {
-            if (Main.map == null || Main.map.mapView == null) return;
-            EastNorth center = Main.map.mapView.getCenter();
-            if (center.equals(oldCenter)) return;
-            oldCenter = center;
-
-            EastNorth offset = offsetServer.getOffset(getInfo(), center);
-            if (offset != null) {
-                setOffset(offset.east(),offset.north());
-            }
-        }
-
-        @Override
-        public void run() {
-            if (!offsetServerSupported) {
-                if (!offsetServer.isLayerSupported(info)) return;
-                offsetServerSupported = true;
-            }
-            offsetServerUsed = true;
-            SwingUtilities.invokeLater(new Runnable() {
-                @Override
-                public void run() {
-                    Main.main.menu.imageryMenu.refreshOffsetMenu();
-                }
-            });
-            try {
-                while (offsetServerUsed) {
-                    updateOffset();
-                    Thread.sleep(1000);
-                }
-            } catch (InterruptedException e) {
-            }
-            offsetServerUsed = false;
-        }
     }
 
     /* (non-Javadoc)
