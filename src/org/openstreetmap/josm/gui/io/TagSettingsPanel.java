70da356b8c2268f7889d388fa22ef92d0cf4448b
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-04-14 21:32:18 +0000

    fix user agent handling a bit (applet code now sets user agent
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5865 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/io/TagSettingsPanel.java
+++ src/org/openstreetmap/josm/gui/io/TagSettingsPanel.java
@@ -12,6 +12,7 @@ import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.Version;
 import org.openstreetmap.josm.data.osm.Changeset;
 import org.openstreetmap.josm.gui.tagging.TagEditorPanel;
 import org.openstreetmap.josm.gui.tagging.TagModel;
@@ -43,16 +44,6 @@ public class TagSettingsPanel extends JPanel implements TableModelListener {
         this.changesetCommentModel.addObserver(new ChangesetCommentObserver());
         build();
         pnlTagEditor.getModel().addTableModelListener(this);
-    }
-
-    /**
-     * Replies the default value for "created_by"
-     *
-     * @return the default value for "created_by"
-     */
-    public static String getDefaultCreatedBy() {
-        Object ua = System.getProperties().get("http.agent");
-        return(ua == null) ? "JOSM" : ua.toString();
     }
 
     protected void setUploadComment(String comment) {
@@ -92,11 +83,12 @@ public class TagSettingsPanel extends JPanel implements TableModelListener {
         if (tags.get("comment") == null) {
             tags.put("comment", currentComment);
         }
+        String agent = Version.getInstance().getAgentString();
         String created_by = tags.get("created_by");
-        if (created_by == null || "".equals(created_by)) {
-            tags.put("created_by", getDefaultCreatedBy());
-        } else if (!created_by.contains(getDefaultCreatedBy())) {
-            tags.put("created_by", created_by + ";" + getDefaultCreatedBy());
+        if (created_by == null || created_by.isEmpty()) {
+            tags.put("created_by", agent);
+        } else if (!created_by.contains(agent)) {
+            tags.put("created_by", created_by + ";" + agent);
         }
         pnlTagEditor.getModel().initFromTags(tags);
     }
