486a14e55e76c0923134f68f1523dea58cc31629
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-04-28 23:20:06 +0000

    rename namespace for consistency
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5910 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/gpx/GpxConstants.java
+++ src/org/openstreetmap/josm/data/gpx/GpxConstants.java
@@ -23,7 +23,7 @@ public interface GpxConstants {
     public static final String META_TIME = META_PREFIX + "time";
     public static final String META_EXTENSIONS = META_PREFIX + "extensions";
 
-    public static final String JOSM_EXTENSIONS_NAMESPACE_URI = "http://josm.openstreetmap.de/gpx-extensions";
+    public static final String JOSM_EXTENSIONS_NAMESPACE_URI = "http://josm.openstreetmap.de/gpx-extensions-1.0";
 
     public static List<String> WPT_KEYS = Arrays.asList("ele", "time", "magvar", "geoidheight",
             "name", "cmt", "desc", "src", META_LINKS, "sym", "number", "type",
