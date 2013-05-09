JOSM/josm

commit 44c404684a8d26e4aac4942d73a87ba914ed55d6

see #4429 - Right click menu "undo, cut, copy, paste, delete, select all" for each text component (originally based on patch by NooN)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@5886 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/search/SearchAction.java
@@ -29,7 +29,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JTextField;
 import javax.swing.text.BadLocationException;
 
 import org.openstreetmap.josm.Main;
@@ -50,6 +49,8 @@ import org.openstreetmap.josm.tools.Predicate;
 import org.openstreetmap.josm.tools.Property;
 import org.openstreetmap.josm.tools.Shortcut;
 import org.openstreetmap.josm.tools.Utils;
+import org.openstreetmap.josm.gui.widgets.JosmTextField;
+
 
 public class SearchAction extends JosmAction implements ParameterizedAction {
 
@@ -194,7 +195,7 @@ public class SearchAction extends JosmAction implements ParameterizedAction {
                     @Override
                     public void mouseClicked(MouseEvent e) {
                         try {
-                            JTextField tf = (JTextField) hcb.getEditor().getEditorComponent();
+                            JosmTextField tf = (JosmTextField) hcb.getEditor().getEditorComponent();
                             tf.getDocument().insertString(tf.getCaretPosition(), " " + insertText, null);
                         } catch (BadLocationException ex) {
                             throw new RuntimeException(ex.getMessage(), ex);

commit 0825ef2b69baf6cf6d7483fc270c63ea92a5adaf

fix #8693 - CCE in Search Dialog caused by r5886
git-svn-id: http://josm.openstreetmap.de/svn/trunk@5948 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/search/SearchAction.java
@@ -30,12 +30,13 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.ActionParameter;
+import org.openstreetmap.josm.actions.ActionParameter.SearchSettingsActionParameter;
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.actions.ParameterizedAction;
-import org.openstreetmap.josm.actions.ActionParameter.SearchSettingsActionParameter;
 import org.openstreetmap.josm.actions.search.SearchCompiler.ParseError;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Filter;
@@ -49,7 +50,6 @@ import org.openstreetmap.josm.tools.Predicate;
 import org.openstreetmap.josm.tools.Property;
 import org.openstreetmap.josm.tools.Shortcut;
 import org.openstreetmap.josm.tools.Utils;
-import org.openstreetmap.josm.gui.widgets.JosmTextField;
 
 
 public class SearchAction extends JosmAction implements ParameterizedAction {
@@ -193,7 +193,7 @@ public class SearchAction extends JosmAction implements ParameterizedAction {
                     @Override
                     public void mouseClicked(MouseEvent e) {
                         try {
-                            JosmTextField tf = (JosmTextField) hcb.getEditor().getEditorComponent();
+                            JTextComponent tf = (JTextComponent) hcb.getEditor().getEditorComponent();
                             tf.getDocument().insertString(tf.getCaretPosition(), " " + insertText, null);
                         } catch (BadLocationException ex) {
                             throw new RuntimeException(ex.getMessage(), ex);