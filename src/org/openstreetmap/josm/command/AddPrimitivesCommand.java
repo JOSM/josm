b242ca8a575a3673b35d9eafb1a5104905b5ca7f
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-05-10 15:13:24 +0000

    fix #8690 - After copying some primitives, only select those that have been directly selected (and not all that have been created) -> Copying a way only selects the new way
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5953 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/command/AddPrimitivesCommand.java
+++ src/org/openstreetmap/josm/command/AddPrimitivesCommand.java
@@ -15,32 +15,63 @@ import org.openstreetmap.josm.data.osm.NodeData;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.PrimitiveData;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.tools.CheckParameterUtil;
 
 /**
  * Add primitives to a data layer.
- *
+ * @since 2305
  */
 public class AddPrimitivesCommand extends Command {
 
     private List<PrimitiveData> data = new ArrayList<PrimitiveData>();
+    private Collection<PrimitiveData> toSelect = new ArrayList<PrimitiveData>();
 
     // only filled on undo
     private List<OsmPrimitive> createdPrimitives = null;
+    private Collection<OsmPrimitive> createdPrimitivesToSelect = null;
 
+    /**
+     * Constructs a new {@code AddPrimitivesCommand} to add data to the current edit layer.
+     * @param data The OSM primitives data to add. Must not be {@code null}
+     */
     public AddPrimitivesCommand(List<PrimitiveData> data) {
-        this.data.addAll(data);
+        this(data, data);
+    }
+
+    /**
+     * Constructs a new {@code AddPrimitivesCommand} to add data to the current edit layer.
+     * @param data The OSM primitives to add. Must not be {@code null}
+     * @param toSelect The OSM primitives to select at the end. Can be {@code null}
+     * @since 5953
+     */
+    public AddPrimitivesCommand(List<PrimitiveData> data, List<PrimitiveData> toSelect) {
+        init(data, toSelect);
+    }
+
+    /**
+     * Constructs a new {@code AddPrimitivesCommand} to add data to the given layer.
+     * @param data The OSM primitives data to add. Must not be {@code null}
+     * @param toSelect The OSM primitives to select at the end. Can be {@code null}
+     * @param layer The target data layer. Must not be {@code null}
+     */
+    public AddPrimitivesCommand(List<PrimitiveData> data, List<PrimitiveData> toSelect, OsmDataLayer layer) {
+        super(layer);
+        init(data, toSelect);
     }
     
-    public AddPrimitivesCommand(List<PrimitiveData> data, OsmDataLayer layer) {
-        super(layer);
+    private final void init(List<PrimitiveData> data, List<PrimitiveData> toSelect) {
+        CheckParameterUtil.ensureParameterNotNull(data, "data");
         this.data.addAll(data);
+        if (toSelect != null) {
+            this.toSelect.addAll(toSelect);
+        }
     }
 
-    @SuppressWarnings("null")
     @Override public boolean executeCommand() {
-        List<OsmPrimitive> newPrimitives;
+        Collection<OsmPrimitive> primitivesToSelect;
         if (createdPrimitives == null) { // first time execution
-            newPrimitives = new ArrayList<OsmPrimitive>(data.size());
+            List<OsmPrimitive> newPrimitives = new ArrayList<OsmPrimitive>(data.size());
+            primitivesToSelect = new ArrayList<OsmPrimitive>(toSelect.size());
 
             for (PrimitiveData pd : data) {
                 OsmPrimitive primitive = getLayer().data.getPrimitiveById(pd);
@@ -55,9 +86,12 @@ public class AddPrimitivesCommand extends Command {
                     getLayer().data.addPrimitive(primitive);
                 }
                 newPrimitives.add(primitive);
+                if (toSelect.contains(pd)) {
+                    primitivesToSelect.add(primitive);
+                }
             }
 
-            //Then load ways and relations
+            // Then load ways and relations
             for (int i=0; i<newPrimitives.size(); i++) {
                 if (!(newPrimitives.get(i) instanceof Node)) {
                     newPrimitives.get(i).load(data.get(i));
@@ -69,10 +103,10 @@ public class AddPrimitivesCommand extends Command {
             for (OsmPrimitive osm : createdPrimitives) {
                 getLayer().data.addPrimitive(osm);
             }
-            newPrimitives = createdPrimitives;
+            primitivesToSelect = createdPrimitivesToSelect;
         }
 
-        getLayer().data.setSelected(newPrimitives);
+        getLayer().data.setSelected(primitivesToSelect);
         return true;
     }
 
@@ -81,9 +115,14 @@ public class AddPrimitivesCommand extends Command {
         
         if (createdPrimitives == null) {
             createdPrimitives = new ArrayList<OsmPrimitive>(data.size());
+            createdPrimitivesToSelect = new ArrayList<OsmPrimitive>(toSelect.size());
             
-            for (PrimitiveData p : data) {
-                createdPrimitives.add(ds.getPrimitiveById(p));
+            for (PrimitiveData pd : data) {
+                OsmPrimitive p = ds.getPrimitiveById(pd);
+                createdPrimitives.add(p);
+                if (toSelect.contains(pd)) {
+                    createdPrimitivesToSelect.add(p);
+                }
             }
             createdPrimitives = PurgeCommand.topoSort(createdPrimitives);
             
@@ -91,6 +130,7 @@ public class AddPrimitivesCommand extends Command {
                 ds.removePrimitive(p);
             }
             data = null;
+            toSelect = null;
             
         } else {
             for (OsmPrimitive osm : createdPrimitives) {
