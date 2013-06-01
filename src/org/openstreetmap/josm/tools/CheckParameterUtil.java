0b8dbbda27375e337a8c0310323487a6412a3490
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-06-01 18:39:28 +0000

    see #8631 - Sanity checks in geometry functions dealing with coordinates + javadoc
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@5980 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/CheckParameterUtil.java
+++ src/org/openstreetmap/josm/tools/CheckParameterUtil.java
@@ -3,38 +3,87 @@ package org.openstreetmap.josm.tools;
 
 import java.text.MessageFormat;
 
+import org.openstreetmap.josm.data.coor.EastNorth;
+import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.PrimitiveId;
 
 /**
  * This utility class provides a collection of static helper methods for checking
  * parameters at run-time.
- *
+ * @ince 2711
  */
 public class CheckParameterUtil {
 
     private CheckParameterUtil(){}
 
+    /**
+     * Ensures an OSM primitive ID is valid
+     * @param id The id to check
+     * @param parameterName The parameter name
+     * @throws IllegalArgumentException if the primitive ID is not valid (negative or zero)
+     */
     public static void ensureValidPrimitiveId(PrimitiveId id, String parameterName) throws IllegalArgumentException {
         ensureParameterNotNull(id, parameterName);
         if (id.getUniqueId() <= 0)
             throw new IllegalArgumentException(MessageFormat.format("Expected unique id > 0 for primitive ''{1}'', got {0}", id.getUniqueId(), parameterName));
     }
 
+    /**
+     * Ensures lat/lon coordinates are valid
+     * @param latlon The lat/lon to check
+     * @param parameterName The parameter name
+     * @throws IllegalArgumentException if the lat/lon are {@code null} or not valid
+     * @since 5980
+     */
+    public static void ensureValidCoordinates(LatLon latlon, String parameterName) throws IllegalArgumentException {
+        ensureParameterNotNull(latlon, parameterName);
+        if (!latlon.isValid())
+            throw new IllegalArgumentException(MessageFormat.format("Expected valid lat/lon for parameter ''{0}'', got {1}", parameterName, latlon));
+    }
+
+    /**
+     * Ensures east/north coordinates are valid
+     * @param eastnorth The east/north to check
+     * @param parameterName The parameter name
+     * @throws IllegalArgumentException if the east/north are {@code null} or not valid
+     * @since 5980
+     */
+    public static void ensureValidCoordinates(EastNorth eastnorth, String parameterName) throws IllegalArgumentException {
+        ensureParameterNotNull(eastnorth, parameterName);
+        if (!eastnorth.isValid())
+            throw new IllegalArgumentException(MessageFormat.format("Expected valid east/north for parameter ''{0}'', got {1}", parameterName, eastnorth));
+    }
+
+    /**
+     * Ensures a version number is valid
+     * @param version The version to check
+     * @param parameterName The parameter name
+     * @throws IllegalArgumentException if the version is not valid (negative)
+     */
     public static void ensureValidVersion(long version, String parameterName) throws IllegalArgumentException {
         if (version < 0)
             throw new IllegalArgumentException(MessageFormat.format("Expected value of type long > 0 for parameter ''{0}'', got {1}", parameterName, version));
     }
 
-    public static void ensureParameterNotNull(Object value, String parameterName) {
+    /**
+     * Ensures a parameter is not {@code null}
+     * @param value The parameter to check
+     * @param parameterName The parameter name
+     * @throws IllegalArgumentException if the parameter is {@code null}
+     */
+    public static void ensureParameterNotNull(Object value, String parameterName) throws IllegalArgumentException {
         if (value == null)
             throw new IllegalArgumentException(MessageFormat.format("Parameter ''{0}'' must not be null", parameterName));
     }
 
     /**
-     * can find line number in the stack trace, so parameter name is optional
+     * Ensures a parameter is not {@code null}. Can find line number in the stack trace, so parameter name is optional
+     * @param value The parameter to check
+     * @throws IllegalArgumentException if the parameter is {@code null}
+     * @since 3871
      */
-    public static void ensureParameterNotNull(Object value) {
+    public static void ensureParameterNotNull(Object value) throws IllegalArgumentException {
         if (value == null)
             throw new IllegalArgumentException("Parameter must not be null");
     }
