a75d8d5da5202915bd5b64b38a9f0444b5b6863b
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-08-09 14:08:30 +0000

    see #7275 - allow script to handle restart
    When JOSM was started with parameter -Djosm.restart=true, a
    user request for restart will simply terminate the application
    with exit code 9.
    The start script is then responsible for the restart.
    
    
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6125 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/PlatformHookOsx.java
+++ src/org/openstreetmap/josm/tools/PlatformHookOsx.java
@@ -50,7 +50,7 @@ public class PlatformHookOsx extends PlatformHookUnixoid implements PlatformHook
         Boolean handled = Boolean.TRUE;
         //System.out.println("Going to handle method "+method+" (short: "+method.getName()+") with event "+args[0]);
         if (method.getName().equals("handleQuit")) {
-            handled = Main.exitJosm(false);
+            handled = Main.exitJosm(false, 0);
         } else if (method.getName().equals("handleAbout")) {
             Main.main.menu.about.actionPerformed(null);
         } else if (method.getName().equals("handlePreferences")) {
