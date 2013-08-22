2803a86f383a4b234c998ce3da38016a7958e717
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-08-22 15:17:43 +0000

    fix #9000 - add `entrance` to presets + deprecate `building=entrance` in favor of it. Icons and map style already ok.
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6176 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/validation/tests/DeprecatedTags.java
+++ src/org/openstreetmap/josm/data/validation/tests/DeprecatedTags.java
@@ -118,6 +118,11 @@ public class DeprecatedTags extends Test {
                 test("fence_type", "chain").
                 alternative("barrier", "chain").
                 alternative("fence_type", "chain_link"));
+        // http://wiki.openstreetmap.org/wiki/Key:fence_type
+        // see #8962
+        checks.add(new DeprecationCheck(2123).
+                test("building", "entrance").
+                alternative("entrance"));
     }
 
     /**
