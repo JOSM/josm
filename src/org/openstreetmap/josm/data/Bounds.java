JOSM/josm

commit 2be64278757ec30cc400fd1f93ef52953e37529e

fix #9024 - bbox/bounds memory optimizations (modified patch by shinigami) + javadoc
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6203 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/Bounds.java
@@ -8,6 +8,7 @@ import java.text.DecimalFormat;
 import java.text.MessageFormat;
 
 import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.osm.BBox;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 
 /**
@@ -26,8 +27,48 @@ public class Bounds {
         return new LatLon(minLat, minLon);
     }
 
+    /**
+     * Returns min latitude of bounds. Efficient shortcut for {@code getMin().lat()}.
+     * 
+     * @return min latitude of bounds.
+     * @since 6203
+     */
+    public double getMinLat() {
+        return minLat;
+    }
+
+    /**
+     * Returns min longitude of bounds. Efficient shortcut for {@code getMin().lon()}.
+     * 
+     * @return min longitude of bounds.
+     * @since 6203
+     */
+    public double getMinLon() {
+        return minLon;
+    }
+
     public LatLon getMax() {
         return new LatLon(maxLat, maxLon);
+    }
+
+    /**
+     * Returns max latitude of bounds. Efficient shortcut for {@code getMax().lat()}.
+     * 
+     * @return max latitude of bounds.
+     * @since 6203
+     */
+    public double getMaxLat() {
+        return maxLat;
+    }
+
+    /**
+     * Returns max longitude of bounds. Efficient shortcut for {@code getMax().lon()}.
+     * 
+     * @return max longitude of bounds.
+     * @since 6203
+     */
+    public double getMaxLon() {
+        return maxLon;
     }
 
     public enum ParseMethod {
@@ -36,7 +77,7 @@ public class Bounds {
     }
 
     /**
-     * Construct bounds out of two points
+     * Construct bounds out of two points. Coords will be rounded.
      */
     public Bounds(LatLon min, LatLon max) {
         this(min.lat(), min.lon(), max.lat(), max.lon());
@@ -50,14 +91,34 @@ public class Bounds {
         this(b, true);
     }
 
+    /**
+     * Single point Bounds defined by lat/lon {@code b}.
+     * Coordinates will be rounded to osm precision if {@code roundToOsmPrecision} is true.
+     * 
+     * @param b lat/lon of given point.
+     * @param roundToOsmPrecision defines if lat/lon will be rounded.
+     */
     public Bounds(LatLon b, boolean roundToOsmPrecision) {
+        this(b.lat(), b.lon(), roundToOsmPrecision);
+    }
+    
+    /**
+     * Single point Bounds defined by point [lat,lon].
+     * Coordinates will be rounded to osm precision if {@code roundToOsmPrecision} is true.
+     * 
+     * @param lat latitude of given point.
+     * @param lon longitude of given point.
+     * @param roundToOsmPrecision defines if lat/lon will be rounded.
+     * @since 6203
+     */
+    public Bounds(double lat, double lon, boolean roundToOsmPrecision) {
         // Do not call this(b, b) to avoid GPX performance issue (see #7028) until roundToOsmPrecision() is improved
         if (roundToOsmPrecision) {
-            this.minLat = LatLon.roundToOsmPrecision(b.lat());
-            this.minLon = LatLon.roundToOsmPrecision(b.lon());
+            this.minLat = LatLon.roundToOsmPrecision(lat);
+            this.minLon = LatLon.roundToOsmPrecision(lon);
         } else {
-            this.minLat = b.lat();
-            this.minLon = b.lon();
+            this.minLat = lat;
+            this.minLon = lon;
         }
         this.maxLat = this.minLat;
         this.maxLon = this.minLon;
@@ -152,8 +213,12 @@ public class Bounds {
         return roundToOsmPrecision ? LatLon.roundToOsmPrecision(value) : value;
     }
 
-    public Bounds(Bounds other) {
-        this(other.getMin(), other.getMax());
+    /**
+     * Creates new {@code Bounds} from an existing one.
+     * @param other The bounds to copy
+     */
+    public Bounds(final Bounds other) {
+        this(other.minLat, other.minLon, other.maxLat, other.maxLon);
     }
 
     public Bounds(Rectangle2D rect) {
@@ -185,6 +250,16 @@ public class Bounds {
         this.maxLon = LatLon.roundToOsmPrecision(LatLon.toIntervalLon(center.lon() + lonExtent / 2));
     }
 
+    /**
+     * Creates BBox with same coordinates.
+     * 
+     * @return BBox with same coordinates.
+     * @since 6203
+     */
+    public BBox toBBox() {
+        return new BBox(minLon, minLat, maxLon, maxLat);
+    }
+    
     @Override public String toString() {
         return "Bounds["+minLat+","+minLon+","+maxLat+","+maxLon+"]";
     }
@@ -202,52 +277,67 @@ public class Bounds {
      */
     public LatLon getCenter()
     {
-        if (crosses180thMeridian()) {
-            LatLon result = new LatLon(minLat, minLon-360.0).getCenter(getMax());
-            if (result.lon() < -180.0) {
-                result = new LatLon(result.lat(), result.lon() + 360.0);
+        if (crosses180thMeridian()) {            
+            double lat = (minLat + maxLat) / 2;
+            double lon = (minLon + maxLon - 360.0) / 2;
+            if (lon < -180.0){
+                lon += 360.0;
             }
-            return result;
+            return new LatLon(lat, lon);
         } else {
-            return getMin().getCenter(getMax());
+            return new LatLon((minLat + maxLat) / 2, (minLon + maxLon) / 2);
         }
     }
 
     /**
      * Extend the bounds if necessary to include the given point.
+     * @param ll The point to include into these bounds
      */
     public void extend(LatLon ll) {
-        if (ll.lat() < minLat) {
-            minLat = LatLon.roundToOsmPrecision(ll.lat());
+        extend(ll.lat(), ll.lon());
+    }
+    
+    /**
+     * Extend the bounds if necessary to include the given point [lat,lon].
+     * Good to use if you know coordinates to avoid creation of LatLon object.
+     * @param lat Latitude of point to include into these bounds
+     * @param lon Longitude of point to include into these bounds
+     * @since 6203
+     */
+    public void extend(final double lat, final double lon) {
+        if (lat < minLat) {
+            minLat = LatLon.roundToOsmPrecision(lat);
         }
-        if (ll.lat() > maxLat) {
-            maxLat = LatLon.roundToOsmPrecision(ll.lat());
+        if (lat > maxLat) {
+            maxLat = LatLon.roundToOsmPrecision(lat);
         }
         if (crosses180thMeridian()) {
-            if (ll.lon() > maxLon && ll.lon() < minLon) {
-                if (Math.abs(ll.lon() - minLon) <= Math.abs(ll.lon() - maxLon)) {
-                    minLon = LatLon.roundToOsmPrecision(ll.lon());
+            if (lon > maxLon && lon < minLon) {
+                if (Math.abs(lon - minLon) <= Math.abs(lon - maxLon)) {
+                    minLon = LatLon.roundToOsmPrecision(lon);
                 } else {
-                    maxLon = LatLon.roundToOsmPrecision(ll.lon());
+                    maxLon = LatLon.roundToOsmPrecision(lon);
                 }
             }
         } else {
-            if (ll.lon() < minLon) {
-                minLon = LatLon.roundToOsmPrecision(ll.lon());
+            if (lon < minLon) {
+                minLon = LatLon.roundToOsmPrecision(lon);
             }
-            if (ll.lon() > maxLon) {
-                maxLon = LatLon.roundToOsmPrecision(ll.lon());
+            if (lon > maxLon) {
+                maxLon = LatLon.roundToOsmPrecision(lon);
             }
         }
     }
 
     public void extend(Bounds b) {
-        extend(b.getMin());
-        extend(b.getMax());
+        extend(b.minLat, b.minLon);
+        extend(b.maxLat, b.maxLon);
     }
 
     /**
-     * Is the given point within this bounds?
+     * Determines if the given point {@code ll} is within these bounds.
+     * @param ll The lat/lon to check
+     * @return {@code true} if {@code ll} is within these bounds, {@code false} otherwise
      */
     public boolean contains(LatLon ll) {
         if (ll.lat() < minLat || ll.lat() > maxLat)
@@ -323,7 +413,7 @@ public class Bounds {
      * @return true, if this bounds are <em>collapsed</em>
      */
     public boolean isCollapsed() {
-        return getMin().equals(getMax());
+        return (minLat == maxLat) && (minLon == maxLon);
     }
 
     public boolean isOutOfTheWorld() {

src/org/openstreetmap/josm/data/osm/BBox.java
@@ -3,7 +3,6 @@ package org.openstreetmap.josm.data.osm;
 
 import java.util.Arrays;
 
-import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.coor.QuadTiling;
 import org.openstreetmap.josm.tools.Utils;
@@ -15,16 +14,35 @@ public class BBox {
     private double ymin = Double.POSITIVE_INFINITY;
     private double ymax = Double.NEGATIVE_INFINITY;
 
-    public BBox(Bounds bounds) {
-        add(bounds.getMin());
-        add(bounds.getMax());
+    /**
+     * Constructs a new {@code BBox} defined by a single point.
+     * 
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @since 6203
+     */
+    public BBox(final double x, final double y) {
+        xmax = xmin = x;
+        ymax = ymin = y;
+        sanity();
     }
 
+    /**
+     * Constructs a new {@code BBox} defined by points <code>a</code> and <code>b</code>.
+     * Result is minimal BBox containing both points.
+     * 
+     * @param a 
+     * @param b
+     */
     public BBox(LatLon a, LatLon b) {
-        add(a);
-        add(b);
+        this(a.lon(), a.lat(), b.lon(), b.lat());
     }
 
+    /**
+     * Constructs a new {@code BBox} from another one.
+     * 
+     * @param copy the BBox to copy
+     */
     public BBox(BBox copy) {
         this.xmin = copy.xmin;
         this.xmax = copy.xmax;
@@ -33,10 +51,23 @@ public class BBox {
     }
 
     public BBox(double a_x, double a_y, double b_x, double b_y)  {
-        xmin = Math.min(a_x, b_x);
-        xmax = Math.max(a_x, b_x);
-        ymin = Math.min(a_y, b_y);
-        ymax = Math.max(a_y, b_y);
+        
+        if (a_x > b_x) {
+            xmax = a_x;
+            xmin = b_x;
+        } else {
+            xmax = b_x;
+            xmin = a_x;
+        }
+        
+        if (a_y > b_y) {
+            ymax = a_y;
+            ymin = b_y;
+        } else {
+            ymax = b_y;
+            ymin = a_y;
+        }
+        
         sanity();
     }
 
@@ -83,16 +114,28 @@ public class BBox {
      * Extends this bbox to include the point (x, y)
      */
     public void add(double x, double y) {
-        xmin = Math.min(xmin, x);
-        xmax = Math.max(xmax, x);
-        ymin = Math.min(ymin, y);
-        ymax = Math.max(ymax, y);
+        
+        if (x < xmin) {
+            xmin = x;
+        } else if (x > xmax) {
+            xmax = x;
+        }
+        
+        if (y < ymin) {
+            ymin = y;
+        } else if (y > ymax) {
+            ymax = y;
+        }
+
         sanity();
     }
 
     public void add(BBox box) {
-        add(box.getTopLeft());
-        add(box.getBottomRight());
+        xmin = Math.min(xmin, box.xmin);
+        xmax = Math.max(xmax, box.xmax);
+        ymin = Math.min(ymin, box.ymin);
+        ymax = Math.max(ymax, box.ymax);
+        sanity();
     }
 
     public void addPrimitive(OsmPrimitive primitive, double extraSpace) {
@@ -150,12 +193,56 @@ public class BBox {
         return true;
     }
 
+    /**
+     * Returns the top-left point.
+     * @return The top-left point
+     */
     public LatLon getTopLeft() {
         return new LatLon(ymax, xmin);
     }
 
+    /**
+     * Returns the latitude of top-left point.
+     * @return The latitude of top-left point
+     * @since 6203
+     */
+    public double getTopLeftLat() {
+        return ymax;
+    }
+
+    /**
+     * Returns the longitude of top-left point.
+     * @return The longitude of top-left point
+     * @since 6203
+     */
+    public double getTopLeftLon() {
+        return xmin;
+    }
+
+    /**
+     * Returns the bottom-right point.
+     * @return The bottom-right point
+     */
     public LatLon getBottomRight() {
         return new LatLon(ymin, xmax);
+    }
+
+    /**
+     * Returns the latitude of bottom-right point.
+     * @return The latitude of bottom-right point
+     * @since 6203
+     */
+    public double getBottomRightLat() {
+        return ymin;
+    }
+
+    /**
+     * Returns the longitude of bottom-right point.
+     * @return The longitude of bottom-right point
+     * @since 6203
+     */
+    public double getBottomRightLon() {
+        return xmax;
     }
 
     public LatLon getCenter() {

src/org/openstreetmap/josm/actions/JumpToAction.java
@@ -126,8 +126,8 @@ public class JumpToAction extends JosmAction {
         if(!url.hasFocus()) return;
         Bounds b = OsmUrlToBounds.parse(url.getText());
         if (b != null) {
-            lat.setText(Double.toString((b.getMin().lat() + b.getMax().lat())/2));
-            lon.setText(Double.toString((b.getMin().lon() + b.getMax().lon())/2));
+            lat.setText(Double.toString((b.getMinLat() + b.getMaxLat())/2));
+            lon.setText(Double.toString((b.getMinLon() + b.getMaxLon())/2));
 
             int zoomLvl = 16;
             String[] args = url.getText().substring(url.getText().indexOf('?')+1).split("&");

src/org/openstreetmap/josm/data/coor/Coordinate.java
@@ -3,6 +3,8 @@ package org.openstreetmap.josm.data.coor;
 
 import java.io.Serializable;
 
+import org.openstreetmap.josm.data.osm.BBox;
+
 /**
  * Base class of points of both coordinate systems.
  *
@@ -86,6 +88,28 @@ abstract class Coordinate implements Serializable {
         final double dx = this.x-px;
         final double dy = this.y-py;
         return dx*dx + dy*dy;
+    }
+
+    /**
+     * Converts to single point BBox.
+     * 
+     * @return single point BBox defined by this coordinate.
+     * @since 6203
+     */
+    public BBox toBBox() {
+        return new BBox(x, y);
+    }
+    
+    /**
+     * Creates bbox around this coordinate. Coordinate defines
+     * center of bbox, its edge will be 2*r.
+     * 
+     * @param r size
+     * @return BBox around this coordinate
+     * @since 6203
+     */
+    public BBox toBBox(final double r) {
+        return new BBox(x - r, y - r, x + r, y + r);
     }
 
     @Override

src/org/openstreetmap/josm/data/coor/LatLon.java
@@ -215,8 +215,8 @@ public class LatLon extends Coordinate {
      */
     public boolean isOutSideWorld() {
         Bounds b = Main.getProjection().getWorldBoundsLatLon();
-        return lat() < b.getMin().lat() || lat() > b.getMax().lat() ||
-                lon() < b.getMin().lon() || lon() > b.getMax().lon();
+        return lat() < b.getMinLat() || lat() > b.getMaxLat() ||
+                lon() < b.getMinLon() || lon() > b.getMaxLon();
     }
 
     /**

src/org/openstreetmap/josm/data/gpx/WayPoint.java
@@ -50,8 +50,8 @@ public class WayPoint extends WithAttributes implements Comparable<WayPoint>, Te
      * We "inline" lat/lon, rather than usinga LatLon internally => reduces memory overhead. Relevant
      * because a lot of GPX waypoints are created when GPS tracks are downloaded from the OSM server.
      */
-    private double lat = 0;
-    private double lon = 0;
+    private final double lat;
+    private final double lon;
 
     /*
      * internal cache of projected coordinates

src/org/openstreetmap/josm/data/osm/DatasetConsistencyTest.java
@@ -80,7 +80,7 @@ public class DatasetConsistencyTest {
             if (!n.isIncomplete() && !n.isDeleted()) {
                 LatLon c = n.getCoor();
                 if (c != null) {
-                    BBox box = new BBox(new LatLon(c.lat() - 0.0001, c.lon() - 0.0001), new LatLon(c.lat() + 0.0001, c.lon() + 0.0001));
+                    BBox box = c.toBBox(0.0001);
                     if (!dataSet.searchNodes(box).contains(n)) {
                         printError("SEARCH NODES", "%s not found using Dataset.searchNodes()", n);
                     }

src/org/openstreetmap/josm/data/osm/QuadBuckets.java
@@ -116,8 +116,7 @@ public class QuadBuckets<T extends OsmPrimitive> implements Collection<T> {
             LatLon bottom_left = this.coor();
             double lat = bottom_left.lat() + parent.height() / 2;
             double lon = bottom_left.lon() + parent.width() / 2;
-            LatLon top_right = new LatLon(lat, lon);
-            return new BBox(bottom_left, top_right);
+            return new BBox(bottom_left.lon(), bottom_left.lat(), lon, lat);
         }
 
         QBLevel<T> findBucket(BBox bbox) {

commit 0ba53a69b301cb13f23d39aa094bea69b0dfdc0d

fix #9024 - fix problems introduced in r6203
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6204 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/Bounds.java
@@ -413,7 +413,8 @@ public class Bounds {
      * @return true, if this bounds are <em>collapsed</em>
      */
     public boolean isCollapsed() {
-        return (minLat == maxLat) && (minLon == maxLon);
+        return Double.doubleToLongBits(minLat) == Double.doubleToLongBits(maxLat) 
+            && Double.doubleToLongBits(minLon) == Double.doubleToLongBits(maxLon);
     }
 
     public boolean isOutOfTheWorld() {

src/org/openstreetmap/josm/data/osm/BBox.java
@@ -114,19 +114,10 @@ public class BBox {
      * Extends this bbox to include the point (x, y)
      */
     public void add(double x, double y) {
-        
-        if (x < xmin) {
-            xmin = x;
-        } else if (x > xmax) {
-            xmax = x;
-        }
-        
-        if (y < ymin) {
-            ymin = y;
-        } else if (y > ymax) {
-            ymax = y;
-        }
-
+        xmin = Math.min(xmin, x);
+        xmax = Math.max(xmax, x);
+        ymin = Math.min(ymin, y);
+        ymax = Math.max(ymax, y);
         sanity();
     }
 