f73984dc374911b86178dc9b9450c20908628389
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-09-07 13:50:39 +0000

    refactor Toggle actions, use Action.SELECTED_KEY since we have switched to Java 6 long ago, javadoc
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6220 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/DialogsToggleAction.java
+++ src/org/openstreetmap/josm/actions/DialogsToggleAction.java
@@ -6,73 +6,55 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.swing.ButtonModel;
 import javax.swing.Icon;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.tools.Shortcut;
 
-/*
-* Action that hides or shows toggle dialogs panel and other panels
-* to free more space for drawing (GIMP-like)
+/**
+* This action toggles visibility of dialogs panel and other panels to free more space for drawing (GIMP-like)
 * @author cmuelle8
+* @since 5965
 */
-public class DialogsToggleAction extends JosmAction {
-    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
-    private boolean selected;
+public class DialogsToggleAction extends ToggleAction {
 
+    /**
+     * Constructs a new {@code DialogsToggleAction}.
+     */
     public DialogsToggleAction() {
-        super(
-                tr("Toggle dialogs panel"),
-                (Icon) null, /* no icon */
-                tr("Toggle dialogs panel, maximize mapview"),
-                Shortcut.registerShortcut("menu:view:dialogspanel", tr("Toggle dialogs panel"),KeyEvent.VK_TAB, Shortcut.DIRECT),
-                true, "dialogspanel", /* register in toolbar */
-                false
+        super(tr("Toggle dialogs panel"),
+              (Icon) null, /* no icon */
+              tr("Toggle dialogs panel, maximize mapview"),
+              Shortcut.registerShortcut("menu:view:dialogspanel", tr("Toggle dialogs panel"),KeyEvent.VK_TAB, Shortcut.DIRECT),
+              true, "dialogspanel", /* register in toolbar */
+              false
         );
         putValue("help", ht("/Action/ToggleDialogsPanel"));
-        selected = Main.pref.getBoolean("draw.dialogspanel", true);
+        setSelected(Main.pref.getBoolean("draw.dialogspanel", true));
         notifySelectedState();
     }
 
-    public void addButtonModel(ButtonModel model) {
-        if (model != null && !buttonModels.contains(model)) {
-            buttonModels.add(model);
-        }
-    }
-
-    public void removeButtonModel(ButtonModel model) {
-        if (model != null && buttonModels.contains(model)) {
-            buttonModels.remove(model);
-        }
-    }
-
-    protected void notifySelectedState() {
-        for (ButtonModel model: buttonModels) {
-            if (model.isSelected() != selected) {
-                model.setSelected(selected);
-            }
-        }
-    }
-
-    protected void toggleSelectedState() {
-        selected = !selected;
-        Main.pref.put("draw.dialogspanel", selected);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        toggleSelectedState();
+        Main.pref.put("draw.dialogspanel", isSelected());
         notifySelectedState();
         setMode();
     }
 
+    /**
+     * To call if this action must be initially run at JOSM startup.
+     */
     public void initial() {
-        if(selected) {
+        if (isSelected()) {
             setMode();
         }
     }
 
     protected void setMode() {
         if (Main.isDisplayingMapView()) {
+            boolean selected = isSelected();
             Main.map.setDialogsPanelVisible(selected);
             Main.map.statusLine.setVisible(selected || Main.pref.getBoolean("statusbar.always-visible", true));
             Main.toolbar.control.setVisible(selected || Main.pref.getBoolean("toolbar.always-visible", true));
@@ -83,10 +65,5 @@ public class DialogsToggleAction extends JosmAction {
             }
             Main.map.mapView.rememberLastPositionOnScreen();
         }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
     }
 }
--- src/org/openstreetmap/josm/actions/WireframeToggleAction.java
+++ src/org/openstreetmap/josm/actions/WireframeToggleAction.java
@@ -5,10 +5,6 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.ButtonModel;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.osm.visitor.paint.MapRendererFactory;
@@ -16,64 +12,26 @@ import org.openstreetmap.josm.data.osm.visitor.paint.StyledMapRenderer;
 import org.openstreetmap.josm.data.osm.visitor.paint.WireframeMapRenderer;
 import org.openstreetmap.josm.tools.Shortcut;
 
-public class WireframeToggleAction extends JosmAction {
-    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
-    //FIXME: replace with property Action.SELECTED_KEY when migrating to
-    // Java 6
-    private boolean selected;
+/**
+ * This class toggles the wireframe rendering mode.
+ * @since 2530
+ */
+public class WireframeToggleAction extends ToggleAction {
+
+    /**
+     * Constructs a new {@code WireframeToggleAction}.
+     */
     public WireframeToggleAction() {
-        super(
-                tr("Wireframe View"),
-                null, /* no icon */
-                tr("Enable/disable rendering the map as wireframe only"),
-                Shortcut.registerShortcut("menu:view:wireframe", tr("Toggle Wireframe view"),KeyEvent.VK_W, Shortcut.CTRL),
-                false /* register toolbar */
+        super(tr("Wireframe View"),
+              null, /* no icon */
+              tr("Enable/disable rendering the map as wireframe only"),
+              Shortcut.registerShortcut("menu:view:wireframe", tr("Toggle Wireframe view"),KeyEvent.VK_W, Shortcut.CTRL),
+              false /* register toolbar */
         );
         putValue("toolbar", "wireframe");
         Main.toolbar.register(this);
-        selected = MapRendererFactory.getInstance().isWireframeMapRendererActive();
+        setSelected(MapRendererFactory.getInstance().isWireframeMapRendererActive());
         notifySelectedState();
-    }
-
-    public void addButtonModel(ButtonModel model) {
-        if (model != null && !buttonModels.contains(model)) {
-            buttonModels.add(model);
-            model.setSelected(selected);
-        }
-    }
-
-    public void removeButtonModel(ButtonModel model) {
-        if (model != null && buttonModels.contains(model)) {
-            buttonModels.remove(model);
-        }
-    }
-
-    protected void notifySelectedState() {
-        for (ButtonModel model: buttonModels) {
-            if (model.isSelected() != selected) {
-                model.setSelected(selected);
-            }
-        }
-    }
-
-    protected void toggleSelectedState() {
-        selected = !selected;
-        //Main.pref.put("draw.wireframe", selected);
-         if (selected){
-         MapRendererFactory.getInstance().activate(WireframeMapRenderer.class);
-            } else {
-                MapRendererFactory.getInstance().activate(StyledMapRenderer.class);
-            }
-
-        notifySelectedState();
-        if (Main.isDisplayingMapView()) {
-            Main.map.mapView.repaint();
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
     }
 
     @Override
@@ -81,7 +39,18 @@ public class WireframeToggleAction extends JosmAction {
         setEnabled(Main.isDisplayingMapView() && Main.main.getEditLayer() != null);
     }
 
-    public boolean isSelected() {
-        return selected;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        toggleSelectedState();
+        if (isSelected()) {
+            MapRendererFactory.getInstance().activate(WireframeMapRenderer.class);
+        } else {
+            MapRendererFactory.getInstance().activate(StyledMapRenderer.class);
+        }
+
+        notifySelectedState();
+        if (Main.isDisplayingMapView()) {
+            Main.map.mapView.repaint();
+        }
     }
 }
