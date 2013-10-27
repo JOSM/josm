JOSM/josm

commit 963e3893f9f7d2bcadf55ed73a0d78897f0cd8fc

refactor Toggle actions, use Action.SELECTED_KEY since we have switched to Java 6 long ago, javadoc
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6220 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/ChangesetManagerToggleAction.java
@@ -9,63 +9,39 @@ import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.ButtonModel;
 
 import org.openstreetmap.josm.gui.dialogs.changeset.ChangesetCacheManager;
 import org.openstreetmap.josm.tools.Shortcut;
+
 /**
  * This action toggles the visibility of the {@link ChangesetCacheManager} dialog.
- *
+ * @since 2685
  */
-public class ChangesetManagerToggleAction extends JosmAction {
-    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
-    //FIXME: replace with property Action.SELECTED_KEY when migrating to
-    // Java 6
-    private boolean selected;
+public class ChangesetManagerToggleAction extends ToggleAction {
     private WindowListener changesetCacheManagerClosedHandler;
 
+    /**
+     * Constructs a new {@code ChangesetManagerToggleAction}.
+     */
     public ChangesetManagerToggleAction() {
-        super(
-                tr("Changeset Manager"),
-                "dialogs/changeset/changesetmanager",
-                tr("Toggle visibility of Changeset Manager window"),
-                Shortcut.registerShortcut("menu:windows:changesetdialog",
-                        tr("Toggle visibility of Changeset Manager window"),
-                        KeyEvent.VK_C, Shortcut.ALT_CTRL),
-                true /* register shortcut */
+        super(tr("Changeset Manager"),
+              "dialogs/changeset/changesetmanager",
+              tr("Toggle visibility of Changeset Manager window"),
+              Shortcut.registerShortcut("menu:windows:changesetdialog",
+                      tr("Toggle visibility of Changeset Manager window"),
+                      KeyEvent.VK_C, Shortcut.ALT_CTRL),
+              true /* register shortcut */
         );
         notifySelectedState();
         changesetCacheManagerClosedHandler = new ChangesetCacheManagerClosedHandler();
         putValue("help", ht("/Dialog/ChangesetManager"));
     }
 
-    public void addButtonModel(ButtonModel model) {
-        if (model != null && !buttonModels.contains(model)) {
-            buttonModels.add(model);
-        }
-    }
-
-    public void removeButtonModel(ButtonModel model) {
-        if (model != null && buttonModels.contains(model)) {
-            buttonModels.remove(model);
-        }
-    }
-
-    protected void notifySelectedState() {
-        for (ButtonModel model: buttonModels) {
-            if (model.isSelected() != selected) {
-                model.setSelected(selected);
-            }
-        }
-    }
-
-    protected void toggleSelectedState() {
-        selected = !selected;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        toggleSelectedState();
         notifySelectedState();
-        if (selected) {
+        if (isSelected()) {
             ChangesetCacheManager.getInstance().addWindowListener(changesetCacheManagerClosedHandler);
             ChangesetCacheManager.getInstance().setVisible(true);
         } else {
@@ -74,15 +50,10 @@ public class ChangesetManagerToggleAction extends JosmAction {
         }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
-    }
-
     private class ChangesetCacheManagerClosedHandler extends WindowAdapter {
         @Override
         public void windowClosed(WindowEvent e) {
-            selected = false;
+            setSelected(false);
             notifySelectedState();
             ChangesetCacheManager.getInstance().removeWindowListener(changesetCacheManagerClosedHandler);
         }

src/org/openstreetmap/josm/actions/DialogsToggleAction.java
@@ -6,73 +6,55 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.swing.ButtonModel;
 import javax.swing.Icon;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.tools.Shortcut;
 
-/*
-* Action that hides or shows toggle dialogs panel and other panels
-* to free more space for drawing (GIMP-like)
+/**
+* This action toggles visibility of dialogs panel and other panels to free more space for drawing (GIMP-like)
 * @author cmuelle8
+* @since 5965
 */
-public class DialogsToggleAction extends JosmAction {
-    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
-    private boolean selected;
+public class DialogsToggleAction extends ToggleAction {
 
+    /**
+     * Constructs a new {@code DialogsToggleAction}.
+     */
     public DialogsToggleAction() {
-        super(
-                tr("Toggle dialogs panel"),
-                (Icon) null, /* no icon */
-                tr("Toggle dialogs panel, maximize mapview"),
-                Shortcut.registerShortcut("menu:view:dialogspanel", tr("Toggle dialogs panel"),KeyEvent.VK_TAB, Shortcut.DIRECT),
-                true, "dialogspanel", /* register in toolbar */
-                false
+        super(tr("Toggle dialogs panel"),
+              (Icon) null, /* no icon */
+              tr("Toggle dialogs panel, maximize mapview"),
+              Shortcut.registerShortcut("menu:view:dialogspanel", tr("Toggle dialogs panel"),KeyEvent.VK_TAB, Shortcut.DIRECT),
+              true, "dialogspanel", /* register in toolbar */
+              false
         );
         putValue("help", ht("/Action/ToggleDialogsPanel"));
-        selected = Main.pref.getBoolean("draw.dialogspanel", true);
+        setSelected(Main.pref.getBoolean("draw.dialogspanel", true));
         notifySelectedState();
     }
 
-    public void addButtonModel(ButtonModel model) {
-        if (model != null && !buttonModels.contains(model)) {
-            buttonModels.add(model);
-        }
-    }
-
-    public void removeButtonModel(ButtonModel model) {
-        if (model != null && buttonModels.contains(model)) {
-            buttonModels.remove(model);
-        }
-    }
-
-    protected void notifySelectedState() {
-        for (ButtonModel model: buttonModels) {
-            if (model.isSelected() != selected) {
-                model.setSelected(selected);
-            }
-        }
-    }
-
-    protected void toggleSelectedState() {
-        selected = !selected;
-        Main.pref.put("draw.dialogspanel", selected);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        toggleSelectedState();
+        Main.pref.put("draw.dialogspanel", isSelected());
         notifySelectedState();
         setMode();
     }
 
+    /**
+     * To call if this action must be initially run at JOSM startup.
+     */
     public void initial() {
-        if(selected) {
+        if (isSelected()) {
             setMode();
         }
     }
 
     protected void setMode() {
         if (Main.isDisplayingMapView()) {
+            boolean selected = isSelected();
             Main.map.setDialogsPanelVisible(selected);
             Main.map.statusLine.setVisible(selected || Main.pref.getBoolean("statusbar.always-visible", true));
             Main.toolbar.control.setVisible(selected || Main.pref.getBoolean("toolbar.always-visible", true));
@@ -83,10 +65,5 @@ public class DialogsToggleAction extends JosmAction {
             }
             Main.map.mapView.rememberLastPositionOnScreen();
         }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
     }
 }

src/org/openstreetmap/josm/actions/ExpertToggleAction.java
@@ -10,14 +10,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.swing.ButtonModel;
-
 import org.openstreetmap.josm.Main;
 
-public class ExpertToggleAction extends JosmAction {
-
-    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
-    private boolean selected;
+/**
+ * This action toggles the Expert mode.
+ * @since 4840
+ */
+public class ExpertToggleAction extends ToggleAction {
 
     public interface ExpertModeChangeListener {
         void expertChanged(boolean isExpert);
@@ -117,61 +116,46 @@ public class ExpertToggleAction extends JosmAction {
         }
     }
 
+    /**
+     * Constructs a new {@code ExpertToggleAction}.
+     */
     public ExpertToggleAction() {
-        super(
-                tr("Expert Mode"),
-                "expert",
-                tr("Enable/disable expert mode"),
-                null,
-                false /* register toolbar */
+        super(tr("Expert Mode"),
+              "expert",
+              tr("Enable/disable expert mode"),
+              null,
+              false /* register toolbar */
         );
         putValue("toolbar", "expertmode");
         Main.toolbar.register(this);
-        selected = Main.pref.getBoolean("expert", false);
+        setSelected(Main.pref.getBoolean("expert", false));
         notifySelectedState();
-    }
-
-    public void addButtonModel(ButtonModel model) {
-        if (model != null && !buttonModels.contains(model)) {
-            buttonModels.add(model);
-            model.setSelected(selected);
-        }
-    }
-
-    public void removeButtonModel(ButtonModel model) {
-        if (model != null && buttonModels.contains(model)) {
-            buttonModels.remove(model);
-        }
     }
 
     protected void notifySelectedState() {
-        for (ButtonModel model: buttonModels) {
-            if (model.isSelected() != selected) {
-                model.setSelected(selected);
-            }
-        }
-        fireExpertModeChanged(selected);
-    }
-
-    protected void toggleSelectedState() {
-        selected = !selected;
-        Main.pref.put("expert", selected);
-        notifySelectedState();
+        super.notifySelectedState();
+        fireExpertModeChanged(isSelected());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         toggleSelectedState();
+        Main.pref.put("expert", isSelected());
+        notifySelectedState();
     }
 
-    public boolean isSelected() {
-        return selected;
-    }
-
+    /**
+     * Replies the unique instance of this action.
+     * @return The unique instance of this action
+     */
     public static ExpertToggleAction getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * Determines if expert mode is enabled.
+     * @return {@code true} if expert mode is enabled, {@code false} otherwise.
+     */
     public static boolean isExpert() {
         return INSTANCE.isSelected();
     }

src/org/openstreetmap/josm/actions/FullscreenToggleAction.java
@@ -15,7 +15,6 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.ButtonModel;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.KeyStroke;
@@ -24,58 +23,47 @@ import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.tools.PlatformHookWindows;
 import org.openstreetmap.josm.tools.Shortcut;
 
-public class FullscreenToggleAction extends JosmAction {
-    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
-    private boolean selected;
+/**
+ * This class toggles the full-screen mode.
+ * @since 2533
+ */
+public class FullscreenToggleAction extends ToggleAction {
     private GraphicsDevice gd;
     private Rectangle prevBounds;
 
+    /**
+     * Constructs a new {@code FullscreenToggleAction}.
+     */
     public FullscreenToggleAction() {
-        super(
-                tr("Fullscreen view"),
-                null, /* no icon */
-                tr("Toggle fullscreen view"),
-                Shortcut.registerShortcut("menu:view:fullscreen", tr("Toggle fullscreen view"),KeyEvent.VK_F11, Shortcut.DIRECT),
-                false /* register */
+        super(tr("Fullscreen view"),
+              null, /* no icon */
+              tr("Toggle fullscreen view"),
+              Shortcut.registerShortcut("menu:view:fullscreen", tr("Toggle fullscreen view"),KeyEvent.VK_F11, Shortcut.DIRECT),
+              false /* register */
         );
         putValue("help", ht("/Action/FullscreenView"));
         putValue("toolbar", "fullscreen");
         Main.toolbar.register(this);
         gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        selected = Main.pref.getBoolean("draw.fullscreen", false);
+        setSelected(Main.pref.getBoolean("draw.fullscreen", false));
         notifySelectedState();
     }
 
-    public void addButtonModel(ButtonModel model) {
-        if (model != null && !buttonModels.contains(model)) {
-            buttonModels.add(model);
-        }
-    }
-
-    public void removeButtonModel(ButtonModel model) {
-        if (model != null && buttonModels.contains(model)) {
-            buttonModels.remove(model);
-        }
-    }
-
-    protected void notifySelectedState() {
-        for (ButtonModel model: buttonModels) {
-            if (model.isSelected() != selected) {
-                model.setSelected(selected);
-            }
-        }
-    }
-
-    protected void toggleSelectedState() {
-        selected = !selected;
-        Main.pref.put("draw.fullscreen", selected);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        toggleSelectedState();
+        Main.pref.put("draw.fullscreen", isSelected());
         notifySelectedState();
         setMode();
     }
 
+    /**
+     * To call if this action must be initially run at JOSM startup.
+     */
     public void initial() {
-        if(selected)
+        if (isSelected()) {
             setMode();
+        }
     }
 
     protected void setMode() {
@@ -88,6 +76,8 @@ public class FullscreenToggleAction extends JosmAction {
                 visibleWindows.add(w);
             }
         }
+        
+        boolean selected = isSelected();
 
         frame.dispose();
         frame.setUndecorated(selected);
@@ -118,10 +108,5 @@ public class FullscreenToggleAction extends JosmAction {
 
         // Free F10 key to allow it to be used by plugins, even after full screen (see #7502)
         frame.getJMenuBar().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_F10, 0), "none");
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
     }
 }

src/org/openstreetmap/josm/actions/ToggleAction.java
@@ -1,0 +1,106 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ButtonModel;
+import javax.swing.Icon;
+
+import org.openstreetmap.josm.tools.Shortcut;
+
+/**
+ * Abtract class for Toggle Actions.
+ * @since 6220
+ */
+public abstract class ToggleAction extends JosmAction {
+
+    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
+
+    /**
+     * Constructs a {@code ToggleAction}.
+     *
+     * @param name the action's text as displayed on the menu (if it is added to a menu)
+     * @param icon the icon to use
+     * @param tooltip  a longer description of the action that will be displayed in the tooltip. Please note
+     *           that html is not supported for menu actions on some platforms.
+     * @param shortcut a ready-created shortcut object or null if you don't want a shortcut. But you always
+     *            do want a shortcut, remember you can always register it with group=none, so you
+     *            won't be assigned a shortcut unless the user configures one. If you pass null here,
+     *            the user CANNOT configure a shortcut for your action.
+     * @param registerInToolbar register this action for the toolbar preferences?
+     * @param toolbarId identifier for the toolbar preferences. The iconName is used, if this parameter is null
+     * @param installAdapters false, if you don't want to install layer changed and selection changed adapters
+     */
+    public ToggleAction(String name, Icon icon, String tooltip, Shortcut shortcut, boolean registerInToolbar, String toolbarId, boolean installAdapters) {
+        super(name, icon, tooltip, shortcut, registerInToolbar, toolbarId, installAdapters);
+    }
+
+    /**
+     * Constructs a {@code ToggleAction}.
+     *
+     * @param name the action's text as displayed on the menu (if it is added to a menu)
+     * @param iconName the name of icon to use
+     * @param tooltip  a longer description of the action that will be displayed in the tooltip. Please note
+     *           that html is not supported for menu actions on some platforms.
+     * @param shortcut a ready-created shortcut object or null if you don't want a shortcut. But you always
+     *            do want a shortcut, remember you can always register it with group=none, so you
+     *            won't be assigned a shortcut unless the user configures one. If you pass null here,
+     *            the user CANNOT configure a shortcut for your action.
+     * @param registerInToolbar register this action for the toolbar preferences?
+     */
+    public ToggleAction(String name, String iconName, String tooltip, Shortcut shortcut, boolean registerInToolbar) {
+        super(name, iconName, tooltip, shortcut, registerInToolbar);
+    }
+
+    protected final void setSelected(boolean selected) {
+        putValue(SELECTED_KEY, selected);
+    }
+
+    /**
+     * Determines if this action is currently being selected.
+     * @return {@code true} if this action is currently being selected, {@code false} otherwise
+     */
+    public final boolean isSelected() {
+        Object selected = getValue(SELECTED_KEY);
+        if (selected instanceof Boolean) {
+            return (Boolean) selected;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * Adds a button model
+     * @param model The button model to add
+     */
+    public final void addButtonModel(ButtonModel model) {
+        if (model != null && !buttonModels.contains(model)) {
+            buttonModels.add(model);
+            model.setSelected(isSelected());
+        }
+    }
+
+    /**
+     * Removes a button model
+     * @param model The button model to remove
+     */
+    public final void removeButtonModel(ButtonModel model) {
+        if (model != null && buttonModels.contains(model)) {
+            buttonModels.remove(model);
+        }
+    }
+    
+    protected void notifySelectedState() {
+        boolean selected = isSelected();
+        for (ButtonModel model: buttonModels) {
+            if (model.isSelected() != selected) {
+                model.setSelected(selected);
+            }
+        }
+    }
+
+    protected final void toggleSelectedState() {
+        setSelected(!isSelected());
+    }
+}

src/org/openstreetmap/josm/actions/ViewportFollowToggleAction.java
@@ -6,60 +6,37 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.ButtonModel;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.tools.Shortcut;
 
-public class ViewportFollowToggleAction extends JosmAction {
-    private final List<ButtonModel> buttonModels = new ArrayList<ButtonModel>();
-    private boolean selected;
+/**
+ * This action toggles automatic moving of the map view to last placed node
+ * @since 3837
+ */
+public class ViewportFollowToggleAction extends ToggleAction {
+    
+    /**
+     * Constructs a new {@code ViewportFollowToggleAction}. 
+     */
     public ViewportFollowToggleAction() {
-        super(
-                tr("Viewport Following"),
-                "viewport-follow",
-                tr("Enable/disable automatic moving of the map view to last placed node"),
-                Shortcut.registerShortcut("menu:view:viewportfollow", tr("Toggle Viewport Following"),
-                KeyEvent.VK_F, Shortcut.CTRL_SHIFT),
-                true /* register shortcut */
+        super(tr("Viewport Following"),
+              "viewport-follow",
+              tr("Enable/disable automatic moving of the map view to last placed node"),
+              Shortcut.registerShortcut("menu:view:viewportfollow", tr("Toggle Viewport Following"),
+              KeyEvent.VK_F, Shortcut.CTRL_SHIFT),
+              true /* register shortcut */
         );
         putValue("help", ht("/Action/ViewportFollowing"));
-        selected = false;
+        setSelected(false);
         notifySelectedState();
     }
 
-    public void addButtonModel(ButtonModel model) {
-        if (model != null && !buttonModels.contains(model)) {
-            buttonModels.add(model);
-            model.setSelected(selected);
-        }
-    }
-
-    public void removeButtonModel(ButtonModel model) {
-        if (model != null && buttonModels.contains(model)) {
-            buttonModels.remove(model);
-        }
-    }
-
-    protected void notifySelectedState() {
-        for (ButtonModel model: buttonModels) {
-            if (model.isSelected() != selected) {
-                model.setSelected(selected);
-            }
-        }
-    }
-
-    protected void toggleSelectedState() {
-        selected = !selected;
-        Main.map.mapView.viewportFollowing = selected;
-        notifySelectedState();
-    }
     @Override
     public void actionPerformed(ActionEvent e) {
         toggleSelectedState();
+        Main.map.mapView.viewportFollowing = isSelected();
+        notifySelectedState();
     }
 
     @Override

commit 1394b3eb517d222aca51e412c546930129268917

fix #9220 - toggle actions not usable via menu items (regression from r6220)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6327 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/ChangesetManagerToggleAction.java
@@ -39,7 +39,7 @@ public class ChangesetManagerToggleAction extends ToggleAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
+        toggleSelectedState(e);
         notifySelectedState();
         if (isSelected()) {
             ChangesetCacheManager.getInstance().addWindowListener(changesetCacheManagerClosedHandler);

src/org/openstreetmap/josm/actions/DialogsToggleAction.java
@@ -37,7 +37,7 @@ public class DialogsToggleAction extends ToggleAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
+        toggleSelectedState(e);
         Main.pref.put("draw.dialogspanel", isSelected());
         notifySelectedState();
         setMode();

src/org/openstreetmap/josm/actions/ExpertToggleAction.java
@@ -139,7 +139,7 @@ public class ExpertToggleAction extends ToggleAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
+        toggleSelectedState(e);
         Main.pref.put("expert", isSelected());
         notifySelectedState();
     }

src/org/openstreetmap/josm/actions/FullscreenToggleAction.java
@@ -51,7 +51,7 @@ public class FullscreenToggleAction extends ToggleAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
+        toggleSelectedState(e);
         Main.pref.put("draw.fullscreen", isSelected());
         notifySelectedState();
         setMode();

src/org/openstreetmap/josm/actions/ToggleAction.java
@@ -1,11 +1,17 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.actions;
 
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ButtonModel;
 import javax.swing.Icon;
+import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JToggleButton;
 
 import org.openstreetmap.josm.tools.Shortcut;
 
@@ -100,7 +106,21 @@ public abstract class ToggleAction extends JosmAction {
         }
     }
 
-    protected final void toggleSelectedState() {
-        setSelected(!isSelected());
+    /**
+     * Toggles the selcted action state, if needed according to the ActionEvent that trigerred the action.
+     * This method will do nothing if the action event comes from a Swing component supporting the SELECTED_KEY property because the component already set the selected state.
+     * This method needs to be called especially if the action is associated with a keyboard shortcut to ensure correct selected state.
+     * @see <a href="https://weblogs.java.net/blog/zixle/archive/2005/11/changes_to_acti.html">Changes to Actions in 1.6</a>
+     * @see <a href="http://docs.oracle.com/javase/6/docs/api/javax/swing/Action.html">Interface Action</a>
+     */
+    protected final void toggleSelectedState(ActionEvent e) {
+        if (e == null || !(e.getSource() instanceof JToggleButton || 
+                           e.getSource() instanceof JCheckBox || 
+                           e.getSource() instanceof JRadioButton || 
+                           e.getSource() instanceof JCheckBoxMenuItem || 
+                           e.getSource() instanceof JRadioButtonMenuItem 
+                           )) {
+            setSelected(!isSelected());
+        }
     }
 }

src/org/openstreetmap/josm/actions/ViewportFollowToggleAction.java
@@ -34,7 +34,7 @@ public class ViewportFollowToggleAction extends ToggleAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
+        toggleSelectedState(e);
         Main.map.mapView.viewportFollowing = isSelected();
         notifySelectedState();
     }

src/org/openstreetmap/josm/actions/WireframeToggleAction.java
@@ -41,7 +41,7 @@ public class WireframeToggleAction extends ToggleAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        toggleSelectedState();
+        toggleSelectedState(e);
         if (isSelected()) {
             MapRendererFactory.getInstance().activate(WireframeMapRenderer.class);
         } else {