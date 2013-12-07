bf77b25eaef115069581cb13879ec7c421ab7bd6
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-12-07 13:47:47 +0000

    fix #9360 - listed Values in Enhanced Preferences cannot be removed completely
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6452 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/Predicate.java
+++ src/org/openstreetmap/josm/tools/Predicate.java
@@ -1,8 +1,18 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.tools;
 
-// Used to identify objects that fulfill a certain condition, e.g. when filtering a collection
+/**
+ * Used to identify objects that fulfill a certain condition, e.g. when filtering a collection.
+ *
+ * @param <T> The objects type
+ * @since 3177
+ */
 public interface Predicate<T> {
-    // @return whether the object passes the test or not
+    
+    /**
+     * Determines whether the object passes the test or not
+     * @param object The object to evaluate
+     * @return {@code true} if the object passes the test, {@code false} otherwise
+     */
     public boolean evaluate(T object);
 }
