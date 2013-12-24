6c91762ca1c9538f70e88e49b7cea2a167e5f85b
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2013-12-24 19:26:51 +0000

    Ask user to change proxy settings when proxy errors occur at startup (useful when a laptop is often used between two locations with different proxy settings)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6523 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/preferences/ServerAccessPreference.java
+++ src/org/openstreetmap/josm/gui/preferences/ServerAccessPreference.java
@@ -7,19 +7,24 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
 import org.openstreetmap.josm.gui.help.HelpUtil;
-import org.openstreetmap.josm.gui.preferences.server.AuthenticationPreferencesPanel;
 import org.openstreetmap.josm.gui.preferences.server.OsmApiUrlInputPanel;
-import org.openstreetmap.josm.gui.preferences.server.ProxyPreferencesPanel;
 import org.openstreetmap.josm.gui.widgets.VerticallyScrollablePanel;
 
+/**
+ * Connection preferences, including authentication and proxy sub-preferences.
+ */
 public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
 
+    /**
+     * Factory used to create a new {@code ServerAccessPreference}.
+     */
     public static class Factory implements PreferenceSettingFactory {
         @Override
         public PreferenceSetting createPreferenceSetting() {
@@ -31,13 +36,8 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
         super("connection", tr("Connection Settings"), tr("Connection Settings for the OSM server."), false, new JTabbedPane());
     }
 
-    private OsmApiUrlInputPanel pnlApiUrlPreferences;
-
     /** indicates whether to use the default OSM URL or not */
-    /** panel for configuring authentication preferences */
-    private AuthenticationPreferencesPanel pnlAuthPreferences;
-    /** panel for configuring proxy preferences */
-    private ProxyPreferencesPanel pnlProxyPreferences;
+    private OsmApiUrlInputPanel pnlApiUrlPreferences;
 
     /**
      * Embeds a vertically scrollable panel in a {@link JScrollPane}
@@ -58,19 +58,7 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
      */
     protected JPanel buildTabbedServerPreferences() {
         JPanel pnl = new JPanel(new BorderLayout());
-
-        JTabbedPane tpServerPreferences = getTabPane();
-        pnlAuthPreferences = new AuthenticationPreferencesPanel();
-        tpServerPreferences.add(wrapVerticallyScrollablePanel(pnlAuthPreferences));
-        pnlProxyPreferences = new ProxyPreferencesPanel();
-        tpServerPreferences.add(wrapVerticallyScrollablePanel(pnlProxyPreferences));
-
-        tpServerPreferences.setTitleAt(0, tr("Authentication"));
-        tpServerPreferences.setTitleAt(1, tr("Proxy settings"));
-        tpServerPreferences.setToolTipTextAt(0, tr("Configure your identity and how to authenticate at the OSM server"));
-        tpServerPreferences.setToolTipTextAt(1, tr("Configure whether to use a proxy server"));
-
-        pnl.add(tpServerPreferences, BorderLayout.CENTER);
+        pnl.add(getTabPane(), BorderLayout.CENTER);
         return pnl;
     }
 
@@ -98,12 +86,17 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
         gc.insets = new Insets(10,0,3,3);
         pnl.add(buildTabbedServerPreferences(), gc);
 
-        // let the AuthPreferencesPanel know when the API URL changes
-        //
-        pnlApiUrlPreferences.addPropertyChangeListener(pnlAuthPreferences);
-
         HelpUtil.setHelpContext(pnl, HelpUtil.ht("/Preferences/Connection"));
         return pnl;
+    }
+
+    /**
+     * Adds a listener that will be notified of API URL change.
+     * @param listener the listener
+     * @since 6523
+     */
+    public final void addApiUrlChangeListener(PropertyChangeListener listener) {
+        pnlApiUrlPreferences.addPropertyChangeListener(listener);
     }
 
     @Override
@@ -123,8 +116,6 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
      */
     public void initFromPreferences() {
         pnlApiUrlPreferences.initFromPreferences();
-        pnlAuthPreferences.initFromPreferences();
-        pnlProxyPreferences.initFromPreferences();
     }
 
     /**
@@ -133,8 +124,6 @@ public final class ServerAccessPreference extends DefaultTabPreferenceSetting {
     @Override
     public boolean ok() {
         pnlApiUrlPreferences.saveToPreferences();
-        pnlAuthPreferences.saveToPreferences();
-        pnlProxyPreferences.saveToPreferences();
         return false;
     }
 }
