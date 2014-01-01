7b8e1036f7991c97b6e6ea60a11795e92f7a7352
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-01-01 13:23:23 +0000

    see #8566 - make URL patterns of `DownloadOsmTask` visible for subclasses
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6589 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmTask.java
+++ src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmTask.java
@@ -36,10 +36,10 @@ import org.xml.sax.SAXException;
  */
 public class DownloadOsmTask extends AbstractDownloadTask {
 
-    private static final String PATTERN_OSM_API_URL           = "http://.*/api/0.6/(map|nodes?|ways?|relations?|\\*).*";
-    private static final String PATTERN_OVERPASS_API_URL      = "http://.*/interpreter\\?data=.*";
-    private static final String PATTERN_OVERPASS_API_XAPI_URL = "http://.*/xapi(\\?.*\\[@meta\\]|_meta\\?).*";
-    private static final String PATTERN_EXTERNAL_OSM_FILE     = "https?://.*/.*\\.osm";
+    protected static final String PATTERN_OSM_API_URL           = "http://.*/api/0.6/(map|nodes?|ways?|relations?|\\*).*";
+    protected static final String PATTERN_OVERPASS_API_URL      = "http://.*/interpreter\\?data=.*";
+    protected static final String PATTERN_OVERPASS_API_XAPI_URL = "http://.*/xapi(\\?.*\\[@meta\\]|_meta\\?).*";
+    protected static final String PATTERN_EXTERNAL_OSM_FILE     = "https?://.*/.*\\.osm";
 
     protected Bounds currentBounds;
     protected DataSet downloadedData;
