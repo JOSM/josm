991b74dd2e75e47cf4aee662024446b0d1aa03d9
xeen <xeen@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-01-03 11:24:30 +0000

    improve zoom to selection/conflict when the objects in question are small (fixes #4413)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6608 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/osm/visitor/BoundingXYVisitor.java
+++ src/org/openstreetmap/josm/data/osm/visitor/BoundingXYVisitor.java
@@ -122,6 +122,70 @@ public class BoundingXYVisitor extends AbstractVisitor {
                 Main.getProjection().latlon2eastNorth(new LatLon(maxLatlon.lat() + enlargeDegree, maxLatlon.lon() + enlargeDegree)));
     }
 
+    /**
+     * Enlarges the bounding box up to <code>maxEnlargePercent</code>, depending on
+     * its size. If the bounding box is small, it will be enlarged more in relation
+     * to its beginning size. The larger the bounding box, the smaller the change,
+     * down to the minimum of 1% enlargement.
+     * 
+     * Warning: if the bounding box only contains a single node, no expansion takes
+     * place because a node has no width/height. Use <code>enlargeToMinDegrees</code>
+     * instead.
+     * 
+     * Example: You specify enlargement to be up to 100%.
+     * 
+     *          Bounding box is a small house: enlargement will be 95–100%, i.e.
+     *          making enough space so that the house fits twice on the screen in
+     *          each direction.
+     * 
+     *          Bounding box is a large landuse, like a forest: Enlargement will
+     *          be 1–10%, i.e. just add a little border around the landuse.
+     * 
+     * If the bounding box has not been set (<code>min</code> or <code>max</code>
+     * equal <code>null</code>) this method does not do anything.
+     * 
+     * @param maxEnlargePercent
+     */
+    public void enlargeBoundingBoxLogarithmically(double maxEnlargePercent) {
+        if (bounds == null)
+            return;
+
+        double diffEast = bounds.getMax().east() - bounds.getMin().east();
+        double diffNorth = bounds.getMax().north() - bounds.getMin().north();
+
+        double enlargeEast = Math.min(maxEnlargePercent - 10*Math.log(diffEast), 1)/100;
+        double enlargeNorth = Math.min(maxEnlargePercent - 10*Math.log(diffNorth), 1)/100;
+        System.out.println(enlargeEast);
+
+        visit(bounds.getMin().add(-enlargeEast/2, -enlargeNorth/2));
+        visit(bounds.getMax().add(+enlargeEast/2, +enlargeNorth/2));
+    }
+
+
+    /**
+     * Specify a degree larger than 0 in order to make the bounding box at least
+     * the specified amount of degrees high and wide. The value is ignored if the
+     * bounding box is already larger than the specified amount.
+     * 
+     * If the bounding box has not been set (<code>min</code> or <code>max</code>
+     * equal <code>null</code>) this method does not do anything.
+     * 
+     * If the bounding box contains objects and is to be enlarged, the objects
+     * will be centered within the new bounding box.
+     * 
+     * @param minDegrees
+     */
+    public void enlargeToMinDegrees(double minDegrees) {
+        if (bounds == null)
+            return;
+
+        EastNorth minEnlarge = Main.getProjection().latlon2eastNorth(new LatLon(0, minDegrees));
+
+        visit(bounds.getMin().add(-minEnlarge.east()/2, -minEnlarge.north()/2));
+        visit(bounds.getMax().add(+minEnlarge.east()/2, +minEnlarge.north()/2));
+    }
+
+
     @Override public String toString() {
         return "BoundingXYVisitor["+bounds+"]";
     }
