66502edb71c56e292f2305a98b484d254a8334b4
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-01-17 20:59:46 +0000

    fix javadoc, comments
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@6718 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/io/OsmBzip2Importer.java
+++ src/org/openstreetmap/josm/io/OsmBzip2Importer.java
@@ -5,15 +5,24 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import org.openstreetmap.josm.actions.ExtensionFileFilter;
 
+/**
+ * OSM data importer that uncompresses it from Bzip2 format.
+ */
 public class OsmBzip2Importer extends OsmImporter {
 
+    /**
+     * File filter used to load/save Bzip2 compressed OSM files.
+     */
     public static final ExtensionFileFilter FILE_FILTER = new ExtensionFileFilter(
             "osm.bz2,osm.bz", "osm.bz2", tr("OSM Server Files bzip2 compressed") + " (*.osm.bz2 *.osm.bz)");
 
+    /**
+     * Constructs a new {@code OsmBzip2Importer}.
+     */
     public OsmBzip2Importer() {
         super(FILE_FILTER);
     }
 
-    // compression handling is preformed in super-class
+    // compression handling is performed in super-class
 
 }
