JOSM/josm

commit 4241a6f4d8fcddd11400ef30aa0f8b42ddd6724b

fix #9459 - initialize default center view of MapView to last download location, if any, instead of (0,0)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6509 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/NavigatableComponent.java
@@ -46,6 +46,7 @@ import org.openstreetmap.josm.data.osm.visitor.paint.PaintColors;
 import org.openstreetmap.josm.data.preferences.IntegerProperty;
 import org.openstreetmap.josm.data.projection.Projection;
 import org.openstreetmap.josm.data.projection.Projections;
+import org.openstreetmap.josm.gui.download.DownloadDialog;
 import org.openstreetmap.josm.gui.help.Helpful;
 import org.openstreetmap.josm.gui.preferences.projection.ProjectionPreference;
 import org.openstreetmap.josm.tools.Predicate;
@@ -192,6 +193,9 @@ public class NavigatableComponent extends JComponent implements Helpful {
     private Rectangle paintRect = null;
     private Polygon paintPoly = null;
 
+    /**
+     * Constructs a new {@code NavigatableComponent}.
+     */
     public NavigatableComponent() {
         setLayout(null);
     }
@@ -201,11 +205,11 @@ public class NavigatableComponent extends JComponent implements Helpful {
     }
 
     private EastNorth calculateDefaultCenter() {
-        Bounds b = Main.getProjection().getWorldBoundsLatLon();
-        double lat = (b.getMaxLat() + b.getMinLat())/2;
-        double lon = (b.getMaxLon() + b.getMinLon())/2;
-        // FIXME is it correct? b.getCenter() makes some adjustments... 
-        return Main.getProjection().latlon2eastNorth(new LatLon(lat, lon));
+        Bounds b = DownloadDialog.getSavedDownloadBounds();
+        if (b == null) {
+            b = Main.getProjection().getWorldBoundsLatLon();
+        }
+        return Main.getProjection().latlon2eastNorth(b.getCenter());
     }
 
     /**
@@ -411,6 +415,9 @@ public class NavigatableComponent extends JComponent implements Helpful {
         }
         int width = getWidth()/2;
         int height = getHeight()/2;
+        if (width == 0 || height == 0) {
+            throw new IllegalStateException("Cannot zoom into undimensioned NavigatableComponent");
+        }
         LatLon l1 = new LatLon(b.getMinLat(), lon);
         LatLon l2 = new LatLon(b.getMaxLat(), lon);
         EastNorth e1 = getProjection().latlon2eastNorth(l1);

src/org/openstreetmap/josm/actions/AutoScaleAction.java
@@ -30,6 +30,7 @@ import org.openstreetmap.josm.gui.MapFrameListener;
 import org.openstreetmap.josm.gui.MapView;
 import org.openstreetmap.josm.gui.dialogs.LayerListDialog;
 import org.openstreetmap.josm.gui.dialogs.ValidatorDialog.ValidatorBoundingXYVisitor;
+import org.openstreetmap.josm.gui.download.DownloadDialog;
 import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.tools.Shortcut;
 
@@ -243,11 +244,12 @@ public class AutoScaleAction extends JosmAction {
             v.enlargeBoundingBox();
         }
         else if (mode.equals("download")) {
-            if (!Main.pref.get("osm-download.bounds").isEmpty()) {
+            Bounds bounds = DownloadDialog.getSavedDownloadBounds();
+            if (bounds != null) {
                 try {
-                    v.visit(new Bounds(Main.pref.get("osm-download.bounds"), ";"));
+                    v.visit(bounds);
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    Main.warn(e);
                 }
             }
         }

src/org/openstreetmap/josm/actions/DownloadAction.java
@@ -25,6 +25,10 @@ import org.openstreetmap.josm.tools.Shortcut;
  * @author imi
  */
 public class DownloadAction extends JosmAction {
+    
+    /**
+     * Constructs a new {@code DownloadAction}.
+     */
     public DownloadAction() {
         super(tr("Download from OSM..."), "download", tr("Download map data from the OSM server."),
                 Shortcut.registerShortcut("file:download", tr("File: {0}", tr("Download from OSM...")), KeyEvent.VK_DOWN, Shortcut.CTRL_SHIFT), true);

src/org/openstreetmap/josm/actions/NewAction.java
@@ -12,8 +12,15 @@ import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.tools.Shortcut;
 
+/**
+ * Creates a blank new OSM data layer.
+ * @since 169
+ */
 public class NewAction extends JosmAction {
 
+    /**
+     * Constructs a {@code NewAction}.
+     */
     public NewAction() {
         super(tr("New Layer"), "new", tr("Create a new map layer."),
                 Shortcut.registerShortcut("system:new", tr("File: {0}", tr("New Layer")), KeyEvent.VK_N, Shortcut.CTRL), true);

src/org/openstreetmap/josm/data/Bounds.java
@@ -275,8 +275,7 @@ public class Bounds {
     /**
      * @return Center of the bounding box.
      */
-    public LatLon getCenter()
-    {
+    public LatLon getCenter() {
         if (crosses180thMeridian()) {            
             double lat = (minLat + maxLat) / 2;
             double lon = (minLon + maxLon - 360.0) / 2;

src/org/openstreetmap/josm/gui/download/DownloadDialog.java
@@ -306,8 +306,7 @@ public class DownloadDialog extends JDialog  {
     }
     
     /**
-     * Remembers the current settings in the download dialog
-     *
+     * Remembers the current settings in the download dialog.
      */
     public void rememberSettings() {
         Main.pref.put("download.tab", Integer.toString(tpDownloadAreaSelectors.getSelectedIndex()));
@@ -319,6 +318,9 @@ public class DownloadDialog extends JDialog  {
         }
     }
 
+    /**
+     * Restores the previous settings in the download dialog.
+     */
     public void restoreSettings() {
         cbDownloadOsmData.setSelected(Main.pref.getBoolean("download.osm", true));
         cbDownloadGpxData.setSelected(Main.pref.getBoolean("download.gps", false));
@@ -338,18 +340,36 @@ public class DownloadDialog extends JDialog  {
             );
             boundingBoxChanged(currentBounds,null);
         }
-        else if (!Main.pref.get("osm-download.bounds").isEmpty()) {
-            // read the bounding box from the preferences
-            try {
-                currentBounds = new Bounds(Main.pref.get("osm-download.bounds"), ";");
-                boundingBoxChanged(currentBounds,null);
-            }
-            catch (Exception e) {
-                e.printStackTrace();
+        else {
+            Bounds bounds = getSavedDownloadBounds();
+            if (bounds != null) {
+                currentBounds = bounds;
+                boundingBoxChanged(currentBounds, null);
             }
         }
     }
+    
+    /**
+     * Returns the previously saved bounding box from preferences.
+     * @return The bounding box saved in preferences if any, {@code null} otherwise
+     * @since 6509
+     */
+    public static Bounds getSavedDownloadBounds() {
+        String value = Main.pref.get("osm-download.bounds");
+        if (!value.isEmpty()) {
+            try {
+                return new Bounds(value, ";");
+            } catch (IllegalArgumentException e) {
+                Main.warn(e);
+            }
+        }
+        return null;
+    }
 
+    /**
+     * Determines if the dialog autorun is enabled in preferences.
+     * @return {@code true} if the download dialog must be open at startup, {@code false} otherwise
+     */
     public static boolean isAutorunEnabled() {
         return Main.pref.getBoolean("download.autorun",false);
     }
@@ -361,7 +381,8 @@ public class DownloadDialog extends JDialog  {
     }
 
     /**
-     * Replies the currently selected download area. May be null, if no download area is selected yet.
+     * Replies the currently selected download area.
+     * @return the currently selected download area. May be {@code null}, if no download area is selected yet.
      */
     public Bounds getSelectedDownloadArea() {
         return currentBounds;

commit 1346ef4639bac09f8a197cbd4b0d5b9b9f8fd388

fix #9692 - IllegalStateException with print and pdfimport plugins (regression from r6509)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@6873 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/NavigatableComponent.java
@@ -415,28 +415,22 @@ public class NavigatableComponent extends JComponent implements Helpful {
         }
         int width = getWidth()/2;
         int height = getHeight()/2;
-        if (width == 0 || height == 0) {
-            throw new IllegalStateException("Cannot zoom into undimensioned NavigatableComponent");
-        }
         LatLon l1 = new LatLon(b.getMinLat(), lon);
         LatLon l2 = new LatLon(b.getMaxLat(), lon);
         EastNorth e1 = getProjection().latlon2eastNorth(l1);
         EastNorth e2 = getProjection().latlon2eastNorth(l2);
         double d = e2.north() - e1.north();
-        if(d < height*newScale)
-        {
+        if (height > 0 && d < height*newScale) {
             double newScaleH = d/height;
             e1 = getProjection().latlon2eastNorth(new LatLon(lat, b.getMinLon()));
             e2 = getProjection().latlon2eastNorth(new LatLon(lat, b.getMaxLon()));
             d = e2.east() - e1.east();
-            if(d < width*newScale) {
+            if (width > 0 && d < width*newScale) {
                 newScale = Math.max(newScaleH, d/width);
             }
-        }
-        else
-        {
+        } else if (height > 0) {
             d = d/(l1.greatCircleDistance(l2)*height*10);
-            if(newScale < d) {
+            if (newScale < d) {
                 newScale = d;
             }
         }