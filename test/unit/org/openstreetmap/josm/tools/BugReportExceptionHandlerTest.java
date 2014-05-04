JOSM/josm

commit 37a04fd46a0f2906b0a3cf8344fe6440037cc873

see #8465 - last batch of `try-with-resources`
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7037 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/tools/BugReportExceptionHandler.java
@@ -210,11 +210,11 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
      * @since 5849
      */
     public static URL getBugReportUrl(String debugText) {
-        try {
+        try (
             ByteArrayOutputStream out = new ByteArrayOutputStream();
-            GZIPOutputStream gzip = new GZIPOutputStream(out);
+            GZIPOutputStream gzip = new GZIPOutputStream(out)
+        ) {
             gzip.write(debugText.getBytes(Utils.UTF_8));
-            Utils.close(gzip);
 
             return new URL(Main.getJOSMWebsite()+"/josmticket?" +
                     "gdata="+Base64.encode(ByteBuffer.wrap(out.toByteArray()), true));

src/org/openstreetmap/josm/actions/OpenFileAction.java
@@ -300,7 +300,6 @@ public class OpenFileAction extends DiskAccessAction {
                                 Main.main.menu.openLocation.openUrl(false, url);
                             }
                         }
-                        Utils.close(reader);
                     } catch (Exception e) {
                         Main.error(e);
                     }

src/org/openstreetmap/josm/data/AutosaveTask.java
@@ -303,16 +303,10 @@ public class AutosaveTask extends TimerTask implements LayerChangeListener, List
                 File pidFile = getPidFile(file);
                 if (pidFile.exists()) {
                     try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(pidFile), Utils.UTF_8))) {
-                        try {
-                            String jvmId = reader.readLine();
-                            if (jvmId != null) {
-                                String pid = jvmId.split("@")[0];
-                                skipFile = jvmPerfDataFileExists(pid);
-                            }
-                        } catch (Throwable t) {
-                            Main.error(t);
-                        } finally {
-                            Utils.close(reader);
+                        String jvmId = reader.readLine();
+                        if (jvmId != null) {
+                            String pid = jvmId.split("@")[0];
+                            skipFile = jvmPerfDataFileExists(pid);
                         }
                     } catch (Throwable t) {
                         Main.error(t);

src/org/openstreetmap/josm/data/imagery/ImageryLayerInfo.java
@@ -73,17 +73,14 @@ public class ImageryLayerInfo {
             if (clearCache) {
                 MirroredInputStream.cleanup(source);
             }
-            MirroredInputStream stream = null;
             try {
                 ImageryReader reader = new ImageryReader(source);
                 Collection<ImageryInfo> result = reader.parse();
                 defaultLayers.addAll(result);
             } catch (IOException ex) {
-                Utils.close(stream);
                 Main.error(ex, false);
                 continue;
             } catch (SAXException ex) {
-                Utils.close(stream);
                 Main.error(ex);
                 continue;
             }

src/org/openstreetmap/josm/data/projection/datum/NTV2GridShiftFile.java
@@ -154,8 +154,6 @@ public class NTV2GridShiftFile implements Serializable {
         }
         topLevelSubGrid = createSubGridTree(subGrid);
         lastSubGrid = topLevelSubGrid[0];
-
-        Utils.close(in);
     }
 
     /**

src/org/openstreetmap/josm/gui/help/HelpContentReader.java
@@ -44,13 +44,13 @@ public class HelpContentReader extends WikiReader {
         if(helpTopicUrl == null)
             throw new MissingHelpContentException(helpTopicUrl);
         HttpURLConnection con = null;
-        BufferedReader in = null;
         try {
             URL u = new URL(helpTopicUrl);
             con = Utils.openHttpConnection(u);
             con.connect();
-            in = new BufferedReader(new InputStreamReader(con.getInputStream(), Utils.UTF_8));
-            return prepareHelpContent(in, dotest, u);
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), Utils.UTF_8))) {
+                return prepareHelpContent(in, dotest, u);
+            }
         } catch(MalformedURLException e) {
             throw new HelpContentReaderException(e);
         } catch(IOException e) {
@@ -63,8 +63,6 @@ public class HelpContentReader extends WikiReader {
                 }
             }
             throw ex;
-        } finally {
-            Utils.close(in);
         }
     }
 

src/org/openstreetmap/josm/gui/oauth/OsmOAuthAuthorizationClient.java
@@ -6,6 +6,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
@@ -196,9 +197,10 @@ public class OsmOAuthAuthorizationClient {
     }
 
     protected String extractToken(HttpURLConnection connection) {
-        BufferedReader r = null;
-        try {
-            r = new BufferedReader(new InputStreamReader(connection.getInputStream(), Utils.UTF_8));
+        try (
+            InputStream is = connection.getInputStream();
+            BufferedReader r = new BufferedReader(new InputStreamReader(is, Utils.UTF_8))
+        ) {
             String c;
             Pattern p = Pattern.compile(".*authenticity_token.*value=\"([^\"]+)\".*");
             while ((c = r.readLine()) != null) {
@@ -210,8 +212,6 @@ public class OsmOAuthAuthorizationClient {
         } catch (IOException e) {
             Main.error(e);
             return null;
-        } finally {
-            Utils.close(r);
         }
         return null;
     }
@@ -363,7 +363,6 @@ public class OsmOAuthAuthorizationClient {
     }
 
     protected void authenticateOsmSession(SessionId sessionId, String userName, String password) throws OsmLoginFailedException {
-        DataOutputStream dout = null;
         try {
             URL url = new URL(buildOsmLoginUrl());
             synchronized(this) {
@@ -391,10 +390,10 @@ public class OsmOAuthAuthorizationClient {
 
             connection.connect();
 
-            dout = new DataOutputStream(connection.getOutputStream());
-            dout.writeBytes(request);
-            dout.flush();
-            Utils.close(dout);
+            try (DataOutputStream dout = new DataOutputStream(connection.getOutputStream())) {
+                dout.writeBytes(request);
+                dout.flush();
+            }
 
             // after a successful login the OSM website sends a redirect to a follow up page. Everything
             // else, including a 200 OK, is a failed login. A 200 OK is replied if the login form with
@@ -408,7 +407,6 @@ public class OsmOAuthAuthorizationClient {
         } catch(IOException e) {
             throw new OsmLoginFailedException(e);
         } finally {
-            Utils.close(dout);
             synchronized(this) {
                 connection = null;
             }
@@ -455,14 +453,13 @@ public class OsmOAuthAuthorizationClient {
         if (privileges.isAllowReadPrefs()) {
             parameters.put("allow_read_prefs", "yes");
         }
-        if(privileges.isAllowModifyNotes()) {
+        if (privileges.isAllowModifyNotes()) {
             parameters.put("allow_write_notes", "yes");
         }
 
         parameters.put("commit", "Save changes");
 
         String request = buildPostRequest(parameters);
-        DataOutputStream dout = null;
         try {
             URL url = new URL(oauthProviderParameters.getAuthoriseUrl());
             synchronized(this) {
@@ -479,17 +476,17 @@ public class OsmOAuthAuthorizationClient {
 
             connection.connect();
 
-            dout = new DataOutputStream(connection.getOutputStream());
-            dout.writeBytes(request);
-            dout.flush();
+            try (DataOutputStream dout = new DataOutputStream(connection.getOutputStream())) {
+                dout.writeBytes(request);
+                dout.flush();
+            }
 
             int retCode = connection.getResponseCode();
             if (retCode != HttpURLConnection.HTTP_OK)
                 throw new OsmOAuthAuthorizationException(tr("Failed to authorize OAuth request  ''{0}''", requestToken.getKey()));
-        } catch(IOException e) {
+        } catch (IOException e) {
             throw new OsmOAuthAuthorizationException(e);
         } finally {
-            Utils.close(dout);
             synchronized(this) {
                 connection = null;
             }

src/org/openstreetmap/josm/gui/preferences/server/ApiUrlTestTask.java
@@ -164,7 +164,6 @@ public class ApiUrlTestTask extends PleaseWaitRunnable{
 
     @Override
     protected void realRun() throws SAXException, IOException, OsmTransferException {
-        BufferedReader bin = null;
         try {
             try {
                 new URL(getNormalizedApiUrl());
@@ -194,10 +193,11 @@ public class ApiUrlTestTask extends PleaseWaitRunnable{
                 return;
             }
             StringBuilder changesets = new StringBuilder();
-            bin = new BufferedReader(new InputStreamReader(connection.getInputStream(), Utils.UTF_8));
-            String line;
-            while ((line = bin.readLine()) != null) {
-                changesets.append(line).append("\n");
+            try (BufferedReader bin = new BufferedReader(new InputStreamReader(connection.getInputStream(), Utils.UTF_8))) {
+                String line;
+                while ((line = bin.readLine()) != null) {
+                    changesets.append(line).append("\n");
+                }
             }
             if (! (changesets.toString().contains("<osm") && changesets.toString().contains("</osm>"))) {
                 // heuristic: if there isn't an opening and closing "<osm>" tag in the returned content,
@@ -214,8 +214,6 @@ public class ApiUrlTestTask extends PleaseWaitRunnable{
             Main.error(e);
             alertConnectionFailed();
             return;
-        } finally {
-            Utils.close(bin);
         }
     }
 

commit 2160b1ff89704dd10d914a0fadbce7822b77861d

fix issue in bug report URL (regression from r7037), add javadoc and unit test
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7060 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/tools/Base64.java
@@ -3,19 +3,41 @@ package org.openstreetmap.josm.tools;
 
 import java.nio.ByteBuffer;
 
+/**
+ * This class implements an encoder for encoding byte and character data using the
+ * <a href="https://en.wikipedia.org/wiki/Base64">Base64</a> encoding scheme as specified in
+ * <a href="http://www.ietf.org/rfc/rfc2045.txt">RFC 2045</a> ("base64", default) and
+ * <a href="http://tools.ietf.org/html/rfc4648#section-4">RFC 4648</a> ("base64url").
+ * @since 195
+ */
 public final class Base64 {
+    // TODO: Remove this class when switching to Java 8 (finally integrated in Java SE as java.util.Base64.Encoder)
 
     private Base64() {
         // Hide default constructor for utils classes
     }
     
+    /** "base64": RFC 2045 default encoding */
     private static String encDefault = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    /** "base64url": RFC 4648 url-safe encoding */
     private static String encUrlSafe = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
+    /**
+     * Encodes all characters from the specified string into a new String using the Base64 default encoding scheme.
+     * @param s the string to encode
+     * @return A new string containing the resulting encoded characters.
+     */
     public static String encode(String s) {
         return encode(s, false);
     }
 
+    /**
+     * Encodes all characters from the specified string into a new String using a supported Base64 encoding scheme.
+     * @param s the string to encode
+     * @param urlsafe if {@code true}, uses "base64url" encoding, otherwise use the "base64" default encoding
+     * @return A new string containing the resulting encoded characters.
+     * @since 3840
+     */
     public static String encode(String s, boolean urlsafe) {
         StringBuilder out = new StringBuilder();
         String enc = urlsafe ? encUrlSafe : encDefault;
@@ -34,10 +56,24 @@ public final class Base64 {
         return out.toString();
     }
 
+    /**
+     * Encodes all remaining bytes from the specified byte buffer into a new string using the Base64 default encoding scheme.
+     * Upon return, the source buffer's position will be updated to its limit; its limit will not have been changed.
+     * @param s the source ByteBuffer to encode
+     * @return A new string containing the resulting encoded characters.
+     */
     public static String encode(ByteBuffer s) {
         return encode(s, false);
     }
 
+    /**
+     * Encodes all remaining bytes from the specified byte buffer into a new string using a supported Base64 encoding scheme.
+     * Upon return, the source buffer's position will be updated to its limit; its limit will not have been changed.
+     * @param s the source ByteBuffer to encode
+     * @param urlsafe if {@code true}, uses "base64url" encoding, otherwise use the "base64" default encoding
+     * @return A new string containing the resulting encoded characters.
+     * @since 3840
+     */
     public static String encode(ByteBuffer s, boolean urlsafe) {
         StringBuilder out = new StringBuilder();
         String enc = urlsafe ? encUrlSafe : encDefault;

src/org/openstreetmap/josm/tools/BugReportExceptionHandler.java
@@ -215,6 +215,7 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
             GZIPOutputStream gzip = new GZIPOutputStream(out)
         ) {
             gzip.write(debugText.getBytes(Utils.UTF_8));
+            gzip.finish();
 
             return new URL(Main.getJOSMWebsite()+"/josmticket?" +
                     "gdata="+Base64.encode(ByteBuffer.wrap(out.toByteArray()), true));

test/unit/org/openstreetmap/josm/tools/BugReportExceptionHandlerTest.java
@@ -1,0 +1,56 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.tools;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.actions.ShowStatusReportAction;
+
+/**
+ * Bug report unit tests.
+ */
+public class BugReportExceptionHandlerTest {
+
+    /**
+     * Setup tests.
+     */
+    @Before
+    public void setUp() {
+        Main.commandLineArgs = new String[0];
+        Main.determinePlatformHook();
+        Main.initApplicationPreferences();
+    }
+
+    /**
+     * Test method for {@link org.openstreetmap.josm.tools.BugReportExceptionHandler#getBugReportUrl(java.lang.String)}.
+     * @throws IOException 
+     */
+    @Test
+    public void testGetBugReportUrl() throws IOException {
+        String report = ShowStatusReportAction.getReportHeader();
+        String url = BugReportExceptionHandler.getBugReportUrl(report).toExternalForm();
+        String prefix = Main.getJOSMWebsite()+"/josmticket?gdata=";
+        assertTrue(url.startsWith(prefix));
+        
+        String gdata = url.substring(prefix.length());
+        // JAXB only provides support for "base64" decoding while we encode url in "base64url", so switch encoding, only for test purpose
+        byte[] data = DatatypeConverter.parseBase64Binary(gdata.replace('-', '+').replace('_', '/'));
+        byte[] buff = new byte[8192];
+        try (GZIPInputStream is = new GZIPInputStream(new ByteArrayInputStream(data))) {
+            StringBuilder sb = new StringBuilder();
+            for (int n = is.read(buff); n > 0; n = is.read(buff)) {
+                sb.append(new String(buff, 0, n, Utils.UTF_8));
+            }
+            assertEquals(report, sb.toString());
+        }
+    }
+}