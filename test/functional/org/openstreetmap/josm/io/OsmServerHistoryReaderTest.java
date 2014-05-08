30c6330fee18d6f90dbb5592eb9e5d02b5826048
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-05-08 13:15:11 +0000

    fix some unit tests
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@7079 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/functional/org/openstreetmap/josm/io/OsmServerHistoryReaderTest.java
+++ test/functional/org/openstreetmap/josm/io/OsmServerHistoryReaderTest.java
@@ -1,6 +1,8 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.io;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openstreetmap.josm.Main;
@@ -9,35 +11,53 @@ import org.openstreetmap.josm.data.osm.history.History;
 import org.openstreetmap.josm.data.osm.history.HistoryDataSet;
 import org.openstreetmap.josm.gui.progress.NullProgressMonitor;
 
+/**
+ * History fetching tests. This test operates with production API.
+ */
 public class OsmServerHistoryReaderTest {
 
+    /**
+     * Setup tests.
+     */
     @BeforeClass
     public static void init() {
-        System.setProperty("josm.home", "C:\\data\\projekte\\osm\\tag-editor-plugin");
-        Main.pref.init(false);
+        Main.initApplicationPreferences();
+        Main.pref.put("osm-server.url", OsmApi.DEFAULT_API_URL);
     }
 
+    /**
+     * Tests node history fetching.
+     * @throws OsmTransferException if any error occurs
+     */
     @Test
-    public void testNode()  throws OsmTransferException {
-        OsmServerHistoryReader reader = new OsmServerHistoryReader(OsmPrimitiveType.NODE,266187);
+    public void testNode() throws OsmTransferException {
+        OsmServerHistoryReader reader = new OsmServerHistoryReader(OsmPrimitiveType.NODE, 266187);
         HistoryDataSet ds = reader.parseHistory(NullProgressMonitor.INSTANCE);
         History h = ds.getHistory(266187, OsmPrimitiveType.NODE);
-        System.out.println("num versions: " + h.getNumVersions());
+        assertTrue("NumVersions", h.getNumVersions() >= 4);
     }
 
+    /**
+     * Tests way history fetching.
+     * @throws OsmTransferException if any error occurs
+     */
     @Test
-    public void testWay()  throws OsmTransferException {
-        OsmServerHistoryReader reader = new OsmServerHistoryReader(OsmPrimitiveType.WAY,3058844);
+    public void testWay() throws OsmTransferException {
+        OsmServerHistoryReader reader = new OsmServerHistoryReader(OsmPrimitiveType.WAY, 3058844);
         HistoryDataSet ds = reader.parseHistory(NullProgressMonitor.INSTANCE);
         History h = ds.getHistory(3058844, OsmPrimitiveType.WAY);
-        System.out.println("num versions: " + h.getNumVersions());
+        assertTrue("NumVersions", h.getNumVersions() >= 13);
     }
 
+    /**
+     * Tests relation history fetching.
+     * @throws OsmTransferException if any error occurs
+     */
     @Test
-    public void testRelation()  throws OsmTransferException {
-        OsmServerHistoryReader reader = new OsmServerHistoryReader(OsmPrimitiveType.RELATION,49);
+    public void testRelation() throws OsmTransferException {
+        OsmServerHistoryReader reader = new OsmServerHistoryReader(OsmPrimitiveType.RELATION, 49);
         HistoryDataSet ds = reader.parseHistory(NullProgressMonitor.INSTANCE);
         History h = ds.getHistory(49, OsmPrimitiveType.RELATION);
-        System.out.println("num versions: " + h.getNumVersions());
+        assertTrue("NumVersions", h.getNumVersions() >= 3);
     }
 }
