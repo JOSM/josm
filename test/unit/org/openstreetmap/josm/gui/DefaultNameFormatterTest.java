f7b71f89ef74521cc63f58f4e4fb66a863787ab2
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-05-10 17:21:46 +0000

    see #9632 - unit test optimization
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@7097 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/DefaultNameFormatterTest.java
+++ test/unit/org/openstreetmap/josm/gui/DefaultNameFormatterTest.java
@@ -1,17 +1,14 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -24,6 +21,7 @@ import org.openstreetmap.josm.gui.tagging.TaggingPresetReader;
 import org.openstreetmap.josm.io.Compression;
 import org.openstreetmap.josm.io.IllegalDataException;
 import org.openstreetmap.josm.io.OsmReader;
+import org.xml.sax.SAXException;
 
 /**
  * Unit tests of {@link DefaultNameFormatter} class.
@@ -43,56 +41,70 @@ public class DefaultNameFormatterTest {
      * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/9632">#9632</a>.
      * @throws IllegalDataException
      * @throws IOException
+     * @throws SAXException
      */
     @Test
-    public void testTicket9632() throws IllegalDataException, IOException {
-        Collection<String> sources = new ArrayList<>();
-        sources.add("resource://data/defaultpresets.xml");
-        sources.add("http://josm.openstreetmap.de/josmfile?page=Presets/BicycleJunction&amp;preset");
-        TaggingPresetPreference.taggingPresets = TaggingPresetReader.readAll(sources, true);
+    public void testTicket9632() throws IllegalDataException, IOException, SAXException {
+        String source = "http://josm.openstreetmap.de/josmfile?page=Presets/BicycleJunction&amp;preset";
+        TaggingPresetPreference.taggingPresets = TaggingPresetReader.readAll(source, true);
 
         Comparator<Relation> comparator = DefaultNameFormatter.getInstance().getRelationComparator();
 
         try (InputStream is = new FileInputStream(TestUtils.getTestDataRoot() + "regress/9632/data.osm.zip")) {
             DataSet ds = OsmReader.parseDataSet(Compression.ZIP.getUncompressedInputStream(is), null);
-            List<Relation> relations = new ArrayList<>(ds.getRelations());
-            System.out.println(Arrays.toString(relations.toArray()));
+            Relation[] relations = new ArrayList<>(ds.getRelations()).toArray(new Relation[0]);
+            System.out.println(Arrays.toString(relations));
             // Check each compare possibility
-            for (int i=0; i<relations.size(); i++) {
-                long start = System.currentTimeMillis();
-                Relation r1 = relations.get(i);
-                String r1s = r1.toString();
-                for (int j=i; j<relations.size(); j++) {
-                    Relation r2 = relations.get(j);
-                    String r2s = r2.toString();
+            for (int i=0; i<relations.length; i++) {
+                Relation r1 = relations[i];
+                for (int j=i; j<relations.length; j++) {
+                    Relation r2 = relations[j];
                     int a = comparator.compare(r1, r2);
                     int b = comparator.compare(r2, r1);
-                    String msg = "Compared\nr1: "+r1s+"\nr2: "+r2s+"gave: "+a+"/"+b;
                     if (i==j || a==b) {
-                        assertTrue(msg, a == 0 && b == 0);
+                        if (a != 0 || b != 0) {
+                            fail(getFailMessage(r1, r2, a, b));
+                        }
                     } else {
-                        assertTrue(msg, a == -b);
+                        if (a != -b) {
+                            fail(getFailMessage(r1, r2, a, b));
+                        }
                     }
-                    for (int k=j; k<relations.size(); k++) {
-                        Relation r3 = relations.get(k);
-                        String r3s = r3.toString();
+                    for (int k=j; k<relations.length; k++) {
+                        Relation r3 = relations[k];
                         int c = comparator.compare(r1, r3);
                         int d = comparator.compare(r2, r3);
-                        String msg2 = msg + "\nCompared\nr1: "+r1s+"\nr3: "+r3s+"gave: "+c+"Compared\nr2: "+r2s+"\nr3: "+r3s+"gave: "+d;
                         if (a > 0 && d > 0) {
-                            assertTrue(msg2, c > 0);
+                            if (c <= 0) {
+                               fail(getFailMessage(r1, r2, r3, a, b, c, d));
+                            }
                         } else if (a == 0 && d == 0) {
-                            assertTrue(msg2, c == 0);
+                            if (c != 0) {
+                                fail(getFailMessage(r1, r2, r3, a, b, c, d));
+                            }
                         } else if (a < 0 && d < 0) {
-                            assertTrue(msg2, c < 0);
+                            if (c >= 0) {
+                                fail(getFailMessage(r1, r2, r3, a, b, c, d));
+                            }
                         }
                     }
                 }
-                long end = System.currentTimeMillis();
-                System.out.println(i+"-> "+(end-start)+" ms");
             }
-            // Sort relation list
-            Collections.sort(relations, comparator);
+            // Sort relation array
+            Arrays.sort(relations, comparator);
         }
+    }
+
+    private static String getFailMessage(Relation r1, Relation r2, int a, int b) {
+        return new StringBuilder("Compared\nr1: ").append(r1).append("\nr2: ")
+        .append(r2).append("\ngave: ").append(a).append("/").append(b)
+        .toString();
+    }
+
+    private static String getFailMessage(Relation r1, Relation r2, Relation r3, int a, int b, int c, int d) {
+        return new StringBuilder(getFailMessage(r1, r2, a, b))
+        .append("\nCompared\nr1: ").append(r1).append("\nr3: ").append(r3).append("\ngave: ").append(c)
+        .append("\nCompared\nr2: ").append(r2).append("\nr3: ").append(r3).append("\ngave: ").append(d)
+        .toString();
     }
 }
