eb60c82df79e88f733bafbac248313b33ec7d622
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-08-13 21:07:06 +0000

    see #10363 - you cannot click on object that is not visible on the map
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@7389 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/NavigatableComponent.java
+++ src/org/openstreetmap/josm/gui/NavigatableComponent.java
@@ -47,6 +47,7 @@ import org.openstreetmap.josm.data.projection.Projection;
 import org.openstreetmap.josm.data.projection.Projections;
 import org.openstreetmap.josm.gui.download.DownloadDialog;
 import org.openstreetmap.josm.gui.help.Helpful;
+import org.openstreetmap.josm.gui.mappaint.MapPaintStyles;
 import org.openstreetmap.josm.gui.preferences.projection.ProjectionPreference;
 import org.openstreetmap.josm.tools.Predicate;
 import org.openstreetmap.josm.tools.Utils;
@@ -82,6 +83,15 @@ public class NavigatableComponent extends JComponent implements Helpful {
          */
         void systemOfMeasurementChanged(String oldSoM, String newSoM);
     }
+
+    public Predicate<OsmPrimitive> isSelectablePredicate = new Predicate<OsmPrimitive>() {
+        @Override
+        public boolean evaluate(OsmPrimitive prim) {
+            if (!prim.isSelectable()) return false;
+            // if it isn't displayed on screen, you cannot click on it
+            return !MapPaintStyles.getStyles().get(prim, getDist100Pixel(), NavigatableComponent.this).isEmpty();
+        }
+    };
 
     public static final IntegerProperty PROP_SNAP_DISTANCE = new IntegerProperty("mappaint.node.snap-distance", 10);
 
