JOSM/josm

commit 108fb6da3e2138aa8af4dc936911e1907fb11b4e

fix #10175 - ask for JOSM update in case of crash with old version
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7401 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/ExtendedDialog.java
@@ -27,11 +27,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
-import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import org.openstreetmap.josm.gui.help.HelpBrowser;
 import org.openstreetmap.josm.gui.help.HelpUtil;
+import org.openstreetmap.josm.gui.util.GuiHelper;
 import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
@@ -537,7 +537,7 @@ public class ExtendedDialog extends JDialog {
 
     /**
      * Sets the button that will react to ENTER.
-     * @param defaultButtonIdx The button index (starts to )
+     * @param defaultButtonIdx The button index (starts to 1)
      * @return {@code this}
      */
     public ExtendedDialog setDefaultButton(int defaultButtonIdx) {
@@ -557,12 +557,13 @@ public class ExtendedDialog extends JDialog {
     }
 
     /**
-     * Don't focus the "do not show this again" check box, but the default button.
+     * Always makes sure the default button has initial focus.
      */
     protected void fixFocus() {
-        if (toggleable && defaultButton != null) {
-            SwingUtilities.invokeLater(new Runnable() {
-                @Override public void run() {
+        if (defaultButton != null) {
+            GuiHelper.runInEDT(new Runnable() {
+                @Override
+                public void run() {
                     defaultButton.requestFocusInWindow();
                 }
             });

src/org/openstreetmap/josm/tools/BugReportExceptionHandler.java
@@ -24,6 +24,7 @@ import javax.swing.SwingUtilities;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.ShowStatusReportAction;
+import org.openstreetmap.josm.data.Version;
 import org.openstreetmap.josm.gui.ExtendedDialog;
 import org.openstreetmap.josm.gui.preferences.plugin.PluginPreference;
 import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
@@ -55,7 +56,7 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
 
         @Override
         public void run() {
-         // Give the user a chance to deactivate the plugin which threw the exception (if it was thrown from a plugin)
+            // Give the user a chance to deactivate the plugin which threw the exception (if it was thrown from a plugin)
             final PluginDownloadTask pluginDownloadTask = PluginHandler.updateOrdisablePluginAfterException(e);
 
             SwingUtilities.invokeLater(new Runnable() {
@@ -63,15 +64,41 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
                 public void run() {
                     // Then ask for submitting a bug report, for exceptions thrown from a plugin too, unless updated to a new version
                     if (pluginDownloadTask == null) {
-                        ExtendedDialog ed = new ExtendedDialog(Main.parent, tr("Unexpected Exception"), new String[] {tr("Do nothing"), tr("Report Bug")});
+                        String[] buttonTexts = new String[] {tr("Do nothing"), tr("Report Bug")};
+                        String[] buttonIcons = new String[] {"cancel", "bug"};
+                        int defaultButtonIdx = 1;
+                        String message = tr("An unexpected exception occurred.<br>" +
+                                "This is always a coding error. If you are running the latest<br>" +
+                                "version of JOSM, please consider being kind and file a bug report."
+                                );
+                        // Check user is running current tested version, the error may already be fixed
+                        int josmVersion = Version.getInstance().getVersion();
+                        if (josmVersion != Version.JOSM_UNKNOWN_VERSION) {
+                            try {
+                                int latestVersion = Integer.parseInt(new WikiReader().
+                                        read(Main.getJOSMWebsite()+"/wiki/TestedVersion?format=txt").trim());
+                                if (latestVersion > josmVersion) {
+                                    buttonTexts = new String[] {tr("Do nothing"), tr("Update JOSM"), tr("Report Bug")};
+                                    buttonIcons = new String[] {"cancel", "download", "bug"};
+                                    defaultButtonIdx = 2;
+                                    message = tr("An unexpected exception occurred. This is always a coding error.<br><br>" +
+                                            "However, you are running an old version of JOSM ({0}),<br>" +
+                                            "instead of using the current tested version (<b>{1}</b>).<br><br>"+
+                                            "<b>Please update JOSM</b> before considering to file a bug report.",
+                                            String.valueOf(josmVersion), String.valueOf(latestVersion));
+                                }
+                            } catch (IOException | NumberFormatException e) {
+                                Main.warn("Unable to detect latest version of JOSM: "+e.getMessage());
+                            }
+                        }
+                        // Show dialog
+                        ExtendedDialog ed = new ExtendedDialog(Main.parent, tr("Unexpected Exception"), buttonTexts);
+                        ed.setButtonIcons(buttonIcons);
                         ed.setIcon(JOptionPane.ERROR_MESSAGE);
+                        ed.setCancelButton(1);
+                        ed.setDefaultButton(defaultButtonIdx);
                         JPanel pnl = new JPanel(new GridBagLayout());
-                        pnl.add(new JLabel(
-                                "<html>" + tr("An unexpected exception occurred.<br>" +
-                                              "This is always a coding error. If you are running the latest<br>" +
-                                              "version of JOSM, please consider being kind and file a bug report."
-                                              )
-                                         + "</html>"), GBC.eol());
+                        pnl.add(new JLabel("<html>" + message + "</html>"), GBC.eol());
                         JCheckBox cbSuppress = null;
                         if (exceptionCounter > 1) {
                             cbSuppress = new JCheckBox(tr("Suppress further error dialogs for this session."));
@@ -82,8 +109,20 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
                         if (cbSuppress != null && cbSuppress.isSelected()) {
                             suppressExceptionDialogs = true;
                         }
-                        if (ed.getValue() != 2) return;
-                        askForBugReport(e);
+                        if (ed.getValue() <= 1) {
+                            // "Do nothing"
+                            return;
+                        } else if (ed.getValue() < buttonTexts.length) {
+                            // "Update JOSM"
+                            try {
+                                Main.platform.openUrl(Main.getJOSMWebsite());
+                            } catch (IOException e) {
+                                Main.warn("Unable to access JOSM website: "+e.getMessage());
+                            }
+                        } else {
+                            // "Report bug"
+                            askForBugReport(e);
+                        }
                     } else {
                         // Ask for restart to install new plugin
                         PluginPreference.notifyDownloadResults(Main.parent, pluginDownloadTask);

src/org/openstreetmap/josm/tools/WikiReader.java
@@ -35,20 +35,28 @@ public class WikiReader {
     /**
      * Read the page specified by the url and return the content.
      *
-     * If the url is within the baseurl path, parse it as an trac wikipage and replace relative
-     * pathes etc..
+     * If the url is within the baseurl path, parse it as an trac wikipage and replace relative paths etc..
+     * @param url the URL to read
+     * @return The page as string
      *
      * @throws IOException Throws, if the page could not be loaded.
      */
     public String read(String url) throws IOException {
         URL u = new URL(url);
         try (BufferedReader in = Utils.openURLReader(u)) {
-            if (url.startsWith(baseurl) && !url.endsWith("?format=txt"))
+            boolean txt = url.endsWith("?format=txt");
+            if (url.startsWith(baseurl) && !txt)
                 return readFromTrac(in, u);
-            return readNormal(in);
+            return readNormal(in, !txt);
         }
     }
 
+    /**
+     * Reads the localized version of the given wiki page.
+     * @param text The page title, without locale prefix
+     * @return the localized version of the given wiki page
+     * @throws IOException if any I/O error occurs
+     */
     public String readLang(String text) throws IOException {
         String languageCode;
         String res = "";
@@ -88,14 +96,14 @@ public class WikiReader {
         }
     }
 
-    private String readNormal(BufferedReader in) throws IOException {
+    private String readNormal(BufferedReader in, boolean html) throws IOException {
         StringBuilder b = new StringBuilder();
         for (String line = in.readLine(); line != null; line = in.readLine()) {
             if (!line.contains("[[TranslatedPages]]")) {
                 b.append(line.replaceAll(" />", ">")).append("\n");
             }
         }
-        return "<html>" + b + "</html>";
+        return html ? "<html>" + b + "</html>" : b.toString();
     }
 
     protected String readFromTrac(BufferedReader in, URL url) throws IOException {

commit 3e909a719998beb53563adb3948bd84d6ed19212

fix focus issues (regression from r7401)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@7407 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/ExtendedDialog.java
@@ -87,6 +87,7 @@ public class ExtendedDialog extends JDialog {
     protected JButton defaultButton = null;
     private Icon icon;
     private boolean modal;
+    private boolean focusOnDefaultButton = false;
 
     /** true, if the dialog should include a help button */
     private boolean showHelpButton;
@@ -277,7 +278,10 @@ public class ExtendedDialog extends JDialog {
         if (defaultButton != null) {
             getRootPane().setDefaultButton(defaultButton);
         }
-        fixFocus();
+        // Don't focus the "do not show this again" check box, but the default button.
+        if (toggleable || focusOnDefaultButton) {
+            requestFocusToDefaultButton();
+        }
         setVisible(true);
         toggleSaveState();
         return this;
@@ -557,9 +561,15 @@ public class ExtendedDialog extends JDialog {
     }
 
     /**
-     * Always makes sure the default button has initial focus.
+     * Makes default button request initial focus or not.
+     * @param focus {@code true} to make default button request initial focus
+     * @since 7407
      */
-    protected void fixFocus() {
+    public void setFocusOnDefaultButton(boolean focus) {
+        focusOnDefaultButton = focus;
+    }
+
+    private void requestFocusToDefaultButton() {
         if (defaultButton != null) {
             GuiHelper.runInEDT(new Runnable() {
                 @Override

src/org/openstreetmap/josm/tools/BugReportExceptionHandler.java
@@ -105,6 +105,7 @@ public final class BugReportExceptionHandler implements Thread.UncaughtException
                             pnl.add(cbSuppress, GBC.eol());
                         }
                         ed.setContent(pnl);
+                        ed.setFocusOnDefaultButton(true);
                         ed.showDialog();
                         if (cbSuppress != null && cbSuppress.isSelected()) {
                             suppressExceptionDialogs = true;