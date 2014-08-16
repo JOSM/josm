acabfcc34f813b93cf1689241110691f40db9932
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-08-16 01:49:41 +0000

    fix #10055 - Display preset dialog on preset link even without dataset
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@7413 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/tagging/TaggingPreset.java
+++ src/org/openstreetmap/josm/gui/tagging/TaggingPreset.java
@@ -12,6 +12,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -32,18 +33,18 @@ import org.openstreetmap.josm.actions.search.SearchCompiler.Match;
 import org.openstreetmap.josm.command.ChangePropertyCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
-import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.Tag;
-import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.gui.ExtendedDialog;
 import org.openstreetmap.josm.gui.MapView;
 import org.openstreetmap.josm.gui.dialogs.relation.RelationEditor;
 import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.gui.preferences.ToolbarPreferences;
 import org.openstreetmap.josm.gui.tagging.TaggingPresetItems.Link;
+import org.openstreetmap.josm.gui.tagging.TaggingPresetItems.PresetLink;
 import org.openstreetmap.josm.gui.tagging.TaggingPresetItems.Role;
 import org.openstreetmap.josm.gui.tagging.TaggingPresetItems.Roles;
 import org.openstreetmap.josm.gui.util.GuiHelper;
@@ -206,11 +207,11 @@ public class TaggingPreset extends AbstractAction implements MapView.LayerChange
         if (data == null)
             return null;
         PresetPanel p = new PresetPanel();
-        LinkedList<TaggingPresetItem> l = new LinkedList<>();
-        LinkedList<TaggingPresetItem> presetLink = new LinkedList<>();
-        if(types != null){
+        List<Link> l = new LinkedList<>();
+        List<PresetLink> presetLink = new LinkedList<>();
+        if (types != null){
             JPanel pp = new JPanel();
-            for(TaggingPresetType t : types){
+            for (TaggingPresetType t : types) {
                 JLabel la = new JLabel(ImageProvider.get(t.getIconName()));
                 la.setToolTipText(tr("Elements of type {0} are supported.", tr(t.getName())));
                 pp.add(la);
@@ -223,13 +224,13 @@ public class TaggingPreset extends AbstractAction implements MapView.LayerChange
 
         boolean presetInitiallyMatches = !selected.isEmpty() && Utils.forAll(selected, this);
         JPanel items = new JPanel(new GridBagLayout());
-        for (TaggingPresetItem i : data){
-            if(i instanceof Link) {
-                l.add(i);
-            } else if (i instanceof TaggingPresetItems.PresetLink) {
-                presetLink.add(i);
+        for (TaggingPresetItem i : data) {
+            if (i instanceof Link) {
+                l.add((Link) i);
+            } else if (i instanceof PresetLink) {
+                presetLink.add((PresetLink) i);
             } else {
-                if(i.addToPanel(items, selected, presetInitiallyMatches)) {
+                if (i.addToPanel(items, selected, presetInitiallyMatches)) {
                     p.hasElements = true;
                 }
             }
@@ -242,13 +243,13 @@ public class TaggingPreset extends AbstractAction implements MapView.LayerChange
         // add PresetLink
         if (!presetLink.isEmpty()) {
             p.add(new JLabel(tr("Edit also …")), GBC.eol().insets(0, 8, 0, 0));
-            for(TaggingPresetItem link : presetLink) {
+            for (PresetLink link : presetLink) {
                 link.addToPanel(p, selected, presetInitiallyMatches);
             }
         }
 
         // add Link
-        for(TaggingPresetItem link : l) {
+        for (Link link : l) {
             link.addToPanel(p, selected, presetInitiallyMatches);
         }
 
@@ -260,8 +261,8 @@ public class TaggingPreset extends AbstractAction implements MapView.LayerChange
     }
 
     public boolean isShowable() {
-        for(TaggingPresetItem i : data) {
-            if(!(i instanceof TaggingPresetItems.Optional || i instanceof TaggingPresetItems.Space || i instanceof TaggingPresetItems.Key))
+        for (TaggingPresetItem i : data) {
+            if (!(i instanceof TaggingPresetItems.Optional || i instanceof TaggingPresetItems.Space || i instanceof TaggingPresetItems.Key))
                 return true;
         }
         return false;
@@ -281,11 +282,22 @@ public class TaggingPreset extends AbstractAction implements MapView.LayerChange
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (Main.main == null) return;
-        if (Main.main.getCurrentDataSet() == null) return;
+        if (Main.main == null) {
+            return;
+        }
+        DataSet ds = Main.main.getCurrentDataSet();
+        Collection<OsmPrimitive> participants = Collections.emptyList();
+        if (Main.main != null && ds != null) {
+            participants = ds.getSelected();
+        }
 
-        Collection<OsmPrimitive> sel = createSelection(Main.main.getCurrentDataSet().getSelected());
+        // Display dialog even if no data layer (used by preset-tagging-tester plugin)
+        Collection<OsmPrimitive> sel = createSelection(participants);
         int answer = showDialog(sel, supportsRelation());
+
+        if (ds == null) {
+            return;
+        }
 
         if (!sel.isEmpty() && answer == DIALOG_ANSWER_APPLY) {
             Command cmd = createCommand(sel, getChangedTags());
@@ -298,7 +310,7 @@ public class TaggingPreset extends AbstractAction implements MapView.LayerChange
             for(Tag t : getChangedTags()) {
                 r.put(t.getKey(), t.getValue());
             }
-            for (OsmPrimitive osm : Main.main.getCurrentDataSet().getSelected()) {
+            for (OsmPrimitive osm : ds.getSelected()) {
                 String role = suggestRoleForOsmPrimitive(osm);
                 RelationMember rm = new RelationMember(role == null ? "" : role, osm);
                 r.addMember(rm);
@@ -311,8 +323,7 @@ public class TaggingPreset extends AbstractAction implements MapView.LayerChange
                 }
             });
         }
-        Main.main.getCurrentDataSet().setSelected(Main.main.getCurrentDataSet().getSelected()); // force update
-
+        ds.setSelected(ds.getSelected()); // force update
     }
 
     private static class PresetDialog extends ExtendedDialog {
