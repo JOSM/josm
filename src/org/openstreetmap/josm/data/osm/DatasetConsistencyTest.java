70b87e689d42635e3f68b745ed338ae02e408886
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-09-05 12:14:11 +0000

    see #10483 - print elapsed time of data consistency test + update javadoc
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@7500 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/osm/DatasetConsistencyTest.java
+++ src/org/openstreetmap/josm/data/osm/DatasetConsistencyTest.java
@@ -1,17 +1,21 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.data.osm;
 
+import static org.openstreetmap.josm.tools.I18n.tr;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 
+import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.tools.Utils;
 
 /**
- * This class can be used to run consistency tests on dataset. Any errors found will be written to provided PrintWriter
+ * This class can be used to run consistency tests on dataset. Any errors found will be written to provided PrintWriter.
  * <br>
- * Texts here should not be translated because they're not intended for users but for josm developers
- *
+ * Texts here should not be translated because they're not intended for users but for josm developers.
+ * @since 2500
  */
 public class DatasetConsistencyTest {
 
@@ -20,6 +24,11 @@ public class DatasetConsistencyTest {
     private final PrintWriter writer;
     private int errorCount;
 
+    /**
+     * Constructs a new {@code DatasetConsistencyTest}.
+     * @param dataSet The dataset to test
+     * @param writer The writer used to write results
+     */
     public DatasetConsistencyTest(DataSet dataSet, Writer writer) {
         this.dataSet = dataSet;
         this.writer = new PrintWriter(writer);
@@ -32,11 +41,15 @@ public class DatasetConsistencyTest {
         }
     }
 
+    /**
+     * Checks that parent primitive is referred from its child members
+     */
     public void checkReferrers() {
+        long startTime = System.currentTimeMillis();
         // It's also error when referred primitive's dataset is null but it's already covered by referredPrimitiveNotInDataset check
-        for (Way way:dataSet.getWays()) {
+        for (Way way : dataSet.getWays()) {
             if (!way.isDeleted()) {
-                for (Node n:way.getNodes()) {
+                for (Node n : way.getNodes()) {
                     if (n.getDataSet() != null && !n.getReferrers().contains(way)) {
                         printError("WAY NOT IN REFERRERS", "%s is part of %s but is not in referrers", n, way);
                     }
@@ -44,39 +57,54 @@ public class DatasetConsistencyTest {
             }
         }
 
-        for (Relation relation:dataSet.getRelations()) {
+        for (Relation relation : dataSet.getRelations()) {
             if (!relation.isDeleted()) {
-                for (RelationMember m:relation.getMembers()) {
+                for (RelationMember m : relation.getMembers()) {
                     if (m.getMember().getDataSet() != null && !m.getMember().getReferrers().contains(relation)) {
                         printError("RELATION NOT IN REFERRERS", "%s is part of %s but is not in referrers", m.getMember(), relation);
                     }
                 }
             }
         }
+        printElapsedTime(startTime);
     }
 
+    /**
+     * Checks for womplete ways with incomplete nodes.
+     */
     public void checkCompleteWaysWithIncompleteNodes() {
-        for (Way way:dataSet.getWays()) {
+        long startTime = System.currentTimeMillis();
+        for (Way way : dataSet.getWays()) {
             if (way.isUsable()) {
-                for (Node node:way.getNodes()) {
+                for (Node node : way.getNodes()) {
                     if (node.isIncomplete()) {
                         printError("USABLE HAS INCOMPLETE", "%s is usable but contains incomplete node '%s'", way, node);
                     }
                 }
             }
         }
+        printElapsedTime(startTime);
     }
 
+    /**
+     * Checks for complete nodes without coordinates.
+     */
     public void checkCompleteNodesWithoutCoordinates() {
-        for (Node node:dataSet.getNodes()) {
+        long startTime = System.currentTimeMillis();
+        for (Node node : dataSet.getNodes()) {
             if (!node.isIncomplete() && node.isVisible() && (node.getCoor() == null || node.getEastNorth() == null)) {
                 printError("COMPLETE WITHOUT COORDINATES", "%s is not incomplete but has null coordinates", node);
             }
         }
+        printElapsedTime(startTime);
     }
 
+    /**
+     * Checks that nodes can be retrieved through their coordinates.
+     */
     public void searchNodes() {
-        for (Node n:dataSet.getNodes()) {
+        long startTime = System.currentTimeMillis();
+        for (Node n : dataSet.getNodes()) {
             if (!n.isIncomplete() && !n.isDeleted()) {
                 LatLon c = n.getCoor();
                 if (c != null) {
@@ -87,14 +115,20 @@ public class DatasetConsistencyTest {
                 }
             }
         }
+        printElapsedTime(startTime);
     }
 
+    /**
+     * Checks that ways can be retrieved through their bounding box.
+     */
     public void searchWays() {
-        for (Way w:dataSet.getWays()) {
+        long startTime = System.currentTimeMillis();
+        for (Way w : dataSet.getWays()) {
             if (!w.isIncomplete() && !w.isDeleted() && w.getNodesCount() >= 2 && !dataSet.searchWays(w.getBBox()).contains(w)) {
                 printError("SEARCH WAYS", "%s not found using Dataset.searchWays()", w);
             }
         }
+        printElapsedTime(startTime);
     }
 
     private void checkReferredPrimitive(OsmPrimitive primitive, OsmPrimitive parent) {
@@ -111,33 +145,56 @@ public class DatasetConsistencyTest {
         }
     }
 
+    /**
+     * Checks that referred primitives are present in dataset.
+     */
     public void referredPrimitiveNotInDataset() {
-        for (Way way:dataSet.getWays()) {
-            for (Node node:way.getNodes()) {
+        long startTime = System.currentTimeMillis();
+        for (Way way : dataSet.getWays()) {
+            for (Node node : way.getNodes()) {
                 checkReferredPrimitive(node, way);
             }
         }
 
-        for (Relation relation:dataSet.getRelations()) {
-            for (RelationMember member:relation.getMembers()) {
+        for (Relation relation : dataSet.getRelations()) {
+            for (RelationMember member : relation.getMembers()) {
                 checkReferredPrimitive(member.getMember(), relation);
             }
         }
+        printElapsedTime(startTime);
     }
 
-
+    /**
+     * Checks for zero and one-node ways.
+     */
     public void checkZeroNodesWays() {
-        for (Way way:dataSet.getWays()) {
+        long startTime = System.currentTimeMillis();
+        for (Way way : dataSet.getWays()) {
             if (way.isUsable() && way.getNodesCount() == 0) {
                 printError("WARN - ZERO NODES", "Way %s has zero nodes", way);
             } else if (way.isUsable() && way.getNodesCount() == 1) {
                 printError("WARN - NO NODES", "Way %s has only one node", way);
             }
         }
+        printElapsedTime(startTime);
     }
 
+    private void printElapsedTime(long startTime) {
+        if (Main.isDebugEnabled()) {
+            StackTraceElement item = Thread.currentThread().getStackTrace()[2];
+            String operation = getClass().getSimpleName() + "." + item.getMethodName();
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            Main.debug(tr("Test ''{0}'' completed in {1}",
+                    operation, Utils.getDurationString(elapsedTime)));
+        }
+    }
+
+    /**
+     * Runs test.
+     */
     public void runTest() {
         try {
+            long startTime = System.currentTimeMillis();
             referredPrimitiveNotInDataset();
             checkReferrers();
             checkCompleteWaysWithIncompleteNodes();
@@ -145,19 +202,25 @@ public class DatasetConsistencyTest {
             searchNodes();
             searchWays();
             checkZeroNodesWays();
+            printElapsedTime(startTime);
             if (errorCount > MAX_ERRORS) {
                 writer.println((errorCount - MAX_ERRORS) + " more...");
             }
+
         } catch (Exception e) {
             writer.println("Exception during dataset integrity test:");
             e.printStackTrace(writer);
         }
     }
 
+    /**
+     * Runs test on the given dataset.
+     * @param dataSet the dataset to test
+     * @return the errors as string
+     */
     public static String runTests(DataSet dataSet) {
         StringWriter writer = new StringWriter();
         new DatasetConsistencyTest(dataSet, writer).runTest();
         return writer.toString();
     }
-
 }
