78d8f3854197f4bb71a4e3ad51171842db3b78e2
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-10-06 17:17:30 +0000

    fix #10598 - style icon not displayed properly in toolbar (and menu)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@7602 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/mappaint/MapPaintMenu.java
+++ src/org/openstreetmap/josm/gui/mappaint/MapPaintMenu.java
@@ -32,7 +32,7 @@ public class MapPaintMenu extends JMenu implements MapPaintSylesUpdateListener {
         private JCheckBoxMenuItem button;
 
         public MapPaintAction(StyleSource style) {
-            super(style.getDisplayString(), style.icon == null ? null : ImageProvider.getIfAvailable(style.icon),
+            super(style.getDisplayString(), style.getIcon(),
                     tr("Select the map painting styles"), null, true, "mappaint/" + style.getDisplayString(), true);
             this.button = new StayOpenCheckBoxMenuItem(this);
             this.style = style;
