2c57564fca1779c75048448d92a99c1d5d8d4806
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2014-12-21 15:23:21 +0000

    cleanup of NoteImporter
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@7870 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/io/NoteImporter.java
+++ src/org/openstreetmap/josm/io/NoteImporter.java
@@ -4,7 +4,6 @@ package org.openstreetmap.josm.io;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -15,11 +14,11 @@ import org.openstreetmap.josm.data.notes.Note;
 import org.openstreetmap.josm.gui.layer.NoteLayer;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.gui.util.GuiHelper;
-import org.openstreetmap.josm.tools.Utils;
 import org.xml.sax.SAXException;
 
 /**
  * File importer that reads note dump files (*.osn and .osn.bz2)
+ * @since 7538
  */
 public class NoteImporter extends FileImporter {
 
@@ -32,32 +31,24 @@ public class NoteImporter extends FileImporter {
     }
 
     @Override
-    public void importData(File file, ProgressMonitor progressMonitor) throws IOException {
-        Main.debug("importing notes file " + file.getAbsolutePath());
-        final String fileName = file.getName();
-        InputStream is;
-        if(fileName.endsWith(".bz2")) {
-            is = Utils.getBZip2InputStream(new FileInputStream(file));
-        } else {
-            is = new FileInputStream(file);
+    public void importData(final File file, ProgressMonitor progressMonitor) throws IOException {
+        if (Main.isDebugEnabled()) {
+            Main.debug("importing notes file " + file.getAbsolutePath());
         }
-        NoteReader reader = new NoteReader(is);
-        try {
-            final List<Note> fileNotes = reader.parse();
+        try (InputStream is = Compression.getUncompressedFileInputStream(file)) {
+            final List<Note> fileNotes = new NoteReader(is).parse();
 
             List<NoteLayer> noteLayers = null;
             if (Main.map != null) {
                 noteLayers = Main.map.mapView.getLayersOfType(NoteLayer.class);
             }
-            if (noteLayers != null && noteLayers.size() > 0) {
-                NoteLayer layer = noteLayers.get(0);
-                layer.getNoteData().addNotes(fileNotes);
+            if (noteLayers != null && !noteLayers.isEmpty()) {
+                noteLayers.get(0).getNoteData().addNotes(fileNotes);
             } else {
                 GuiHelper.runInEDT(new Runnable() {
                     @Override
                     public void run() {
-                        NoteLayer layer = new NoteLayer(fileNotes, fileName);
-                        Main.main.addLayer(layer);
+                        Main.main.addLayer(new NoteLayer(fileNotes, file.getName()));
                     }
                 });
             }
@@ -66,9 +57,5 @@ public class NoteImporter extends FileImporter {
             Main.error(e, true);
             throw new IOException(e.getMessage(), e);
         }
-        finally {
-            Utils.close(is);
-        }
     }
-
 }
