26bc603aacea6345dd394a6204f0e0792c61dc94
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-04-18 00:23:03 +0000

    see #11147 see #11321 - Add "Download notes in current view" to file menu and notes toggle dialog
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@8212 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/actions/DownloadNotesInViewAction.java
@@ -0,0 +1,46 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.actions;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.event.ActionEvent;
+import java.util.concurrent.Future;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.actions.downloadtasks.DownloadNotesTask;
+import org.openstreetmap.josm.actions.downloadtasks.PostDownloadHandler;
+import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.io.OnlineResource;
+
+/**
+ * Action that downloads the notes within the current view from the server.
+ *
+ * No interaction is required.
+ */
+public class DownloadNotesInViewAction extends JosmAction {
+
+    private DownloadNotesInViewAction(String iconName) {
+        super(tr("Download notes in current view"), iconName, tr("Download notes in current view"), null, false);
+    }
+
+    public static DownloadNotesInViewAction newActionWithNoteIcon() {
+        return new DownloadNotesInViewAction("dialogs/notes/note_open");
+    }
+
+    public static DownloadNotesInViewAction newActionWithDownloadIcon() {
+        return new DownloadNotesInViewAction("download");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        final Bounds bounds = Main.map.mapView.getRealBounds();
+        DownloadNotesTask task = new DownloadNotesTask();
+        Future<?> future = task.download(false, bounds, null);
+        Main.worker.submit(new PostDownloadHandler(task, future));
+    }
+
+    @Override
+    protected void updateEnabledState() {
+        setEnabled(Main.map != null && Main.map.mapView != null && !Main.isOffline(OnlineResource.OSM_API));
+    }
+}
