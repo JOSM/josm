fb55cb12c70aef827543ae0f99b4b059049f4722
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-04-19 22:15:43 +0000

    Open Location: enable loading of nodes/ways/relations/changesets from OSM website URL
    Up to now, only corresponding API URLs have been supported.
    
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@8240 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmIdTask.java
@@ -0,0 +1,46 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.actions.downloadtasks;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.Collections;
+import java.util.concurrent.Future;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.PrimitiveId;
+import org.openstreetmap.josm.data.osm.SimplePrimitiveId;
+import org.openstreetmap.josm.gui.io.DownloadPrimitivesWithReferrersTask;
+import org.openstreetmap.josm.gui.progress.ProgressMonitor;
+
+public class DownloadOsmIdTask extends DownloadOsmTask {
+
+    private static final String URL_ID_PATTERN = "https?://www\\.(osm|openstreetmap)\\.org/(node|way|relation)/(\\p{Digit}+).*";
+
+    @Override
+    public String[] getPatterns() {
+        return new String[]{URL_ID_PATTERN};
+    }
+
+    @Override
+    public Future<?> loadUrl(boolean newLayer, String url, ProgressMonitor progressMonitor) {
+        final Matcher matcher = Pattern.compile(URL_ID_PATTERN).matcher(url);
+        if (matcher.matches()) {
+            final OsmPrimitiveType type = OsmPrimitiveType.from(matcher.group(2));
+            final long id = Long.parseLong(matcher.group(3));
+            final PrimitiveId primitiveId = new SimplePrimitiveId(id, type);
+            final DownloadPrimitivesWithReferrersTask downloadTask = new DownloadPrimitivesWithReferrersTask(
+                    newLayer, Collections.singletonList(primitiveId), true, true, null, null);
+            return Main.worker.submit(downloadTask);
+        } else {
+            throw new IllegalStateException("Failed to parse id from " + url);
+        }
+    }
+
+    @Override
+    public String getTitle() {
+        return tr("Download OSM object by ID");
+    }
+}
