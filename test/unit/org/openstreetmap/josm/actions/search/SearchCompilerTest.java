aefd85089702a8bfffaed8812fe28c663c28a19c
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-04-23 16:59:15 +0000

    fix #11358 - Search: allow negative indices for `nth:` to search backward
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@8250 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/actions/search/SearchCompilerTest.java
+++ test/unit/org/openstreetmap/josm/actions/search/SearchCompilerTest.java
@@ -1,11 +1,15 @@
 package org.openstreetmap.josm.actions.search;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openstreetmap.josm.JOSMFixture;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Way;
 
 public class SearchCompilerTest {
 
@@ -60,6 +64,36 @@ public class SearchCompilerTest {
         final SearchCompiler.Match c4 = SearchCompiler.compile("\"start_date\"<1960", false, false);
         Assert.assertTrue(c4.match(newPrimitive("start_date", "1950-01-01")));
         Assert.assertFalse(c4.match(newPrimitive("start_date", "2000")));
+
+    }
+
+    @Test
+    public void testNth() throws Exception {
+        final DataSet dataSet = new DataSet();
+        final Way way = new Way();
+        final Node node0 = new Node(new LatLon(1, 1));
+        final Node node1 = new Node(new LatLon(2, 2));
+        final Node node2 = new Node(new LatLon(3, 3));
+        dataSet.addPrimitive(way);
+        dataSet.addPrimitive(node0);
+        dataSet.addPrimitive(node1);
+        dataSet.addPrimitive(node2);
+        way.addNode(node0);
+        way.addNode(node1);
+        way.addNode(node2);
+        Assert.assertFalse(SearchCompiler.compile("nth:2", false, false).match(node1));
+        Assert.assertTrue(SearchCompiler.compile("nth:1", false, false).match(node1));
+        Assert.assertFalse(SearchCompiler.compile("nth:0", false, false).match(node1));
+        Assert.assertTrue(SearchCompiler.compile("nth:0", false, false).match(node0));
+        Assert.assertTrue(SearchCompiler.compile("nth:2", false, false).match(node2));
+        Assert.assertTrue(SearchCompiler.compile("nth:-1", false, false).match(node2));
+        Assert.assertTrue(SearchCompiler.compile("nth:-2", false, false).match(node1));
+        Assert.assertTrue(SearchCompiler.compile("nth:-3", false, false).match(node0));
+    }
+
+    @Test
+    public void testNthParseNegative() throws Exception {
+        Assert.assertThat(SearchCompiler.compile("nth:-1", false, false).toString(), CoreMatchers.is("Nth{nth=-1, modulo=false}"));
 
     }
 }
