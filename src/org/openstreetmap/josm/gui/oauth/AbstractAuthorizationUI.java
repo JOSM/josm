b57ea2191c62ecfc65f9ca236a33980b352fe28a
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-05-02 13:38:29 +0000

    revert build breaking change
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@8312 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/oauth/AbstractAuthorizationUI.java
+++ src/org/openstreetmap/josm/gui/oauth/AbstractAuthorizationUI.java
@@ -7,8 +7,6 @@ import org.openstreetmap.josm.data.oauth.OAuthToken;
 import org.openstreetmap.josm.gui.widgets.VerticallyScrollablePanel;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 
-import com.sun.org.apache.xerces.internal.utils.Objects;
-
 /**
  * This is the abstract base class for the three authorisation UIs.
  *
@@ -22,7 +20,7 @@ public abstract class AbstractAuthorizationUI extends VerticallyScrollablePanel 
 
     private String apiUrl;
     private final AdvancedOAuthPropertiesPanel pnlAdvancedProperties;
-    private transient OAuthToken accessToken;
+    private OAuthToken accessToken;
 
     protected void fireAccessTokenChanged(OAuthToken oldValue, OAuthToken newValue) {
         firePropertyChange(ACCESS_TOKEN_PROP, oldValue, newValue);
@@ -100,7 +98,7 @@ public abstract class AbstractAuthorizationUI extends VerticallyScrollablePanel 
             fireAccessTokenChanged(oldValue, this.accessToken);
         } else if (oldValue == null && this.accessToken == null) {
             // no change - don't fire an event
-        } else if (!Objects.equals(oldValue, this.accessToken)) {
+        } else if (! oldValue.equals(this.accessToken)) {
             fireAccessTokenChanged(oldValue, this.accessToken);
         }
     }
