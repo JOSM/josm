0068d6d654b2d16f58bd954893137d7fd09e4b74
JOSM GitHub mirror <openstreetmap@v.nix.is>
2015-05-19 02:22:49 +0200

    josm-mirror: bumped externals

---
+++ src/org/openstreetmap/gui/jmapviewer/tilesources/AbstractTMSTileSource.java
@@ -0,0 +1,151 @@
+// License: GPL. For details, see Readme.txt file.
+package org.openstreetmap.gui.jmapviewer.tilesources;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.openstreetmap.gui.jmapviewer.OsmMercator;
+
+public abstract class AbstractTMSTileSource extends AbstractTileSource {
+
+    protected String name;
+    protected String baseUrl;
+    protected String id;
+    private Map<String, String> noTileHeaders;
+    protected int tileSize;
+    protected OsmMercator osmMercator;
+
+    public AbstractTMSTileSource(TileSourceInfo info) {
+        this.name = info.getName();
+        this.baseUrl = info.getUrl();
+        if(baseUrl != null && baseUrl.endsWith("/")) {
+            baseUrl = baseUrl.substring(0,baseUrl.length()-1);
+        }
+        this.id = info.getUrl();
+        this.noTileHeaders = info.getNoTileHeaders();
+        this.tileSize = info.getTileSize();
+        osmMercator = new OsmMercator(this.tileSize);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public int getMaxZoom() {
+        return 21;
+    }
+
+    @Override
+    public int getMinZoom() {
+        return 0;
+    }
+
+    public String getExtension() {
+        return "png";
+    }
+
+    /**
+     * @throws IOException when subclass cannot return the tile URL
+     */
+    public String getTilePath(int zoom, int tilex, int tiley) throws IOException {
+        return "/" + zoom + "/" + tilex + "/" + tiley + "." + getExtension();
+    }
+
+    public String getBaseUrl() {
+        return this.baseUrl;
+    }
+
+    @Override
+    public String getTileUrl(int zoom, int tilex, int tiley) throws IOException {
+        return this.getBaseUrl() + getTilePath(zoom, tilex, tiley);
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    @Override
+    public String getTileType() {
+        return "png";
+    }
+
+    /*
+     * Most tilesources use OsmMercator projection.
+     */
+    @Override
+    public int getTileSize() {
+        return tileSize;
+    }
+
+    @Override
+    public double getDistance(double lat1, double lon1, double lat2, double lon2) {
+        return osmMercator.getDistance(lat1, lon1, lat2, lon2);
+    }
+
+    @Override
+    public int LonToX(double lon, int zoom) {
+        return (int )osmMercator.LonToX(lon, zoom);
+    }
+
+    @Override
+    public int LatToY(double lat, int zoom) {
+        return (int )osmMercator.LatToY(lat, zoom);
+    }
+
+    @Override
+    public double XToLon(int x, int zoom) {
+        return osmMercator.XToLon(x, zoom);
+    }
+
+    @Override
+    public double YToLat(int y, int zoom) {
+        return osmMercator.YToLat(y, zoom);
+    }
+
+    @Override
+    public double latToTileY(double lat, int zoom) {
+        return osmMercator.LatToY(lat, zoom) / tileSize;
+    }
+
+    @Override
+    public double lonToTileX(double lon, int zoom) {
+        return osmMercator.LonToX(lon, zoom) / tileSize;
+    }
+
+    @Override
+    public double tileYToLat(int y, int zoom) {
+        return osmMercator.YToLat(y * tileSize, zoom);
+    }
+
+    @Override
+    public double tileXToLon(int x, int zoom) {
+        return osmMercator.XToLon(x * tileSize, zoom);
+    }
+
+    @Override
+    public boolean isNoTileAtZoom(Map<String, List<String>> headers, int statusCode, byte[] content) {
+        if (noTileHeaders != null && headers != null) {
+            for (Entry<String, String> searchEntry: noTileHeaders.entrySet()) {
+                List<String> headerVals = headers.get(searchEntry.getKey());
+                if (headerVals != null) {
+                    for (String headerValue: headerVals) {
+                        if (headerValue.matches(searchEntry.getValue())) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return super.isNoTileAtZoom(headers, statusCode, content);
+    }
+}
---
+++ src/org/openstreetmap/gui/jmapviewer/tilesources/TileSourceInfo.java
@@ -0,0 +1,129 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.gui.jmapviewer.tilesources;
+
+import java.util.Map;
+
+import org.openstreetmap.gui.jmapviewer.OsmMercator;
+
+/**
+ * Data class that keeps basic information about a tile source.
+ */
+public class TileSourceInfo {
+    /** id for this imagery entry, optional at the moment */
+    protected String id;
+    /** URL of the imagery service */
+    protected  String url = null;
+
+    /** name of the imagery layer */
+    protected String name;
+
+    /** headers meaning, that there is no tile at this zoom level */
+    protected Map<String, String> notileHeaders;
+
+    /** minimum zoom level supported by the tile source */
+    protected int minZoom;
+
+    /** maximum zoom level supported by the tile source */
+    protected int maxZoom;
+
+    /** cookies that needs to be sent to tile source */
+    protected String cookies;
+
+    /** tile size of the displayed tiles */
+    private int tileSize = OsmMercator.DEFAUL_TILE_SIZE;
+
+    /**
+     * Create a TileSourceInfo class
+     *
+     * @param name
+     * @param base_url
+     * @param id
+     */
+    public TileSourceInfo(String name, String base_url, String id) {
+        this(name);
+        this.url = base_url;
+        this.id = id;
+    }
+
+    /**
+     * Create a TileSourceInfo class
+     *
+     * @param name
+     */
+    public TileSourceInfo(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Creates empty TileSourceInfo class
+     */
+    public TileSourceInfo() {
+    }
+
+    /**
+     * Request name of the tile source
+     * @return name of the tile source
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Request URL of the tile source
+     * @return url of the tile source
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Request header information for empty tiles for servers delivering such tile types
+     * @return map of headers, that when set, means that this is "no tile at this zoom level" situation
+     */
+    public Map<String, String> getNoTileHeaders() {
+        return notileHeaders;
+    }
+
+    /**
+     * Request supported minimum zoom level
+     * @return minimum zoom level supported by tile source
+     */
+    public int getMinZoom() {
+        return minZoom;
+    }
+
+    /**
+     * Request supported maximum zoom level
+     * @return maximum zoom level supported by tile source
+     */
+    public int getMaxZoom() {
+        return maxZoom;
+    }
+
+    /**
+     * Request cookies to be sent together with request
+     * @return cookies to be sent along with request to tile source
+     */
+    public String getCookies() {
+        return cookies;
+    }
+
+    /**
+     * Request tile size of this tile source
+     * @return tile size provided by this tile source
+     */
+    public int getTileSize() {
+        return tileSize;
+    }
+
+    /**
+     * Sets the tile size provided by this tile source
+     * @param tileSize
+     */
+    public void setTileSize(int tileSize) {
+        if (tileSize <= 0) {
+            throw new AssertionError("Invalid tile size: " + tileSize);
+        }
+        this.tileSize = tileSize;
+    }
+}
