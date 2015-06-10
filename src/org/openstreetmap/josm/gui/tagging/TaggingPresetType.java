367a7f5a80b7a94cf09ec44ebbb9506bf64fc508
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-06-10 22:17:21 +0000

    see #11266 - tagging presets icons: avoid unnecessary I/O access to non-existing png file + javadoc
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@8484 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/tagging/TaggingPresetType.java
+++ src/org/openstreetmap/josm/gui/tagging/TaggingPresetType.java
@@ -9,30 +9,52 @@ import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
  * @since 6068
  */
 public enum TaggingPresetType {
+    /** Node */
     NODE(/* ICON */ "Mf_node", "node"),
+    /** Way */
     WAY(/* ICON */ "Mf_way", "way"),
+    /** Relation */
     RELATION(/* ICON */ "Mf_relation", "relation"),
+    /** Closed way */
     CLOSEDWAY(/* ICON */ "Mf_closedway", "closedway");
     private final String iconName;
     private final String name;
 
     TaggingPresetType(String iconName, String name) {
-        this.iconName = iconName;
+        this.iconName = iconName + ".svg";
         this.name = name;
     }
 
+    /**
+     * Replies the SVG icon name.
+     * @return the SVG icon name
+     */
     public String getIconName() {
         return iconName;
     }
 
+    /**
+     * Replies the name, as used in XML presets.
+     * @return the name: "node", "way", "relation" or "closedway"
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Determines the {@code TaggingPresetType} of a given primitive.
+     * @param p The OSM primitive
+     * @return the {@code TaggingPresetType} of {@code p}
+     */
     public static TaggingPresetType forPrimitive(OsmPrimitive p) {
         return forPrimitiveType(p.getDisplayType());
     }
 
+    /**
+     * Determines the {@code TaggingPresetType} of a given primitive type.
+     * @param type The OSM primitive type
+     * @return the {@code TaggingPresetType} of {@code type}
+     */
     public static TaggingPresetType forPrimitiveType(OsmPrimitiveType type) {
         if (type == OsmPrimitiveType.NODE) return NODE;
         if (type == OsmPrimitiveType.WAY) return WAY;
@@ -42,6 +64,11 @@ public enum TaggingPresetType {
         throw new IllegalArgumentException("Unexpected primitive type: " + type);
     }
 
+    /**
+     * Determines the {@code TaggingPresetType} from a given string.
+     * @param type The OSM primitive type as string ("node", "way", "relation" or "closedway")
+     * @return the {@code TaggingPresetType} from {@code type}
+     */
     public static TaggingPresetType fromString(String type) {
         for (TaggingPresetType t : TaggingPresetType.values()) {
             if (t.getName().equals(type)) {
@@ -50,5 +77,4 @@ public enum TaggingPresetType {
         }
         return null;
     }
-
 }
