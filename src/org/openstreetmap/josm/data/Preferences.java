79d58ebad53c599c3b7189021f107f0c2e3c6bba
wiktorn <wiktorn@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-06-28 12:38:26 +0000

    Cast JsonValue to JsonString and use getString instead of toString on preferences deserialization. Prevents from adding additional quotation marks in values. Closes: #11611
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@8541 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/Preferences.java
+++ src/org/openstreetmap/josm/data/Preferences.java
@@ -44,6 +44,7 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
+import javax.json.JsonString;
 import javax.json.JsonValue;
 import javax.json.JsonWriter;
 import javax.swing.JOptionPane;
@@ -1360,7 +1361,13 @@ public class Preferences {
             JsonObject object = reader.readObject();
             ret = new HashMap(object.size());
             for (Entry<String, JsonValue> e: object.entrySet()) {
-                ret.put(e.getKey(), e.getValue().toString());
+                JsonValue value = e.getValue();
+                if (value instanceof JsonString) {
+                    // in some cases, when JsonValue.toString() is called, then additional quotation marks are left in value
+                    ret.put(e.getKey(), ((JsonString)value).getString());
+                } else {
+                    ret.put(e.getKey(), e.getValue().toString());
+                }
             }
         }
         return ret;
