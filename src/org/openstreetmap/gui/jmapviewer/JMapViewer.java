362be4eb8204e92a9add553dbbdb906588f670e8
JOSM GitHub mirror <openstreetmap@v.nix.is>
2015-09-08 22:30:25 +0000

    josm-mirror: bumped externals

--- src/org/openstreetmap/gui/jmapviewer/JMapViewer.java
+++ src/org/openstreetmap/gui/jmapviewer/JMapViewer.java
@@ -109,7 +109,7 @@ public class JMapViewer extends JPanel implements TileLoaderListener {
     /**
      * Creates a new {@link JMapViewer} instance.
      * @param tileCache The cache where to store tiles
-     * @depraceted @param downloadThreadCount not used anymore
+     * @deprecated @param downloadThreadCount not used anymore
      */
     @Deprecated
     public JMapViewer(TileCache tileCache, int downloadThreadCount) {
