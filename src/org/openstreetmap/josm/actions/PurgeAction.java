JOSM/josm

commit 8bafe8d3b4eb29cbaf11fc51c79aafbe17347ca6

sonar minor fixes
git-svn-id: http://josm.openstreetmap.de/svn/trunk@8905 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/PurgeAction.java
@@ -180,8 +180,8 @@ public class PurgeAction extends JosmAction {
              * Add higher level relations (list gets extended while looping over it)
              */
             List<Relation> relLst = new ArrayList<>(relSet);
-            for (int i = 0; i < relLst.size(); ++i) {
-                for (OsmPrimitive parent : relLst.get(i).getReferrers()) {
+            for (Relation r : relLst) {
+                for (OsmPrimitive parent : r.getReferrers()) {
                     if (!(toPurgeChecked.contains(parent))
                             && hasOnlyIncompleteMembers((Relation) parent, toPurgeChecked, relLst)) {
                         relLst.add((Relation) parent);

src/org/openstreetmap/josm/gui/layer/geoimage/ThumbsLoader.java
@@ -50,12 +50,12 @@ public class ThumbsLoader implements Runnable {
     public void run() {
         Main.debug("Load Thumbnails");
         tracker = new MediaTracker(Main.map.mapView);
-        for (int i = 0; i < data.size(); i++) {
+        for (ImageEntry entry : data) {
             if (stop) return;
 
             // Do not load thumbnails that were loaded before.
-            if (data.get(i).thumbnail == null) {
-                data.get(i).thumbnail = loadThumb(data.get(i));
+            if (entry.thumbnail == null) {
+                entry.thumbnail = loadThumb(entry);
 
                 if (Main.isDisplayingMapView()) {
                     layer.updateOffscreenBuffer = true;

src/org/openstreetmap/josm/gui/util/GuiHelper.java
@@ -359,7 +359,7 @@ public final class GuiHelper {
         while (keys.hasMoreElements()) {
             Object key = keys.nextElement();
             Object value = UIManager.get(key);
-            if (value != null && value instanceof FontUIResource) {
+            if (value instanceof FontUIResource) {
                 FontUIResource fui = (FontUIResource) value;
                 UIManager.put(key, new FontUIResource(name, fui.getStyle(), fui.getSize()));
             }

test/unit/org/openstreetmap/josm/gui/NavigatableComponentTest.java
@@ -34,7 +34,7 @@ public class NavigatableComponentTest {
      * Setup test.
      */
     @BeforeClass
-    public static void setUp() {
+    public static void setUpBeforeClass() {
         JOSMFixture.createUnitTestFixture().init();
     }
 
@@ -42,7 +42,7 @@ public class NavigatableComponentTest {
      * Create a new, fresh {@link NavigatableComponent}
      */
     @Before
-    public void setup() {
+    public void setUp() {
         component = new NavigatableComponent();
         component.setBounds(new Rectangle(WIDTH, HEIGHT));
         // wait for the event to be propagated.

commit 068c460919d282443b7850166519a4b7cf3baf32

fix #12038 -  ConcurrentModificationException using purge command
Regression from r8905.

git-svn-id: http://josm.openstreetmap.de/svn/trunk@8981 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/PurgeAction.java
@@ -180,8 +180,8 @@ public class PurgeAction extends JosmAction {
              * Add higher level relations (list gets extended while looping over it)
              */
             List<Relation> relLst = new ArrayList<>(relSet);
-            for (Relation r : relLst) {
-                for (OsmPrimitive parent : r.getReferrers()) {
+            for (int i = 0; i < relLst.size(); ++i) { // foreach loop not applicable since list gets extended while looping over it
+                for (OsmPrimitive parent : relLst.get(i).getReferrers()) {
                     if (!(toPurgeChecked.contains(parent))
                             && hasOnlyIncompleteMembers((Relation) parent, toPurgeChecked, relLst)) {
                         relLst.add((Relation) parent);