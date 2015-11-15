b8431f6c0d1c8a291ce8e6d9e9dc782279826f87
Klumbumbus <Klumbumbus@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-11-15 19:18:38 +0000

    fix #12100 - improve message string
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9012 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/bbox/TileSelectionBBoxChooser.java
+++ src/org/openstreetmap/josm/gui/bbox/TileSelectionBBoxChooser.java
@@ -448,7 +448,7 @@ public class TileSelectionBBoxChooser extends JPanel implements BBoxChooser{
             JPanel pnl = new JPanel(new BorderLayout());
             HtmlPanel msg = new HtmlPanel();
             msg.setText(tr("<html>Alternatively you may enter a <strong>tile address</strong> for a single tile "
-                    + "in the format <i>zoomlevel/x/y</i>, i.e. <i>15/256/223</i>. Tile addresses "
+                    + "in the format <i>zoomlevel/x/y</i>, e.g. <i>15/256/223</i>. Tile addresses "
                     + "in the format <i>zoom,x,y</i> or <i>zoom;x;y</i> are valid too.</html>"));
             pnl.add(msg);
             return pnl;
