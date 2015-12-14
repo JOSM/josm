90373dcecdc4c96e6bee6163b51fc75185935554
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2015-12-14 21:05:12 +0000

    see #12189 - try again to update presets and style URL's
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9119 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/Preferences.java
+++ src/org/openstreetmap/josm/data/Preferences.java
@@ -1816,10 +1816,10 @@ public class Preferences {
      * see something with an expiry date in the past, remove it from the list.
      */
     public void removeObsolete() {
-        // drop this block end of 2015
-        // update old style JOSM server links to use zip now, see #10581
+        // drop this block march 2016
+        // update old style JOSM server links to use zip now, see #10581, #12189
         // actually also cache and mirror entries should be cleared
-        if (getInteger("josm.version", Version.getInstance().getVersion()) < 8099) {
+        if (getInteger("josm.version", Version.getInstance().getVersion()) < 9119) {
             for (String key: new String[]{"mappaint.style.entries", "taggingpreset.entries"}) {
                 Collection<Map<String, String>> data = getListOfStructs(key, (Collection<Map<String, String>>) null);
                 if (data != null) {
