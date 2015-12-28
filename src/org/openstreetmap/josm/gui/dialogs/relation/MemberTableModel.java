JOSM/josm

commit 239948af6981221f12aa6b1954faa03d6dc4110a

fix EDT violation seen with "Download all children" action of relation editor
git-svn-id: http://josm.openstreetmap.de/svn/trunk@9020 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/MemberTableModel.java
@@ -38,9 +38,10 @@ import org.openstreetmap.josm.gui.dialogs.relation.sort.RelationSorter;
 import org.openstreetmap.josm.gui.dialogs.relation.sort.WayConnectionType;
 import org.openstreetmap.josm.gui.dialogs.relation.sort.WayConnectionTypeCalculator;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
-import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetHandler;
 import org.openstreetmap.josm.gui.tagging.presets.TaggingPreset;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetHandler;
 import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetType;
+import org.openstreetmap.josm.gui.util.GuiHelper;
 import org.openstreetmap.josm.gui.widgets.OsmPrimitivesTableModel;
 
 public class MemberTableModel extends AbstractTableModel
@@ -104,7 +105,11 @@ implements TableModelListener, SelectionChangedListener, DataSetListener, OsmPri
     public void dataChanged(DataChangedEvent event) {
         // just trigger a repaint - the display name of the relation members may have changed
         Collection<RelationMember> sel = getSelectedMembers();
-        fireTableDataChanged();
+        GuiHelper.runInEDTAndWait(new Runnable() {
+            public void run() {
+                fireTableDataChanged();
+            }
+        });
         setSelectedMembers(sel);
     }
 

commit 0519eda3c7b8d4b646ce71aca86ddd37a56897f1

fix #12245 - fix EDT violation properly without hanging the UI (regression from r9020)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@9195 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/MemberTableModel.java
@@ -105,7 +105,7 @@ implements TableModelListener, SelectionChangedListener, DataSetListener, OsmPri
     public void dataChanged(DataChangedEvent event) {
         // just trigger a repaint - the display name of the relation members may have changed
         Collection<RelationMember> sel = getSelectedMembers();
-        GuiHelper.runInEDTAndWait(new Runnable() {
+        GuiHelper.runInEDT(new Runnable() {
             public void run() {
                 fireTableDataChanged();
             }