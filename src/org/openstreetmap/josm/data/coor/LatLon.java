9448d46418a86fe8fba99e1e00b87975c6bd1f25
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-01-09 23:40:31 +0000

    Java 7: use `Objects.equals` and `Objects.hash` (fixup r9371)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9375 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/coor/LatLon.java
+++ src/org/openstreetmap/josm/data/coor/LatLon.java
@@ -428,14 +428,16 @@ public class LatLon extends Coordinate {
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode());
+        return Objects.hash(x, y);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
-        return super.equals(obj);
+        LatLon that = (LatLon) obj;
+        return Double.compare(that.x, x) == 0 &&
+                Double.compare(that.y, y) == 0;
     }
 
     public ICoordinate toCoordinate() {
