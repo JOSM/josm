36d2eca2cef8963dbfbfcf4c978cec912a22a336
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-01-09 22:37:08 +0000

    see #7612 - use basic authentication for unit tests
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9374 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/functional/org/openstreetmap/josm/io/MultiFetchServerObjectReaderTest.java
+++ test/functional/org/openstreetmap/josm/io/MultiFetchServerObjectReaderTest.java
@@ -133,6 +133,8 @@ public class MultiFetchServerObjectReaderTest {
         logger.info("initializing ...");
         JOSMFixture.createFunctionalTestFixture().init();
 
+        Main.pref.put("osm-server.auth-method", "basic");
+
         // don't use atomic upload, the test API server can't cope with large diff uploads
         Main.pref.put("osm-server.atomic-upload", false);
 
