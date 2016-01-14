3085b6aa7fbbaf121bd650d7d4134b9fc3bf5d10
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-01-14 17:01:53 +0000

    fix #12352 - Add "Delete Layer" in file menu (patch by kolesar, slightly modified)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9443 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/actions/DeleteLayerAction.java
@@ -0,0 +1,45 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.actions;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.Collections;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.tools.Shortcut;
+import org.openstreetmap.josm.gui.layer.Layer;
+
+/**
+ * Deletes the active layer.
+ */
+public final class DeleteLayerAction extends JosmAction {
+
+    /**
+     * Constructs a {@link DeleteLayerAction} which will delete the active layer.
+     */
+    public DeleteLayerAction() {
+        super(tr("Delete Layer"), "dialogs/delete", tr("Delete the active layer. Does not delete the associated file."),
+                Shortcut.registerShortcut("system:deletelayer", tr("File: {0}", tr("Delete Layer")), KeyEvent.VK_F4, Shortcut.CTRL), true);
+        putValue("help", ht("/Action/DeleteLayer"));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Layer activeLayer = Main.main.getActiveLayer();
+        if (activeLayer == null) {
+            return;
+        }
+        if (!Main.saveUnsavedModifications(Collections.singletonList(activeLayer), false)) {
+            return;
+        }
+        Main.main.removeLayer(activeLayer);
+    }
+
+    @Override
+    protected void updateEnabledState() {
+        setEnabled(Main.isDisplayingMapView() && Main.map.mapView.getActiveLayer() != null);
+    }
+}
