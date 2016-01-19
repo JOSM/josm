bb233b739d6ec9860fa717d935d9a07f351f0b77
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-01-19 09:52:33 +0000

    Refactoring (make `Tag` implement the `Tagged` interface)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9536 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/dialogs/properties/SearchBasedRowFilter.java
+++ src/org/openstreetmap/josm/gui/dialogs/properties/SearchBasedRowFilter.java
@@ -1,15 +1,11 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.dialogs.properties;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
-
 import javax.swing.RowFilter;
 import javax.swing.table.TableModel;
 
 import org.openstreetmap.josm.actions.search.SearchCompiler;
-import org.openstreetmap.josm.data.osm.Tagged;
+import org.openstreetmap.josm.data.osm.Tag;
 
 /**
  * A {@link RowFilter} implementation which matches tags w.r.t. the specified filter's
@@ -33,56 +29,7 @@ class SearchBasedRowFilter extends RowFilter<TableModel, Integer> {
     public boolean include(Entry entry) {
         final String key = entry.getStringValue(0);
         final String value = entry.getStringValue(1);
-        return filter.match(new OneKeyValue(key, value));
+        return filter.match(new Tag(key, value));
     }
 
-    static class OneKeyValue implements Tagged {
-        private final String key;
-        private final String value;
-
-        OneKeyValue(String key, String value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        @Override
-        public void setKeys(Map<String, String> keys) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Map<String, String> getKeys() {
-            return Collections.singletonMap(key, value);
-        }
-
-        @Override
-        public void put(String key, String value) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public String get(String k) {
-            return key.equals(k) ? value : null;
-        }
-
-        @Override
-        public void remove(String key) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public boolean hasKeys() {
-            return true;
-        }
-
-        @Override
-        public Collection<String> keySet() {
-            return Collections.singleton(key);
-        }
-
-        @Override
-        public void removeAll() {
-            throw new UnsupportedOperationException();
-        }
-    }
 }
