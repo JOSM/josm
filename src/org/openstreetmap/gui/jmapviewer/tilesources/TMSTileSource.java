0613bbddce986e782bc38bf86a4789985142553d
JOSM GitHub mirror <openstreetmap@v.nix.is>
2016-01-24 22:30:24 +0000

    josm-mirror: bumped externals

--- src/org/openstreetmap/gui/jmapviewer/tilesources/TMSTileSource.java
+++ src/org/openstreetmap/gui/jmapviewer/tilesources/TMSTileSource.java
@@ -1,15 +1,24 @@
 // License: GPL. For details, see Readme.txt file.
 package org.openstreetmap.gui.jmapviewer.tilesources;
 
+import java.awt.Point;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.OsmMercator;
+import org.openstreetmap.gui.jmapviewer.TileXY;
+import org.openstreetmap.gui.jmapviewer.interfaces.ICoordinate;
+
 public class TMSTileSource extends AbstractTMSTileSource {
 
     protected int maxZoom;
     protected int minZoom;
+    protected OsmMercator osmMercator;
 
     public TMSTileSource(TileSourceInfo info) {
         super(info);
         minZoom = info.getMinZoom();
         maxZoom = info.getMaxZoom();
+        this.osmMercator = new OsmMercator(this.getTileSize());
     }
 
     @Override
@@ -20,5 +29,41 @@ public class TMSTileSource extends AbstractTMSTileSource {
     @Override
     public int getMaxZoom() {
         return (maxZoom == 0) ? super.getMaxZoom() : maxZoom;
+    }
+    @Override
+    public double getDistance(double lat1, double lon1, double lat2, double lon2) {
+        return osmMercator.getDistance(lat1, lon1, lat2, lon2);
+    }
+
+    @Override
+    public Point latLonToXY(double lat, double lon, int zoom) {
+        return new Point(
+                (int) osmMercator.lonToX(lon, zoom),
+                (int) osmMercator.latToY(lat, zoom)
+                );
+    }
+
+    @Override
+    public ICoordinate xyToLatLon(int x, int y, int zoom) {
+        return new Coordinate(
+                osmMercator.yToLat(y, zoom),
+                osmMercator.xToLon(x, zoom)
+                );
+    }
+
+    @Override
+    public TileXY latLonToTileXY(double lat, double lon, int zoom) {
+        return new TileXY(
+                osmMercator.lonToX(lon, zoom) / getTileSize(),
+                osmMercator.latToY(lat, zoom) / getTileSize()
+                );
+    }
+
+    @Override
+    public ICoordinate tileXYToLatLon(int x, int y, int zoom) {
+        return new Coordinate(
+                osmMercator.yToLat(y * getTileSize(), zoom),
+                osmMercator.xToLon(x * getTileSize(), zoom)
+                );
     }
 }
