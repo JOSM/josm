0dbfe1eeea7fd91edc842ccefce8a76838e2217f
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-01-31 10:08:34 +0000

    fix #12453 - Add icon for "discourage upload" (icon by Klumbumbus)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9692 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/ToggleUploadDiscouragedLayerAction.java
+++ src/org/openstreetmap/josm/actions/ToggleUploadDiscouragedLayerAction.java
@@ -5,15 +5,26 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+
 import org.openstreetmap.josm.gui.dialogs.LayerListDialog;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.tools.ImageProvider;
 
-public class ToggleUploadDiscouragedLayerAction extends JosmAction {
+/**
+ * An action enabling/disabling the {@linkplain OsmDataLayer#setUploadDiscouraged(boolean) discouraged upload flag}
+ * of the layer specified in the constructor.
+ */
+public class ToggleUploadDiscouragedLayerAction extends AbstractAction {
 
     private final transient OsmDataLayer layer;
 
+    /**
+     * Constructs a new {@code ToggleUploadDiscouragedLayerAction}.
+     * @param layer the layer for which to toggle the {@linkplain OsmDataLayer#setUploadDiscouraged(boolean) discouraged upload flag}
+     */
     public ToggleUploadDiscouragedLayerAction(OsmDataLayer layer) {
-        super(tr("Encourage/discourage upload"), null, null, null, false);
+        super(tr("Encourage/discourage upload"), ImageProvider.get("no_upload"));
         this.layer = layer;
     }
 
