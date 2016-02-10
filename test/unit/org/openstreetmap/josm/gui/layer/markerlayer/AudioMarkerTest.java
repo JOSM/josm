33a444049e2cc271199e9c3d07d72bce09497d05
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-02-10 22:41:26 +0000

    attempt to fix unit test
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9782 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/layer/markerlayer/AudioMarkerTest.java
+++ test/unit/org/openstreetmap/josm/gui/layer/markerlayer/AudioMarkerTest.java
@@ -3,17 +3,19 @@ package org.openstreetmap.josm.gui.layer.markerlayer;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openstreetmap.josm.JOSMFixture;
+import org.openstreetmap.josm.TestUtils;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.gpx.Extensions;
 import org.openstreetmap.josm.data.gpx.GpxConstants;
 import org.openstreetmap.josm.data.gpx.GpxData;
 import org.openstreetmap.josm.data.gpx.WayPoint;
+import org.openstreetmap.josm.tools.AudioPlayer;
 
 /**
  * Unit tests of {@link AudioMarker} class.
@@ -37,10 +39,14 @@ public class AudioMarkerTest {
         AudioMarker marker = new AudioMarker(
                 LatLon.ZERO,
                 null,
-                new URL("file://something.wav"),
+                new File(TestUtils.getRegressionDataFile(6851, "20111003_121226.wav")).toURI().toURL(),
                 new MarkerLayer(new GpxData(), null, null, null),
                 1d, 2d);
-        marker.actionPerformed(null);
+        try {
+            marker.actionPerformed(null);
+        } finally {
+            AudioPlayer.reset();
+        }
         assertEquals("2", marker.getText());
         WayPoint wpt = marker.convertToWayPoint();
         assertEquals(LatLon.ZERO, wpt.getCoor());
