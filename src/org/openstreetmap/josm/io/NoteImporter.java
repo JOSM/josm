bc5a2789c425b569cdb71a1aa4e4cbe7908ff8f0
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-02-14 18:08:23 +0000

    fix #12531 - NPE when importing notes files (regression from r9746)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9797 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/io/NoteImporter.java
+++ src/org/openstreetmap/josm/io/NoteImporter.java
@@ -37,7 +37,7 @@ public class NoteImporter extends FileImporter {
         }
         try (InputStream is = Compression.getUncompressedFileInputStream(file)) {
             final NoteLayer layer = loadLayer(is, file, file.getName(), progressMonitor);
-            if (!Main.map.mapView.hasLayer(layer)) {
+            if (Main.map == null || !Main.map.mapView.hasLayer(layer)) {
                 GuiHelper.runInEDT(new Runnable() {
                     @Override
                     public void run() {
