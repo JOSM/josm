6b081c77c2721ea3eb7a2d8f07af380ec67420d3
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-02-26 07:53:47 +0000

    fix #12568 - IndexOutOfBoundsException in NativeScaleLayer$ScaleList - regression from r9818
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@9883 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ test/unit/org/openstreetmap/josm/gui/layer/NativeScaleLayerTest.java
@@ -0,0 +1,33 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.layer;
+
+import static org.junit.Assert.assertNull;
+
+import java.util.Collections;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openstreetmap.josm.JOSMFixture;
+
+/**
+ * Unit tests of {@link NativeScaleLayer} class.
+ */
+public class NativeScaleLayerTest {
+
+    /**
+     * Setup tests
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        JOSMFixture.createUnitTestFixture().init();
+    }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/12255">#12255</a>.
+     */
+    @Test
+    public void testTicket12255() {
+        assertNull(new NativeScaleLayer.ScaleList(Collections.<Double>emptyList()).getSnapScale(10, 2, false));
+        assertNull(new NativeScaleLayer.ScaleList(new double[0]).getSnapScale(10, 2, false));
+    }
+}
