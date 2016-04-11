8b45a2b5685633511fea244e99b4a0d022b7d72e
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-04-11 09:59:42 +0000

    remove unnecessary import
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@10139 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/ShowStatusReportAction.java
+++ src/org/openstreetmap/josm/actions/ShowStatusReportAction.java
@@ -19,7 +19,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.regex.Pattern;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Version;
