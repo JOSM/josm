e8e4eeb4e999929ef15be58d1101e7ec52b4b61a
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-06-30 13:51:46 +0000

    fix icon checker for new function call
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@10503 0c6e7542-c601-0410-84e7-c038aed88b3b

--- geticons.pl
+++ geticons.pl
@@ -42,7 +42,7 @@ for my $arg (@ARGV ? @ARGV : @default)
         $img = $val if((!-f "images/$img") && -f "images/$val");
         ++$icons{$img};
       }
-      if($l =~ /ImageProvider(?:\.get)?\(\"([^\"]*?)\"\)/)
+      if($l =~ /ImageProvider(?:\.get)?\(\"([^\"]*?)\"/)
       {
         my $i = $1;
         $i = "styles/standard/$i" if $i eq "misc/no_icon";
