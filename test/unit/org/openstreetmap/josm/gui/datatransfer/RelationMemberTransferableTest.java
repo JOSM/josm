3a5312cd398a00854668c0e43b3cac532d5371f5
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-07-24 11:21:38 +0000

    fix #13199 - Fix RelationMemberTransferableTest (patch by michael2402) - gsoc-core
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@10623 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/datatransfer/RelationMemberTransferableTest.java
+++ test/unit/org/openstreetmap/josm/gui/datatransfer/RelationMemberTransferableTest.java
@@ -3,7 +3,6 @@ package org.openstreetmap.josm.gui.datatransfer;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.openstreetmap.josm.gui.datatransfer.RelationMemberTransferable.RELATION_MEMBER_DATA;
 
@@ -16,8 +15,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openstreetmap.josm.JOSMFixture;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.PrimitiveData;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.RelationMemberData;
+import org.openstreetmap.josm.gui.datatransfer.data.PrimitiveTransferData;
 
 /**
  * Unit tests of {@link RelationMemberTransferable} class.
@@ -38,9 +39,10 @@ public class RelationMemberTransferableTest {
     @Test
     public void testGetTransferDataFlavors() {
         DataFlavor[] flavors = new RelationMemberTransferable(Collections.<RelationMember>emptyList()).getTransferDataFlavors();
-        assertEquals(2, flavors.length);
+        assertEquals(3, flavors.length);
         assertEquals(RELATION_MEMBER_DATA, flavors[0]);
-        assertEquals(DataFlavor.stringFlavor, flavors[1]);
+        assertEquals(PrimitiveTransferData.DATA_FLAVOR, flavors[1]);
+        assertEquals(DataFlavor.stringFlavor, flavors[2]);
     }
 
     /**
@@ -50,6 +52,7 @@ public class RelationMemberTransferableTest {
     public void testIsDataFlavorSupported() {
         RelationMemberTransferable transferable = new RelationMemberTransferable(Collections.<RelationMember>emptyList());
         assertTrue(transferable.isDataFlavorSupported(RELATION_MEMBER_DATA));
+        assertTrue(transferable.isDataFlavorSupported(PrimitiveTransferData.DATA_FLAVOR));
         assertFalse(transferable.isDataFlavorSupported(null));
     }
 
@@ -64,7 +67,13 @@ public class RelationMemberTransferableTest {
         Collection<RelationMemberData> td = ((RelationMemberTransferable.Data) rmt.getTransferData(RELATION_MEMBER_DATA))
                 .getRelationMemberData();
         assertEquals(1, td.size());
-        assertNotNull(td.iterator().next());
+        assertEquals(1, td.iterator().next().getMemberId());
+        assertEquals("test", td.iterator().next().getRole());
+
+        Collection<PrimitiveData> primitives = ((PrimitiveTransferData) rmt.getTransferData(PrimitiveTransferData.DATA_FLAVOR))
+                .getDirectlyAdded();
+        assertEquals(1, primitives.size());
+        assertEquals(1, primitives.iterator().next().getId());
     }
 
     /**
