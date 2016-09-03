00b887da2471fa68cc312cec881fb4cab51f6376
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-09-03 18:01:46 +0000

    fix #13395 - fix serialization issue causing bugs in copy/paste
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@10946 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/osm/NodeDataTest.java
+++ test/unit/org/openstreetmap/josm/data/osm/NodeDataTest.java
@@ -1,8 +1,12 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.data.osm;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
@@ -11,6 +15,17 @@ import org.junit.Test;
 import org.openstreetmap.josm.data.coor.LatLon;
 
 public class NodeDataTest {
+
+    private static NodeData serializeUnserialize(NodeData data) throws IOException, ClassNotFoundException {
+        try (ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(bytes)) {
+            out.writeObject(data);
+            try (ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes.toByteArray()))) {
+                return (NodeData) in.readObject();
+            }
+        }
+    }
+
     @Test
     public void testSerializationForDragAndDrop() throws Exception {
         final NodeData data = new NodeData();
@@ -18,14 +33,27 @@ public class NodeDataTest {
         data.setId(314);
         data.setVersion(14);
         data.setChangesetId(314159);
-        final Object readData;
-        try (ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-             ObjectOutputStream out = new ObjectOutputStream(bytes)) {
-            out.writeObject(data);
-            try (ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes.toByteArray()))) {
-                readData = in.readObject();
-            }
-        }
+        final NodeData readData = serializeUnserialize(data);
         Assert.assertEquals(data.toString(), readData.toString());
+    }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/13395">#13395</a>.
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void testTicket13395() throws Exception {
+        Node n = new Node(1925320646, 1);
+        n.setCoor(null);
+        assertNull(n.getCoor());
+        assertTrue(n.isIncomplete());
+
+        NodeData data = n.save();
+        assertNull(data.getCoor());
+        assertTrue(data.isIncomplete());
+
+        NodeData readData = serializeUnserialize(data);
+        assertNull(readData.getCoor());
+        assertTrue(readData.isIncomplete());
     }
 }
