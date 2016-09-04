8f90836ab3eff2c0f3d62c843ce55a7d1fa9fb80
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-09-04 09:37:06 +0000

    increase unit test timeout
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@10955 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/actions/upload/ValidateUploadHookTest.java
+++ test/unit/org/openstreetmap/josm/actions/upload/ValidateUploadHookTest.java
@@ -18,7 +18,7 @@ public class ValidateUploadHookTest {
      */
     @Rule
     @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-    public JOSMTestRules test = new JOSMTestRules().platform();
+    public JOSMTestRules test = new JOSMTestRules().platform().timeout(15000);
 
     /**
      * Test of {@link ValidateUploadHook#checkUpload} method.
