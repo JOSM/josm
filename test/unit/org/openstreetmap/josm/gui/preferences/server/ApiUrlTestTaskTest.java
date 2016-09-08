9f6727814a1ec290d3c5cf037bbf0017aaa85563
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-09-08 23:41:16 +0000

    add unit test for `ApiUrlTestTask`
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@10979 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ test/unit/org/openstreetmap/josm/gui/preferences/server/ApiUrlTestTaskTest.java
@@ -0,0 +1,89 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.preferences.server;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Component;
+
+import javax.swing.JLabel;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.josm.io.OsmApi;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+/**
+ * Unit tests of {@link ApiUrlTestTask} class.
+ */
+public class ApiUrlTestTaskTest {
+
+    /**
+     * Setup tests
+     */
+    @Rule
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public JOSMTestRules test = new JOSMTestRules().preferences().noTimeout();
+
+    private static final Component PARENT = new JLabel();
+
+    /**
+     * Unit test of {@link ApiUrlTestTask#ApiUrlTestTask} - null url.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullApiUrl() {
+        new ApiUrlTestTask(PARENT, null);
+    }
+
+    /**
+     * Unit test of {@link ApiUrlTestTask} - nominal url.
+     */
+    @Test
+    public void testNominalUrl() {
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, OsmApi.DEFAULT_API_URL);
+        task.run();
+        assertTrue(task.isSuccess());
+    }
+
+    /**
+     * Unit test of {@link ApiUrlTestTask#alertInvalidUrl} - malformed url.
+     */
+    @Test
+    public void testAlertInvalidUrl() {
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "malformed url");
+        task.run();
+        assertFalse(task.isSuccess());
+    }
+
+    /**
+     * Unit test of {@link ApiUrlTestTask} - unknown host.
+     */
+    @Test
+    public void testUnknownHost() {
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "http://unknown");
+        task.run();
+        assertFalse(task.isSuccess());
+    }
+
+    /**
+     * Unit test of {@link ApiUrlTestTask#alertInvalidServerResult} - http 404.
+     */
+    @Test
+    public void testAlertInvalidServerResult() {
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "http://www.openstreetmap.org");
+        task.run();
+        assertFalse(task.isSuccess());
+    }
+
+    /**
+     * Unit test of {@link ApiUrlTestTask#alertInvalidCapabilities} - invalid contents.
+     */
+    @Test
+    public void testAlertInvalidCapabilities() {
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "https://josm.openstreetmap.de/export/10979/josm/trunk/test/data/invalid_api");
+        task.run();
+        assertFalse(task.isSuccess());
+    }
+}
