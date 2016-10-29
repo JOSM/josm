JOSM/josm

commit 6087159546bab05a3699ef78822a0b1844684736

Fix 13636: Use line clipping for computing the one way arrows, too.
git-svn-id: http://josm.openstreetmap.de/svn/trunk@11147 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/osm/visitor/paint/StyledMapRenderer.java
@@ -1448,15 +1448,14 @@ public class StyledMapRenderer extends AbstractMapRenderer {
 
         MapViewPath path = new MapViewPath(mapState);
         MapViewPath orientationArrows = showOrientation ? new MapViewPath(mapState) : null;
-        MapViewPath onewayArrows = showOneway ? new MapViewPath(mapState) : null;
-        MapViewPath onewayArrowsCasing = showOneway ? new MapViewPath(mapState) : null;
+        MapViewPath onewayArrows;
+        MapViewPath onewayArrowsCasing;
         Rectangle bounds = g.getClipBounds();
         if (bounds != null) {
             // avoid arrow heads at the border
             bounds.grow(100, 100);
         }
 
-        boolean initialMoveToNeeded = true;
         List<Node> wayNodes = way.getNodes();
         if (wayNodes.size() < 2) return;
 
@@ -1481,6 +1480,7 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         MapViewPoint lastPoint = null;
         double wayLength = 0;
         Iterator<MapViewPoint> it = new OffsetIterator(wayNodes, offset);
+        boolean initialMoveToNeeded = true;
         while (it.hasNext()) {
             MapViewPoint p = it.next();
             if (lastPoint != null) {
@@ -1502,24 +1502,38 @@ public class StyledMapRenderer extends AbstractMapRenderer {
                 if (showOneway) {
                     final double segmentLength = p1.distanceToInView(p2);
                     if (segmentLength != 0) {
-                        final double nx = (p2.getInViewX() - p1.getInViewX()) / segmentLength;
-                        final double ny = (p2.getInViewY() - p1.getInViewY()) / segmentLength;
-
-                        final double interval = 60;
-                        // distance from p1
-                        double dist = interval - (wayLength % interval);
-
-                        while (dist < segmentLength) {
-                            appenOnewayPath(onewayReversed, p1, nx, ny, dist, 3d, onewayArrowsCasing);
-                            appenOnewayPath(onewayReversed, p1, nx, ny, dist, 2d, onewayArrows);
-                            dist += interval;
-                        }
                     }
                     wayLength += segmentLength;
                 }
             }
             lastPoint = p;
         }
+        if (showOneway) {
+            onewayArrows = new MapViewPath(mapState);
+            onewayArrowsCasing = new MapViewPath(mapState);
+            double interval = 60;
+
+            path.visitClippedLine(0, 60, (inLineOffset, start, end, startIsOldEnd) -> {
+                double segmentLength = start.distanceToInView(end);
+                if (segmentLength > 0.001) {
+                    final double nx = (end.getInViewX() - start.getInViewX()) / segmentLength;
+                    final double ny = (end.getInViewY() - start.getInViewY()) / segmentLength;
+
+                    // distance from p1
+                    double dist = interval - (inLineOffset % interval);
+
+                    while (dist < segmentLength) {
+                        appenOnewayPath(onewayReversed, start, nx, ny, dist, 3d, onewayArrowsCasing);
+                        appenOnewayPath(onewayReversed, start, nx, ny, dist, 2d, onewayArrows);
+                        dist += interval;
+                    }
+                }
+            });
+        } else {
+            onewayArrows = null;
+            onewayArrowsCasing = null;
+        }
+
         if (way.isHighlighted()) {
             drawPathHighlight(path, line);
         }

src/org/openstreetmap/josm/gui/draw/MapViewPath.java
@@ -177,51 +177,108 @@ public class MapViewPath extends MapPath2D {
      * The resulting line is not intended to fill areas.
      * @param stroke The stroke to compute the line for.
      * @return The new line shape.
+     * @since 11147
      */
     public Shape computeClippedLine(Stroke stroke) {
-        if (stroke instanceof BasicStroke && ((BasicStroke) stroke).getDashArray() != null) {
-            float length = 0;
-            for (float f : ((BasicStroke) stroke).getDashArray()) {
-                length += f;
+        MapPath2D clamped = new MapPath2D();
+        if (visitClippedLine(stroke, (inLineOffset, start, end, startIsOldEnd) -> {
+            if (!startIsOldEnd) {
+                clamped.moveTo(start);
             }
-            return computeClippedLine(((BasicStroke) stroke).getDashPhase(), length);
-        } else {
-            return computeClippedLine(0, 0);
-        }
-    }
-
-    private Shape computeClippedLine(double strokeOffset, double strokeLength) {
-        ClampingPathVisitor path = new ClampingPathVisitor(state.getViewClipRectangle(), strokeOffset, strokeLength);
-        if (path.visit(getPathIterator(null))) {
-            return path;
+            clamped.lineTo(end);
+        })) {
+            return clamped;
         } else {
             // could not clip the path.
             return this;
         }
     }
 
-    private class ClampingPathVisitor extends MapPath2D {
+    /**
+     * Visits all straight segments of this path. The segments are clamped to the view.
+     * If they are clamped, the start points are aligned with the pattern.
+     * @param stroke The stroke to take the dash information from.
+     * @param consumer The consumer to call for each segment
+     * @return false if visiting the path failed because there e.g. were non-straight segments.
+     * @since 11147
+     */
+    public boolean visitClippedLine(Stroke stroke, PathSegmentConsumer consumer) {
+        if (stroke instanceof BasicStroke && ((BasicStroke) stroke).getDashArray() != null) {
+            float length = 0;
+            for (float f : ((BasicStroke) stroke).getDashArray()) {
+                length += f;
+            }
+            return visitClippedLine(((BasicStroke) stroke).getDashPhase(), length, consumer);
+        } else {
+            return visitClippedLine(0, 0, consumer);
+        }
+    }
+
+    /**
+     * Visits all straight segments of this path. The segments are clamped to the view.
+     * If they are clamped, the start points are aligned with the pattern.
+     * @param strokeOffset The initial offset of the pattern
+     * @param strokeLength The dash pattern length. 0 to use no pattern.
+     * @param consumer The consumer to call for each segment
+     * @return false if visiting the path failed because there e.g. were non-straight segments.
+     * @since 11147
+     */
+    public boolean visitClippedLine(double strokeOffset, double strokeLength, PathSegmentConsumer consumer) {
+        return new ClampingPathVisitor(state.getViewClipRectangle(), strokeOffset, strokeLength, consumer)
+            .visit(this);
+    }
+
+
+    /**
+     * This class is used to visit the segments of this path.
+     * @author Michael Zangl
+     * @since 11147
+     */
+    public interface PathSegmentConsumer {
+
+        /**
+         * Add a line segment between two points
+         * @param inLineOffset The offset of start in the line
+         * @param start The start point
+         * @param end The end point
+         * @param startIsOldEnd If the start point equals the last end point.
+         */
+        void addLineBetween(double inLineOffset, MapViewPoint start, MapViewPoint end, boolean startIsOldEnd);
+
+    }
+
+    private class ClampingPathVisitor {
         private final MapViewRectangle clip;
-        private double strokeProgress;
+        private final PathSegmentConsumer consumer;
+        protected double strokeProgress;
         private final double strokeLength;
         private MapViewPoint lastMoveTo;
 
         private MapViewPoint cursor;
         private boolean cursorIsActive = false;
 
-        ClampingPathVisitor(MapViewRectangle clip, double strokeOffset, double strokeLength) {
+        /**
+         * Create a new {@link ClampingPathVisitor}
+         * @param clip View clip rectangle
+         * @param strokeOffset Initial stroke offset
+         * @param strokeLength Total length of a stroke sequence
+         * @param consumer The consumer to notify of the path segments.
+         */
+        ClampingPathVisitor(MapViewRectangle clip, double strokeOffset, double strokeLength, PathSegmentConsumer consumer) {
             this.clip = clip;
             this.strokeProgress = Math.min(strokeLength - strokeOffset, 0);
             this.strokeLength = strokeLength;
+            this.consumer = consumer;
         }
 
         /**
          * Append a path to this one. The path is clipped to the current view.
-         * @param it The iterator
+         * @param mapViewPath The iterator
          * @return true if adding the path was successful.
          */
-        public boolean visit(PathIterator it) {
+        public boolean visit(MapViewPath mapViewPath) {
             double[] coords = new double[8];
+            PathIterator it = mapViewPath.getPathIterator(null);
             while (!it.isDone()) {
                 int type = it.currentSegment(coords);
                 switch (type) {
@@ -251,6 +308,7 @@ public class MapViewPath extends MapPath2D {
             MapViewPoint point = state.getForView(x, y);
             lastMoveTo = point;
             cursor = point;
+            cursorIsActive = false;
         }
 
         void visitLineTo(double x, double y) {
@@ -263,15 +321,15 @@ public class MapViewPath extends MapPath2D {
                 MapViewPoint exit = clip.getLineEntry(next, cursor);
                 if (!cursorIsActive || !entry.equals(cursor)) {
                     entry = alignStrokeOffset(entry, cursor);
-                    moveTo(entry);
                 }
-                lineTo(exit);
+                consumer.addLineBetween(strokeProgress + cursor.distanceToInView(entry), entry, exit, cursorIsActive);
                 cursorIsActive = exit.equals(next);
             }
             strokeProgress += cursor.distanceToInView(next);
 
             cursor = next;
         }
+
 
         private MapViewPoint alignStrokeOffset(MapViewPoint entry, MapViewPoint originalStart) {
             double distanceSq = entry.distanceToInViewSq(originalStart);

commit 185eafbc69675ff4608eab9f5c7cc51fd4faa3e8

fix warnings caused by r11147
git-svn-id: http://josm.openstreetmap.de/svn/trunk@11190 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/osm/visitor/paint/StyledMapRenderer.java
@@ -1454,7 +1454,6 @@ public class StyledMapRenderer extends AbstractMapRenderer {
         }
 
         MapViewPoint lastPoint = null;
-        double wayLength = 0;
         Iterator<MapViewPoint> it = new OffsetIterator(wayNodes, offset);
         boolean initialMoveToNeeded = true;
         while (it.hasNext()) {
@@ -1474,12 +1473,6 @@ public class StyledMapRenderer extends AbstractMapRenderer {
                     //TODO: Cache
                     ArrowPaintHelper drawHelper = new ArrowPaintHelper(PHI, 10 + line.getLineWidth());
                     drawHelper.paintArrowAt(orientationArrows, p2, p1);
-                }
-                if (showOneway) {
-                    final double segmentLength = p1.distanceToInView(p2);
-                    if (segmentLength != 0) {
-                    }
-                    wayLength += segmentLength;
                 }
             }
             lastPoint = p;