03448ed9d5aaac179ce5ce632987613a05b6730d
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-11-18 17:27:07 +0000

    Fix OsmLinkPasterTest due to r11279
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11282 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/datatransfer/importers/OsmLinkPasterTest.java
+++ test/unit/org/openstreetmap/josm/gui/datatransfer/importers/OsmLinkPasterTest.java
@@ -1,12 +1,12 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.datatransfer.importers;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
-import org.openstreetmap.josm.data.osm.SimplePrimitiveId;
+import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -24,26 +24,13 @@ public class OsmLinkPasterTest {
     public JOSMTestRules test = new JOSMTestRules();
 
     /**
-     * Test of {@link OsmLinkPaster#parseIds(String)}
+     * Test of {@link OsmLinkPaster#parseLatLon(String)}
      */
     @Test
-    public void testParseIds() {
-        assertArrayEquals(new Object[] {new SimplePrimitiveId(1234, OsmPrimitiveType.NODE) },
-                OsmLinkPaster.parseIds("http://www.openstreetmap.org/node/1234").toArray());
-        assertArrayEquals(new Object[] {new SimplePrimitiveId(1234, OsmPrimitiveType.WAY) },
-                OsmLinkPaster.parseIds("http://www.openstreetmap.org/way/1234").toArray());
-        assertArrayEquals(new Object[] {new SimplePrimitiveId(1234, OsmPrimitiveType.RELATION) },
-                OsmLinkPaster.parseIds("http://www.openstreetmap.org/relation/1234").toArray());
-
-        assertArrayEquals(new Object[] {new SimplePrimitiveId(1234, OsmPrimitiveType.NODE) },
-                OsmLinkPaster.parseIds("http://www.osm.org/node/1234").toArray());
-        assertArrayEquals(new Object[] {new SimplePrimitiveId(1234, OsmPrimitiveType.WAY) },
-                OsmLinkPaster.parseIds("http://osm.org/way/1234").toArray());
-        assertArrayEquals(new Object[] {new SimplePrimitiveId(1234, OsmPrimitiveType.RELATION) },
-                OsmLinkPaster.parseIds("https://www.openstreetmap.org/relation/1234").toArray());
-
-        assertArrayEquals(new Object[0], OsmLinkPaster.parseIds("http://www.openstreetmap.org/xx/1234").toArray());
-        assertArrayEquals(new Object[0], OsmLinkPaster.parseIds("http://www.openstreetmap.org/way/1234x").toArray());
-        assertArrayEquals(new Object[0], OsmLinkPaster.parseIds("").toArray());
+    public void testParseLatLon() {
+        assertEquals(new LatLon(51.71873, 8.76164),
+                OsmLinkPaster.parseLatLon("https://www.openstreetmap.org/#map=17/51.71873/8.76164"));
+        assertNull(OsmLinkPaster.parseLatLon("http://www.openstreetmap.org/"));
+        assertNull(OsmLinkPaster.parseLatLon("foo-bar"));
     }
 }
