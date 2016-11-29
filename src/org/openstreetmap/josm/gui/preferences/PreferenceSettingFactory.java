8c7de1a50492badb30ad71a40c58ac7bc4919566
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-11-29 01:39:12 +0000

    javadoc
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11344 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/preferences/PreferenceSettingFactory.java
+++ src/org/openstreetmap/josm/gui/preferences/PreferenceSettingFactory.java
@@ -1,8 +1,16 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.preferences;
 
+/**
+ * Factory for {@link PreferenceSetting}.
+ * @since 1742
+ */
 @FunctionalInterface
 public interface PreferenceSettingFactory {
 
+    /**
+     * Creates preference settings.
+     * @return created preference settings
+     */
     PreferenceSetting createPreferenceSetting();
 }
--- src/org/openstreetmap/josm/plugins/PluginPreferenceFactory.java
+++ src/org/openstreetmap/josm/plugins/PluginPreferenceFactory.java
@@ -4,10 +4,18 @@ package org.openstreetmap.josm.plugins;
 import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
 import org.openstreetmap.josm.gui.preferences.PreferenceSettingFactory;
 
+/**
+ * Preference settings factory for plugins.
+ * @since 1742
+ */
 public class PluginPreferenceFactory implements PreferenceSettingFactory {
 
     private final PluginProxy plugin;
 
+    /**
+     * Constructs a new {@code PluginPreferenceFactory}.
+     * @param plugin plugin proxy
+     */
     public PluginPreferenceFactory(PluginProxy plugin) {
         this.plugin = plugin;
     }
