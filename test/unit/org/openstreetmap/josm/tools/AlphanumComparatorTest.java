d324b158536067957abbf3a006c68e42c2d52939
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-12-15 20:36:30 +0000

    fix asserts
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11405 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/tools/AlphanumComparatorTest.java
+++ test/unit/org/openstreetmap/josm/tools/AlphanumComparatorTest.java
@@ -1,6 +1,8 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.tools;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -19,7 +21,7 @@ public class AlphanumComparatorTest {
     public void testNumeric() {
         List<String> lst = Arrays.asList("1", "20", "-1", "00999", "100");
         Collections.sort(lst, AlphanumComparator.getInstance());
-        assert lst == Arrays.asList("-1", "1", "20", "100", "00999");
+        assertEquals(Arrays.asList("-1", "1", "20", "100", "00999"), lst);
     }
 
     /**
@@ -29,6 +31,6 @@ public class AlphanumComparatorTest {
     public void testMixed() {
         List<String> lst = Arrays.asList("b1", "b20", "a5", "a00999", "a100");
         Collections.sort(lst, AlphanumComparator.getInstance());
-        assert lst == Arrays.asList("a5", "a100", "a00999", "b1", "b20");
+        assertEquals(Arrays.asList("a5", "a100", "a00999", "b1", "b20"), lst);
     }
 }
