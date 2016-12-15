1b866c6b5398fe1372f7afd0af06417b6bcc357a
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2016-12-15 20:34:01 +0000

    replace groovy validator unit tests by java ones
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11404 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ test/unit/org/openstreetmap/josm/tools/AlphanumComparatorTest.java
@@ -0,0 +1,34 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.tools;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Unit tests of {@link AlphanumComparator}.
+ */
+public class AlphanumComparatorTest {
+
+    /**
+     * Test numeric strings.
+     */
+    @Test
+    public void testNumeric() {
+        List<String> lst = Arrays.asList("1", "20", "-1", "00999", "100");
+        Collections.sort(lst, AlphanumComparator.getInstance());
+        assert lst == Arrays.asList("-1", "1", "20", "100", "00999");
+    }
+
+    /**
+     * Test mixed character strings.
+     */
+    @Test
+    public void testMixed() {
+        List<String> lst = Arrays.asList("b1", "b20", "a5", "a00999", "a100");
+        Collections.sort(lst, AlphanumComparator.getInstance());
+        assert lst == Arrays.asList("a5", "a100", "a00999", "b1", "b20");
+    }
+}
