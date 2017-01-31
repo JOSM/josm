069d8e98efeaac21de5a5359050f3974e9fb2d12
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-01-31 22:00:21 +0000

    sonar - fb-contrib:OC_OVERZEALOUS_CASTING - Correctness - Method manually casts the right hand side of an assignment more specifically than needed
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11517 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/CustomConfigurator.java
+++ src/org/openstreetmap/josm/data/CustomConfigurator.java
@@ -23,7 +23,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -1025,11 +1024,11 @@ public final class CustomConfigurator {
         @SuppressWarnings("unchecked")
         Map<String, String> stringMap = (Map<String, String>) engine.get("stringMap");
         @SuppressWarnings("unchecked")
-        Map<String, List<String>> listMap = (SortedMap<String, List<String>>) engine.get("listMap");
+        Map<String, List<String>> listMap = (Map<String, List<String>>) engine.get("listMap");
         @SuppressWarnings("unchecked")
-        Map<String, List<Collection<String>>> listlistMap = (SortedMap<String, List<Collection<String>>>) engine.get("listlistMap");
+        Map<String, List<Collection<String>>> listlistMap = (Map<String, List<Collection<String>>>) engine.get("listlistMap");
         @SuppressWarnings("unchecked")
-        Map<String, List<Map<String, String>>> listmapMap = (SortedMap<String, List<Map<String, String>>>) engine.get("listmapMap");
+        Map<String, List<Map<String, String>>> listmapMap = (Map<String, List<Map<String, String>>>) engine.get("listmapMap");
 
         tmpPref.settingsMap.clear();
 
