9db0c0a306a849c290d1b2427240f935424f326b
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-02-25 13:29:42 +0000

    see #14374 - sonar - pmd-unit-tests:UseAssertSameInsteadOfAssertTrue - Use assertSame instead of assertTrue
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11614 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/conflict/tags/TagConflictResolutionUtilTest.java
+++ test/unit/org/openstreetmap/josm/gui/conflict/tags/TagConflictResolutionUtilTest.java
@@ -4,6 +4,7 @@ package org.openstreetmap.josm.gui.conflict.tags;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
@@ -80,7 +81,7 @@ public class TagConflictResolutionUtilTest {
         Tag otherSource = new Tag("source", "other");
         tc.add(otherSource); // other source should prevent resolution
         TagConflictResolutionUtil.applyAutomaticTagConflictResolution(tc);
-        assertTrue(tc.getValues("source").size() == 18);
+        assertSame(18, tc.getValues("source").size());
         tc.remove(otherSource);
         TagConflictResolutionUtil.applyAutomaticTagConflictResolution(tc);
         assertEquals(Sets.newHashSet("cadastre-dgi-fr source : Direction Generale des Finances Publiques - Cadastre. Mise a jour : 2015"),
@@ -371,14 +372,14 @@ public class TagConflictResolutionUtilTest {
         @Test
         public void testGroupChoices() {
             Collection<AutomaticChoiceGroup> groups = AutomaticChoiceGroup.groupChoices(Arrays.asList(choiceKey1Group1, choiceKey1Group2));
-            assertTrue(groups.size() == 2);
+            assertSame(2, groups.size());
 
             groups = AutomaticChoiceGroup.groupChoices(Arrays.asList(
                 choiceKey1Group1, choiceKey1Group2, choiceKey2Group1, choiceKey2Group2, choiceEmpty));
-            assertTrue(groups.size() == 5);
+            assertSame(5, groups.size());
 
             groups = AutomaticChoiceGroup.groupChoices(Arrays.asList(choiceKey1Group1, choiceKey1Group1bis));
-            assertTrue(groups.size() == 1);
+            assertSame(1, groups.size());
             AutomaticChoiceGroup group1 = groups.iterator().next();
             assertEquals(group1.key, choiceKey1Group1.key);
             assertEquals(group1.group, choiceKey1Group1.group);
@@ -387,7 +388,7 @@ public class TagConflictResolutionUtilTest {
             groups = AutomaticChoiceGroup.groupChoices(Arrays.asList(
                 choiceKey1Group1, choiceKey1Group1bis, choiceKey1Group2, choiceKey1Group2bis,
                 choiceKey2Group1, choiceKey2Group1bis, choiceKey2Group2, choiceKey2Group2bis));
-            assertTrue(groups.size() == 4);
+            assertSame(4, groups.size());
             for (AutomaticChoiceGroup group: groups) {
                 for (AutomaticChoice choice: group.choices) {
                     assertEquals(choice.key, group.key);
