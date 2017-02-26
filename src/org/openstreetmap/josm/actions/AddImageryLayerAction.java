f8a504817fbd763c244b54c63703f2e230087991
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-02-26 12:44:47 +0000

    see #12313 - fix unit tests properly
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11635 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/AddImageryLayerAction.java
+++ src/org/openstreetmap/josm/actions/AddImageryLayerAction.java
@@ -66,10 +66,7 @@ public class AddImageryLayerAction extends JosmAction implements AdaptableAction
         super(info.getMenuName(), /* ICON */"imagery_menu", tr("Add imagery layer {0}", info.getName()), null, false, false);
         putValue("toolbar", "imagery_" + info.getToolbarName());
         putValue("help", ht("/Preferences/Imagery"));
-        String tooltip = info.getToolTipText();
-        if (tooltip != null) {
-            setTooltip(tooltip.replaceAll("</?html>", ""));
-        }
+        setTooltip(info.getToolTipText().replaceAll("</?html>", ""));
         this.info = info;
         installAdapters();
 
