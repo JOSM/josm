4697c313f76d4598463425ef6ab907c211c1ce1a
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-03-17 18:00:17 +0000

    add unit test for join areas action
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@11738 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/actions/JoinAreasActionTest.java
+++ test/unit/org/openstreetmap/josm/actions/JoinAreasActionTest.java
@@ -2,10 +2,16 @@
 package org.openstreetmap.josm.actions;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,13 +19,18 @@ import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.TestUtils;
 import org.openstreetmap.josm.actions.search.SearchAction;
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.gui.progress.NullProgressMonitor;
 import org.openstreetmap.josm.io.IllegalDataException;
 import org.openstreetmap.josm.io.OsmReader;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
+import org.openstreetmap.josm.tools.MultiMap;
 import org.openstreetmap.josm.tools.Utils;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -80,5 +91,97 @@ public class JoinAreasActionTest {
                 System.out.println(" ==> OK");
             }
         }
+    }
+
+    @Test
+    public void testExamples() throws Exception {
+        DataSet dsToJoin, dsExpected;
+        try (InputStream is = new FileInputStream("data_nodist/Join_Areas_Tests.osm")) {
+            dsToJoin = OsmReader.parseDataSet(is, NullProgressMonitor.INSTANCE);
+        }
+        try (InputStream is = new FileInputStream("data_nodist/Join_Areas_Tests_joined.osm")) {
+            dsExpected = OsmReader.parseDataSet(is, NullProgressMonitor.INSTANCE);
+        }
+        Collection<OsmPrimitive> testPrims = dsToJoin.getPrimitives(osm -> osm.get("test") != null);
+        MultiMap<String, OsmPrimitive> tests = new MultiMap<>();
+        for (OsmPrimitive testPrim : testPrims) {
+            tests.put(testPrim.get("test"), testPrim);
+        }
+        for (String test : tests.keySet()) {
+            Collection<OsmPrimitive> primitives = tests.get(test);
+            for (OsmPrimitive osm : primitives) {
+                assertTrue(test + "; expected way, but got: " + osm, osm instanceof Way);
+            }
+            new JoinAreasAction().join((Collection) primitives);
+            Collection<OsmPrimitive> joinedCol = dsToJoin.getPrimitives(osm -> !osm.isDeleted() && Objects.equals(osm.get("test"), test));
+            assertEquals("in test " + test + ":", 1, joinedCol.size());
+            Collection<OsmPrimitive> expectedCol = dsExpected.getPrimitives(osm -> !osm.isDeleted() && Objects.equals(osm.get("test"), test));
+            assertEquals("in test " + test + ":", 1, expectedCol.size());
+            OsmPrimitive osmJoined = joinedCol.iterator().next();
+            OsmPrimitive osmExpected = expectedCol.iterator().next();
+            assertTrue("difference in test " + test, isSemanticallyEqual(osmExpected, osmJoined));
+        }
+    }
+
+    /**
+     * Check if 2 primitives are semantically equal as result of a join areas
+     * operation.
+     * @param osm1 first primitive
+     * @param osm2 second primitive
+     * @return true if both primitives are semantically equal
+     */
+    private boolean isSemanticallyEqual(OsmPrimitive osm1, OsmPrimitive osm2) {
+        if (osm1 instanceof Node && osm2 instanceof Node)
+            return isSemanticallyEqualNode((Node) osm1, (Node) osm2);
+        if (osm1 instanceof Way && osm2 instanceof Way)
+            return isSemanticallyEqualWay((Way) osm1, (Way) osm2);
+        if (osm1 instanceof Relation && osm2 instanceof Relation)
+            return isSemanticallyEqualRelation((Relation) osm1, (Relation) osm2);
+        return false;
+    }
+
+    private boolean isSemanticallyEqualRelation(Relation r1, Relation r2) {
+        if (!r1.getKeys().equals(r2.getKeys())) return false;
+        if (r1.getMembersCount() != r2.getMembersCount()) return false;
+        Set<RelationMember> matchCandidates = new HashSet<>(r2.getMembers());
+        for (RelationMember rm : r1.getMembers()) {
+            RelationMember matched = null;
+            for (RelationMember cand : matchCandidates) {
+                if (!rm.getRole().equals(cand.getRole())) continue;
+                if (!isSemanticallyEqual(rm.getMember(), cand.getMember())) continue;
+                matched = cand;
+                break;
+            }
+            if (matched == null) return false;
+            matchCandidates.remove(matched);
+        }
+        return true;
+    }
+
+    private boolean isSemanticallyEqualWay(Way w1, Way w2) {
+        if (!w1.isClosed() || !w2.isClosed()) throw new UnsupportedOperationException();
+        if (!w1.getKeys().equals(w2.getKeys())) return false;
+        if (w1.getNodesCount() != w2.getNodesCount()) return false;
+        int n = w1.getNodesCount() - 1;
+        for (int dir : Arrays.asList(1, -1)) {
+            for (int i = 0; i < n; i++) {
+                boolean different = false;
+                for (int j = 0; j < n; j++) {
+                    Node n1 = w1.getNode(j);
+                    Node n2 = w2.getNode(Utils.mod(i + dir*j, n));
+                    if (!isSemanticallyEqualNode(n1, n2)) {
+                        different = true;
+                        break;
+                    }
+                }
+                if (!different)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isSemanticallyEqualNode(Node n1, Node n2) {
+        return n1.hasEqualSemanticAttributes(n2);
     }
 }
