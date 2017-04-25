b3bfc200bc14f52f2183fae62e56c77bea131ad2
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-04-25 23:26:25 +0000

    see #14692 - checkstyle
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@12006 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/validation/tests/RelationChecker.java
+++ src/org/openstreetmap/josm/data/validation/tests/RelationChecker.java
@@ -9,7 +9,6 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
