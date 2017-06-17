4f26c8a63d27eae1302f0024558b6d772058b6a4
JOSM GitHub mirror <openstreetmap@v.nix.is>
2017-06-17 21:00:18 +0200

    josm-mirror: bumped externals

--- src/org/apache/commons/compress/utils/ChecksumCalculatingInputStream.java
+++ src/org/apache/commons/compress/utils/ChecksumCalculatingInputStream.java
@@ -31,6 +31,15 @@ public class ChecksumCalculatingInputStream extends InputStream {
     private final Checksum checksum;
 
     public ChecksumCalculatingInputStream(final Checksum checksum, final InputStream in) {
+
+        if ( checksum == null ){
+            throw new NullPointerException("Parameter checksum must not be null");
+        }
+
+        if ( in == null ){
+            throw new NullPointerException("Parameter in must not be null");
+        }
+
         this.checksum = checksum;
         this.in = in;
     }
