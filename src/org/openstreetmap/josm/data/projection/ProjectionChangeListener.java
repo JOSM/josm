86ea8c4a75c882562f53550be398d6fd7f26fa32
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-07-04 12:39:26 +0000

    see #14794 - remaining javadoc for the josm/data/** packages
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@12453 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/projection/ProjectionChangeListener.java
+++ src/org/openstreetmap/josm/data/projection/ProjectionChangeListener.java
@@ -1,6 +1,9 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.data.projection;
 
+/**
+ * Interface for listeners to get notified when the (global) projection changes.
+ */
 @FunctionalInterface
 public interface ProjectionChangeListener {
     void projectionChanged(Projection oldValue, Projection newValue);
