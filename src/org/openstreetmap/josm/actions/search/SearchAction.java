c5ff4f1396d36c2acc1d9530ed0c005f75a4cad6
Afonins <b.afonins@student.tue.nl>
2017-07-06 19:04:26 +0200

    removed redundant imports

--- src/org/openstreetmap/josm/actions/search/SearchAction.java
+++ src/org/openstreetmap/josm/actions/search/SearchAction.java
@@ -12,7 +12,6 @@ import java.awt.FlowLayout;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
