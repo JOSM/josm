709f0a997f479fe498af1866870a3e06502da522
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-07-31 09:38:47 +0000

    see #14794 - javadoc
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@12546 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/ParameterizedAction.java
+++ src/org/openstreetmap/josm/actions/ParameterizedAction.java
@@ -5,9 +5,22 @@ import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Interface for (toolbar-)actions that have additional parameters which need
+ * to be saved to the preferences (and loaded back).
+ */
 public interface ParameterizedAction extends AdaptableAction {
 
+    /**
+     * Get the list of parameters that describe the action.
+     * @return the list of parameters that describe the action
+     */
     List<ActionParameter<?>> getActionParameters();
 
+    /**
+     * Invoke action using the given parameters.
+     * @param e the ActionEvent
+     * @param parameters parameter map
+     */
     void actionPerformed(ActionEvent e, Map<String, Object> parameters);
 }
