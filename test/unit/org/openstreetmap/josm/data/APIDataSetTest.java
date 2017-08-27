24d2ba0813f95d51de9c30d67d265029c0fbaeaf
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-08-27 15:24:14 +0000

    see #15182 - move `CyclicUploadDependencyException` from `actions.upload` to `data.osm` (used in `data.APIDataSet`)
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@12673 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/APIDataSetTest.java
+++ test/unit/org/openstreetmap/josm/data/APIDataSetTest.java
@@ -1,5 +1,5 @@
 // License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.data.osm;
+package org.openstreetmap.josm.data;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -10,9 +10,14 @@ import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.openstreetmap.josm.actions.upload.CyclicUploadDependencyException;
-import org.openstreetmap.josm.data.APIDataSet;
 import org.openstreetmap.josm.data.conflict.ConflictCollection;
+import org.openstreetmap.josm.data.osm.CyclicUploadDependencyException;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
