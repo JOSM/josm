faf6e7fe8e434cd313f353565c7668f798938df8
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-08-28 22:12:01 +0000

    see #15167 - checkstyle
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@12694 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/download/DownloadDialog.java
+++ src/org/openstreetmap/josm/gui/download/DownloadDialog.java
@@ -8,7 +8,6 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
