e6112e1ef4dd90a996e37952f31c7b02229c15cd
bastiK <bastiK@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-09-07 19:10:49 +0000

    see #15229 - remove GUI references from `BugReport` and `BugReportQueue`
    signature of `BugReport#getReportText` changed without deprecation
    (probably not used by any external plugin)
    
    
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@12770 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/bugreport/DebugTextDisplay.java
+++ src/org/openstreetmap/josm/gui/bugreport/DebugTextDisplay.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 
 import javax.swing.JScrollPane;
 
+import org.openstreetmap.josm.actions.ShowStatusReportAction;
 import org.openstreetmap.josm.gui.datatransfer.ClipboardUtils;
 import org.openstreetmap.josm.gui.widgets.JosmTextArea;
 import org.openstreetmap.josm.tools.Utils;
@@ -48,8 +49,8 @@ public class DebugTextDisplay extends JScrollPane {
      */
     public DebugTextDisplay(BugReport report) {
         this();
-        setCodeText(report.getReportText());
-        report.addChangeListener(e -> setCodeText(report.getReportText()));
+        setCodeText(report.getReportText(ShowStatusReportAction.getReportHeader()));
+        report.addChangeListener(e -> setCodeText(report.getReportText(ShowStatusReportAction.getReportHeader())));
     }
 
     /**
