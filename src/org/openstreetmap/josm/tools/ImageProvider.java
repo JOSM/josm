88f37a41f502b52a98d04ce9978474cb57a27395
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2017-10-25 21:26:40 +0000

    fix #15476 - Antialiased text and better resize quality when viewing photos
    git-svn-id: http://josm.openstreetmap.de/svn/trunk@13038 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/tools/ImageProvider.java
+++ src/org/openstreetmap/josm/tools/ImageProvider.java
@@ -1393,6 +1393,54 @@ public class ImageProvider {
     }
 
     /**
+     * Returns a scaled instance of the provided {@code BufferedImage}.
+     * This method will use a multi-step scaling technique that provides higher quality than the usual
+     * one-step technique (only useful in downscaling cases, where {@code targetWidth} or {@code targetHeight} is
+     * smaller than the original dimensions, and generally only when the {@code BILINEAR} hint is specified).
+     *
+     * From https://community.oracle.com/docs/DOC-983611: "The Perils of Image.getScaledInstance()"
+     *
+     * @param img the original image to be scaled
+     * @param targetWidth the desired width of the scaled instance, in pixels
+     * @param targetHeight the desired height of the scaled instance, in pixels
+     * @param hint one of the rendering hints that corresponds to
+     * {@code RenderingHints.KEY_INTERPOLATION} (e.g.
+     * {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+     * {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+     * {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @return a scaled version of the original {@code BufferedImage}
+     * @since 13038
+     */
+    public static BufferedImage createScaledImage(BufferedImage img, int targetWidth, int targetHeight, Object hint) {
+        int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        // start with original size, then scale down in multiple passes with drawImage() until the target size is reached
+        BufferedImage ret = img;
+        int w = img.getWidth(null);
+        int h = img.getHeight(null);
+        do {
+            if (w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+            if (h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+        return ret;
+    }
+
+    /**
      * Replies the icon for an OSM primitive type
      * @param type the type
      * @return the icon
@@ -1907,6 +1955,24 @@ public class ImageProvider {
             IMAGE_FETCHER.shutdownNow();
         } else {
             IMAGE_FETCHER.shutdown();
+        }
+    }
+
+    /**
+     * Converts an {@link Image} to a {@link BufferedImage} instance.
+     * @param image image to convert
+     * @return a {@code BufferedImage} instance for the given {@code Image}.
+     * @since 13038
+     */
+    public static BufferedImage toBufferedImage(Image image) {
+        if (image instanceof BufferedImage) {
+            return (BufferedImage) image;
+        } else {
+            BufferedImage buffImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = buffImage.createGraphics();
+            g2.drawImage(image, 0, 0, null);
+            g2.dispose();
+            return buffImage;
         }
     }
 }
