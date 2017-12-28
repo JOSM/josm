JOSM/josm

commit bbda63054bbbd67895a39f837a405bee06061f36

fix #15654 - repair asynchronous fetching of icons (regression from r10714) + fetch https icons asynchronously (omission from r10731)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@13250 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/AddImageryLayerAction.java
@@ -75,7 +75,7 @@ public class AddImageryLayerAction extends JosmAction implements AdaptableAction
         // change toolbar icon from if specified
         String icon = info.getIcon();
         if (icon != null) {
-            new ImageProvider(icon).setOptional(true).getResourceAsync().thenAccept(result -> {
+            new ImageProvider(icon).setOptional(true).getResourceAsync().thenAcceptAsync(result -> {
                 if (result != null) {
                     GuiHelper.runInEDT(() -> result.attachImageIcon(this));
                 }

src/org/openstreetmap/josm/gui/mappaint/styleelement/MapImage.java
@@ -138,7 +138,7 @@ public class MapImage {
                 .setWidth(width)
                 .setHeight(height)
                 .setOptional(true)
-                .getAsync().thenAccept(result -> {
+                .getAsync().thenAcceptAsync(result -> {
                     synchronized (this) {
                         if (result == null) {
                             source.logWarning(tr("Failed to locate image ''{0}''", name));

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPreset.java
@@ -216,7 +216,7 @@ public class TaggingPreset extends AbstractAction implements ActiveLayerChangeLi
         imgProv.setId("presets");
         imgProv.setArchive(arch);
         imgProv.setOptional(true);
-        imgProv.getResourceAsync().thenAccept(result -> {
+        imgProv.getResourceAsync().thenAcceptAsync(result -> {
             if (result != null) {
                 GuiHelper.runInEDT(() -> result.attachImageIcon(this));
             } else {

src/org/openstreetmap/josm/tools/ImageProvider.java
@@ -630,6 +630,15 @@ public class ImageProvider {
     }
 
     /**
+     * Determines if this icon is located on a remote location (http, https, wiki).
+     * @return {@code true} if this icon is located on a remote location (http, https, wiki)
+     * @since 13250
+     */
+    public boolean isRemote() {
+        return name.startsWith(HTTP_PROTOCOL) || name.startsWith(HTTPS_PROTOCOL) || name.startsWith(WIKI_PROTOCOL);
+    }
+
+    /**
      * Execute the image request and scale result.
      * @return the requested image or null if the request failed
      */
@@ -654,7 +663,7 @@ public class ImageProvider {
      * @since 10714
      */
     public CompletableFuture<ImageIcon> getAsync() {
-        return name.startsWith(HTTP_PROTOCOL) || name.startsWith(WIKI_PROTOCOL)
+        return isRemote()
                 ? CompletableFuture.supplyAsync(this::get, IMAGE_FETCHER)
                 : CompletableFuture.completedFuture(get());
     }
@@ -698,7 +707,7 @@ public class ImageProvider {
      * @since 10714
      */
     public CompletableFuture<ImageResource> getResourceAsync() {
-        return name.startsWith(HTTP_PROTOCOL) || name.startsWith(WIKI_PROTOCOL)
+        return isRemote()
                 ? CompletableFuture.supplyAsync(this::getResource, IMAGE_FETCHER)
                 : CompletableFuture.completedFuture(getResource());
     }

commit 3f2855a8b88f0f330fb315debc273c212ba58bff

see #15654 - fix icon flickering (regression from r13250)
git-svn-id: http://josm.openstreetmap.de/svn/trunk@13252 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/AddImageryLayerAction.java
@@ -75,7 +75,7 @@ public class AddImageryLayerAction extends JosmAction implements AdaptableAction
         // change toolbar icon from if specified
         String icon = info.getIcon();
         if (icon != null) {
-            new ImageProvider(icon).setOptional(true).getResourceAsync().thenAcceptAsync(result -> {
+            new ImageProvider(icon).setOptional(true).getResourceAsync(result -> {
                 if (result != null) {
                     GuiHelper.runInEDT(() -> result.attachImageIcon(this));
                 }

src/org/openstreetmap/josm/gui/mappaint/styleelement/MapImage.java
@@ -138,7 +138,7 @@ public class MapImage {
                 .setWidth(width)
                 .setHeight(height)
                 .setOptional(true)
-                .getAsync().thenAcceptAsync(result -> {
+                .getAsync(result -> {
                     synchronized (this) {
                         if (result == null) {
                             source.logWarning(tr("Failed to locate image ''{0}''", name));

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPreset.java
@@ -216,7 +216,7 @@ public class TaggingPreset extends AbstractAction implements ActiveLayerChangeLi
         imgProv.setId("presets");
         imgProv.setArchive(arch);
         imgProv.setOptional(true);
-        imgProv.getResourceAsync().thenAcceptAsync(result -> {
+        imgProv.getResourceAsync(result -> {
             if (result != null) {
                 GuiHelper.runInEDT(() -> result.attachImageIcon(this));
             } else {

src/org/openstreetmap/josm/tools/ImageProvider.java
@@ -45,6 +45,7 @@ import java.util.TreeSet;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
@@ -658,14 +659,15 @@ public class ImageProvider {
      * Load the image in a background thread.
      *
      * This method returns immediately and runs the image request asynchronously.
+     * @param action the action that will deal with the image
      *
      * @return the future of the requested image
-     * @since 10714
+     * @since 13252
      */
-    public CompletableFuture<ImageIcon> getAsync() {
+    public CompletableFuture<Void> getAsync(Consumer<? super ImageIcon> action) {
         return isRemote()
-                ? CompletableFuture.supplyAsync(this::get, IMAGE_FETCHER)
-                : CompletableFuture.completedFuture(get());
+                ? CompletableFuture.supplyAsync(this::get, IMAGE_FETCHER).thenAcceptAsync(action)
+                : CompletableFuture.completedFuture(get()).thenAccept(action);
     }
 
     /**
@@ -702,14 +704,15 @@ public class ImageProvider {
      * Load the image in a background thread.
      *
      * This method returns immediately and runs the image request asynchronously.
+     * @param action the action that will deal with the image
      *
      * @return the future of the requested image
-     * @since 10714
+     * @since 13252
      */
-    public CompletableFuture<ImageResource> getResourceAsync() {
+    public CompletableFuture<Void> getResourceAsync(Consumer<? super ImageResource> action) {
         return isRemote()
-                ? CompletableFuture.supplyAsync(this::getResource, IMAGE_FETCHER)
-                : CompletableFuture.completedFuture(getResource());
+                ? CompletableFuture.supplyAsync(this::getResource, IMAGE_FETCHER).thenAcceptAsync(action)
+                : CompletableFuture.completedFuture(getResource()).thenAccept(action);
     }
 
     /**