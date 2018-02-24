201634fba51c551a1819608eaa0c724dd83ec118
Robert Scott <code@humanleg.org.uk>
2018-02-24 11:48:11 +0000

    tests: add PluginHandlerJOSMTooOldTest
    this test is a good example which exercises ExtendedDialogMocker and
    HelpAwareOptionPaneMocker

---
+++ test/unit/org/openstreetmap/josm/plugins/PluginHandlerJOSMTooOldTest.java
@@ -0,0 +1,199 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.TestUtils;
+import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.testutils.ExtendedDialogMocker;
+import org.openstreetmap.josm.testutils.HelpAwareOptionPaneMocker;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import com.google.common.io.CharStreams;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+/**
+ * Test parts of {@link PluginHandler} class when the reported JOSm version is too old for the plugin.
+ */
+public class PluginHandlerJOSMTooOldTest {
+    /**
+     * Setup test.
+     */
+    @Rule
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public JOSMTestRules test = new JOSMTestRules().preferences().main().assumeRevision(
+        "Revision: 6000\n"
+    );
+
+    /**
+     * HTTP mock.
+     */
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(
+        options().dynamicPort().usingFilesUnderDirectory(TestUtils.getTestDataRoot())
+    );
+
+    @Before
+    public void setUp() throws Exception {
+        try (
+            FileReader pluginListReader = new FileReader(
+                new File(TestUtils.getTestDataRoot(), "plugin/remotePluginsList")
+            );
+        ) {
+            final String pluginList = String.format(
+                CharStreams.toString(pluginListReader),
+                this.wireMockRule.port()
+            );
+
+            this.wireMockRule.stubFor(
+                WireMock.get(WireMock.urlEqualTo("/plugins")).willReturn(
+                    WireMock.aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "text/plain")
+                    .withBody(pluginList)
+                )
+            );
+            ImmutableList.of("dummy_plugin", "baz_plugin").forEach(plugin_name -> this.wireMockRule.stubFor(
+                WireMock.get(WireMock.urlEqualTo("/plugin/" + plugin_name + ".jar")).willReturn(
+                    WireMock.aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "application/java-archive")
+                    .withBodyFile("plugin/" + plugin_name + ".jar")
+                )
+            ));
+        }
+
+        Config.getPref().putInt("pluginmanager.version", 999);
+        Config.getPref().put("pluginmanager.lastupdate", "999");
+        Config.getPref().putList("plugins", ImmutableList.of("dummy_plugin", "baz_plugin"));
+        Config.getPref().putList("pluginmanager.sites",
+            ImmutableList.of(String.format("http://localhost:%s/plugins", this.wireMockRule.port()))
+        );
+    }
+
+    private final String bazPluginVersionReqString = "JOSM version 8,001 required for plugin baz_plugin.";
+    private final String dummyPluginVersionReqString = "JOSM version 7,001 required for plugin dummy_plugin.";
+    private final String dummyPluginFailedString = "<html>Updating the following plugin has failed:<ul><li>dummy_plugin</li></ul>"
+        + "Please open the Preference Dialog after JOSM has started and try to update it manually.</html>";
+
+    /**
+     * test update of plugins when those plugins turn out to require a higher JOSM version, but the
+     * user chooses to update them anyway.
+     */
+    @Test
+    public void testUpdatePluginsDownloadBoth() {
+        final ExtendedDialogMocker edMocker = new ExtendedDialogMocker(ImmutableMap.<String, Object>builder()
+            .put(this.bazPluginVersionReqString, "Download Plugin")
+            .put(this.dummyPluginVersionReqString, "Download Plugin")
+            .build()
+        );
+
+        final List<PluginInformation> updatedPlugins = PluginHandler.updatePlugins(
+            Main.parent,
+            null,
+            null,
+            false
+        ).stream().sorted((a, b) -> a.name.compareTo(b.name)).collect(ImmutableList.toImmutableList());
+
+        assertEquals(
+            ImmutableList.of(
+                this.dummyPluginVersionReqString,
+                this.bazPluginVersionReqString
+            ),
+            edMocker.getInvocationLog().stream().map(
+                invocationEntry -> invocationEntry[1]
+            ).sorted().collect(ImmutableList.toImmutableList())
+        );
+
+        assertEquals(2, updatedPlugins.size());
+
+        assertEquals(updatedPlugins.get(0).name, "baz_plugin");
+        assertEquals("7", updatedPlugins.get(0).localversion);
+
+        assertEquals(updatedPlugins.get(1).name, "dummy_plugin");
+        assertEquals("31772", updatedPlugins.get(1).localversion);
+
+        this.wireMockRule.verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/plugins")));
+        this.wireMockRule.verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/plugin/dummy_plugin.jar")));
+        this.wireMockRule.verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/plugin/baz_plugin.jar")));
+
+        assertEquals(Config.getPref().getInt("pluginmanager.version", 111), 6000);
+        // not mocking the time so just check it's not its original value
+        assertNotEquals(Config.getPref().get("pluginmanager.lastupdate", "999"), "999");
+    }
+
+    /**
+     * test update of plugins when those plugins turn out to require a higher JOSM version, but the
+     * user chooses to update one and skip the other.
+     */
+    @Test
+    public void testUpdatePluginsSkipOne() {
+        final ExtendedDialogMocker edMocker = new ExtendedDialogMocker(ImmutableMap.<String, Object>builder()
+            .put(this.bazPluginVersionReqString, "Download Plugin")
+            .put(this.dummyPluginVersionReqString, "Skip Download")
+            .build()
+        );
+        final HelpAwareOptionPaneMocker haMocker = new HelpAwareOptionPaneMocker(ImmutableMap.<String, Object>builder()
+            .put(this.dummyPluginFailedString, "OK")
+            .build()
+        );
+
+        final List<PluginInformation> updatedPlugins = PluginHandler.updatePlugins(
+            Main.parent,
+            null,
+            null,
+            false
+        ).stream().sorted((a, b) -> a.name.compareTo(b.name)).collect(ImmutableList.toImmutableList());
+
+        assertEquals(
+            ImmutableList.of(
+                this.dummyPluginVersionReqString,
+                this.bazPluginVersionReqString
+            ),
+            edMocker.getInvocationLog().stream().map(
+                invocationEntry -> invocationEntry[1]
+            ).sorted().collect(ImmutableList.toImmutableList())
+        );
+
+        assertEquals(
+            ImmutableList.of(
+                this.dummyPluginFailedString
+            ),
+            haMocker.getInvocationLog().stream().map(
+                invocationEntry -> invocationEntry[1]
+            ).sorted().collect(ImmutableList.toImmutableList())
+        );
+
+        assertEquals(2, updatedPlugins.size());
+
+        assertEquals(updatedPlugins.get(0).name, "baz_plugin");
+        assertEquals("7", updatedPlugins.get(0).localversion);
+
+        assertEquals(updatedPlugins.get(1).name, "dummy_plugin");
+        assertEquals(null, updatedPlugins.get(1).localversion);
+
+        this.wireMockRule.verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/plugins")));
+        this.wireMockRule.verify(0, WireMock.getRequestedFor(WireMock.urlEqualTo("/plugin/dummy_plugin.jar")));
+        this.wireMockRule.verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/plugin/baz_plugin.jar")));
+
+        // shouldn't have been updated
+        assertEquals(Config.getPref().getInt("pluginmanager.version", 111), 999);
+        assertEquals(Config.getPref().get("pluginmanager.lastupdate", "999"), "999");
+    }
+}
