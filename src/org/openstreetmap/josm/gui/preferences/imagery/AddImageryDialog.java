846d60f88140647725b5528c806cd965d3f0a10e
wiktorn <wiktorn@0c6e7542-c601-0410-84e7-c038aed88b3b>
2018-05-15 20:52:56 +0000

    Remember Add Imagery dialog position
    See: #16249
    
    
    
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@13774 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/preferences/imagery/AddImageryDialog.java
+++ src/org/openstreetmap/josm/gui/preferences/imagery/AddImageryDialog.java
@@ -6,14 +6,18 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Component;
 import java.awt.Dimension;
 
+import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.ExtendedDialog;
 import org.openstreetmap.josm.gui.preferences.imagery.AddImageryPanel.ContentValidationListener;
+import org.openstreetmap.josm.gui.util.WindowGeometry;
 
 /**
  * Dialog shown to add a new imagery (WMS/TMS/WMTS) source from imagery preferences.
  * @since 5731
  */
 public class AddImageryDialog extends ExtendedDialog implements ContentValidationListener {
+
+    private final String saveGeometryEntryName;
 
     /**
      * Constructs a new AddImageryDialog.
@@ -28,6 +32,7 @@ public class AddImageryDialog extends ExtendedDialog implements ContentValidatio
         setContent(panel, false);
         setMinimumSize(new Dimension(300, 400));
         panel.addContentValidationListener(this);
+        this.saveGeometryEntryName = panel.getClass().getName() + ".geometry";
     }
 
     @Override
@@ -39,5 +44,16 @@ public class AddImageryDialog extends ExtendedDialog implements ContentValidatio
     @Override
     public void contentChanged(boolean isValid) {
         buttons.get(0).setEnabled(isValid);
+    }
+
+    @Override
+    public void setVisible(boolean visible) {
+        if (visible) {
+            new WindowGeometry(saveGeometryEntryName, WindowGeometry.centerInWindow(Main.parent,
+                    new Dimension(800, 600))).applySafe(this);
+        } else if (isShowing()) { // Avoid IllegalComponentStateException like in #8775
+            new WindowGeometry(this).remember(saveGeometryEntryName);
+        }
+        super.setVisible(visible);
     }
 }
