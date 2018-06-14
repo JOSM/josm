1fbb0460330f342b0d3ac164ed6450871a5e6e3d
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2018-06-14 17:38:30 +0000

    see #11000 - restore old (deprecated) method signatures to avoid plugins breakage
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@13932 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmTask.java
+++ src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmTask.java
@@ -76,9 +76,42 @@ public class DownloadOsmTask extends AbstractDownloadTask<DataSet> {
         }
     }
 
+    /**
+     * Asynchronously launches the download task for a given bounding box.
+     * @param newLayer true, if the data is to be downloaded into a new layer. If false, the task
+     * selects one of the existing layers as download layer, preferably the active layer.
+     *
+     * @param downloadArea the area to download
+     * @param progressMonitor the progressMonitor
+     * @return the future representing the asynchronous task
+     * @deprecated Use {@link #download(DownloadParams, Bounds, ProgressMonitor)}
+     */
+    @Deprecated
+    public Future<?> download(boolean newLayer, Bounds downloadArea, ProgressMonitor progressMonitor) {
+        return download(new BoundingBoxDownloader(downloadArea),
+                new DownloadParams().withNewLayer(newLayer), downloadArea, progressMonitor);
+    }
+
     @Override
     public Future<?> download(DownloadParams settings, Bounds downloadArea, ProgressMonitor progressMonitor) {
         return download(new BoundingBoxDownloader(downloadArea), settings, downloadArea, progressMonitor);
+    }
+
+    /**
+     * Asynchronously launches the download task for a given bounding box.
+     *
+     * @param reader the reader used to parse OSM data (see {@link OsmServerReader#parseOsm})
+     * @param newLayer newLayer true, if the data is to be downloaded into a new layer. If false, the task
+     * selects one of the existing layers as download layer, preferably the active layer.
+     * @param downloadArea the area to download
+     * @param progressMonitor the progressMonitor
+     * @return the future representing the asynchronous task
+     * @deprecated Use {@link #download(OsmServerReader, DownloadParams, Bounds, ProgressMonitor)}
+     */
+    @Deprecated
+    public Future<?> download(OsmServerReader reader, boolean newLayer, Bounds downloadArea, ProgressMonitor progressMonitor) {
+        return download(new DownloadTask(new DownloadParams().withNewLayer(newLayer), reader, progressMonitor, zoomAfterDownload),
+                downloadArea);
     }
 
     /**
@@ -116,6 +149,7 @@ public class DownloadOsmTask extends AbstractDownloadTask<DataSet> {
      * @param downloadArea the area to download
      * @param progressMonitor the progressMonitor
      * @return the future representing the asynchronous task
+     * @since 13927
      */
     public Future<?> download(OsmServerReader reader, DownloadParams settings, Bounds downloadArea, ProgressMonitor progressMonitor) {
         return download(new DownloadTask(settings, reader, progressMonitor, zoomAfterDownload), downloadArea);
