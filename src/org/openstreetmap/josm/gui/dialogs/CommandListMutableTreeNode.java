JOSM/josm

commit e96444403cd1f6faa549daf1b0de4ceb597a6568

fix #16260 - fix major performance problems seen during first JOSM launch: - new API to notify about undo/redo changes - use this API in Command Stack dialog to alter lightly the undo/redo trees instead of rebuilding them entirely at each command (very costly operation for large trees) - purge the command stack after computing the left/right traffic data to avoid starting JOSM with already many thousands nodes in undo/redo trees - avoid memory leak with JoinAreasAction when computing the left/right traffic data
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13729 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/CommandStackDialog.java
@@ -32,13 +32,18 @@ import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
 import org.openstreetmap.josm.actions.AutoScaleAction;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.PseudoCommand;
-import org.openstreetmap.josm.data.UndoRedoHandler.CommandQueueListener;
+import org.openstreetmap.josm.data.UndoRedoHandler.CommandAddedEvent;
+import org.openstreetmap.josm.data.UndoRedoHandler.CommandQueueCleanedEvent;
+import org.openstreetmap.josm.data.UndoRedoHandler.CommandQueuePreciseListener;
+import org.openstreetmap.josm.data.UndoRedoHandler.CommandRedoneEvent;
+import org.openstreetmap.josm.data.UndoRedoHandler.CommandUndoneEvent;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.MainApplication;
@@ -55,13 +60,16 @@ import org.openstreetmap.josm.tools.SubclassFilteredCollection;
  * Dialog displaying list of all executed commands (undo/redo buffer).
  * @since 94
  */
-public class CommandStackDialog extends ToggleDialog implements CommandQueueListener {
+public class CommandStackDialog extends ToggleDialog implements CommandQueuePreciseListener {
 
     private final DefaultTreeModel undoTreeModel = new DefaultTreeModel(new DefaultMutableTreeNode());
     private final DefaultTreeModel redoTreeModel = new DefaultTreeModel(new DefaultMutableTreeNode());
 
     private final JTree undoTree = new JTree(undoTreeModel);
     private final JTree redoTree = new JTree(redoTreeModel);
+
+    private DefaultMutableTreeNode undoRoot;
+    private DefaultMutableTreeNode redoRoot;
 
     private final transient UndoRedoSelectionListener undoSelectionListener;
     private final transient UndoRedoSelectionListener redoSelectionListener;
@@ -228,7 +236,7 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
         for (IEnabledStateUpdating listener : showNotifyListener) {
             listener.updateEnabledState();
         }
-        MainApplication.undoRedo.addCommandQueueListener(this);
+        MainApplication.undoRedo.addCommandQueuePreciseListener(this);
     }
 
     /**
@@ -242,9 +250,9 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
 
     @Override
     public void hideNotify() {
-        undoTreeModel.setRoot(new DefaultMutableTreeNode());
-        redoTreeModel.setRoot(new DefaultMutableTreeNode());
-        MainApplication.undoRedo.removeCommandQueueListener(this);
+        undoTreeModel.setRoot(undoRoot = new DefaultMutableTreeNode());
+        redoTreeModel.setRoot(redoRoot = new DefaultMutableTreeNode());
+        MainApplication.undoRedo.removeCommandQueuePreciseListener(this);
     }
 
     /**
@@ -253,22 +261,32 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
      */
     private void buildTrees() {
         setTitle(tr("Command Stack"));
-        if (MainApplication.getLayerManager().getEditLayer() == null)
-            return;
+        buildUndoTree();
+        buildRedoTree();
+        ensureTreesConsistency();
+    }
 
+    private void buildUndoTree() {
         List<Command> undoCommands = MainApplication.undoRedo.commands;
-        DefaultMutableTreeNode undoRoot = new DefaultMutableTreeNode();
+        undoRoot = new DefaultMutableTreeNode();
         for (int i = 0; i < undoCommands.size(); ++i) {
             undoRoot.add(getNodeForCommand(undoCommands.get(i), i));
         }
         undoTreeModel.setRoot(undoRoot);
+    }
 
+    private void buildRedoTree() {
         List<Command> redoCommands = MainApplication.undoRedo.redoCommands;
-        DefaultMutableTreeNode redoRoot = new DefaultMutableTreeNode();
+        redoRoot = new DefaultMutableTreeNode();
         for (int i = 0; i < redoCommands.size(); ++i) {
             redoRoot.add(getNodeForCommand(redoCommands.get(i), i));
         }
         redoTreeModel.setRoot(redoRoot);
+    }
+
+    private void ensureTreesConsistency() {
+        List<Command> undoCommands = MainApplication.undoRedo.commands;
+        List<Command> redoCommands = MainApplication.undoRedo.redoCommands;
         if (redoTreeModel.getChildCount(redoRoot) > 0) {
             redoTree.scrollRowToVisible(0);
             scrollPane.getHorizontalScrollBar().setValue(0);
@@ -341,10 +359,43 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueueList
     }
 
     @Override
-    public void commandChanged(int queueSize, int redoSize) {
-        if (!isVisible())
-            return;
-        buildTrees();
+    public void cleaned(CommandQueueCleanedEvent e) {
+        if (isVisible()) {
+            buildTrees();
+        }
+    }
+
+    @Override
+    public void commandAdded(CommandAddedEvent e) {
+        if (isVisible()) {
+            undoRoot.add(getNodeForCommand(e.getCommand(), undoRoot.getChildCount()));
+            undoTreeModel.nodeStructureChanged(undoRoot);
+            ensureTreesConsistency();
+        }
+    }
+
+    @Override
+    public void commandUndone(CommandUndoneEvent e) {
+        if (isVisible()) {
+            swapNode(undoTreeModel, undoRoot, undoRoot.getChildCount() - 1, redoTreeModel, redoRoot, 0);
+        }
+    }
+
+    @Override
+    public void commandRedone(CommandRedoneEvent e) {
+        if (isVisible()) {
+            swapNode(redoTreeModel, redoRoot, 0, undoTreeModel, undoRoot, undoRoot.getChildCount());
+        }
+    }
+
+    private void swapNode(DefaultTreeModel srcModel, DefaultMutableTreeNode srcRoot, int srcIndex,
+                          DefaultTreeModel dstModel, DefaultMutableTreeNode dstRoot, int dstIndex) {
+        MutableTreeNode node = (MutableTreeNode) srcRoot.getChildAt(srcIndex);
+        srcRoot.remove(node);
+        srcModel.nodeStructureChanged(srcRoot);
+        dstRoot.insert(node, dstIndex);
+        dstModel.nodeStructureChanged(dstRoot);
+        ensureTreesConsistency();
     }
 
     /**

src/org/openstreetmap/josm/actions/JoinAreasAction.java
@@ -455,14 +455,14 @@ public class JoinAreasAction extends JosmAction {
     }
 
     /**
-     * Constructs a new {@code JoinAreasAction} with optional shortcut.
-     * @param addShortcut controls whether the shortcut should be registered or not, as for toolbar registration
+     * Constructs a new {@code JoinAreasAction} with optional shortcut and adapters.
+     * @param addShortcutToolbarAdapters controls whether the shortcut should be registered or not, as for toolbar registration and adapters
      * @since 11611
      */
-    public JoinAreasAction(boolean addShortcut) {
-        super(tr("Join overlapping Areas"), "joinareas", tr("Joins areas that overlap each other"), addShortcut ?
+    public JoinAreasAction(boolean addShortcutToolbarAdapters) {
+        super(tr("Join overlapping Areas"), "joinareas", tr("Joins areas that overlap each other"), addShortcutToolbarAdapters ?
         Shortcut.registerShortcut("tools:joinareas", tr("Tool: {0}", tr("Join overlapping Areas")), KeyEvent.VK_J, Shortcut.SHIFT)
-        : null, addShortcut);
+        : null, addShortcutToolbarAdapters, null, addShortcutToolbarAdapters);
     }
 
     /**

src/org/openstreetmap/josm/data/UndoRedoHandler.java
@@ -1,8 +1,10 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.data;
 
+import java.util.EventObject;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Objects;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.command.Command;
@@ -29,6 +31,7 @@ public class UndoRedoHandler {
     public final LinkedList<Command> redoCommands = new LinkedList<>();
 
     private final LinkedList<CommandQueueListener> listenerCommands = new LinkedList<>();
+    private final LinkedList<CommandQueuePreciseListener> preciseListenerCommands = new LinkedList<>();
 
     /**
      * Constructs a new {@code UndoRedoHandler}.
@@ -38,7 +41,8 @@ public class UndoRedoHandler {
     }
 
     /**
-     * A listener that gets notified of command queue (undo/redo) size changes.
+     * A simple listener that gets notified of command queue (undo/redo) size changes.
+     * @see CommandQueuePreciseListener
      * @since 12718 (moved from {@code OsmDataLayer}
      */
     @FunctionalInterface
@@ -49,6 +53,163 @@ public class UndoRedoHandler {
          * @param redoSize Redo stack size
          */
         void commandChanged(int queueSize, int redoSize);
+    }
+
+    /**
+     * A listener that gets notified of command queue (undo/redo) operations individually.
+     * @see CommandQueueListener
+     * @since 13729
+     */
+    public interface CommandQueuePreciseListener {
+
+        /**
+         * Notifies the listener about a new command added to the queue.
+         * @param e event
+         */
+        void commandAdded(CommandAddedEvent e);
+
+        /**
+         * Notifies the listener about commands being cleaned.
+         * @param e event
+         */
+        void cleaned(CommandQueueCleanedEvent e);
+
+        /**
+         * Notifies the listener about a command that has been undone.
+         * @param e event
+         */
+        void commandUndone(CommandUndoneEvent e);
+
+        /**
+         * Notifies the listener about a command that has been redone.
+         * @param e event
+         */
+        void commandRedone(CommandRedoneEvent e);
+    }
+
+    abstract static class CommandQueueEvent extends EventObject {
+        protected CommandQueueEvent(UndoRedoHandler source) {
+            super(Objects.requireNonNull(source));
+        }
+
+        /**
+         * Calls the appropriate method of the listener for this event.
+         * @param listener dataset listener to notify about this event
+         */
+        abstract void fire(CommandQueuePreciseListener listener);
+
+        @Override
+        public final UndoRedoHandler getSource() {
+            return (UndoRedoHandler) super.getSource();
+        }
+    }
+
+    /**
+     * Event fired after a command has been added to the command queue.
+     * @since xxx
+     */
+    public static final class CommandAddedEvent extends CommandQueueEvent {
+
+        private final Command cmd;
+
+        private CommandAddedEvent(UndoRedoHandler source, Command cmd) {
+            super(source);
+            this.cmd = Objects.requireNonNull(cmd);
+        }
+
+        /**
+         * Returns the added command.
+         * @return the added command
+         */
+        public Command getCommand() {
+            return cmd;
+        }
+
+        @Override
+        void fire(CommandQueuePreciseListener listener) {
+            listener.commandAdded(this);
+        }
+    }
+
+    /**
+     * Event fired after the command queue has been cleaned.
+     * @since xxx
+     */
+    public static final class CommandQueueCleanedEvent extends CommandQueueEvent {
+
+        private final DataSet ds;
+
+        private CommandQueueCleanedEvent(UndoRedoHandler source, DataSet ds) {
+            super(source);
+            this.ds = ds;
+        }
+
+        /**
+         * Returns the affected dataset.
+         * @return the affected dataset, or null if the queue has been globally emptied
+         */
+        public DataSet getDataSet() {
+            return ds;
+        }
+
+        @Override
+        void fire(CommandQueuePreciseListener listener) {
+            listener.cleaned(this);
+        }
+    }
+
+    /**
+     * Event fired after a command has been undone.
+     * @since xxx
+     */
+    public static final class CommandUndoneEvent extends CommandQueueEvent {
+
+        private final Command cmd;
+
+        private CommandUndoneEvent(UndoRedoHandler source, Command cmd) {
+            super(source);
+            this.cmd = Objects.requireNonNull(cmd);
+        }
+
+        /**
+         * Returns the undone command.
+         * @return the undone command
+         */
+        public Command getCommand() {
+            return cmd;
+        }
+
+        @Override
+        void fire(CommandQueuePreciseListener listener) {
+            listener.commandUndone(this);
+        }
+    }
+
+    /**
+     * Event fired after a command has been redone.
+     * @since xxx
+     */
+    public static final class CommandRedoneEvent extends CommandQueueEvent {
+
+        private final Command cmd;
+
+        private CommandRedoneEvent(UndoRedoHandler source, Command cmd) {
+            super(source);
+            this.cmd = Objects.requireNonNull(cmd);
+        }
+
+        /**
+         * Returns the redone command.
+         * @return the redone command
+         */
+        public Command getCommand() {
+            return cmd;
+        }
+
+        @Override
+        void fire(CommandQueuePreciseListener listener) {
+            listener.commandRedone(this);
+        }
     }
 
     /**
@@ -79,8 +240,12 @@ public class UndoRedoHandler {
 
     /**
      * Fires a commands change event after adding a command.
+     * @param cmd command added
      */
-    public void afterAdd() {
+    public void afterAdd(Command cmd) {
+        if (cmd != null) {
+            fireEvent(new CommandAddedEvent(this, cmd));
+        }
         fireCommandsChanged();
     }
 
@@ -90,7 +255,7 @@ public class UndoRedoHandler {
      */
     public synchronized void add(final Command c) {
         addNoRedraw(c);
-        afterAdd();
+        afterAdd(c);
     }
 
     /**
@@ -116,6 +281,7 @@ public class UndoRedoHandler {
                 final Command c = commands.removeLast();
                 c.undoCommand();
                 redoCommands.addFirst(c);
+                fireEvent(new CommandUndoneEvent(this, c));
                 if (commands.isEmpty()) {
                     break;
                 }
@@ -146,6 +312,7 @@ public class UndoRedoHandler {
             final Command c = redoCommands.removeFirst();
             c.executeCommand();
             commands.add(c);
+            fireEvent(new CommandRedoneEvent(this, c));
             if (redoCommands.isEmpty()) {
                 break;
             }
@@ -162,12 +329,17 @@ public class UndoRedoHandler {
         }
     }
 
+    private void fireEvent(CommandQueueEvent e) {
+        preciseListenerCommands.forEach(e::fire);
+    }
+
     /**
      * Resets the undo/redo list.
      */
     public void clean() {
         redoCommands.clear();
         commands.clear();
+        fireEvent(new CommandQueueCleanedEvent(this, null));
         fireCommandsChanged();
     }
 
@@ -193,6 +365,7 @@ public class UndoRedoHandler {
             }
         }
         if (changed) {
+            fireEvent(new CommandQueueCleanedEvent(this, dataSet));
             fireCommandsChanged();
         }
     }
@@ -207,10 +380,29 @@ public class UndoRedoHandler {
 
     /**
      * Adds a command queue listener.
-     * @param l The commands queue listener to add
+     * @param l The command queue listener to add
      * @return {@code true} if the listener has been added, {@code false} otherwise
      */
     public boolean addCommandQueueListener(CommandQueueListener l) {
         return listenerCommands.add(l);
+    }
+
+    /**
+     * Removes a precise command queue listener.
+     * @param l The precise command queue listener to remove
+     * @since 13729
+     */
+    public void removeCommandQueuePreciseListener(CommandQueuePreciseListener l) {
+        preciseListenerCommands.remove(l);
+    }
+
+    /**
+     * Adds a precise command queue listener.
+     * @param l The precise command queue listener to add
+     * @return {@code true} if the listener has been added, {@code false} otherwise
+     * @since 13729
+     */
+    public boolean addCommandQueuePreciseListener(CommandQueuePreciseListener l) {
+        return preciseListenerCommands.add(l);
     }
 }

src/org/openstreetmap/josm/gui/dialogs/ValidatorDialog.java
@@ -646,7 +646,7 @@ public class ValidatorDialog extends ToggleDialog implements SelectionChangedLis
                 }
                 monitor.subTask(tr("Updating map ..."));
                 SwingUtilities.invokeAndWait(() -> {
-                    MainApplication.undoRedo.afterAdd();
+                    MainApplication.undoRedo.afterAdd(null);
                     invalidateValidatorLayers();
                     tree.resetErrors();
                 });

src/org/openstreetmap/josm/tools/RightAndLefthandTraffic.java
@@ -29,6 +29,7 @@ import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.UploadPolicy;
 import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.io.IllegalDataException;
 import org.openstreetmap.josm.io.OsmReader;
 import org.openstreetmap.josm.io.OsmWriter;
@@ -134,6 +135,8 @@ public final class RightAndLefthandTraffic {
             Logging.warn("Unable to join left-driving countries polygons");
             optimizedWays.addAll(ways);
         }
+        // Clean command stack
+        MainApplication.undoRedo.clean(data);
         return optimizedWays;
     }
 

commit 123bf3b8b0de207920985a060b028e3b8763f3b2

fix #16389 - fix wrong undo/redo behaviour in command stack dialog (regression from r13729)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13943 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/CommandListMutableTreeNode.java
@@ -12,17 +12,14 @@ import org.openstreetmap.josm.command.PseudoCommand;
 public class CommandListMutableTreeNode extends DefaultMutableTreeNode {
 
     protected final transient PseudoCommand cmd;
-    protected final int idx;
 
     /**
      * Constructs a new {@code CommandListMutableTreeNode}.
      * @param cmd command
-     * @param idx index
      */
-    public CommandListMutableTreeNode(PseudoCommand cmd, int idx) {
+    public CommandListMutableTreeNode(PseudoCommand cmd) {
         super(new JLabel(cmd.getDescriptionText(), cmd.getDescriptionIcon(), JLabel.HORIZONTAL));
         this.cmd = cmd;
-        this.idx = idx;
     }
 
     /**
@@ -38,6 +35,6 @@ public class CommandListMutableTreeNode extends DefaultMutableTreeNode {
      * @return the index
      */
     public int getIndex() {
-        return idx;
+        return getParent().getIndex(this);
     }
 }

src/org/openstreetmap/josm/gui/dialogs/CommandStackDialog.java
@@ -272,7 +272,7 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueuePrec
         List<Command> undoCommands = MainApplication.undoRedo.commands;
         undoRoot = new DefaultMutableTreeNode();
         for (int i = 0; i < undoCommands.size(); ++i) {
-            undoRoot.add(getNodeForCommand(undoCommands.get(i), i));
+            undoRoot.add(getNodeForCommand(undoCommands.get(i)));
         }
         undoTreeModel.setRoot(undoRoot);
     }
@@ -281,7 +281,7 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueuePrec
         List<Command> redoCommands = MainApplication.undoRedo.redoCommands;
         redoRoot = new DefaultMutableTreeNode();
         for (int i = 0; i < redoCommands.size(); ++i) {
-            redoRoot.add(getNodeForCommand(redoCommands.get(i), i));
+            redoRoot.add(getNodeForCommand(redoCommands.get(i)));
         }
         redoTreeModel.setRoot(redoRoot);
     }
@@ -329,15 +329,14 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueuePrec
      * Wraps a command in a CommandListMutableTreeNode.
      * Recursively adds child commands.
      * @param c the command
-     * @param idx index
      * @return the resulting node
      */
-    protected CommandListMutableTreeNode getNodeForCommand(PseudoCommand c, int idx) {
-        CommandListMutableTreeNode node = new CommandListMutableTreeNode(c, idx);
+    protected CommandListMutableTreeNode getNodeForCommand(PseudoCommand c) {
+        CommandListMutableTreeNode node = new CommandListMutableTreeNode(c);
         if (c.getChildren() != null) {
             List<PseudoCommand> children = new ArrayList<>(c.getChildren());
             for (int i = 0; i < children.size(); ++i) {
-                node.add(getNodeForCommand(children.get(i), i));
+                node.add(getNodeForCommand(children.get(i)));
             }
         }
         return node;
@@ -370,7 +369,7 @@ public class CommandStackDialog extends ToggleDialog implements CommandQueuePrec
     @Override
     public void commandAdded(CommandAddedEvent e) {
         if (isVisible()) {
-            undoRoot.add(getNodeForCommand(e.getCommand(), undoRoot.getChildCount()));
+            undoRoot.add(getNodeForCommand(e.getCommand()));
             undoTreeModel.nodeStructureChanged(undoRoot);
             ensureTreesConsistency();
         }