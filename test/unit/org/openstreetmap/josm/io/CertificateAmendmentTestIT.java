5738f0ef481635ede2cb1aa88826644886a084d5
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2018-08-05 14:51:25 +0000

    Most of certificate amendment tests are integration tests, not unit tests (Overpass API certificate has expired)
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@14089 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ test/unit/org/openstreetmap/josm/io/CertificateAmendmentTestIT.java
@@ -0,0 +1,99 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.io;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.SSLHandshakeException;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+/**
+ * Integration tests of {@link CertificateAmendment} class.
+ */
+public class CertificateAmendmentTestIT {
+
+    /**
+     * Setup rule
+     */
+    @Rule
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public JOSMTestRules test = new JOSMTestRules().platform().https().timeout(20000);
+
+    /**
+     * Test a well-known certificate.
+     * @throws IOException in case of I/O error
+     */
+    @Test
+    public void testDefault() throws IOException {
+        // something that is not embedded
+        connect("https://www.bing.com", true);
+    }
+
+    /**
+     * Test <a href="https://letsencrypt.org">Let's Encrypt</a>.
+     * @throws IOException in case of I/O error
+     */
+    @Test
+    public void testLetsEncrypt() throws IOException {
+        // signed by letsencrypt's own ISRG root
+        // (not included yet)
+        // TODO: they switched to cross-sign CA, re-enable it if ISRG root is used again
+        // connect("https://helloworld.letsencrypt.org", false);
+        // signed by letsencrypt's cross-sign CA
+        connect("https://letsencrypt.org", true);
+        // signed by letsencrypt's cross-sign CA, requires SNI
+        connect("https://acme-v01.api.letsencrypt.org", true);
+    }
+
+    /**
+     * Test a broken certificate.
+     * @throws IOException in case of I/O error
+     */
+    @Test
+    public void testBrokenCert() throws IOException {
+        // broken at the moment (may get fixed some day)
+        connect("https://www.pcwebshop.co.uk", false);
+    }
+
+    /**
+     * Test overpass API.
+     * @throws IOException in case of I/O error
+     */
+    @Test
+    public void testOverpass() throws IOException {
+        connect("https://overpass-api.de", true);
+    }
+
+    /**
+     * Test Dutch government.
+     * @throws IOException in case of I/O error
+     */
+    @Test
+    public void testDutchGovernment() throws IOException {
+        connect("https://geodata.nationaalgeoregister.nl", true);
+    }
+
+    private static void connect(String url, boolean shouldWork) throws IOException {
+        URLConnection connection = new URL(url).openConnection();
+        try {
+            connection.connect();
+        } catch (SSLHandshakeException e) {
+            if (shouldWork) {
+                e.printStackTrace();
+                Assert.fail("Untrusted: " + url);
+            } else {
+                return;
+            }
+        }
+        if (!shouldWork) {
+            Assert.fail("Expected error: " + url);
+        }
+    }
+}
