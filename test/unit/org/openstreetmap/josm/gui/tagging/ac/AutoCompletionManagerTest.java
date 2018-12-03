38529901267abe125832afb5d005300a4d76c882
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2018-12-03 18:49:06 +0000

    see #17064 - add non regression test
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@14503 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/tagging/ac/AutoCompletionManagerTest.java
+++ test/unit/org/openstreetmap/josm/gui/tagging/ac/AutoCompletionManagerTest.java
@@ -4,6 +4,9 @@ package org.openstreetmap.josm.gui.tagging.ac;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openstreetmap.josm.TestUtils;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.gui.tagging.ac.AutoCompletionManager.UserInputTag;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
 
@@ -21,6 +24,18 @@ public class AutoCompletionManagerTest {
     @Rule
     @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
     public JOSMTestRules test = new JOSMTestRules();
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/17064">#17064</a>.
+     */
+    @Test
+    public void testTicket17064() {
+        DataSet ds = new DataSet();
+        OsmDataLayer layer = new OsmDataLayer(ds, "testTicket17064", null);
+        MainApplication.getLayerManager().addLayer(layer);
+        AutoCompletionManager.of(ds);
+        MainApplication.getLayerManager().removeLayer(layer); // NPE in #17064
+    }
 
     /**
      * Unit test of methods {@link UserInputTag#equals} and {@link UserInputTag#hashCode}.
