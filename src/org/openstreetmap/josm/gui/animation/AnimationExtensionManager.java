4f1c81b3312497274778250a459f2094dd0f06d7
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2018-12-20 23:22:27 +0000

    It's Christmas time! Animate our "Getting Started" page with shining stars.
    Adapted from Icedtea-Web, original code by Jiri Vanek (Red Hat).
    See http://icedtea.classpath.org/hg/icedtea-web/rev/87d3081ab573
    
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@14578 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/gui/animation/AnimationExtensionManager.java
@@ -0,0 +1,38 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.animation;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Animation extension manager. Copied from Icedtea-Web.
+ * @author Jiri Vanek (Red Hat)
+ * @see <a href="http://icedtea.classpath.org/hg/icedtea-web/rev/87d3081ab573">Initial commit</a>
+ * @since 14578
+ */
+public final class AnimationExtensionManager {
+
+    private static AnimationExtension currentExtension;
+
+    private AnimationExtensionManager() {
+        // Hide default constructor for utility classes
+    }
+
+    /**
+     * Returns the current animation extension.
+     * @return the current animation extension
+     */
+    public static AnimationExtension getExtension() {
+        if (currentExtension == null) {
+            currentExtension = isChristmas() ? new ChristmasExtension() : new NoExtension();
+        }
+        return currentExtension;
+    }
+
+    private static boolean isChristmas() {
+        Calendar c = new GregorianCalendar();
+        c.setTime(new Date());
+        return c.get(Calendar.DAY_OF_YEAR) > 350;
+    }
+}
---
+++ src/org/openstreetmap/josm/gui/animation/NoExtension.java
@@ -0,0 +1,28 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.animation;
+
+import java.awt.Graphics;
+
+/**
+ * Animation no-op extension. Copied from Icedtea-Web.
+ * @author Jiri Vanek (Red Hat)
+ * @see <a href="http://icedtea.classpath.org/hg/icedtea-web/rev/87d3081ab573">Initial commit</a>
+ * @since 14578
+ */
+public class NoExtension implements AnimationExtension {
+
+    NoExtension() {
+    }
+
+    @Override
+    public void adjustForSize(int w, int h) {
+    }
+
+    @Override
+    public void animate() {
+    }
+
+    @Override
+    public void paint(Graphics g) {
+    }
+}
