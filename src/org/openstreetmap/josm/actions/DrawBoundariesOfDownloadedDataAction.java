c80a277e286d564da9030b37354a86f946340d0b
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2019-01-05 21:03:23 +0000

    fix #17067 - Duplicate settings for display of hatching area
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@14648 0c6e7542-c601-0410-84e7-c038aed88b3b

---
+++ src/org/openstreetmap/josm/actions/DrawBoundariesOfDownloadedDataAction.java
@@ -0,0 +1,42 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.actions;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.event.ActionEvent;
+
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.preferences.display.DrawingPreference;
+
+/**
+ * This class toggles whether to draw boundaries of downloaded data.
+ *
+ * @since 14648
+ */
+public class DrawBoundariesOfDownloadedDataAction extends PreferenceToggleAction {
+
+    /**
+     * Constructs a new {@link DrawBoundariesOfDownloadedDataAction}.
+     */
+    public DrawBoundariesOfDownloadedDataAction() {
+        super(tr("Draw boundaries of downloaded data"),
+                tr("Enable/disable hatched background rendering of areas outside of the downloaded areas."),
+                DrawingPreference.SOURCE_BOUNDS_PROP.getKey(),
+                DrawingPreference.SOURCE_BOUNDS_PROP.getDefaultValue()
+        );
+    }
+
+    @Override
+    protected void updateEnabledState() {
+        setEnabled(MainApplication.getLayerManager().getEditLayer() != null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        super.actionPerformed(e);
+        if (MainApplication.isDisplayingMapView()) {
+            MainApplication.getMap().mapView.repaint();
+        }
+    }
+
+}
--- src/org/openstreetmap/josm/gui/MainMenu.java
+++ src/org/openstreetmap/josm/gui/MainMenu.java
@@ -52,7 +52,7 @@ import org.openstreetmap.josm.actions.ExpertToggleAction;
 import org.openstreetmap.josm.actions.FollowLineAction;
 import org.openstreetmap.josm.actions.FullscreenToggleAction;
 import org.openstreetmap.josm.actions.GpxExportAction;
-import org.openstreetmap.josm.actions.HatchAreaOutsideDownloadAction;
+import org.openstreetmap.josm.actions.DrawBoundariesOfDownloadedDataAction;
 import org.openstreetmap.josm.actions.HelpAction;
 import org.openstreetmap.josm.actions.HistoryInfoAction;
 import org.openstreetmap.josm.actions.HistoryInfoWebAction;
@@ -222,7 +222,7 @@ public class MainMenu extends JMenuBar {
     /** View / Wireframe View */
     public final WireframeToggleAction wireFrameToggleAction = new WireframeToggleAction();
     /** View / Hatch area outside download */
-    public final HatchAreaOutsideDownloadAction hatchAreaOutsideDownloadAction = new HatchAreaOutsideDownloadAction();
+    public final DrawBoundariesOfDownloadedDataAction drawBoundariesOfDownloadedDataAction = new DrawBoundariesOfDownloadedDataAction();
     /** View / Advanced info */
     public final InfoAction info = new InfoAction();
     /** View / Advanced info (web) */
@@ -718,7 +718,7 @@ public class MainMenu extends JMenuBar {
         viewMenu.add(wireframe);
         wireframe.setAccelerator(wireFrameToggleAction.getShortcut().getKeyStroke());
         wireFrameToggleAction.addButtonModel(wireframe.getModel());
-        final JCheckBoxMenuItem hatchAreaOutsideDownloadMenuItem = hatchAreaOutsideDownloadAction.getCheckbox();
+        final JCheckBoxMenuItem hatchAreaOutsideDownloadMenuItem = drawBoundariesOfDownloadedDataAction.getCheckbox();
         viewMenu.add(hatchAreaOutsideDownloadMenuItem);
         ExpertToggleAction.addVisibilitySwitcher(hatchAreaOutsideDownloadMenuItem);
 
