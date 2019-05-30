398d5f0a687f3970590b3f9d06127faa502c4707
GerdP <GerdP@0c6e7542-c601-0410-84e7-c038aed88b3b>
2019-05-30 08:01:42 +0000

    see #17767 and #17768: add non regression unit tests Test `testTicket17768()` fails with current code and is ignored for now.
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@15141 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/actions/CreateMultipolygonActionTest.java
+++ test/unit/org/openstreetmap/josm/actions/CreateMultipolygonActionTest.java
@@ -2,6 +2,8 @@
 package org.openstreetmap.josm.actions;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,17 +11,22 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openstreetmap.josm.TestUtils;
 import org.openstreetmap.josm.command.SequenceCommand;
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.data.osm.search.SearchCompiler;
 import org.openstreetmap.josm.data.osm.search.SearchParseError;
 import org.openstreetmap.josm.data.osm.search.SearchSetting;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.layer.Layer;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.io.OsmReader;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
 import org.openstreetmap.josm.tools.Pair;
@@ -37,7 +44,7 @@ public class CreateMultipolygonActionTest {
      */
     @Rule
     @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-    public JOSMTestRules test = new JOSMTestRules().projection();
+    public JOSMTestRules test = new JOSMTestRules().projection().main().preferences();
 
     private static Map<String, String> getRefToRoleMap(Relation relation) {
         Map<String, String> refToRole = new TreeMap<>();
@@ -105,5 +112,49 @@ public class CreateMultipolygonActionTest {
         assertEquals("{1=outer, 1.1.1=inner}", getRefToRoleMap(mp).toString());
         Relation mp2 = createMultipolygon(ds.getWays(), "ref=1.1 OR ref=1.2 OR ref=1.1.2", mp, false);
         assertEquals("{1=outer, 1.1=inner, 1.1.1=outer, 1.1.2=outer, 1.2=inner}", getRefToRoleMap(mp2).toString());
+    }
+
+    /**
+     * Non-regression test for <a href="https://josm.openstreetmap.de/ticket/17767">Bug #17767</a>.
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void testTicket17767() throws Exception {
+        DataSet ds = OsmReader.parseDataSet(TestUtils.getRegressionDataStream(17767, "upd-mp.osm"), null);
+        Layer layer = new OsmDataLayer(ds, null, null);
+        MainApplication.getLayerManager().addLayer(layer);
+        try {
+            CreateMultipolygonAction updateAction = new CreateMultipolygonAction(true);
+            CreateMultipolygonAction createAction = new CreateMultipolygonAction(false);
+            assertFalse(updateAction.isEnabled());
+            assertFalse(createAction.isEnabled());
+            ds.setSelected(ds.getPrimitiveById(189944949L, OsmPrimitiveType.WAY));
+            assertFalse(updateAction.isEnabled());
+            assertTrue(createAction.isEnabled());
+        } finally {
+            MainApplication.getLayerManager().removeLayer(layer);
+        }
+    }
+
+    /**
+     * Non-regression test for <a href="https://josm.openstreetmap.de/ticket/17768">Bug #17768</a>.
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Ignore
+    public void testTicket17768() throws Exception {
+        DataSet ds = OsmReader.parseDataSet(TestUtils.getRegressionDataStream(17768, "dupmem.osm"), null);
+        Layer layer = new OsmDataLayer(ds, null, null);
+        MainApplication.getLayerManager().addLayer(layer);
+        try {
+            Relation old = (Relation) ds.getPrimitiveById(580092, OsmPrimitiveType.RELATION);
+            assertEquals(3, old.getMembersCount());
+            Relation mp = createMultipolygon(ds.getWays(), "type:way", old, true);
+            assertEquals(mp.getPrimitiveId(), old.getPrimitiveId());
+            assertEquals(2, mp.getMembersCount());
+        } finally {
+            MainApplication.getLayerManager().removeLayer(layer);
+        }
+
     }
 }
