4d25bc2ee740c38e4069395f4e309373b3b0115f
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2019-07-06 21:39:16 +0000

    see #17871 - add basic unit test
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@15216 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/actions/downloadtasks/DownloadTaskListTest.java
+++ test/unit/org/openstreetmap/josm/actions/downloadtasks/DownloadTaskListTest.java
@@ -1,10 +1,16 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.actions.downloadtasks;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.awt.geom.Area;
+import java.util.Collections;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.gui.progress.NullProgressMonitor;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -27,5 +33,23 @@ public class DownloadTaskListTest {
     @Test
     public void testDownloadTaskList() {
         assertTrue(new DownloadTaskList().getDownloadedPrimitives().isEmpty());
+    }
+
+    /**
+     * Unit test of {@code DownloadTaskList#download} - empty cases.
+     * @throws Exception in case of error
+     */
+    @Test
+    public void testDownloadAreaEmpty() throws Exception {
+        DownloadTaskList list = new DownloadTaskList();
+        assertNull(list.download(false,
+                Collections.<Area>emptyList(), true, true, NullProgressMonitor.INSTANCE).get());
+        assertTrue(list.getDownloadedPrimitives().isEmpty());
+        assertNull(list.download(false,
+                Collections.<Area>emptyList(), false, false, NullProgressMonitor.INSTANCE).get());
+        assertTrue(list.getDownloadedPrimitives().isEmpty());
+        assertNull(list.download(false,
+                Collections.<Area>singletonList(new Area(new Bounds(0, 0, true).asRect())), false, false, NullProgressMonitor.INSTANCE).get());
+        assertTrue(list.getDownloadedPrimitives().isEmpty());
     }
 }
