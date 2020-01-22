c764d41833e9bf4e1ff076266f385c6b9154742e
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-01-22 19:57:57 +0000

    SimilarNamedWaysTest: simplify by joining tests
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@15749 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/validation/tests/SimilarNamedWaysTest.java
+++ test/unit/org/openstreetmap/josm/data/validation/tests/SimilarNamedWaysTest.java
@@ -3,18 +3,10 @@ package org.openstreetmap.josm.data.validation.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.openstreetmap.josm.JOSMFixture;
-import org.openstreetmap.josm.data.coor.LatLon;
-import org.openstreetmap.josm.data.osm.DataSet;
-import org.openstreetmap.josm.data.osm.Node;
-import org.openstreetmap.josm.data.osm.Way;
-import org.openstreetmap.josm.data.validation.TestError;
 
 /**
  * Unit test of {@link SimilarNamedWays}
@@ -31,57 +23,6 @@ public class SimilarNamedWaysTest {
         JOSMFixture.createUnitTestFixture().init();
     }
 
-    private static List<TestError> testWays(String namea, String nameb) {
-        DataSet ds = new DataSet();
-
-        Node n00 = new Node(LatLon.ZERO);
-        Node n10 = new Node(new LatLon(1, 0));
-        Node n20 = new Node(new LatLon(2, 0));
-        Node n30 = new Node(new LatLon(3, 0));
-        Node n40 = new Node(new LatLon(4, 0));
-
-        ds.addPrimitive(n00);
-        ds.addPrimitive(n10);
-        ds.addPrimitive(n20);
-        ds.addPrimitive(n30);
-        ds.addPrimitive(n40);
-
-        Way waya = new Way();
-        waya.addNode(n00);
-        waya.addNode(n10);
-        waya.addNode(n20);
-        waya.put("name", namea);
-        Way wayb = new Way();
-        wayb.addNode(n20);
-        wayb.addNode(n30);
-        wayb.addNode(n40);
-        wayb.put("name", nameb);
-
-        ds.addPrimitive(waya);
-        ds.addPrimitive(wayb);
-
-        assertTrue(waya.isUsable());
-        assertTrue(wayb.isUsable());
-
-        SimilarNamedWays t = new SimilarNamedWays();
-        t.startTest(null);
-        t.visit(waya);
-        t.visit(wayb);
-        return t.getErrors();
-    }
-
-    @Test
-    public void testCombinations() {
-        assertTrue(testWays("Church Street", "Water Street").isEmpty());
-        assertFalse(testWays("Main Street", "Maim Street").isEmpty());
-        assertFalse(testWays("First Street", "Frist Street").isEmpty());
-
-        assertTrue(testWays("1st Street", "2nd Street").isEmpty());
-        assertTrue(testWays("First Avenue", "Second Avenue").isEmpty());
-        assertTrue(testWays("West Main Street", "East Main Street").isEmpty());
-        assertTrue(testWays("A Street", "B Street").isEmpty());
-    }
-
     private void checkSimilarity(String message, String name1, String name2, boolean expected) {
         boolean actual = test.similaryName(name1, name2);
         assertEquals(message, expected, actual);
@@ -95,10 +36,13 @@ public class SimilarNamedWaysTest {
         checkSimilarity("same string", "Testname", "Testname", false);
         checkSimilarity("different case", "Testname", "TestName", true);
         checkSimilarity("typo", "Testname", "Testxame", true);
+        checkSimilarity("typo", "Main Street", "Maim Street", true);
+        checkSimilarity("typo", "First Street", "Frist Street", true);
         checkSimilarity("missing char", "Testname", "Testame", true);
         checkSimilarity("additional char", "Testname", "Testxname", true);
         checkSimilarity("2 changes", "Testname", "Tostxname", true);
         checkSimilarity("3 changes", "Testname", "Tostxnam", false);
+        checkSimilarity("many changes", "Church Street", "Water Street", false);
 
         // regular expression rule
         checkSimilarity("same number", "track 1", "track 1", false);
@@ -115,6 +59,7 @@ public class SimilarNamedWaysTest {
 
         // synonyms
         checkSimilarity("east and west", "East Foothill Drive", "West Foothill Drive", false);
+        checkSimilarity("east and west", "East Main Street", "West Main Street", false);
         checkSimilarity("east and west case", "east Foothill Drive", "West Foothill Drive", true);
         checkSimilarity("first and second", "First Street", "Second Street", false);
         checkSimilarity("first and second case", "First Street", "second Street", true);
@@ -122,6 +67,7 @@ public class SimilarNamedWaysTest {
         checkSimilarity("first and second typo2", "First Street", "Socond Street", true);
         checkSimilarity("first and second 2 changes", "First Street", "Soconds Street", true);
         checkSimilarity("first and second 3 changes", "First Street", "Soconds Stret", false);
+        checkSimilarity("A and B", "A Street", "B Street", false);
 
         // case only, see #14858
         checkSimilarity("case only", "Rua São João", "Rua Sao Joao", true);
