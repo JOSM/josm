8fcdd1a5c6810510d67d6b11f05fd3f8ed80e7e5
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-02-17 20:18:05 +0000

    checkstyle
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@15882 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/tools/date/DateUtilsTest.java
+++ test/unit/org/openstreetmap/josm/tools/date/DateUtilsTest.java
@@ -2,7 +2,6 @@
 package org.openstreetmap.josm.tools.date;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
