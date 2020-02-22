299029c555d85149ce594ba681d62f15f949dc24
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-02-22 18:40:20 +0000

    Fix typo in code comment
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@15900 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/validation/tests/MapCSSTagChecker.java
+++ src/org/openstreetmap/josm/data/validation/tests/MapCSSTagChecker.java
@@ -286,7 +286,7 @@ public class MapCSSTagChecker extends Test.TagTest {
         protected final GroupedMapCSSRule rule;
         /** Commands to apply in order to fix a matching primitive */
         protected final List<FixCommand> fixCommands = new ArrayList<>();
-        /** Tags (or arbitraty strings) of alternatives to be presented to the user */
+        /** Tags (or arbitrary strings) of alternatives to be presented to the user */
         protected final List<String> alternatives = new ArrayList<>();
         /** An {@link org.openstreetmap.josm.gui.mappaint.mapcss.Instruction.AssignmentInstruction}-{@link Severity} pair.
          * Is evaluated on the matching primitive to give the error message. Map is checked to contain exactly one element. */
