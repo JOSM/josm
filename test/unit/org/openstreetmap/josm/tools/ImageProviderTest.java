971712d4ba51beef3607fbbc638ded32df02106a
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-02-22 12:50:09 +0000

    see #14088 - Add ImageProviderTest.testGetPadded
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@15897 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/tools/ImageProviderTest.java
+++ test/unit/org/openstreetmap/josm/tools/ImageProviderTest.java
@@ -4,11 +4,14 @@ package org.openstreetmap.josm.tools;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import java.awt.Dimension;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.EnumSet;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -16,9 +19,14 @@ import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.openstreetmap.josm.JOSMFixture;
 import org.openstreetmap.josm.TestUtils;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmUtils;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+import org.openstreetmap.josm.tools.ImageProvider.GetPaddedOptions;
 
 import com.kitfox.svg.SVGConst;
 
@@ -28,6 +36,13 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
  * Unit tests of {@link ImageProvider} class.
  */
 public class ImageProviderTest {
+    
+    /**
+     * Setup test.
+     */
+    @Rule
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public JOSMTestRules test = new JOSMTestRules().mapStyles().presets();
 
     private static final class LogHandler14319 extends Handler {
         boolean failed;
@@ -114,5 +129,23 @@ public class ImageProviderTest {
         assertNotNull(ImageProvider.get("data:image/png;base64," +
                 "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4"+
                 "//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="));
+    }
+
+    /**
+     * Unit test of {@link ImageProvider#getPadded}.
+     */
+    @Test
+    public void testGetPadded() {
+        final EnumSet<GetPaddedOptions> noDefault = EnumSet.of(GetPaddedOptions.NO_DEFAULT);
+        final Dimension iconSize = new Dimension(16, 16);
+
+        assertNull(ImageProvider.getPadded(new Node(), new Dimension(0, 0)));
+        assertNotNull(ImageProvider.getPadded(new Node(), iconSize));
+        assertNull(ImageProvider.getPadded(new Node(), iconSize, noDefault));
+        assertNotNull(ImageProvider.getPadded(OsmUtils.createPrimitive("node amenity=restaurant"), iconSize, noDefault));
+        assertNull(ImageProvider.getPadded(OsmUtils.createPrimitive("node barrier=hedge"), iconSize,
+                EnumSet.of(GetPaddedOptions.NO_DEFAULT, GetPaddedOptions.NO_DEPRECATED)));
+        assertNotNull(ImageProvider.getPadded(OsmUtils.createPrimitive("way waterway=stream"), iconSize, noDefault));
+        assertNotNull(ImageProvider.getPadded(OsmUtils.createPrimitive("relation type=route route=railway"), iconSize, noDefault));
     }
 }
