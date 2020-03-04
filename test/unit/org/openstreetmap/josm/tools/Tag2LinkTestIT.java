ef97a8bbee9318faad5199ad7d8e1e30eb3b83d0
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-03-04 20:07:05 +0000

    see #13901 - double test timeout
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@16037 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/tools/Tag2LinkTestIT.java
+++ test/unit/org/openstreetmap/josm/tools/Tag2LinkTestIT.java
@@ -1,11 +1,12 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.tools;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 /**
  * Integration tests for the {@link Tag2Link}
@@ -17,14 +18,14 @@ public class Tag2LinkTestIT {
      */
     @ClassRule
     @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-    public static JOSMTestRules test = new JOSMTestRules();
+    public static JOSMTestRules test = new JOSMTestRules().timeout(20_000);
 
     /**
      * Integration test of function {@link org.openstreetmap.josm.tools.Tag2Link#initialize()}.
      */
     @Test
     public void testInitialize() {
-        org.openstreetmap.josm.tools.Tag2Link.initialize();
+        Tag2Link.initialize();
         Assert.assertTrue("obtails at least 40 rules", Tag2Link.wikidataRules.size() > 40);
     }
 }
