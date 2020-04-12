f26b4aaa64ca6742823d2467197341bcda5b61c4
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-04-12 14:21:14 +0000

    see #19075 - Generify HelpTagAction
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@16274 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/dialogs/properties/HelpTagAction.java
+++ src/org/openstreetmap/josm/gui/dialogs/properties/HelpTagAction.java
@@ -7,9 +7,11 @@ import java.awt.event.ActionEvent;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.IntFunction;
+import java.util.function.Supplier;
 
 import javax.swing.JTable;
 
+import org.openstreetmap.josm.data.osm.Tag;
 import org.openstreetmap.josm.gui.MainApplication;
 
 /**
@@ -17,9 +19,17 @@ import org.openstreetmap.josm.gui.MainApplication;
  * @since 15581
  */
 public class HelpTagAction extends HelpAction {
-    private final JTable tagTable;
-    private final IntFunction<String> tagKeySupplier;
-    private final IntFunction<Map<String, Integer>> tagValuesSupplier;
+    private final Supplier<Tag> tagSupplier;
+
+    /**
+     * Constructs a new {@code HelpAction}.
+     * @param tagSupplier Supplies the tag for which the help should be shown
+     * @since 16274
+     */
+    public HelpTagAction(Supplier<Tag> tagSupplier) {
+        this.tagSupplier = Objects.requireNonNull(tagSupplier);
+        putValue(NAME, tr("Go to OSM wiki for tag help"));
+    }
 
     /**
      * Constructs a new {@code HelpAction}.
@@ -28,22 +38,26 @@ public class HelpTagAction extends HelpAction {
      * @param tagValuesSupplier Finds the values from given row of tag table (map of values and number of occurrences). Cannot be null
      */
     public HelpTagAction(JTable tagTable, IntFunction<String> tagKeySupplier, IntFunction<Map<String, Integer>> tagValuesSupplier) {
-        this.tagTable = Objects.requireNonNull(tagTable);
-        this.tagKeySupplier = Objects.requireNonNull(tagKeySupplier);
-        this.tagValuesSupplier = Objects.requireNonNull(tagValuesSupplier);
+        this.tagSupplier = () -> {
+            if (tagTable.getSelectedRowCount() == 1) {
+                int row = tagTable.getSelectedRow();
+                String key = tagKeySupplier.apply(row);
+                Map<String, Integer> m = tagValuesSupplier.apply(row);
+                if (!m.isEmpty()) {
+                    String val = m.entrySet().iterator().next().getKey();
+                    return new Tag(key, val);
+                }
+            }
+            return null;
+        };
         putValue(NAME, tr("Go to OSM wiki for tag help"));
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (tagTable.getSelectedRowCount() == 1) {
-            int row = tagTable.getSelectedRow();
-            String key = tagKeySupplier.apply(row);
-            Map<String, Integer> m = tagValuesSupplier.apply(row);
-            if (!m.isEmpty()) {
-                String val = m.entrySet().iterator().next().getKey();
-                MainApplication.worker.execute(() -> displayTagHelp(key, val));
-            }
+        Tag tag = tagSupplier.get();
+        if (tag != null) {
+            MainApplication.worker.execute(() -> displayTagHelp(tag.getKey(), tag.getValue()));
         } else {
             super.actionPerformed(e);
         }
