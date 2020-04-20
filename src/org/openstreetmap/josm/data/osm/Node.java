e1ce84ca9f38aa068104d5d11bd33e25b314548d
GerdP <GerdP@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-04-20 09:06:25 +0000

    fix checkstyle issues
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@16364 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/osm/Node.java
+++ src/org/openstreetmap/josm/data/osm/Node.java
@@ -356,7 +356,7 @@ public final class Node extends OsmPrimitive implements INode {
      * @param hops number of hops
      * @param predicate predicate to match
      * @return {@code true} if {@code this} node mets the conditions
-     * @deprecated: Was used by UnconnectedWays test
+     * @deprecated Was used by UnconnectedWays test
      */
     @Deprecated
     public boolean isConnectedTo(final Collection<Node> otherNodes, final int hops, Predicate<Node> predicate) {
@@ -367,6 +367,7 @@ public final class Node extends OsmPrimitive implements INode {
                 ? isConnectedTo(otherNodes, hops, predicate, null)
                 : isConnectedTo(otherNodes, hops, predicate, new TreeSet<>());
     }
+
     @Deprecated
     private boolean isConnectedTo(final Collection<Node> otherNodes, final int hops, Predicate<Node> predicate, Set<Node> visited) {
         if (otherNodes.contains(this)) {
