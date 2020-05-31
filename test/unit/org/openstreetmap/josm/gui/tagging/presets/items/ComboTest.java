50106d304e1a9685c37e98a8e6d7bb7704b11c04
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-05-31 19:06:14 +0000

    see #8352 - PropertiesDialog: add unit test for color tags
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@16533 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/tagging/presets/items/ComboTest.java
+++ test/unit/org/openstreetmap/josm/gui/tagging/presets/items/ComboTest.java
@@ -4,6 +4,7 @@ package org.openstreetmap.josm.gui.tagging.presets.items;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.awt.Color;
 import java.util.Collections;
 
 import javax.swing.JPanel;
@@ -25,7 +26,7 @@ public class ComboTest {
      */
     @Rule
     @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-    public JOSMTestRules test = new JOSMTestRules().main();
+    public JOSMTestRules test = new JOSMTestRules().main().i18n("de");
 
     /**
      * Unit test for {@link Combo#addToPanel}.
@@ -36,5 +37,32 @@ public class ComboTest {
         assertEquals(0, p.getComponentCount());
         assertTrue(new Combo().addToPanel(p, Collections.<OsmPrimitive>emptyList(), false));
         assertTrue(p.getComponentCount() > 0);
+    }
+
+    @Test
+    public void testColor() {
+        Combo combo = new Combo();
+        combo.key = "colour";
+        combo.values = "red;green;blue;black";
+        combo.values_context = "color";
+        combo.delimiter = ';';
+        combo.addToPanel(new JPanel(), Collections.<OsmPrimitive>emptyList(), false);
+        assertEquals(5, combo.combobox.getItemCount());
+        combo.presetListEntries.stream().filter(e -> "red".equals(e.value)).findFirst().ifPresent(combo.combobox::setSelectedItem);
+        assertEquals("red", combo.getSelectedValue());
+        assertEquals("Rot", combo.getSelectedItem().toString());
+        assertEquals(new Color(0xFF0000), combo.getColor());
+        combo.presetListEntries.stream().filter(e -> "green".equals(e.value)).findFirst().ifPresent(combo.combobox::setSelectedItem);
+        assertEquals("green", combo.getSelectedValue());
+        assertEquals("Grün", combo.getSelectedItem().toString());
+        assertEquals(new Color(0x008000), combo.getColor());
+        combo.combobox.setSelectedItem("#135");
+        assertEquals("#135", combo.getSelectedValue());
+        assertEquals(new Color(0x113355), combo.getColor());
+        combo.combobox.setSelectedItem("#123456");
+        assertEquals("#123456", combo.getSelectedValue());
+        assertEquals(new Color(0x123456), combo.getColor());
+        combo.setColor(new Color(0x448822));
+        assertEquals("#448822", combo.getSelectedValue());
     }
 }
