ff391dfb7d7a6e568c4a1c4336212552f6dbeda1
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-10-08 20:32:52 +0000

    see #18200 - Fix WebMarkerTest after JMockit update
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@17125 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/layer/markerlayer/WebMarkerTest.java
+++ test/unit/org/openstreetmap/josm/gui/layer/markerlayer/WebMarkerTest.java
@@ -1,23 +1,25 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.layer.markerlayer;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URL;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openstreetmap.josm.JOSMFixture;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openstreetmap.josm.TestUtils;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.gpx.GpxData;
 import org.openstreetmap.josm.data.gpx.WayPoint;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
 import org.openstreetmap.josm.tools.PlatformManager;
 import org.openstreetmap.josm.tools.PlatformHook;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import mockit.Expectations;
 import mockit.Injectable;
+import mockit.Mocked;
 
 /**
  * Unit tests of {@link WebMarker} class.
@@ -27,23 +29,22 @@ public class WebMarkerTest {
     /**
      * Setup tests
      */
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        JOSMFixture.createUnitTestFixture().init();
-    }
+    @RegisterExtension
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public JOSMTestRules test = new JOSMTestRules().preferences().https();
 
     /**
      * Unit test of {@link WebMarker#WebMarker}.
      * @param mockPlatformHook platform hook mock
+     * @param platformManager {@link PlatformManager} mock
      * @throws Exception  in case of error
      */
     @Test
-    public void testWebMarker(@Injectable final PlatformHook mockPlatformHook) throws Exception {
+    public void testWebMarker(@Injectable final PlatformHook mockPlatformHook,
+                              @Mocked final PlatformManager platformManager) throws Exception {
         TestUtils.assumeWorkingJMockit();
-        new Expectations(PlatformManager.class) {{
-            PlatformManager.getPlatform(); result = mockPlatformHook;
-        }};
         new Expectations() {{
+            PlatformManager.getPlatform(); result = mockPlatformHook;
             mockPlatformHook.openUrl("http://example.com"); result = null; times = 1;
         }};
 
