bf6c0db53fd2746da00a4801b1a977d4c2325f57
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2020-10-13 19:36:06 +0000

    see #15102 - see #16637 - get rid of real HTTP calls to https://api.openstreetmap.org in ApiUrlTestTaskTest, mock them
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@17192 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/gui/preferences/server/ApiUrlTestTaskTest.java
+++ test/unit/org/openstreetmap/josm/gui/preferences/server/ApiUrlTestTaskTest.java
@@ -1,6 +1,12 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.preferences.server;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -10,10 +16,13 @@ import javax.swing.JLabel;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.TestUtils;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
+import org.openstreetmap.josm.tools.Logging;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 /**
  * Unit tests of {@link ApiUrlTestTask} class.
@@ -26,6 +35,12 @@ public class ApiUrlTestTaskTest {
     @Rule
     @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
     public JOSMTestRules test = new JOSMTestRules().preferences().timeout(30000);
+
+    /**
+     * HTTP mock.
+     */
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort().usingFilesUnderDirectory(TestUtils.getTestDataRoot()));
 
     private static final Component PARENT = new JLabel();
 
@@ -42,7 +57,7 @@ public class ApiUrlTestTaskTest {
      */
     @Test
     public void testNominalUrl() {
-        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, Config.getUrls().getDefaultOsmApiUrl());
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, wireMockRule.url("/__files/osm_api"));
         task.run();
         assertTrue(task.isSuccess());
     }
@@ -52,9 +67,12 @@ public class ApiUrlTestTaskTest {
      */
     @Test
     public void testAlertInvalidUrl() {
+        Logging.clearLastErrorAndWarnings();
         ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "malformed url");
         task.run();
         assertFalse(task.isSuccess());
+        assertThat(Logging.getLastErrorAndWarnings().toString(), containsString(
+                "<html>'malformed url' is not a valid OSM API URL.<br>Please check the spelling and validate again.</html>"));
     }
 
     /**
@@ -62,9 +80,12 @@ public class ApiUrlTestTaskTest {
      */
     @Test
     public void testUnknownHost() {
+        Logging.clearLastErrorAndWarnings();
         ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "http://unknown");
         task.run();
         assertFalse(task.isSuccess());
+        assertThat(Logging.getLastErrorAndWarnings().toString(), containsString(
+                "java.net.UnknownHostException: unknown"));
     }
 
     /**
@@ -72,9 +93,15 @@ public class ApiUrlTestTaskTest {
      */
     @Test
     public void testAlertInvalidServerResult() {
-        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "http://www.openstreetmap.org");
+        Logging.clearLastErrorAndWarnings();
+        wireMockRule.stubFor(get(urlEqualTo("/does-not-exist/0.6/capabilities"))
+                .willReturn(aResponse().withStatus(404)));
+
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, wireMockRule.url("/does-not-exist"));
         task.run();
         assertFalse(task.isSuccess());
+        assertThat(Logging.getLastErrorAndWarnings().toString(), containsString(
+                "The server responded with the return code 404 instead of 200."));
     }
 
     /**
@@ -82,8 +109,12 @@ public class ApiUrlTestTaskTest {
      */
     @Test
     public void testAlertInvalidCapabilities() {
-        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, "https://josm.openstreetmap.de/export/10979/josm/trunk/test/data/invalid_api");
+        Logging.clearLastErrorAndWarnings();
+        ApiUrlTestTask task = new ApiUrlTestTask(PARENT, wireMockRule.url("/__files/invalid_api"));
         task.run();
         assertFalse(task.isSuccess());
+        assertThat(Logging.getLastErrorAndWarnings().toString(), containsString(
+                "The OSM API server at 'XXX' did not return a valid response.<br>It is likely that 'XXX' is not an OSM API server."
+                        .replace("XXX", wireMockRule.url("/__files/invalid_api"))));
     }
 }
