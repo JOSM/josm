JOSM/josm

commit a8d231bba077aede1c33c8c405999bdb2db8926e

fix #16355 - Ignored validation findings reappear on upload (patch by bhatchl)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13893 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/upload/ValidateUploadHook.java
@@ -73,17 +73,6 @@ public class ValidateUploadHook implements UploadHook {
                 }
             }
         }
-        OsmDataLayer editLayer = MainApplication.getLayerManager().getEditLayer();
-        if (editLayer != null) {
-            editLayer.validationErrors.clear();
-            editLayer.validationErrors.addAll(errors);
-        }
-        MapFrame map = MainApplication.getMap();
-        if (map != null) {
-            map.validatorDialog.tree.setErrors(errors);
-        }
-        if (errors.isEmpty())
-            return true;
 
         if (ValidatorPrefHelper.PREF_USE_IGNORE.get()) {
             int nume = 0;
@@ -104,6 +93,19 @@ public class ValidateUploadHook implements UploadHook {
             if (nume == 0)
                 return true;
         }
+
+        OsmDataLayer editLayer = MainApplication.getLayerManager().getEditLayer();
+        if (editLayer != null) {
+            editLayer.validationErrors.clear();
+            editLayer.validationErrors.addAll(errors);
+        }
+        MapFrame map = MainApplication.getMap();
+        if (map != null) {
+            map.validatorDialog.tree.setErrors(errors);
+        }
+        if (errors.isEmpty())
+            return true;
+
         return displayErrorScreen(errors);
     }
 

commit 0d6aa377ad3aca7c408076ccfaa3129e9109dfb8

see #20342: Validator results not cleared - make sure that validator tree is rebuild even if all found errors are ignored (regression from r13893)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@17436 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/upload/ValidateUploadHook.java
@@ -78,15 +78,8 @@ public class ValidateUploadHook implements UploadHook {
             test.setBeforeUpload(false);
         }
 
-        if (ValidatorPrefHelper.PREF_USE_IGNORE.get()) {
-            boolean allIgnored = true;
-            for (TestError error : errors) {
-                if (!error.updateIgnored()) {
-                    allIgnored = false;
-                }
-            }
-            if (allIgnored)
-                return true;
+        if (Boolean.TRUE.equals(ValidatorPrefHelper.PREF_USE_IGNORE.get())) {
+            errors.forEach(TestError::updateIgnored);
         }
 
         OsmDataLayer editLayer = MainApplication.getLayerManager().getEditLayer();
@@ -98,7 +91,7 @@ public class ValidateUploadHook implements UploadHook {
         if (map != null) {
             map.validatorDialog.tree.setErrors(errors);
         }
-        if (errors.isEmpty())
+        if (errors.stream().allMatch(TestError::isIgnored))
             return true;
 
         return displayErrorScreen(errors);