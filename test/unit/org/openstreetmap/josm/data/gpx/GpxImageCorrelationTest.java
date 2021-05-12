0c65c30efcf3359ee7ee6f3cbb217284cfb97016
simon04 <simon04@0c6e7542-c601-0410-84e7-c038aed88b3b>
2021-05-12 21:31:57 +0000

    Checkstyle
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@17888 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/gpx/GpxImageCorrelationTest.java
+++ test/unit/org/openstreetmap/josm/data/gpx/GpxImageCorrelationTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.MethodOrderer.MethodName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
