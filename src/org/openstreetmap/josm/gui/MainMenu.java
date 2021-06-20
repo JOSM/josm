ca93f4c68af17b3ba1fd5b9ed47d38850136c745
Klumbumbus <Klumbumbus@0c6e7542-c601-0410-84e7-c038aed88b3b>
2021-06-20 18:27:12 +0000

    fix #21000, see #18768 - Fix Windows menu help topic (patch by skyper)
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@17925 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/MainMenu.java
+++ src/org/openstreetmap/josm/gui/MainMenu.java
@@ -409,7 +409,7 @@ public class MainMenu extends JMenuBar {
      * group is for currently open windows that cannot be toggled, e.g. relation editors. It's recommended
      * to use WINDOW_MENU_GROUP to determine the group integer.
      */
-    public final WindowMenu windowMenu = addMenu(new WindowMenu(), /* untranslated name */ "Windows", KeyEvent.VK_W, 11, ht("/Help/Menu/Windows"));
+    public final WindowMenu windowMenu = addMenu(new WindowMenu(), /* untranslated name */ "Windows", KeyEvent.VK_W, 11, ht("/Menu/Windows"));
     // CHECKSTYLE.ON: LineLength
 
     /**
