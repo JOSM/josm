ccf3426b4ba0671fbd1d123ff5f1264bd1a52dcb
Taylor Smock <tsmock@fb.com>
2021-07-19 09:03:07 -0600

    JOSM #21139: Update static IPreferences in @BasicPreferences
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- test/unit/org/openstreetmap/josm/testutils/annotations/BasicPreferences.java
+++ test/unit/org/openstreetmap/josm/testutils/annotations/BasicPreferences.java
@@ -1,12 +1,6 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.testutils.annotations;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
@@ -14,11 +8,23 @@ import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.platform.commons.util.ClassFilter;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.openstreetmap.josm.data.Preferences;
+import org.openstreetmap.josm.data.preferences.AbstractProperty;
 import org.openstreetmap.josm.data.preferences.JosmBaseDirectories;
 import org.openstreetmap.josm.data.preferences.JosmUrls;
 import org.openstreetmap.josm.spi.preferences.Config;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
+import org.openstreetmap.josm.tools.Logging;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.nio.file.Path;
 
 /**
  * Allow tests to use JOSM preferences (see {@link JOSMTestRules#preferences()}).
@@ -42,6 +48,9 @@ public @interface BasicPreferences {
         @Override
         public void afterAll(ExtensionContext context) throws Exception {
             AnnotationUtils.resetStaticClass(Config.class);
+
+            // Ensure that config tests are consistent
+            resetConfigVariables(null);
         }
 
         @Override
@@ -65,12 +74,38 @@ public @interface BasicPreferences {
 
             // Store the pref for other extensions
             context.getStore(Namespace.create(BasicPreferencesExtension.class)).put("preferences", pref);
+
+            resetConfigVariables(pref);
         }
 
         @Override
         public void beforeEach(ExtensionContext context) throws Exception {
             if (AnnotationUtils.elementIsAnnotated(context.getElement(), BasicPreferences.class) || Config.getPref() == null) {
                 this.beforeAll(context);
+            }
+        }
+
+        /**
+         * Reset all {@link AbstractProperty} preferences to the new preference
+         * @param pref The preference to use
+         * @throws ReflectiveOperationException if reflection fails
+         */
+        private void resetConfigVariables(Preferences pref) throws ReflectiveOperationException {
+            final Field configField = AbstractProperty.class.getDeclaredField("preferences");
+            configField.setAccessible(true);
+            for (Path classPath : ReflectionUtils.getAllClasspathRootDirectories()) {
+                for (Class<?> clazz : ReflectionUtils.findAllClassesInClasspathRoot(classPath.toUri(), ClassFilter.of(tClazz -> true))) {
+                    try {
+                        for (Field field : clazz.getDeclaredFields()) {
+                            if (ReflectionUtils.isStatic(field) && AbstractProperty.class.isAssignableFrom(field.getType())) {
+                                field.setAccessible(true);
+                                configField.set(field.get(null), pref);
+                            }
+                        }
+                    } catch (NoClassDefFoundError noClassDefFoundError) {
+                        Logging.warn("{0}: No class definition found", clazz.getName());
+                    }
+                }
             }
         }
     }
