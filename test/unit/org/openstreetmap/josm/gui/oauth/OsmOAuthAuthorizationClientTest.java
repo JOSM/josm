adcb850c4c6f79831963eebf4db97ee8b08176ae
Taylor Smock <tsmock@fb.com>
2021-07-21 13:27:53 -0600

    OsmOAuthAuthorizationClientTest: Update to JUnit5
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- test/unit/org/openstreetmap/josm/gui/oauth/OsmOAuthAuthorizationClientTest.java
+++ test/unit/org/openstreetmap/josm/gui/oauth/OsmOAuthAuthorizationClientTest.java
@@ -4,44 +4,40 @@ package org.openstreetmap.josm.gui.oauth;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.URI;
 import java.util.Collections;
 
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.openstreetmap.josm.data.oauth.OAuthParameters;
 import org.openstreetmap.josm.data.oauth.OAuthToken;
 import org.openstreetmap.josm.io.OsmTransferCanceledException;
-import org.openstreetmap.josm.testutils.JOSMTestRules;
+import org.openstreetmap.josm.testutils.annotations.BasicPreferences;
+import org.openstreetmap.josm.testutils.annotations.BasicWiremock;
+import org.openstreetmap.josm.testutils.annotations.HTTP;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 /**
  * Unit tests of {@link OsmOAuthAuthorizationClient} class.
  */
-public class OsmOAuthAuthorizationClientTest {
-
-    /**
-     * Setup tests
-     */
-    @Rule
-    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-    public JOSMTestRules test = new JOSMTestRules().timeout(20000);
-
+@Timeout(20)
+@BasicWiremock
+// Needed for OAuthParameters
+@BasicPreferences
+@HTTP
+class OsmOAuthAuthorizationClientTest {
     /**
      * HTTP mock.
      */
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort());
+    @BasicWiremock
+    WireMockServer wireMockServer;
 
     /**
      * Unit test of {@link OsmOAuthAuthorizationClient}.
@@ -49,29 +45,30 @@ public class OsmOAuthAuthorizationClientTest {
      * @throws OsmTransferCanceledException  if OSM transfer error occurs
      */
     @Test
-    public void testOsmOAuthAuthorizationClient() throws OsmTransferCanceledException, OsmOAuthAuthorizationException {
+    void testOsmOAuthAuthorizationClient() throws OsmTransferCanceledException, OsmOAuthAuthorizationException {
         // request token
-        wireMockRule.stubFor(get(urlEqualTo("/oauth/request_token"))
+        wireMockServer.stubFor(get(urlEqualTo("/oauth/request_token"))
                 .willReturn(aResponse().withStatus(200).withBody(String.join("&",
                         "oauth_token=entxUGuwRKV6KyVDF0OWScdGhbqXGMGmosXuiChR",
                         "oauth_token_secret=nsBD2Hr5lLGDUeNoh3SnLaGsUV1TiPYM4qUr7tPB"))));
-        OsmOAuthAuthorizationClient client = new OsmOAuthAuthorizationClient(OAuthParameters.createDefault(wireMockRule.url("/api")));
+        OsmOAuthAuthorizationClient client = new OsmOAuthAuthorizationClient(OAuthParameters.createDefault(
+                wireMockServer.url("/api")));
 
         OAuthToken requestToken = client.getRequestToken(null);
-        assertEquals("requestToken.key", "entxUGuwRKV6KyVDF0OWScdGhbqXGMGmosXuiChR", requestToken.getKey());
-        assertEquals("requestToken.secret", "nsBD2Hr5lLGDUeNoh3SnLaGsUV1TiPYM4qUr7tPB", requestToken.getSecret());
+        assertEquals("entxUGuwRKV6KyVDF0OWScdGhbqXGMGmosXuiChR", requestToken.getKey(), "requestToken.key");
+        assertEquals("nsBD2Hr5lLGDUeNoh3SnLaGsUV1TiPYM4qUr7tPB", requestToken.getSecret(), "requestToken.secret");
         String url = client.getAuthoriseUrl(requestToken);
-        assertEquals("url", wireMockRule.url("/oauth/authorize?oauth_token=entxUGuwRKV6KyVDF0OWScdGhbqXGMGmosXuiChR"), url);
+        assertEquals(wireMockServer.url("/oauth/authorize?oauth_token=entxUGuwRKV6KyVDF0OWScdGhbqXGMGmosXuiChR"), url, "url");
 
         // access token
-        wireMockRule.stubFor(get(urlEqualTo("/oauth/access_token"))
+        wireMockServer.stubFor(get(urlEqualTo("/oauth/access_token"))
                 .willReturn(aResponse().withStatus(200).withBody(String.join("&",
                         "oauth_token=eGMGmosXuiChRntxUGuwRKV6KyVDF0OWScdGhbqX",
                         "oauth_token_secret=nsBUeNor7tPh3SHr5lLaGsGDUD2PYMV1TinL4qUB"))));
 
         OAuthToken accessToken = client.getAccessToken(null);
-        assertEquals("accessToken.key", "eGMGmosXuiChRntxUGuwRKV6KyVDF0OWScdGhbqX", accessToken.getKey());
-        assertEquals("accessToken.secret", "nsBUeNor7tPh3SHr5lLaGsGDUD2PYMV1TinL4qUB", accessToken.getSecret());
+        assertEquals("eGMGmosXuiChRntxUGuwRKV6KyVDF0OWScdGhbqX", accessToken.getKey(), "accessToken.key");
+        assertEquals("nsBUeNor7tPh3SHr5lLaGsGDUD2PYMV1TinL4qUB", accessToken.getSecret(), "accessToken.secret");
     }
 
     /**
@@ -81,21 +78,21 @@ public class OsmOAuthAuthorizationClientTest {
      * @throws Exception if any error occurs
      */
     @Test
-    public void testCookieHandlingMock() throws Exception {
-        wireMockRule.stubFor(get(urlEqualTo("/login?cookie_test=true"))
+    void testCookieHandlingMock() throws Exception {
+        wireMockServer.stubFor(get(urlEqualTo("/login?cookie_test=true"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Set-Cookie", "_osm_session=7fe8e2ea36c6b803cb902301b28e0a; path=/; HttpOnly; SameSite=Lax")
                 .withBody("<input type=\"hidden\" " +
                         "name=\"authenticity_token\" " +
                         "value=\"fzp6CWJhp6Vns09re3s2Tw==\" />")));
-        final OAuthParameters parameters = OAuthParameters.createDefault(wireMockRule.url("/api"));
+        final OAuthParameters parameters = OAuthParameters.createDefault(wireMockServer.url("/api"));
         final OsmOAuthAuthorizationClient client = new OsmOAuthAuthorizationClient(parameters);
         final OsmOAuthAuthorizationClient.SessionId sessionId = client.fetchOsmWebsiteSessionId();
         assertNotNull(sessionId);
-        assertEquals("sessionId.id", "7fe8e2ea36c6b803cb902301b28e0a", sessionId.id);
-        assertEquals("sessionId.token", "fzp6CWJhp6Vns09re3s2Tw==", sessionId.token);
-        assertNull("sessionId.userName", sessionId.userName);
+        assertEquals("7fe8e2ea36c6b803cb902301b28e0a", sessionId.id, "sessionId.id");
+        assertEquals("fzp6CWJhp6Vns09re3s2Tw==", sessionId.token, "sessionId.token");
+        assertNull(sessionId.userName, "sessionId.userName");
     }
 
     /**
@@ -105,7 +102,7 @@ public class OsmOAuthAuthorizationClientTest {
      * @throws Exception if any error occurs
      */
     @Test
-    public void testCookieHandlingCookieManager() throws Exception {
+    void testCookieHandlingCookieManager() throws Exception {
         // emulate Java Web Start behaviour
         // see https://docs.oracle.com/javase/tutorial/deployment/doingMoreWithRIA/accessingCookies.html
         final OAuthParameters parameters = OAuthParameters.createDefault();
