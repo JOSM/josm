ee8ce2a1b5c81aabd7b3f3c1be876d545b4ee4da
Don-vip <Don-vip@0c6e7542-c601-0410-84e7-c038aed88b3b>
2021-08-04 19:45:06 +0000

    fix #21196 - add help topic for lasso select mode (patch by skyper)
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@18122 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/actions/mapmode/SelectLassoAction.java
+++ src/org/openstreetmap/josm/actions/mapmode/SelectLassoAction.java
@@ -1,6 +1,7 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.actions.mapmode;
 
+import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.KeyEvent;
@@ -26,6 +27,7 @@ public class SelectLassoAction extends MapMode {
                 tr("Lasso selection mode: select objects within a hand-drawn region"),
                 Shortcut.registerShortcut("mapmode:lasso", tr("Mode: {0}", tr("Lasso Mode")), KeyEvent.CHAR_UNDEFINED, Shortcut.NONE),
                 ImageProvider.getCursor("normal", "rope"));
+        setHelpId(ht("/Action/LassoMode"));
     }
 
     @Override
