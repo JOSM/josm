0a1c36e8985db1e716104abd7107026548e778f3
Florian Kargl <f.kargl@posteo.de>
2022-10-05 17:48:19 +0200

    Add new PrimitiveHoverListener allowing client code to be notified when the mouse pointer is moved onto or off of a node or way
    Can be used for e.g. highlighting the hovered object

---
+++ src/org/openstreetmap/josm/gui/PrimitiveHoverListener.java
@@ -0,0 +1,68 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui;
+
+import java.awt.event.MouseEvent;
+
+import org.openstreetmap.josm.data.osm.IPrimitive;
+
+/**
+ * Interface to notify listeners when the user moves the mouse pointer onto or off of a primitive.
+ * @since xxx
+ */
+@FunctionalInterface
+public interface PrimitiveHoverListener {
+    /**
+     * Method called when the primitive under the mouse pointer changes.
+     * @param e Event object describing the hovered primitive and related information
+     */
+    void primitiveHovered(PrimitiveHoverEvent e);
+
+    /**
+     * Event that is fired when the mouse pointer is moved over a primitive.
+     */
+    class PrimitiveHoverEvent {
+        /**
+         * The primitive that is being hovered over by the mouse pointer.
+         * Can be null if the mouse pointer is not over any primitive.
+         */
+        private final IPrimitive hoveredPrimitive;
+        private final IPrimitive previousPrimitive;
+        private final MouseEvent mouseEvent;
+
+        /**
+         * Construct a new {@code PrimitiveHoverEvent}
+         * @param hoveredPrimitive Primitive that is hovered by the mouse pointer
+         * @param previousPrimitive Previously hovered primitive
+         * @param mouseEvent {@link MouseEvent} that triggered this hover event
+         */
+        public PrimitiveHoverEvent(IPrimitive hoveredPrimitive, IPrimitive previousPrimitive, MouseEvent mouseEvent) {
+            this.hoveredPrimitive = hoveredPrimitive;
+            this.previousPrimitive = previousPrimitive;
+            this.mouseEvent = mouseEvent;
+        }
+
+        /**
+         * Get the primitive that is being hovered over with the mouse pointer
+         * @return The primitive that is being hovered over
+         */
+        public IPrimitive getHoveredPrimitive() {
+            return hoveredPrimitive;
+        }
+
+        /**
+         * Get the previously hovered primitive
+         * @return The previously hovered primitive
+         */
+        public IPrimitive getPreviousPrimitive() {
+            return previousPrimitive;
+        }
+
+        /**
+         * Get the {@link MouseEvent} object that triggered this hover event
+         * @return The {@link MouseEvent} that triggered this hover event
+         */
+        public MouseEvent getMouseEvent() {
+            return mouseEvent;
+        }
+    }
+}
