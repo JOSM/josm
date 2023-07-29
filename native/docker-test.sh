ade64ed519815a547d93b4f5d55c2eefcb606a54
gaben <23311361+gabortim@users.noreply.github.com>
2023-07-29 09:47:06 +0200

    Shell fixes

--- native/docker-test.sh
+++ native/docker-test.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 docker build . -t josm/josm
 mkdir -p test/report
-docker run -it --name josm -v `pwd`/test/report:/josm/test/report josm/josm
+docker run -it --name josm -v "$(pwd)"/test/report:/josm/test/report josm/josm
 docker rm josm
 docker rmi josm/josm
