aa4ddbd6f2331ec815d1afe9c063ec3f340c2104
gaben <23311361+gabortim@users.noreply.github.com>
2023-07-31 22:01:41 +0200

    Improve the deprecation message by including revision

--- src/org/openstreetmap/josm/data/osm/OsmUtils.java
+++ src/org/openstreetmap/josm/data/osm/OsmUtils.java
@@ -35,19 +35,19 @@ public final class OsmUtils {
 
     /**
      * Discouraged synonym for {@link #TRUE_VALUE}
-     * @deprecated Use {@link OsmUtils#TRUE_VALUE} instead.
+     * @deprecated since xxx, use {@link #TRUE_VALUE} instead.
      */
     @Deprecated
     public static final String trueval = TRUE_VALUE;
     /**
      * Discouraged synonym for {@link #FALSE_VALUE}
-     * @deprecated Use {@link OsmUtils#FALSE_VALUE} instead.
+     * @deprecated since xxx, use {@link #FALSE_VALUE} instead.
      */
     @Deprecated
     public static final String falseval = FALSE_VALUE;
     /**
      * Discouraged synonym for {@link #REVERSE_VALUE}
-     * @deprecated Use {@link OsmUtils#REVERSE_VALUE} instead.
+     * @deprecated since xxx, use {@link #REVERSE_VALUE} instead.
      */
     @Deprecated
     public static final String reverseval = REVERSE_VALUE;
