fc9ef3caeb4a0f8a0dadb21517bf8f8759b01b81
taylor.smock <taylor.smock@0c6e7542-c601-0410-84e7-c038aed88b3b>
2023-08-07 20:49:03 +0000

    Fix #21364: GPX export of image new position use old coordinates
    This was due to not clearing cached data (done for performance reasons)
    
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@18797 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/gui/layer/geoimage/GeoImageLayer.java
+++ src/org/openstreetmap/josm/gui/layer/geoimage/GeoImageLayer.java
@@ -961,6 +961,8 @@ public class GeoImageLayer extends AbstractModifiableLayer implements
 
     @Override
     public void imageDataUpdated(ImageData data) {
+        this.gpxFauxLayer = null;
+        this.gpxFauxData = null;
         updateBufferAndRepaint();
     }
 
