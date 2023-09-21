6ef825462646a948841e11240d2a08cf62fcc817
taylor.smock <taylor.smock@0c6e7542-c601-0410-84e7-c038aed88b3b>
2023-09-21 18:29:59 +0000

    Revert r18838, see #23008: `natural=coastline` does not work well with the changes
    This adds a test to ensure future changes don't break the `natural=coastline`
    branch.
    
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@18840 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/validation/tests/PowerLinesTest.java
+++ test/unit/org/openstreetmap/josm/data/validation/tests/PowerLinesTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openstreetmap.josm.TestUtils;
@@ -15,6 +17,7 @@ import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.TagMap;
 import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.gui.progress.NullProgressMonitor;
 import org.openstreetmap.josm.testutils.annotations.BasicPreferences;
 import org.openstreetmap.josm.testutils.annotations.Projection;
 
@@ -128,5 +131,32 @@ class PowerLinesTest {
         final Relation powerLine = TestUtils.newRelation("natural=water water=river",
                 new RelationMember("", TestUtils.newWay("", new Node(), new Node())));
         assertDoesNotThrow(() -> this.powerLines.visit(powerLine));
+    }
+
+    @Test
+    void testPowerlineCrossingOcean() {
+        final Way powerLine = TestUtils.newWay("power=line",
+                new Node(new LatLon(52.6759313, 8.3845078)),
+                new Node(new LatLon(52.6759857, 8.3898977)),
+                new Node(new LatLon(52.6759857, 8.3949282)),
+                new Node(new LatLon(52.6758768, 8.3978927)),
+                new Node(new LatLon(52.6759313, 8.4322981)),
+                new Node(new LatLon(52.6759857, 8.4419999)),
+                new Node(new LatLon(52.6758768, 8.4488271)));
+        final Way coastline = TestUtils.newWay("natural=coastline",
+                new Node(new LatLon(52.6813231, 8.4253811)),
+                new Node(new LatLon(52.6684687, 8.4254709)),
+                new Node(new LatLon(52.6680328, 8.4033724)),
+                new Node(new LatLon(52.6838282, 8.4070555)));
+        for (Node node : powerLine.getNodes()) {
+            node.put("power", "tower");
+        }
+        this.ds.addPrimitiveRecursive(powerLine);
+        this.ds.addPrimitiveRecursive(coastline);
+
+        this.powerLines.startTest(NullProgressMonitor.INSTANCE);
+        this.powerLines.visit(new ArrayList<>(ds.getWays()));
+        this.powerLines.endTest();
+        assertTrue(this.powerLines.getErrors().isEmpty());
     }
 }
