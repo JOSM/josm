0d78f37c0c5d51ac9737bd6741a8a07050a09f4d
taylor.smock <taylor.smock@0c6e7542-c601-0410-84e7-c038aed88b3b>
2023-10-31 18:06:59 +0000

    Fix #23256: AbstractPrimitive::putAll not updating values (patch by marcello, modified)
    Modifications are as follows:
    * Unit tests were split out into discrete units for better test feedback in the
      future.
    
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@18890 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/osm/AbstractPrimitiveTest.java
+++ test/unit/org/openstreetmap/josm/data/osm/AbstractPrimitiveTest.java
@@ -1,12 +1,17 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.data.osm;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
+import org.openstreetmap.josm.TestUtils;
 
 /**
  * Unit tests of the {@code AbstractPrimitive} class.
@@ -64,5 +69,68 @@ class AbstractPrimitiveTest {
         assertTrue(p.hasTagDifferent("foo", "bar"));
         assertTrue(p.hasTagDifferent("foo", "bar", "baz"));
         assertTrue(p.hasTagDifferent("foo", Collections.singleton("bar")));
+    }
+
+    /**
+     * Unit test of {@link AbstractPrimitive#putAll}
+     */
+    @Test
+    void testPutAllInsert() {
+        AbstractPrimitive p = new Node();
+        Map<String, String> tags = new HashMap<>();
+
+        tags.put("a", "va1");
+        tags.put("b", "vb1");
+        p.putAll(tags);
+        assertEquals("va1", p.get("a"));
+        assertEquals("vb1", p.get("b"));
+
+    }
+
+    /**
+     * Unit test of {@link AbstractPrimitive#putAll}
+     */
+    @Test
+    void testPutAllChange() {
+        AbstractPrimitive p = TestUtils.newNode("a=va1 b=vb1");
+        Map<String, String> tags = new HashMap<>();
+
+        tags.put("a", "va2");
+        p.putAll(tags);
+        assertEquals("va2", p.get("a"));
+        assertEquals("vb1", p.get("b"));
+    }
+
+    /**
+     * Unit test of {@link AbstractPrimitive#putAll}
+     */
+    @Test
+    void testPutAllChangeAndInsert() {
+        AbstractPrimitive p = TestUtils.newNode("a=va2 b=vb1");
+        Map<String, String> tags = new HashMap<>();
+
+        tags.put("b", "vb3");
+        tags.put("c", "vc3");
+        p.putAll(tags);
+        assertEquals("va2", p.get("a"));
+        assertEquals("vb3", p.get("b"));
+        assertEquals("vc3", p.get("c"));
+    }
+
+    /**
+     * Unit test of {@link AbstractPrimitive#putAll}
+     */
+    @Test
+    void testPutAllChangeAndRemove() {
+        AbstractPrimitive p = TestUtils.newNode("a=va2 b=vb3 c=vc3");
+        Map<String, String> tags = new HashMap<>();
+
+        tags.put("a", "va4");
+        tags.put("b", null);
+        tags.put(null, null);
+        p.putAll(tags);
+        assertEquals("va4", p.get("a"));
+        assertNull(p.get("b"));
+        assertEquals("vc3", p.get("c"));
     }
 }
