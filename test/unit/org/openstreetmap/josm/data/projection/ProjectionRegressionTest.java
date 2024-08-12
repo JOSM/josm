46bb7a8f9636b3f513908ab6b0613791db7ec382
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2024-08-12 18:27:49 +0000

    output the ULP factor in case of an error
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@19185 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/projection/ProjectionRegressionTest.java
+++ test/unit/org/openstreetmap/josm/data/projection/ProjectionRegressionTest.java
@@ -14,6 +14,7 @@ import java.nio.file.Paths;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
@@ -27,6 +28,7 @@ import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.testutils.annotations.ProjectionNadGrids;
 import org.openstreetmap.josm.tools.Pair;
+import org.openstreetmap.josm.tools.Utils;
 
 /**
  * This test is used to monitor changes in projection code.
@@ -151,6 +153,9 @@ class ProjectionRegressionTest {
              }
         }
 
+        double fact = 12;
+        if(Utils.getSystemProperty("os.name").toLowerCase(Locale.ENGLISH).startsWith("mac os x"))
+            fact = 1700;
         for (TestData data : allData) {
             Projection proj = Projections.getProjectionByCode(data.code);
             if (proj == null) {
@@ -159,18 +164,26 @@ class ProjectionRegressionTest {
             }
             EastNorth en = proj.latlon2eastNorth(data.ll);
             LatLon ll2 = proj.eastNorth2latlon(data.en);
-            if (!equalsJava9(en, data.en)) {
+            if (!equalsJava9(en, data.en, fact)) {
                 String error = String.format("%s (%s): Projecting latlon(%s,%s):%n" +
                         "        expected: eastnorth(%s,%s),%n" +
-                        "        but got:  eastnorth(%s,%s)!%n",
-                        proj, data.code, data.ll.lat(), data.ll.lon(), data.en.east(), data.en.north(), en.east(), en.north());
+                        "        but got:  eastnorth(%s,%s)!%n" +
+                        "        test:     [%s,%s] factor [%s,%s] threshold [%s,%s]%n",
+                        proj, data.code, data.ll.lat(), data.ll.lon(), data.en.east(), data.en.north(), en.east(), en.north(),
+                        Math.abs(en.east()-data.en.east()), Math.abs(en.north()-data.en.north()),
+                        Math.abs(en.east()-data.en.east())/Math.ulp(en.east()), Math.abs(en.north()-data.en.north())/Math.ulp(en.north()),
+                        Math.ulp(en.east()), Math.ulp(en.north()));
                 fail.append(error);
             }
-            if (!equalsJava9(ll2, data.ll2)) {
+            if (!equalsJava9(ll2, data.ll2, fact)) {
                 String error = String.format("%s (%s): Inverse projecting eastnorth(%s,%s):%n" +
                         "        expected: latlon(%s,%s),%n" +
-                        "        but got:  latlon(%s,%s)!%n",
-                        proj, data.code, data.en.east(), data.en.north(), data.ll2.lat(), data.ll2.lon(), ll2.lat(), ll2.lon());
+                        "        but got:  latlon(%s,%s)!%n" +
+                        "        test:     [%s,%s] factor [%s,%s], threshold [%s,%s]%n",
+                        proj, data.code, data.en.east(), data.en.north(), data.ll2.lat(), data.ll2.lon(), ll2.lat(), ll2.lon(),
+                        Math.abs(ll2.lat()-data.ll2.lat()), Math.abs(ll2.lon()-data.ll2.lon()),
+                        Math.abs(ll2.lat()-data.ll2.lat())/Math.ulp(ll2.lat()), Math.abs(ll2.lon()-data.ll2.lon())/Math.ulp(ll2.lon()),
+                        Math.ulp(ll2.lat()), Math.ulp(ll2.lon()));
                 fail.append(error);
             }
         }
@@ -181,23 +194,22 @@ class ProjectionRegressionTest {
         }
     }
 
-    private static boolean equalsDoubleMaxUlp(double d1, double d2) {
+    private static boolean equalsDoubleMaxUlp(double d1, double d2, double fact) {
         // Due to error accumulation in projection computation, the difference can reach hundreds of ULPs
         // The worst error is 1168 ULP (followed by 816 ULP then 512 ULP) with:
         // NAD83 / Colorado South (EPSG:26955): Projecting latlon(32.24604527892822,-125.93039495227096):
         // expected: eastnorth(-1004398.8994415681,24167.8944844745),
         // but got:  eastnorth(-1004398.8994415683,24167.894484478747)!
-        // MacOS has higher errors, otherwise 1200 would be enough
-        return Math.abs(d1 - d2) <= 1700 * Math.ulp(d1);
+        return Math.abs(d1 - d2) <= fact * Math.ulp(d1);
     }
 
-    private static boolean equalsJava9(EastNorth en1, EastNorth en2) {
-        return equalsDoubleMaxUlp(en1.east(), en2.east()) &&
-               equalsDoubleMaxUlp(en1.north(), en2.north());
+    private static boolean equalsJava9(EastNorth en1, EastNorth en2, double fact) {
+        return equalsDoubleMaxUlp(en1.east(), en2.east(), fact) &&
+               equalsDoubleMaxUlp(en1.north(), en2.north(), fact);
     }
 
-    private static boolean equalsJava9(LatLon ll1, LatLon ll2) {
-        return equalsDoubleMaxUlp(ll1.lat(), ll2.lat()) &&
-               equalsDoubleMaxUlp(ll1.lon(), ll2.lon());
+    private static boolean equalsJava9(LatLon ll1, LatLon ll2, double fact) {
+        return equalsDoubleMaxUlp(ll1.lat(), ll2.lat(), fact) &&
+               equalsDoubleMaxUlp(ll1.lon(), ll2.lon(), fact);
     }
 }
