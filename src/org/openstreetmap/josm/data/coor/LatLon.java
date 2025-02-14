080e84e711ae943d43e3723c230600ce7196eb01
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2025-02-14 09:30:06 +0000

    see #24104, add deprecated dates for remaining used functions, patch by gaben (stripped down a lot)
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@19319 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/coor/LatLon.java
+++ src/org/openstreetmap/josm/data/coor/LatLon.java
@@ -177,21 +177,9 @@ public class LatLon extends Coordinate implements ILatLon {
      * values, only differing by no more than 1 / {@link #MAX_SERVER_PRECISION MAX_SERVER_PRECISION}.
      * @deprecated since 18464 (use {@link ILatLon#equalsEpsilon(ILatLon)} instead)
      */
-    @Deprecated
+    @Deprecated(since = "18464", forRemoval = true)
     public boolean equalsEpsilon(LatLon other) {
         return ILatLon.super.equalsEpsilon(other);
-    }
-
-    /**
-     * Determines if this lat/lon is outside of the world
-     * @return <code>true</code>, if the coordinate is outside the world, compared by using lat/lon.
-     * @deprecated use {@link Node#isOutSideWorld} instead, see also #13538.
-     */
-    @Deprecated
-    public boolean isOutSideWorld() {
-        Bounds b = ProjectionRegistry.getProjection().getWorldBoundsLatLon();
-        return lat() < b.getMinLat() || lat() > b.getMaxLat() ||
-                lon() < b.getMinLon() || lon() > b.getMaxLon();
     }
 
     /**
@@ -220,7 +208,7 @@ public class LatLon extends Coordinate implements ILatLon {
      * @return distance in metres.
      * @deprecated since 18494 (use {@link ILatLon#greatCircleDistance(ILatLon)} instead)
      */
-    @Deprecated
+    @Deprecated(since = "18494", forRemoval = true)
     public double greatCircleDistance(LatLon other) {
         return ILatLon.super.greatCircleDistance(other);
     }
@@ -241,7 +229,7 @@ public class LatLon extends Coordinate implements ILatLon {
      * @since 9796
      * @deprecated since 18494 (use {@link ILatLon#bearing(ILatLon)} instead)
      */
-    @Deprecated
+    @Deprecated(since = "18494", forRemoval = true)
     public double bearing(LatLon other) {
         return ILatLon.super.bearing(other);
     }
