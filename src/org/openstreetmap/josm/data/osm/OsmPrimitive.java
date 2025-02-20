49774ebe058eda3726a7432755680c5b40255009
stoecker <stoecker@0c6e7542-c601-0410-84e7-c038aed88b3b>
2025-02-20 18:41:39 +0000

    checkstyle
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@19331 0c6e7542-c601-0410-84e7-c038aed88b3b

--- src/org/openstreetmap/josm/data/osm/OsmPrimitive.java
+++ src/org/openstreetmap/josm/data/osm/OsmPrimitive.java
@@ -7,7 +7,6 @@ import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
