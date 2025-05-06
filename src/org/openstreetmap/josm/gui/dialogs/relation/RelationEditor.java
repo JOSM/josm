JOSM/josm

commit e7455ca552546492265dd86044997bb502c7766d

Auto relation refresh, fix #21840 - patch by Woazboat
git-svn-id: https://josm.openstreetmap.de/svn/trunk@19398 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/RelationEditor.java
@@ -147,7 +147,17 @@ public abstract class RelationEditor extends ExtendedDialog implements IRelation
 
     @Override
     public final boolean isDirtyRelation() {
-        return !relation.hasEqualSemanticAttributes(relationSnapshot);
+        return isDirtyRelation(false);
+    }
+
+    @Override
+    public final boolean isDirtyRelation(boolean ignoreUninterestingTags) {
+        if (relation != null && relation.getDataSet() == null &&
+            relationSnapshot != null && relationSnapshot.getDataSet() == null) {
+            return false;
+        }
+
+        return !relation.hasEqualSemanticAttributes(relationSnapshot, ignoreUninterestingTags);
     }
 
     /* ----------------------------------------------------------------------- */

src/org/openstreetmap/josm/data/conflict/ConflictCollection.java
@@ -322,8 +322,8 @@ public class ConflictCollection implements Iterable<Conflict<? extends OsmPrimit
     }
 
     /**
-     * Returns the list of conflicts involving nodes.
-     * @return The list of conflicts involving nodes.
+     * Returns the list of conflicts involving ways.
+     * @return The list of conflicts involving ways.
      * @since 6555
      */
     public final Collection<Conflict<? extends OsmPrimitive>> getWayConflicts() {
@@ -331,8 +331,8 @@ public class ConflictCollection implements Iterable<Conflict<? extends OsmPrimit
     }
 
     /**
-     * Returns the list of conflicts involving nodes.
-     * @return The list of conflicts involving nodes.
+     * Returns the list of conflicts involving relations.
+     * @return The list of conflicts involving relations.
      * @since 6555
      */
     public final Collection<Conflict<? extends OsmPrimitive>> getRelationConflicts() {

src/org/openstreetmap/josm/gui/dialogs/relation/GenericRelationEditor.java
@@ -64,6 +64,7 @@ import org.openstreetmap.josm.data.validation.tests.RelationChecker;
 import org.openstreetmap.josm.gui.ConditionalOptionPaneUtil;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.gui.Notification;
 import org.openstreetmap.josm.gui.ScrollViewport;
 import org.openstreetmap.josm.gui.datatransfer.ClipboardUtils;
 import org.openstreetmap.josm.gui.dialogs.relation.actions.AbstractRelationEditorAction;
@@ -1061,10 +1062,27 @@ public class GenericRelationEditor extends RelationEditor implements CommandQueu
     @Override
     public void commandChanged(int queueSize, int redoSize) {
         Relation r = getRelation();
-        if (r != null && r.getDataSet() == null) {
-            // see #19915
-            setRelation(null);
-            applyAction.updateEnabledState();
+        if (r != null) {
+            if (r.getDataSet() == null) {
+                // see #19915
+                setRelation(null);
+                applyAction.updateEnabledState();
+            } else if (isDirtyRelation()) {
+                if (!isDirtyEditor()) {
+                    reloadDataFromRelation();
+                } else {
+                    new Notification(tr("Relation modified outside of relation editor with pending changes. Conflict resolution required."))
+                    .setIcon(JOptionPane.WARNING_MESSAGE).show();
+                }
+            }
         }
+    }
+
+    @Override
+    public boolean isDirtyEditor() {
+        Relation snapshot = getRelationSnapshot();
+        Relation relation = getRelation();
+        return (snapshot != null && !memberTableModel.hasSameMembersAs(snapshot)) ||
+                tagEditorPanel.getModel().isDirty() || relation == null || relation.getDataSet() == null;
     }
 }

src/org/openstreetmap/josm/gui/dialogs/relation/IRelationEditor.java
@@ -39,7 +39,31 @@ public interface IRelationEditor {
      *
      * @return true if the currently edited relation has been changed elsewhere.
      */
-    boolean isDirtyRelation();
+    default boolean isDirtyRelation() {
+        return isDirtyRelation(false);
+    }
+
+    /**
+     * Replies true if the currently edited relation has been changed elsewhere.
+     *
+     * In this case a relation editor can't apply updates to the relation directly. Rather,
+     * it has to create a conflict.
+     *
+     * @param ignoreUninterestingTags whether to ignore uninteresting tag changes
+     * @return true if the currently edited relation has been changed elsewhere.
+     * @since xxx
+     */
+    boolean isDirtyRelation(boolean ignoreUninterestingTags);
+
+    /**
+     * Replies true if the relation has been changed in the editor (but not yet applied).
+     *
+     * Reloading data from the relation would cause the pending changes to be lost.
+     *
+     * @return true if the currently edited relation has been changed in the editor.
+     * @since xxx
+     */
+    boolean isDirtyEditor();
 
     /**
      * Reloads data from relation.

src/org/openstreetmap/josm/gui/dialogs/relation/actions/RefreshAction.java
@@ -67,17 +67,7 @@ public class RefreshAction extends SavingAction implements CommandQueueListener 
 
     @Override
     public void updateEnabledState() {
-        Relation snapshot = getEditor().getRelationSnapshot();
-        Relation relation = getEditor().getRelation();
-        if (relation != null && relation.getDataSet() == null)
-            relation = null; // see #19915
-        if (relation != null && snapshot != null && snapshot.getDataSet() == null) {
-            // relation was changed outside of the editor
-            // either it was modified or deleted or changed by an undo
-            setEnabled(!snapshot.hasEqualSemanticAttributes(relation, false /* don't ignore uninteresting keys */));
-            return;
-        }
-        setEnabled(false);
+        setEnabled(getEditor().isDirtyRelation());
     }
 
     protected int confirmDiscardDirtyData() {

src/org/openstreetmap/josm/gui/dialogs/relation/actions/SavingAction.java
@@ -192,8 +192,6 @@ abstract class SavingAction extends AbstractRelationEditorAction {
     }
 
     protected boolean isEditorDirty() {
-        Relation snapshot = editorAccess.getEditor().getRelationSnapshot();
-        return (snapshot != null && !getMemberTableModel().hasSameMembersAs(snapshot)) || getTagModel().isDirty()
-                || getEditor().getRelation() == null || getEditor().getRelation().getDataSet() == null;
+        return editorAccess.getEditor().isDirtyEditor();
     }
 }

test/unit/org/openstreetmap/josm/gui/dialogs/relation/GenericRelationEditorTest.java
@@ -94,6 +94,16 @@ public class GenericRelationEditorTest {
             }
 
             @Override
+            public boolean isDirtyRelation(boolean ignoreUninterestingTags) {
+                return false;
+            }
+
+            @Override
+            public boolean isDirtyEditor() {
+                return false;
+            }
+
+            @Override
             public Relation getRelationSnapshot() {
                 return r;
             }

commit 3172be7cb42f887950ae2035186bfab2f321c404

fix boolean logic broken in r19398, see #21840
git-svn-id: https://josm.openstreetmap.de/svn/trunk@19402 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/RelationEditor.java
@@ -152,8 +152,8 @@ public abstract class RelationEditor extends ExtendedDialog implements IRelation
 
     @Override
     public final boolean isDirtyRelation(boolean ignoreUninterestingTags) {
-        if (relation != null && relation.getDataSet() == null &&
-            relationSnapshot != null && relationSnapshot.getDataSet() == null) {
+        if (relation == null || relation.getDataSet() == null ||
+            relationSnapshot == null || relationSnapshot.getDataSet() == null) {
             return false;
         }
 