JOSM/josm

commit 3172be7cb42f887950ae2035186bfab2f321c404

fix boolean logic broken in r19398, see #21840
git-svn-id: https://josm.openstreetmap.de/svn/trunk@19402 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/RelationEditor.java
@@ -152,8 +152,8 @@ public abstract class RelationEditor extends ExtendedDialog implements IRelation
 
     @Override
     public final boolean isDirtyRelation(boolean ignoreUninterestingTags) {
-        if (relation != null && relation.getDataSet() == null &&
-            relationSnapshot != null && relationSnapshot.getDataSet() == null) {
+        if (relation == null || relation.getDataSet() == null ||
+            relationSnapshot == null || relationSnapshot.getDataSet() == null) {
             return false;
         }
 

commit 0812dab8b2e418a40d06a6806a10da5dd2af0a2a

fix #24423: Relationeditor map sync broken - with #21840 some actions automatically update an open relation editor window if the relation is in sync with the dataset, else a notification about a pending conflict is shown. This logic was broken with r19402. This patch simplifies the logic in isDirtyRelation(): don't return false just because field relationSnapshot has no dataset.
git-svn-id: https://josm.openstreetmap.de/svn/trunk@19432 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/relation/RelationEditor.java
@@ -152,12 +152,7 @@ public abstract class RelationEditor extends ExtendedDialog implements IRelation
 
     @Override
     public final boolean isDirtyRelation(boolean ignoreUninterestingTags) {
-        if (relation == null || relation.getDataSet() == null ||
-            relationSnapshot == null || relationSnapshot.getDataSet() == null) {
-            return false;
-        }
-
-        return !relation.hasEqualSemanticAttributes(relationSnapshot, ignoreUninterestingTags);
+        return relation != null && !relation.hasEqualSemanticAttributes(relationSnapshot, ignoreUninterestingTags);
     }
 
     /* ----------------------------------------------------------------------- */