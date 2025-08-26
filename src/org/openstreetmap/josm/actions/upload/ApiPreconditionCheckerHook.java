JOSM/josm

commit 1ae1a68d3a098cb54eb01a5d6fe08d7d777c106f

remove OsmPrimitive.entrySet() using keySet()/get() or getKeys() instead
git-svn-id: http://josm.openstreetmap.de/svn/trunk@2906 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/upload/ApiPreconditionCheckerHook.java
@@ -4,7 +4,6 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map.Entry;
 
 import javax.swing.JOptionPane;
 
@@ -49,23 +48,24 @@ public class ApiPreconditionCheckerHook implements UploadHook {
 
     private boolean checkMaxNodes(Collection<OsmPrimitive> primitives, long maxNodes) {
         for (OsmPrimitive osmPrimitive : primitives) {
-            for (Entry<String,String> e : osmPrimitive.entrySet()) {
-                if(e.getValue().length() > 255) {
+            for (String key: osmPrimitive.keySet()) {
+                String value = osmPrimitive.get(key);
+                if(key.length() > 255) {
                     if (osmPrimitive.isDeleted()) {
                         // if OsmPrimitive is going to be deleted we automatically shorten the
                         // value
                         System.out.println(
                                 tr("Warning: automatically truncating value of tag ''{0}'' on deleted object {1}",
-                                        e.getKey(),
+                                        key,
                                         Long.toString(osmPrimitive.getId())
                                 )
                         );
-                        osmPrimitive.put(e.getKey(), e.getValue().substring(0, 255));
+                        osmPrimitive.put(key, value.substring(0, 255));
                         continue;
                     }
                     JOptionPane.showMessageDialog(Main.parent,
                             tr("Length of value for tag ''{0}'' on object {1} exceeds the max. allowed length {2}. Values length is {3}.",
-                                    e.getKey(), Long.toString(osmPrimitive.getId()), 255, e.getValue().length()
+                                    key, Long.toString(osmPrimitive.getId()), 255, value.length()
                             ),
                             tr("Precondition Violation"),
                             JOptionPane.ERROR_MESSAGE

src/org/openstreetmap/josm/actions/JoinAreasAction.java
@@ -249,14 +249,14 @@ public class JoinAreasAction extends JosmAction {
         ways.add(a);
         ways.add(b);
 
-        // This is mostly copied and pasted from CombineWayAction.java and one day should be moved into tools
+        // FIXME: This is mostly copied and pasted from CombineWayAction.java and one day should be moved into tools
         Map<String, Set<String>> props = new TreeMap<String, Set<String>>();
         for (Way w : ways) {
-            for (Entry<String,String> e : w.entrySet()) {
-                if (!props.containsKey(e.getKey())) {
-                    props.put(e.getKey(), new TreeSet<String>());
+            for (String key: w.keySet()) {
+                if (!props.containsKey(key)) {
+                    props.put(key, new TreeSet<String>());
                 }
-                props.get(e.getKey()).add(e.getValue());
+                props.get(key).add(w.get(key));
             }
         }
 

src/org/openstreetmap/josm/actions/search/SearchCompiler.java
@@ -6,7 +6,6 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.io.PushbackReader;
 import java.io.StringReader;
-import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -193,9 +192,8 @@ public class SearchCompiler {
                  * and only then try to match against the value
                  */
 
-                for (Entry<String, String> e : osm.entrySet()) {
-                    String k = e.getKey();
-                    String v = e.getValue();
+                for (String k: osm.keySet()) {
+                    String v = osm.get(k);
 
                     Matcher matcherKey = keyPattern.matcher(k);
                     boolean matchedKey = matcherKey.find();
@@ -329,10 +327,10 @@ public class SearchCompiler {
                 return false;
             case ANY_VALUE_REGEXP:
             case EXACT_REGEXP:
-                for (Entry<String, String> entry:osm.entrySet()) {
-                    if (keyPattern.matcher(entry.getKey()).matches()) {
+                for (String key: osm.keySet()) {
+                    if (keyPattern.matcher(key).matches()) {
                         if (mode == Mode.ANY_VALUE_REGEXP
-                                || valuePattern.matcher(entry.getValue()).matches())
+                                || valuePattern.matcher(osm.get(key)).matches())
                             return true;
                     }
                 }
@@ -383,10 +381,9 @@ public class SearchCompiler {
 
             // is not Java 1.5
             //search = java.text.Normalizer.normalize(search, java.text.Normalizer.Form.NFC);
-            for (Entry<String, String> e : osm.entrySet()) {
+            for (String key: osm.keySet()) {
+                String value = osm.get(key);
                 if (searchRegex != null) {
-                    String key = e.getKey();
-                    String value = e.getValue();
 
                     // is not Java 1.5
                     //value = java.text.Normalizer.normalize(value, java.text.Normalizer.Form.NFC);
@@ -400,8 +397,10 @@ public class SearchCompiler {
                     if (keyMatchFound || valMatchFound)
                         return true;
                 } else {
-                    String key = caseSensitive ? e.getKey() : e.getKey().toLowerCase();
-                    String value = caseSensitive ? e.getValue() : e.getValue().toLowerCase();
+                    if (caseSensitive) {
+                        key = key.toLowerCase();
+                        value = value.toLowerCase();
+                    }
 
                     // is not Java 1.5
                     //value = java.text.Normalizer.normalize(value, java.text.Normalizer.Form.NFC);

src/org/openstreetmap/josm/data/osm/OsmPrimitive.java
@@ -46,60 +46,6 @@ abstract public class OsmPrimitive implements Comparable<OsmPrimitive>, Tagged, 
         return idCounter.decrementAndGet();
     }
 
-    private static class KeysEntry implements Entry<String, String>{
-
-        private final String key;
-        private final String value;
-
-        private KeysEntry(String key, String value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public String setValue(String value) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((key == null) ? 0 : key.hashCode());
-            result = prime * result + ((value == null) ? 0 : value.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            KeysEntry other = (KeysEntry) obj;
-            if (key == null) {
-                if (other.key != null)
-                    return false;
-            } else if (!key.equals(other.key))
-                return false;
-            if (value == null) {
-                if (other.value != null)
-                    return false;
-            } else if (!value.equals(other.value))
-                return false;
-            return true;
-        }
-    }
-
     private static final int FLAG_MODIFIED = 1 << 0;
     private static final int FLAG_VISIBLE  = 1 << 1;
     private static final int FLAG_DISABLED = 1 << 2;
@@ -555,11 +501,11 @@ abstract public class OsmPrimitive implements Comparable<OsmPrimitive>, Tagged, 
 
         // FIXME: incline=\"-*\" search pattern does not work.
         String reversedDirectionDefault = "oneway=\"-1\" | incline=down | incline=\"-*\"";
-        
+
         String directionDefault = "oneway? | incline=* | aerialway=* | "+
-                                  "waterway=stream | waterway=river | waterway=canal | waterway=drain | waterway=rapids | "+
-                                  "\"piste:type\"=downhill | \"piste:type\"=sled | man_made=\"piste:halfpipe\" | "+
-                                  "junction=roundabout";
+        "waterway=stream | waterway=river | waterway=canal | waterway=drain | waterway=rapids | "+
+        "\"piste:type\"=downhill | \"piste:type\"=sled | man_made=\"piste:halfpipe\" | "+
+        "junction=roundabout";
 
         try {
             reversedDirectionKeys = SearchCompiler.compile(Main.pref.get("tags.reversed_direction", reversedDirectionDefault), false, false);
@@ -845,19 +791,6 @@ abstract public class OsmPrimitive implements Comparable<OsmPrimitive>, Tagged, 
         return null;
     }
 
-    // FIXME: why replying a collection of entries? Should be replaced by
-    // a method Map<String,String> getTags()
-    //
-    public final Collection<Entry<String, String>> entrySet() {
-        if (keys == null || keys.length ==0)
-            return Collections.emptySet();
-        Set<Entry<String, String>> result = new HashSet<Entry<String,String>>();
-        for (int i=0; i<keys.length; i+=2) {
-            result.add(new KeysEntry(keys[i], keys[i+1]));
-        }
-        return result;
-    }
-
     public final Collection<String> keySet() {
         if (keys == null || keys.length == 0)
             return Collections.emptySet();
@@ -909,7 +842,7 @@ abstract public class OsmPrimitive implements Comparable<OsmPrimitive>, Tagged, 
      * @return true if other isn't null and has the same tags (key/value-pairs) as this.
      */
     public boolean hasSameTags(OsmPrimitive other) {
-        return entrySet().equals(other.entrySet());
+        return keySet().equals(other.keySet());
     }
 
     /*------------

src/org/openstreetmap/josm/gui/MapStatus.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.ConcurrentModificationException;
 import java.util.List;
-import java.util.Map.Entry;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -435,8 +434,8 @@ public class MapStatus extends JPanel implements Helpful {
                 text.append(" [" + tr("User:") + " " + osm.getUser().getName() + "]");
             }
 
-            for (Entry<String, String> e1 : osm.entrySet()) {
-                text.append("<br>" + e1.getKey() + "=" + e1.getValue());
+            for (String key : osm.keySet()) {
+                text.append("<br>" + key + "=" + osm.get(key));
             }
 
             final JLabel l = new JLabel(

src/org/openstreetmap/josm/gui/dialogs/properties/ListOfUsedTags.java
@@ -69,8 +69,8 @@ public class ListOfUsedTags implements DataSetListener {
     }
 
     private void addPrimitive(OsmPrimitive primitive) {
-        for (Entry<String, String> entry: primitive.entrySet()) {
-            addKey(entry.getKey(), entry.getValue());
+        for (String key: primitive.keySet()) {
+            addKey(key, primitive.get(key));
         }
     }
 

src/org/openstreetmap/josm/gui/dialogs/properties/PropertiesDialog.java
@@ -728,15 +728,16 @@ public class PropertiesDialog extends ToggleDialog implements SelectionChangedLi
             } else {
                 ++ways;
             }
-            for (Entry<String, String> e : osm.entrySet()) {
-                keyCount.put(e.getKey(), keyCount.containsKey(e.getKey()) ? keyCount.get(e.getKey())+1 : 1);
-                if (valueCount.containsKey(e.getKey())) {
-                    Map<String, Integer> v = valueCount.get(e.getKey());
-                    v.put(e.getValue(), v.containsKey(e.getValue())? v.get(e.getValue())+1 : 1 );
+            for (String key: osm.keySet()) {
+                String value = osm.get(key);
+                keyCount.put(key, keyCount.containsKey(key) ? keyCount.get(key) + 1 : 1);
+                if (valueCount.containsKey(key)) {
+                    Map<String, Integer> v = valueCount.get(key);
+                    v.put(value, v.containsKey(value)? v.get(value) + 1 : 1 );
                 } else {
                     TreeMap<String,Integer> v = new TreeMap<String, Integer>();
-                    v.put(e.getValue(), 1);
-                    valueCount.put(e.getKey(), v);
+                    v.put(value, 1);
+                    valueCount.put(key, v);
                 }
             }
         }

src/org/openstreetmap/josm/gui/mappaint/ElemStyles.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map.Entry;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.osm.Node;
@@ -39,9 +38,8 @@ public class ElemStyles
         private IconElemStyle getNode(OsmPrimitive primitive)
         {
             IconElemStyle ret = null;
-            for (Entry<String, String> entry:primitive.entrySet()) {
-                String key = entry.getKey();
-                String val = entry.getValue();
+            for (String key: primitive.keySet()) {
+                String val = primitive.get(key);
                 IconElemStyle style;
                 if((style = icons.get("n" + key + "=" + val)) != null)
                 {
@@ -76,9 +74,8 @@ public class ElemStyles
             LineElemStyle retLine = null;
             String linestring = null;
             HashMap<String, LineElemStyle> over = new HashMap<String, LineElemStyle>();
-            for (Entry<String, String> entry:primitive.entrySet()) {
-                String key = entry.getKey();
-                String val = entry.getValue();
+            for (String key: primitive.keySet()) {
+                String val = primitive.get(key);
                 AreaElemStyle styleArea;
                 LineElemStyle styleLine;
                 String idx = "n" + key + "=" + val;

commit 1f6fdbaa2cfec4bf68e74b1e1970feba02e30726

fix #24446: Error loading way-history Turned out that the OSM server counts the number of unicode code points, not the number of characters when checking the maximum lengths of keys and tags in tags. - implement new methods in Util to calculate the number of code points or check agains a given limit - fix long standing bug in ApiPreconditionCheckerHook.java which was introduced with r2906 (key -> value)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@19437 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/upload/ApiPreconditionCheckerHook.java
@@ -22,6 +22,7 @@ import org.openstreetmap.josm.io.OsmApi;
 import org.openstreetmap.josm.io.OsmApiInitializationException;
 import org.openstreetmap.josm.io.OsmTransferCanceledException;
 import org.openstreetmap.josm.tools.Logging;
+import org.openstreetmap.josm.tools.Utils;
 
 /**
  * Checks certain basic conditions, that are listed in the OSM API
@@ -63,7 +64,7 @@ public class ApiPreconditionCheckerHook implements UploadHook {
             for (Map.Entry<String, String> entry: osmPrimitive.getKeys().entrySet()) {
                 String key = entry.getKey();
                 String value = entry.getValue();
-                if (key.length() > Tagged.MAX_TAG_LENGTH) {
+                if (!Utils.checkCodePointCount(value, Tagged.MAX_TAG_LENGTH)) {
                     if (osmPrimitive.isDeleted()) {
                         // if OsmPrimitive is going to be deleted we automatically shorten the value
                         Logging.warn(
@@ -72,12 +73,12 @@ public class ApiPreconditionCheckerHook implements UploadHook {
                                         Long.toString(osmPrimitive.getId())
                                 )
                         );
-                        osmPrimitive.put(key, value.substring(0, Tagged.MAX_TAG_LENGTH));
+                        osmPrimitive.put(key, Utils.shortenString(value, Tagged.MAX_TAG_LENGTH));
                         continue;
                     }
                     JOptionPane.showMessageDialog(MainApplication.getMainFrame(),
                             tr("Length of value for tag ''{0}'' on object {1} exceeds the max. allowed length {2}. Values length is {3}.",
-                                    key, Long.toString(osmPrimitive.getId()), Tagged.MAX_TAG_LENGTH, value.length()
+                                    key, Long.toString(osmPrimitive.getId()), Tagged.MAX_TAG_LENGTH, Utils.getCodePointCount(value)
                             ),
                             tr("Precondition violation"),
                             JOptionPane.ERROR_MESSAGE

src/org/openstreetmap/josm/data/osm/Changeset.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
+import org.openstreetmap.josm.tools.Utils;
 
 /**
  * Represents a single changeset in JOSM. For now its only used during
@@ -312,7 +313,7 @@ public final class Changeset implements Tagged, Comparable<Changeset> {
     public void setKeys(Map<String, String> keys) {
         CheckParameterUtil.ensureParameterNotNull(keys, "keys");
         keys.values().stream()
-                .filter(value -> value != null && value.length() > MAX_CHANGESET_TAG_LENGTH)
+                .filter(value -> !Utils.checkCodePointCount(value, MAX_CHANGESET_TAG_LENGTH))
                 .findFirst()
                 .ifPresent(value -> {
                 throw new IllegalArgumentException("Changeset tag value is too long: "+value);
@@ -339,9 +340,10 @@ public final class Changeset implements Tagged, Comparable<Changeset> {
     @Override
     public void put(String key, String value) {
         CheckParameterUtil.ensureParameterNotNull(key, "key");
-        if (value != null && value.length() > MAX_CHANGESET_TAG_LENGTH) {
-            throw new IllegalArgumentException("Changeset tag value is too long: "+value);
+        if (!Utils.checkCodePointCount(value, MAX_CHANGESET_TAG_LENGTH)) {
+            throw new IllegalArgumentException("Changeset tag value is too long: " + value);
         }
+
         this.tags.put(key, value);
     }
 

src/org/openstreetmap/josm/tools/TextTagParser.java
@@ -154,8 +154,8 @@ public final class TextTagParser {
                 r = callback.warning(tr("Suspicious characters in key:"), key, "tags.paste.keydoesnotmatch");
                 if (r == 2 || r == 3) return false; if (r == 4) return true;
             }
-            if (value.length() > MAX_VALUE_LENGTH) {
-                r = callback.warning(tr("Value is too long (max {0} characters):", MAX_VALUE_LENGTH), value, "tags.paste.valuetoolong");
+            if (!Utils.checkCodePointCount(value, MAX_VALUE_LENGTH)) {
+                r = callback.warning(tr("Value is too long (max {0} characters):", MAX_VALUE_LENGTH), value, "");
                 if (r == 2 || r == 3) return false; if (r == 4) return true;
             }
         }

src/org/openstreetmap/josm/tools/Utils.java
@@ -2025,4 +2025,28 @@ public final class Utils {
             default: throw new IllegalArgumentException("Invalid length unit: " + unit);
         }
     }
+
+    /**
+     * Calculate the number of unicode code points. See #24446
+     * @param s the string
+     * @return 0 if s is null or empty, else the number of code points
+     * @since 19437
+     */
+    public static int getCodePointCount(String s) {
+        if (s == null)
+            return 0;
+        return s.codePointCount(0, s.length());
+    }
+
+    /**
+     * Check if a given string has more than the allowed number of code points.
+     * See #24446. The OSM server checks this number, not the value returned by String.length()
+     * @param s the string
+     * @param maxLen the maximum number of code points
+     * @return true if s is null or within the given limit, false else
+     * @since 19437
+     */
+    public static boolean checkCodePointCount(String s, int maxLen) {
+        return getCodePointCount(s) <= maxLen;
+    }
 }

src/org/openstreetmap/josm/data/validation/tests/TagChecker.java
@@ -1015,9 +1015,10 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
                     .build());
             withErrors.put(p, "UUCV");
         }
-        if ((value.length() > Tagged.MAX_TAG_LENGTH) && !withErrors.contains(p, "LV")) {
+        final int codePoints = Utils.getCodePointCount(value);
+        if (codePoints > Tagged.MAX_TAG_LENGTH && !withErrors.contains(p, "LV")) {
             errors.add(TestError.builder(this, Severity.ERROR, LONG_VALUE)
-                    .message(tr("Tag value longer than {0} characters ({1} characters)", Tagged.MAX_TAG_LENGTH, value.length()), s, key)
+                    .message(tr("Tag value longer than {0} characters ({1} characters)", Tagged.MAX_TAG_LENGTH, codePoints), s, key)
                     .primitives(p)
                     .build());
             withErrors.put(p, "LV");
@@ -1064,9 +1065,10 @@ public class TagChecker extends TagTest implements TaggingPresetListener {
                     .build());
             withErrors.put(p, "ICK");
         }
-        if (key.length() > Tagged.MAX_TAG_LENGTH && !withErrors.contains(p, "LK")) {
+        final int codePoints = Utils.getCodePointCount(key);
+        if (codePoints > Tagged.MAX_TAG_LENGTH && !withErrors.contains(p, "LK")) {
             errors.add(TestError.builder(this, Severity.ERROR, LONG_KEY)
-                    .message(tr("Tag key longer than {0} characters ({1} characters)", Tagged.MAX_TAG_LENGTH, key.length()), s, key)
+                    .message(tr("Tag key longer than {0} characters ({1} characters)", Tagged.MAX_TAG_LENGTH, codePoints), s, key)
                     .primitives(p)
                     .build());
             withErrors.put(p, "LK");